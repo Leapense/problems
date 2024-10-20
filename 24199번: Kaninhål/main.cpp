9ea2f491dda146a4512ed01cf7edb615ec4a35b0
Leapense <iso9661@gmail.com>
2024-10-20 18:10:53 +0900

    Refactor code for 24199번: Kaninhål/main.cpp

--- 24199번: Kaninhål/main.cpp
+++ 24199번: Kaninhål/main.cpp
@@ -6,13 +6,38 @@
 //   @Test:   코드를 작성 후 "BOJ: 테스트"통해서 테스트를 해보세요.
 //=====================================================================
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-            
-int main() {
-  int t;
-	cin >> t;
 
-  
-  return 0;
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> final_order(N);
+	for (auto &x : final_order) cin >> x;
+	for (int d1 = 1; d1 < N; d1++) {
+		for (int d2 = 1; d2 < N; d2++) {
+			for (int d3 = 1; d3 < N; d3++) {
+				vector<int> current_order;
+
+				for (int i = N; i >= 1; i--) current_order.push_back(i);
+				vector<int> depths = {d1, d2, d3};
+
+				for (auto d : depths) {
+					vector<int> last_d(current_order.end() - d, current_order.end());
+
+					reverse(last_d.begin(), last_d.end());
+					vector<int> new_order = last_d;
+					new_order.insert(new_order.end(), current_order.begin(), current_order.end() - d);
+					current_order = new_order;
+				}
+
+				if (current_order == final_order) {
+					cout << d1 << " " << d2 << " " << d3 << "\n";
+					return 0;
+				}
+			}
+		}
+	}
 }
