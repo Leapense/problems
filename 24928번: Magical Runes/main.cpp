a15f9e98c7e54e4eb2f3599b367fc86096b87d62
Leapense <iso9661@gmail.com>
2024-10-24 20:34:31 +0900

    Refactor code for 24928번: Magical Runes/main.cpp and 24928번: Magical Runes/README.md

--- 24928번: Magical Runes/main.cpp
+++ 24928번: Magical Runes/main.cpp
@@ -6,13 +6,36 @@
 //   @Test:   코드를 작성 후 "BOJ: 테스트"통해서 테스트를 해보세요.
 //=====================================================================
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-            
-int main() {
-  int t;
-	cin >> t;
 
-  
-  return 0;
+typedef unsigned long long ull;
+
+int main()
+{
+	string S;
+	ull D;
+	cin >> S >> D;
+
+	int N = S.length();
+
+	ull num = 0;
+	for (int i = 0; i < N; i++) {
+		if (S[i] == 'B') {
+			num |= (1ULL << i);
+		}
+	}
+	num += D;
+	ull mask = (N == 64) ? ~0ULL : ((1ULL << N) - 1);
+	num &= mask;
+	string res = "";
+
+	for (int i = 0; i < N; i++) {
+		if (num & (1ULL << i)) res += 'B';
+		else res += 'A';
+	}
+
+	cout << res << "\n";
+
+	return 0;
 }
