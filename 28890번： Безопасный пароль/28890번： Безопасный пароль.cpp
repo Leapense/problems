1b9194aa98c32cd22c1d14887e7553caee5bcc9e
Leapense <iso9661@gmail.com>
2024-11-03 19:11:56 +0900

    "Refactored code: replaced `#include <bits/stdc++.h>` with individual includes, changed variable declarations, and modified logic for character replacement in a string."

--- 28890번： Безопасный пароль/28890번： Безопасный пароль.cpp
+++ 28890번： Безопасный пароль/28890번： Безопасный пароль.cpp
@@ -1,37 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
-
-int main()
-{
+int main() {
     string s;
     cin >> s;
-
     int n = s.length();
-    int max_freq = n / 2;
-    int freq[26] = {0};
+    vector<int> freq(26, 0);
     for (char c : s) freq[c - 'a']++;
-    for (int i = 0; i < n; i++) {
-        bool need_replace = false;
-        if (freq[s[i] - 'a'] > max_freq) {
-            need_replace = true;
+    int maxFreq = n / 2;
+    int i = 0;
+    while (i < n) {
+        int j = i;
+        while (j + 1 < n && s[j + 1] == s[i]) j++;
+        int len = j - i + 1;
+        for (int k = i + 2; k <= j; k += 3) {
+            freq[s[k] - 'a']--;
+            char c_new = 'a';
+            while ((c_new == s[k - 1] || (k + 1 < n && c_new == s[k + 1])) || freq[c_new - 'a'] >= maxFreq) c_new++;
+            s[k] = c_new;
+            freq[c_new - 'a']++;
         }
-        if (i >= 2 && s[i] == s[i - 1] && s[i] == s[i - 2]) {
-            need_replace = true;
-        }
-
-        if (need_replace) {
-            for (char d = 'a'; d <= 'z'; d++) {
-                if (d != s[i - 1] && (i < 2 || d != s[i - 2]) && freq[d - 'a'] < max_freq) {
-                    freq[s[i] - 'a']--;
-                    freq[d - 'a']++;
-                    s[i] = d;
+        i = j + 1;
+    }
+    for (int c = 0; c < 26; ++c) {
+        while (freq[c] > maxFreq) {
+            for (int i = 0; i < n; ++i) {
+                if (s[i] == 'a' + c) {
+                    char prev = (i > 0) ? s[i - 1] : '\0';
+                    char next = (i + 1 < n) ? s[i + 1] : '\0';
+                    char c_new = 'a';
+                    while ((c_new == prev || c_new == next || c_new == s[i]) || freq[c_new - 'a'] >= maxFreq) {
+                        c_new++;
+                        if (c_new > 'z') break;
+                    }
+                    if (c_new > 'z') continue;
+                    freq[c]--;
+                    s[i] = c_new;
+                    freq[c_new - 'a']++;
                     break;
                 }
             }
         }
     }
-
-    cout << s << "\n";
-
+    cout << s << endl;
     return 0;
 }
