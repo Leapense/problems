9125fb55ccdd29907c28ea5395c9d83149853da3
Leapense <iso9661@gmail.com>
2024-11-22 22:25:51 +0900

    "Added implementation for problem 2103, including header inclusions, main function, and logic for calculating perimeter."

--- 2103번： 직교다각형 복원/직교다각형 복원.cpp
+++ 2103번： 직교다각형 복원/직교다각형 복원.cpp
@@ -1,11 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 2103                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/2103                           #+#        #+#      #+#    */
-/*   Solved: 2024/11/22 21:46:03 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<vector<int>> y_to_x(10001, vector<int>());
+    vector<vector<int>> x_to_y(10001, vector<int>());
+    
+    for(int i=0; i<N; i++){
+        int x, y;
+        cin >> x >> y;
+        y_to_x[y].push_back(x);
+        x_to_y[x].push_back(y);
+    }
+
+    long long perimeter = 0;
+
+    for (int y = 0; y <= 10000; y++) {
+        if (y_to_x[y].size() >= 2) {
+            vector<int> &xs = y_to_x[y];
+
+            sort(xs.begin(), xs.end());
+            for (int i = 0; i < xs.size(); i += 2) {
+                perimeter += (long long)(xs[i + 1] - xs[i]);
+            }
+        }
+    }
+
+    for (int x = 0; x <= 10000; x++) {
+        if (x_to_y[x].size() >= 2) {
+            vector<int> &ys = x_to_y[x];
+            sort(ys.begin(), ys.end());
+
+            for (int i = 0; i < ys.size(); i += 2) {
+                perimeter += (long long)(ys[i + 1] - ys[i]);
+            }
+        }
+    }
+
+    cout << perimeter << "\n";
+
+    return 0;
+}
