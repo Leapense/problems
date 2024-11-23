ac44d27f80edf7926fdf1d8b7ed85025634f6b63
Leapense <iso9661@gmail.com>
2024-11-23 16:56:37 +0900

    "Added implementation of truncated binary encoding algorithm and main function to read input and print encoded codes."

--- 3496번： Binary Encoding/Binary Encoding.py
+++ 3496번： Binary Encoding/Binary Encoding.py
@@ -1,11 +1,32 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 3496                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/3496                           #+#        #+#      #+#     #
-#    Solved: 2024/11/23 16:34:05 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import math
+
+def truncated_binary_encoding(m):
+    n = math.ceil(math.log2(m))
+    total_bits = 2 ** n
+    k = total_bits - m
+    
+    codes = []
+
+    for i in range(k):
+        code = format(i, f'0{n-1}b')
+        codes.append(code)
+
+    start = total_bits - (m - k)
+    for i in range(start, total_bits):
+        code = format(i, f'0{n}b')
+        codes.append(code)
+
+    return codes
+
+def main():
+    import sys
+    input = sys.stdin.read
+    m = int(input())
+
+    encoded_codes = truncated_binary_encoding(m)
+
+    for code in encoded_codes:
+        print(code)
+
+if __name__ == '__main__':
+    main()
