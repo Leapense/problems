3bbaf512999dc1c101a0029e0736791c6c9c56d3
Leapense <iso9661@gmail.com>
2024-11-23 12:32:10 +0900

    "Added implementation to problem 2200 solution, replacing header comment with code"

--- 2200번： 계산기/계산기.cpp
+++ 2200번： 계산기/계산기.cpp
@@ -1,11 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 2200                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/2200                           #+#        #+#      #+#    */
-/*   Solved: 2024/11/22 22:50:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<long long> c(N + 1);
+    for (auto &x : c) cin >> x;
+
+    long long sum_digits = 0;
+
+    int num_terms = 0;
+
+    for (int i = 1; i <= N; i++) if (c[i] > 0) {
+        num_terms++;
+        sum_digits += floor(log10((double)c[i])) + 1;
+    }
+
+    long long K = 1 + 2 * (N - 1) + num_terms + sum_digits + 1;
+    cout << K << "\n";
+
+    return 0;
+}
