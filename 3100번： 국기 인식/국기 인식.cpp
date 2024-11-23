cbac74600532a5c00a0985da46df9c0ab4b9e3bf
Leapense <iso9661@gmail.com>
2024-11-23 15:11:59 +0900

    "Added implementation for problem 3100, replacing existing comment block with C++ code."

--- 3100번： 국기 인식/국기 인식.cpp
+++ 3100번： 국기 인식/국기 인식.cpp
@@ -1,11 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 3100                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/3100                           #+#        #+#      #+#    */
-/*   Solved: 2024/11/23 14:43:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<string> grid(6);
+    for (auto &s : grid) cin >> s;
+
+    int min_changes = INT32_MAX;
+    for (char top = 'A'; top <= 'Z'; top++) {
+        for (char mid = 'A'; mid <= 'Z'; mid++) if (mid != top) {
+            for (char bot = 'A'; bot <= 'Z'; bot++) if (bot != mid) {
+                int changes = 0;
+                for (int i = 0; i < 2; i++) for (char c : grid[i]) if (c != top) changes++;
+                for (int i = 2; i < 4; i++) for (char c : grid[i]) if (c != mid) changes++;
+                for (int i = 4; i < 6; i++) for (char c : grid[i]) if (c != bot) changes++;
+
+                min_changes = min(min_changes, changes);
+            }
+        }
+    }
+
+    cout << min_changes << "\n";
+
+    return 0;
+}
