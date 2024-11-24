e5579e8076eae9489632b4becbc2192b83fdbbad
Leapense <iso9661@gmail.com>
2024-11-24 15:34:13 +0900

    "Added implementation for Problem 4426, replacing existing comment block with C++ code."

--- 4426번： Dick and Jane/Dick and Jane.cpp
+++ 4426번： Dick and Jane/Dick and Jane.cpp
@@ -1,11 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 4426                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/4426                           #+#        #+#      #+#    */
-/*   Solved: 2024/11/24 15:24:01 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int s, p, y, j;
+    while (cin >> s >> p >> y >> j) {
+        int d = 12;
+        long long sum_total = d + j;
+
+        long long Y = (sum_total - y - p) / 3;
+
+        if (Y < 0) {
+            Y = 0;
+        }
+
+        long long P = Y + p;
+        long long S = Y + y;
+
+        long long current_sum = 3 * Y + p + y;
+
+        if (current_sum < sum_total) {
+            long long delta = sum_total - current_sum;
+            S += delta;
+        }
+
+        cout << S << " " << P << " " << Y << "\n";
+    }
+
+    return 0;
+}
