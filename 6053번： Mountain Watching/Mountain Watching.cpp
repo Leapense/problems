8f87c7ddb91293e1e5e8197f860358a04b8be721
Leapense <iso9661@gmail.com>
2024-11-29 21:02:15 +0900

    "Added implementation for problem 6053 Mountain Watching"

--- 6053번： Mountain Watching/Mountain Watching.cpp
+++ 6053번： Mountain Watching/Mountain Watching.cpp
@@ -1,11 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 6053                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/6053                           #+#        #+#      #+#    */
-/*   Solved: 2024/11/29 20:51:43 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<long long> H(N);
+    for (auto &x : H) cin >> x;
+    vector<int> left(N,1), right(N,1);
+    for(int i=1;i<N;i++) {
+        if(H[i] >= H[i-1]) left[i]=left[i-1]+1;
+        else left[i]=1;
+    }
+
+    for(int i=N-2;i>=0;i--){
+        if(H[i] >= H[i+1]) right[i]=right[i+1]+1;
+        else right[i]=1;
+    }
+
+    int max_width = 0;
+    for (int i = 0; i < N; i++) max_width = max(max_width, left[i] + right[i] - 1);
+    cout << max_width << "\n";
+
+    return 0;
+}
