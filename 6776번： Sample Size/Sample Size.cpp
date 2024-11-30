f15a8a5b446a6528b51fb321cb58a74ef8f7edc6
Leapense <iso9661@gmail.com>
2024-11-30 16:14:22 +0900

    "Refactored Sample Size.cpp: removed unused variables, simplified logic, and improved code organization"

--- 6776번： Sample Size/Sample Size.cpp
+++ 6776번： Sample Size/Sample Size.cpp
@@ -1,52 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    
+    cin.tie(nullptr);
     int M;
     cin >> M;
+    vector<int> Ps(M);
 
-    vector<int> percentages(M);
-    unordered_set<int> unique_P;
-    for(int i=0; i<M; ++i){
-        cin >> percentages[i];
-        unique_P.insert(percentages[i]);
-    }
+    for (auto &p : Ps) cin >> p;
+    int N = 1;
 
-    for(int N=1; N<=100000; ++N){
-        bool valid = true;
-        for(auto P : unique_P){
-            if(P <0 || P >100){
-                valid = false;
+    while (true) {
+        bool ok = true;
+        for (auto P : Ps) {
+            double lower = (P - 0.5) / 100.0 * N;
+            double upper = (P + 0.5) / 100.0 * N;
+            int kl = ceil(lower);
+            int ku = floor(upper - 1e-9);
+
+            if (kl > ku) {
+                ok = false;
                 break;
             }
-            if(P ==0){
-                continue;
-            }
-            else if(P ==100){
-                long long numerator = 199LL * N + 199;
-                long long k_low = numerator / 200;
-                long long k_high = N;
-                if(k_low > k_high){
-                    valid = false;
-                    break;
-                }
-            }
-            else{
-                long long k_low = ((2LL * P -1) * N + 199) / 200;
-                long long k_high = ((2LL * P +1) * N ) / 200;
-                if(k_low > k_high){
-                    valid = false;
-                    break;
-                }
-            }
         }
-        if(valid){
-            cout << N << "\n";
+        if (ok) {
+            cout << N;
             return 0;
         }
+        N++;
     }
-    return 0;
 }
