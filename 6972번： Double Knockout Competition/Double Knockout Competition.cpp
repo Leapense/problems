a07c975cc191e809f5405ab6e15b33d8243b1f19
Leapense <iso9661@gmail.com>
2024-11-30 18:52:51 +0900

    "Added implementation for Double Knockout Competition problem (BOJ 6972)"

--- 6972번： Double Knockout Competition/Double Knockout Competition.cpp
+++ 6972번： Double Knockout Competition/Double Knockout Competition.cpp
@@ -1,11 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 6972                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/6972                           #+#        #+#      #+#    */
-/*   Solved: 2024/11/30 18:22:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; // number of test cases
+    cin >> n;
+    while (n--) {
+        int t;
+        cin >> t;
+
+        int round = 0;
+        int num_undefeated = t;
+        int num_one_loss = 0;
+        int num_eliminated = 0;
+
+        cout << "Round " << round << ": " << num_undefeated << " undefeated, " << num_one_loss << " one-loss, " << num_eliminated << " eliminated\n";
+
+        while (num_undefeated + num_one_loss > 1) {
+            round++;
+
+            // Special case: one undefeated and one one-loss team remaining
+            if (num_undefeated + num_one_loss == 2 && num_undefeated == 1 && num_one_loss == 1) {
+                // Undefeated team loses, becomes one-loss
+                num_undefeated = 0;
+                num_one_loss = 2;
+            } else {
+                // Simulate matches among undefeated teams
+                int num_undefeated_matches = num_undefeated / 2;
+                int num_undefeated_byes = num_undefeated % 2;
+                int num_undefeated_winners = num_undefeated_byes + num_undefeated_matches;
+                int num_undefeated_losers = num_undefeated_matches;
+
+                // Simulate matches among one-loss teams
+                int num_one_loss_matches = num_one_loss / 2;
+                int num_one_loss_byes = num_one_loss % 2;
+                int num_one_loss_winners = num_one_loss_byes + num_one_loss_matches;
+                int num_one_loss_losers = num_one_loss_matches;
+
+                num_eliminated += num_one_loss_losers;
+
+                // Update counts
+                num_undefeated = num_undefeated_winners;
+                num_one_loss = num_one_loss_winners + num_undefeated_losers;
+            }
+
+            cout << "Round " << round << ": " << num_undefeated << " undefeated, " << num_one_loss << " one-loss, " << num_eliminated << " eliminated\n";
+        }
+
+        cout << "There are " << round << " rounds\n";
+
+        if (n) {
+            cout << endl; // Separate outputs for different test cases
+        }
+    }
+
+    return 0;
+}
