0023267ae8888ef008bce34401fae26358e977e9
Leapense <iso9661@gmail.com>
2024-12-01 18:55:17 +0900

    "Added implementation for problem 8586, replacing existing comment block with code"

--- 8586번： Zapałki/Zapałki.cpp
+++ 8586번： Zapałki/Zapałki.cpp
@@ -1,11 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8586                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8586                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 18:30:30 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+
+    vector<int> x(n);
+    for (auto &xi : x) cin >> xi;
+    long long prev0 = (x[0] != 0) ? 1 : 0;
+    long long prev1 = (x[0] != 1) ? 1 : 0;
+
+    for (int i = 1; i < n; i++) {
+        long long current0 = min(prev0, prev1) + ((x[i] != 0) ? 1 : 0);
+        long long current1 = prev1 + ((x[i] != 1) ? 1 : 0);
+        prev0 = current0;
+        prev1 = current1;
+    }
+
+    cout << min(prev0, prev1);
+
+    return 0;
+}
