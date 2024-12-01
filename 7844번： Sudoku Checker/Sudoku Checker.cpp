0d7d32d2cb1ff3e7d4f1eb0a3c6b9ff8d9752319
Leapense <iso9661@gmail.com>
2024-12-01 15:27:11 +0900

    "Added Sudoku checker implementation to Sudoku Checker.cpp, replacing previous comment block."

--- 7844번： Sudoku Checker/Sudoku Checker.cpp
+++ 7844번： Sudoku Checker/Sudoku Checker.cpp
@@ -1,11 +1,70 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 7844                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/7844                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 14:56:57 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    int size = N * N;
+
+    vector<vector<int>> grid(size, vector<int>(size));
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        vector<bool> seen(size + 1, false);
+
+        for (int j = 0; j < size; j++) {
+            int val = grid[i][j];
+            if (val != 0) {
+                if (val < 1 || val > size || seen[val]) {
+                    cout << "INCORRECT";
+                    return 0;
+                }
+
+                seen[val] = true;
+            }
+        }
+    }
+
+    for (int j = 0; j < size; j++) {
+        vector<bool> seen(size + 1, false);
+        for (int i = 0; i < size; i++) {
+            int val = grid[i][j];
+            if (val != 0) {
+                if (val < 1 || val > size || seen[val]) {
+                    cout << "INCORRECT";
+                    return 0;
+                }
+                seen[val] = true;
+            }
+        }
+    }
+
+    for (int block_i = 0; block_i < N; block_i++) {
+        for (int block_j = 0; block_j < N; block_j++) {
+            vector<bool> seen(size + 1, false);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    int val = grid[block_i * N + i][block_j * N + j];
+                    if (val != 0) {
+                        if (val < 1 || val > size || seen[val]) {
+                            cout << "INCORRECT";
+                            return 0;
+                        }
+                        seen[val] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "CORRECT";
+
+    return 0;
+}
