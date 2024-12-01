096383e24f02ec832cf5144b7a98d50cb1ada228
Leapense <iso9661@gmail.com>
2024-12-01 18:29:16 +0900

    "Removed comment block at top of file, added include statement and namespace, and implemented Fibonacci sequence calculation in main function."

--- 8394번： 악수/악수.cpp
+++ 8394번： 악수/악수.cpp
@@ -1,11 +1,24 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8394                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8394                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 18:07:48 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 1) {
+        cout << "1";
+        return 0;
+    }
+
+    int prev_prev = 1;
+    int prev = 1;
+    for (int i = 2; i <= n; i++) {
+        int current = (prev + prev_prev) % 10;
+        prev_prev = prev;
+        prev = current;
+    }
+
+    cout << prev;
+
+    return 0;
+}
