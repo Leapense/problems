32adf94f6b0d8161e03bfaba433a63b0ef2e35ea
Leapense <iso9661@gmail.com>
2024-12-01 16:12:26 +0900

    "Added implementation for problem 7908, replacing existing comments with code"

--- 7908번： Will It Stop？/Will It Stop？.cpp
+++ 7908번： Will It Stop？/Will It Stop？.cpp
@@ -1,11 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 7908                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/7908                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 16:07:24 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll n;
+    cin >> n;
+    unordered_set<ll> seen;
+
+    while(n > 1) {
+        if (seen.find(n) != seen.end()) {
+            cout << "NIE";
+            return 0;
+        }
+
+        seen.insert(n);
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 3;
+        }
+    }
+
+    cout << "TAK";
+
+    return 0;
+}
