06d2909536584a465c962725b85cfc515633c8e3
Leapense <iso9661@gmail.com>
2024-12-01 20:25:13 +0900

    "Added implementation for Problem 8600 on boj.kr, including comments and solution code."

--- 8600번： Pociąg/Pociąg.cpp
+++ 8600번： Pociąg/Pociąg.cpp
@@ -1,11 +1,44 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8600                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8600                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 20:07:47 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    
+    vector<int> w(n);
+    vector<int> m(n);
+    unordered_map<int, int> resource_to_city;
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> w[i];
+    }
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> m[i];
+        resource_to_city[m[i]] = i;
+    }
+    
+    vector<bool> visited(n, false);
+    int cycles = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i]) {
+            int current = i;
+            while (!visited[current]) {
+                visited[current] = true;
+                current = resource_to_city[w[current]];
+            }
+            cycles++;
+        }
+    }
+    
+    cout << cycles << '\n';
+    
+    return 0;
+}
