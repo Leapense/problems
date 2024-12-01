f196a774a53bc59b51ac109440869333fe6fc9f4
Leapense <iso9661@gmail.com>
2024-12-01 18:07:06 +0900

    "Added implementation of ROT13 problem solution in C++"

--- 8385번： ROT13/ROT13.cpp
+++ 8385번： ROT13/ROT13.cpp
@@ -1,11 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8385                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8385                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 17:57:37 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+string ROT13(const string& s) {
+    string res = s;
+    for (char& c : res) {
+        if ('a' <= c && c <= 'z') {
+            c = (c - 'a' + 13) % 26 + 'a';
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    unordered_set<string> words;
+    for (int i = 0; i < n; ++i) {
+        string w;
+        cin >> w;
+        words.insert(w);
+    }
+
+    int count = 0;
+    for (const auto& w : words) {
+        string w_rot13 = ROT13(w);
+        if (words.find(w_rot13) != words.end()) {
+            ++count;
+        }
+    }
+
+    cout << count << "\n";
+
+    return 0;
+}
