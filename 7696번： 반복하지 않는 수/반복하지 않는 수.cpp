66bfc77431e2bc403b6ddbbc9cf1f88b55b4c913
Leapense <iso9661@gmail.com>
2024-12-01 13:12:13 +0900

    "Updated VSCode settings to use SF Mono font and added a new C++ file with a backtrack function implementation."

--- 7696번： 반복하지 않는 수/반복하지 않는 수.cpp
+++ 7696번： 반복하지 않는 수/반복하지 않는 수.cpp
@@ -1,11 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 7696                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/7696                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 13:05:54 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+void backtrack(int num, int used, int length, vector<int> &v) {
+    if (num > 0) {
+        v.push_back(num);
+    }
+    if (length == 10) {
+        return;
+    }
+
+    for (int d = 0; d <= 9; d++) {
+        if (num == 0 && d == 0) {
+            continue;
+        }
+        if (!(used & (1 << d))) {
+            backtrack(num * 10 + d, used | (1 << d), length + 1, v);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> v;
+    backtrack(0, 0, 0, v);
+    int n;
+    while(cin >> n && n != 0) {
+        cout << v[n - 1] << "\n";
+    }
+    return 0;
+}
