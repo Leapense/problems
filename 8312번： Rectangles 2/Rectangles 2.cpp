2936bf215fd00f01fc52de314160d0d84301e72b
Leapense <iso9661@gmail.com>
2024-12-01 17:40:10 +0900

    "Added implementation for problem 8312, including comments and code for calculating total rectangles with perimeter greater than or equal to p."

--- 8312번： Rectangles 2/Rectangles 2.cpp
+++ 8312번： Rectangles 2/Rectangles 2.cpp
@@ -1,11 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8312                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8312                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 17:27:24 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m, p;
+    cin >> n >> m >> p;
+
+    int max_perimeter = 2 * (n + m);
+
+    vector<long long> total_perimeters(max_perimeter + 1, 0);
+
+    for (int h = 1; h <= n; ++h) {
+        int count_h = n - h + 1;
+        for (int w = 1; w <= m; ++w) {
+            int count_w = m - w + 1;
+            long long count = (long long)count_h * count_w;
+            int perimeter = 2 * (h + w);
+            total_perimeters[perimeter] += count;
+        }
+    }
+    long long total = 0;
+
+    for (int i = p; i <= max_perimeter; ++i) {
+        total += total_perimeters[i];
+    }
+
+    cout << total << endl;
+    return 0;
+}
