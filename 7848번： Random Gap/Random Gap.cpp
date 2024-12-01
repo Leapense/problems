5ba7421f9e92aa1d90139b36ea65d200db68eb39
Leapense <iso9661@gmail.com>
2024-12-01 16:05:44 +0900

    "Added implementation for Problem 7848: Random Gap"

--- 7848번： Random Gap/Random Gap.cpp
+++ 7848번： Random Gap/Random Gap.cpp
@@ -1,11 +1,31 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 7848                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/7848                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 15:28:48 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long a, c, m, R0;
+
+    cin >> a >> c >> m >> R0;
+
+    vector<bool> seen(m, false);
+    vector<int> vals;
+
+    long long R = R0;
+    while (!seen[R]) {
+        seen[R] = true;
+        vals.push_back(R);
+        R = (a * R + c) % m;
+    }
+
+    sort(vals.begin(), vals.end());
+    long long max_gap = 0;
+    for (int i = 1; i < vals.size(); i++) {
+        long long gap = vals[i] - vals[i - 1];
+        if (gap > max_gap) max_gap = gap;
+    }
+
+    cout << max_gap << "\n";
+
+    return 0;
+}
