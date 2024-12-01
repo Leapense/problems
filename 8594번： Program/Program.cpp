be3fa65c0e1c8d11e447babf07e267c45879a9eb
Leapense <iso9661@gmail.com>
2024-12-01 19:45:50 +0900

    "Added implementation of Problem 8594 solution in C++"

--- 8594번： Program/Program.cpp
+++ 8594번： Program/Program.cpp
@@ -1,11 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8594                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8594                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/01 18:55:33 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    
+    vector<char> stack;
+
+    int current_depth = 0;
+    int max_depth = 0;
+
+    for (char c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            stack.push_back(c);
+            current_depth++;
+            if (current_depth > max_depth) max_depth = current_depth;
+        }
+        else {
+            if (stack.empty()) {
+                cout << "NIE";
+                return 0;
+            }
+
+            char top = stack.back();
+            if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                stack.pop_back();
+                current_depth--;
+            } else {
+                cout << "NIE";
+                return 0;
+            }
+        }
+    }
+
+    if (stack.empty()) {
+        cout << max_depth;
+    } else {
+        cout << "NIE";
+    }
+
+    return 0;
+}
