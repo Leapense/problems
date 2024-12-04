1fcd6271ef01266a7750f3cef4d94582dd7e0718
Leapense <iso9661@gmail.com>
2024-12-04 23:30:08 +0900

    "Added implementation to Krany.cpp, replacing existing comments with a C++ program that reads input, processes data, and outputs a result."

--- 8673번： Krany/Krany.cpp
+++ 8673번： Krany/Krany.cpp
@@ -1,11 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8673                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8673                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/04 22:29:55 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, w;
+    cin >> n >> w;
+    vector<int> temps;
+    temps.reserve(n);
+
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        if (t > 0) {
+            temps.push_back(t);
+        }
+    }
+
+    if (temps.empty()) {
+        cout << "NIE";
+        return 0;
+    }
+
+    sort(temps.begin(), temps.end());
+    
+    ll sum = 0;
+    int count = temps.size();
+    for (auto t : temps) {
+        sum += t;
+    }
+
+    int removed = 0;
+
+    while (count > 0 && (double)sum / count < w) {
+        sum -= temps[removed];
+        count--;
+        removed++;
+    }
+
+    if (count > 0 && (double)sum / count >= w) {
+        cout << removed;
+    } else {
+        cout << "NIE";
+    }
+
+    return 0;
+}
