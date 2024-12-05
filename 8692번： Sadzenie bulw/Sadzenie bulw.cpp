127f3994f68b700a8b43597f6f3b002d24cb157e
Leapense <iso9661@gmail.com>
2024-12-05 23:20:00 +0900

    "Added implementation for problem 8692 on boj.kr, including header inclusions, namespace, and solution logic."

--- 8692번： Sadzenie bulw/Sadzenie bulw.cpp
+++ 8692번： Sadzenie bulw/Sadzenie bulw.cpp
@@ -1,11 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8692                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8692                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/05 22:16:32 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll n;
+    cin >> n;
+    vector<ll> b(n);
+    for (auto &x : b)
+        cin >> x;
+    ll m;
+    cin >> m;
+    sort(b.begin(), b.end(), greater<ll>());
+    ll sum = 0;
+    for (auto x : b)
+    {
+        if (m == 0)
+            break;
+        ll k = min(x, m);
+        sum += k * k;
+        m -= k;
+    }
+    cout << sum;
+
+    return 0;
+}
