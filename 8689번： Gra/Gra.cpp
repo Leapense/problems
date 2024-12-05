ada084bc879f8b8e9f9741b0ef04ed334d45a4d3
Leapense <iso9661@gmail.com>
2024-12-05 22:05:09 +0900

    "Added implementation of problem 8689 solution in Gra.cpp, replacing existing header comment with code."

--- 8689번： Gra/Gra.cpp
+++ 8689번： Gra/Gra.cpp
@@ -1,11 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8689                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8689                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/05 21:18:19 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> w(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> w[i];
+    vector<ll> dp(n + 1, LLONG_MIN);
+    dp[1] = w[1];
+    deque<int> dq;
+    dq.push_back(1);
+    for (int i = 2; i <= n; i++)
+    {
+        while (!dq.empty() && dq.front() < i - 6)
+            dq.pop_front();
+        if (!dq.empty())
+            dp[i] = w[i] + dp[dq.front()];
+        while (!dq.empty() && dp[i] >= dp[dq.back()])
+            dq.pop_back();
+        dq.push_back(i);
+    }
+    cout << dp[n];
+}
