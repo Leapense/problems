0c6f3d69bb3aa557f8a9c63236c15cb0791c43e8
Leapense <iso9661@gmail.com>
2024-12-05 21:16:43 +0900

    "Added implementation to Miasta.cpp file, replacing existing comments with a C++ program that reads input, processes data, and prints output."

--- 8677번： Miasta/Miasta.cpp
+++ 8677번： Miasta/Miasta.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8677                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8677                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/05 20:45:08 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> d(n - 1);
+    for (auto &x : d)
+        cin >> x;
+    vector<int> right_reach(n + 1);
+    for (int i = 1; i <= n; i++)
+        right_reach[i] = i;
+    for (int i = n - 1; i >= 1; i--)
+    {
+        if (d[i - 1] == 0 || d[i - 1] == 2)
+        {
+            right_reach[i] = right_reach[i + 1];
+        }
+    }
+    vector<int> left_reach(n + 1);
+    for (int i = 1; i <= n; i++)
+        left_reach[i] = i;
+    for (int i = 2; i <= n; i++)
+    {
+        if (d[i - 2] == 1 || d[i - 2] == 2)
+        {
+            left_reach[i] = left_reach[i - 1];
+        }
+    }
+    vector<int> w(n);
+    for (int i = 1; i <= n; i++)
+    {
+        w[i - 1] = (right_reach[i] - i) + (i - left_reach[i]);
+    }
+    for (int i = 0; i < n; i++)
+        cout << w[i] << (i < n - 1 ? " " : "\n");
+
+    return 0;
+}
