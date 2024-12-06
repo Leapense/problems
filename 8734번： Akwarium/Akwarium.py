cc265fe687cb032ed20d9278c9c4e7cb06a9c343
Leapense <iso9661@gmail.com>
2024-12-06 23:17:12 +0900

    Added BOJ 8738

--- 8734번： Akwarium/Akwarium.py
+++ 8734번： Akwarium/Akwarium.py
@@ -1,11 +1,73 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 8734                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/8734                           #+#        #+#      #+#     #
-#    Solved: 2024/12/06 21:46:02 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+import heapq
+input = sys.stdin.readline
+INF = 10**15
+
+n = int(input().strip())
+fish_data = [None]*(n+1)
+for i in range(1, n+1):
+    m, w = map(int, input().split())
+    fish_data[i] = (m, w)
+
+z = int(input().strip())
+queries = [tuple(map(int, input().split())) for _ in range(z)]
+
+dayOfDeath = [INF]*(n+1)
+
+# Initial population
+population = [(fish_data[i][0], fish_data[i][1], i) for i in range(1, n+1)]
+
+day = 0
+while population:
+    day += 1
+    # Build heaps
+    eaters_heap = []
+    victims_heap = []
+    for (m, w, i) in population:
+        heapq.heappush(eaters_heap, (-m, -w, i))
+        heapq.heappush(victims_heap, (m, w, i))
+
+    valid = set(i for _, _, i in population)
+    new_survivors = []
+
+    while eaters_heap:
+        em, ew, eid = heapq.heappop(eaters_heap)
+        em = -em
+        ew = -ew
+        if eid not in valid:
+            continue
+
+        while victims_heap and victims_heap[0][2] not in valid:
+            heapq.heappop(victims_heap)
+
+        if victims_heap:
+            vm, vw, vid = victims_heap[0]
+            if vm < em:
+                # eat victim
+                heapq.heappop(victims_heap)
+                valid.remove(vid)
+                dayOfDeath[vid] = day
+                # update eater mass
+                new_mass = em + (vm // 2)
+                # eater survives
+                new_survivors.append((new_mass, ew, eid))
+            else:
+                # no suitable victim
+                valid.remove(eid)
+                dayOfDeath[eid] = day
+        else:
+            # no victims
+            valid.remove(eid)
+            dayOfDeath[eid] = day
+
+    if not new_survivors:
+        break
+
+    population = new_survivors
+
+
+for r, x in queries:
+    if x < dayOfDeath[r]:
+        print("TAK")
+    else:
+        print("NIE")
---
+++ 8738번： Rolki papieru/README.md
@@ -0,0 +1,36 @@
+# 8738번: Rolki papieru - <img src="https://static.solved.ac/tier_small/8.svg" style="height:20px" /> Silver III
+
+<!-- performance -->
+
+<!-- 문제 제출 후 깃허브에 푸시를 했을 때 제출한 코드의 성능이 입력될 공간입니다.-->
+
+<!-- end -->
+
+## 문제
+
+[문제 링크](https://boj.kr/8738)
+
+
+<p>Ostatnio Kozik, będąc na wszystkich lekcjach w szkole, był zmuszony do odwiedzenia toalety. Zdziwił się bardzo, gdyż znajdowało się tam <em>n</em> rolek papieru toaletowego. Zdenerwowało go jednak to, że nie wszystkie rolki wyglądały tak samo, gdyż nie były rozwinięte na te same długości. Postanowił więc poprawić wygląd toalety i rozwinąć lub zwinąć rolki tak, aby wszystkie były rozwinięte na tą samą długość.</p>
+
+<p>Jeden ruch polega na zwinięciu lub rozwinięciu 1 cm rolki. Każda rolka ma określoną długość oraz rozwinięcie. Długość rozwinięcia nie może przekraczać długości rolki. Pomóż Kozikowi uporządkować rolki w jak najmniejszej liczbie ruchów.</p>
+
+
+
+## 입력
+
+
+<p>W pierwszym wierszu wejścia znajduje się jedna liczba całkowita <em>n</em> (1 ≤ <em>n</em> ≤ 10<sup>6</sup>), oznaczająca liczbę rolek. W <em>n</em> kolejnych wierszach znajduje się opis <em>i</em> – tej rolki w postaci dwóch liczb całkowitych <em>d<sub>i</sub></em> i <em>r<sub>i</sub></em> oznaczających odpowiednio długość i rozwinięcie <em>i</em> – tej rolki (0 ≤ <em>r<sub>i</sub></em> ≤ <em>d<sub>i</sub></em> ≤ 10<sup>9</sup>).</p>
+
+
+
+## 출력
+
+
+<p>W jedynym wierszu wyjścia powinna znajdować się jedna liczba całkowita, równa minimalnej liczbie ruchów Kozika.</p>
+
+
+
+## 소스코드
+
+[소스코드 보기](Rolki%20papieru.py)
