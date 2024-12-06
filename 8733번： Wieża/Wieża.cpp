33cf15295242b60adacea40a9fce51d3af89fb94
Leapense <iso9661@gmail.com>
2024-12-06 20:45:18 +0900

    "Added implementation to Wie305a.cpp, replacing existing comments with a C++ program that reads input, processes data, and outputs results."

--- 8733번： Wieża/Wieża.cpp
+++ 8733번： Wieża/Wieża.cpp
@@ -1,11 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8733                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8733                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/05 23:23:32 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    
+    vector<long long> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<long long> p(n);
+    p[0] = a[0];
+
+    for (int i = 1; i < n; i++) {
+        p[i] = max(p[i - 1], a[i]);
+    }
+
+    for(int i = 0; i < m; i++) {
+        long long h;
+        cin >> h;
+
+        int idx = (int)(upper_bound(p.begin(), p.end(), h - 1) - p.begin());
+        cout << idx << (i == m - 1 ? "\n" : " ");
+    }
+
+    return 0;
+}
