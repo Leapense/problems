a719ab2481298e0ae61237b98698c55b106e13a8
Leapense <iso9661@gmail.com>
2024-12-07 17:20:58 +0900

    `Refactored Stuttering Strings problem solution with improved code structure and functionality`

--- 8959번： Stuttering Strings/Stuttering Strings.cpp
+++ 8959번： Stuttering Strings/Stuttering Strings.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8959                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8959                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/07 17:11:26 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, nstar, nbang;
+int caseCount = 0;
+
+void backtrack(string &current, int length, int cstar, int cbang) {
+    if ((int)current.size() == length) {
+        cout << current << "\n";
+        return;
+    }
+
+    if (cstar < nstar) {
+        current.push_back('*');
+        backtrack(current, length, cstar + 1, 0);
+        current.pop_back();
+    }
+
+    if (cbang < nbang) {
+        current.push_back('!');
+        backtrack(current, length, 0, cbang + 1);
+        current.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while(true) {
+        cin >> n >> nstar >> nbang;
+        if (!cin || (n == 0 && nstar == 0 && nbang == 0)) break;
+        cout << "Sequence set " << ++caseCount << "\n";
+
+        string s;
+        backtrack(s, n, 0, 0);
+        cout << "\n";
+    }
+
+    return 0;
+}
