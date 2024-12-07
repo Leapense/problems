e3ad1066b47dbdc45f670da3a7902c8b85686dce
Leapense <iso9661@gmail.com>
2024-12-07 15:44:15 +0900

    `Added missing include and main function for problem 8857`

--- 8857번： Kodowanie/Kodowanie.cpp
+++ 8857번： Kodowanie/Kodowanie.cpp
@@ -1,11 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8857                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8857                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/07 15:35:14 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    static long long fib[46];
+    fib[1] = 1; fib[2] = 1;
+    for (int i = 3; i <= 45; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    while(T--) {
+        int n;
+        cin >> n;
+
+        cout << fib[n] << "\n";
+    }
+
+    return 0;
+}
