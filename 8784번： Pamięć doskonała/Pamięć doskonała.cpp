bef7e3c75ca8b52554cc7031d1b22202faf2b50f
Leapense <iso9661@gmail.com>
2024-12-07 14:55:43 +0900

    `Added new solution for problem 8784, including new code and diff from previous version.`

--- 8784번： Pamięć doskonała/Pamięć doskonała.cpp
+++ 8784번： Pamięć doskonała/Pamięć doskonała.cpp
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 8784                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8784                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/07 14:44:10 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Z;
+    cin >> Z;
+
+    while(Z--) {
+        int N;
+        cin >> N;
+        vector<long long> S(N), P(N), A(N);
+        for (int i = 0; i < N; i++) cin >> S[i];
+
+        bool ok = true;
+        for (int i = 0; i < N / 2; i++) {
+            long long x = S[i], y = S[N - 1 - i];
+            if (((x + y) % 2) != 0 || ((x - y) % 2) != 0) {
+                ok = false;
+                break;
+            }
+
+            long long pp = (x + y) / 2;
+            long long aa = (x - y) / 2;
+            P[i] = pp;
+            P[N - 1 - i] = pp;
+            A[i] = aa;
+            A[N - 1 - i] = -aa;
+        }
+
+        if (!ok) {
+            cout << "NIE\n";
+            continue;
+        }
+
+        for (int i = 0; i < N; i++) cout << P[i] << " ";
+        cout << "\n";
+        for (int i = 0; i < N; i++) cout << A[i] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
