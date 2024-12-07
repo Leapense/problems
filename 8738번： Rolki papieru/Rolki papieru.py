0619c8e856e2adbe55d721616d961f9a04a74102
Leapense <iso9661@gmail.com>
2024-12-07 14:43:57 +0900

    `Refactored code for problem 8738: added input handling and calculation of result`

--- 8738번： Rolki papieru/Rolki papieru.py
+++ 8738번： Rolki papieru/Rolki papieru.py
@@ -1,11 +1,32 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 8738                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/8738                           #+#        #+#      #+#     #
-#    Solved: 2024/12/06 22:43:30 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+input_data = sys.stdin.read().strip().split()
+n = int(input_data[0])
+rolls = input_data[1:]
+
+d = []
+r = []
+
+for i in range(n):
+    di = int(rolls[2 * i])
+    ri = int(rolls[2 * i + 1])
+    d.append(di)
+    r.append(ri)
+
+D_min = min(d)
+
+r.sort()
+if n % 2 == 1:
+    median_r = r[n // 2]
+else:
+    median_r = r[n // 2 - 1]
+
+R = median_r
+if R > D_min:
+    R = D_min
+
+result = 0
+for ri in r:
+    result += abs(ri - R)
+
+print(result)
