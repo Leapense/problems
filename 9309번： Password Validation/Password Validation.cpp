566afbb25897b922f5613f6ebe050dfe2f8fb9e9
Leapense <iso9661@gmail.com>
2024-12-08 13:32:07 +0900

    `Refactored password validation code with improved readability and adherence to C++ best practices.`

--- 9309번： Password Validation/Password Validation.cpp
+++ 9309번： Password Validation/Password Validation.cpp
@@ -1,47 +1,69 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-    int N;
-    cin >> N;
-    
-    vector<string> forbidden = {"password", "virginia", "cavalier", "code"};
-
-    while(N--) {
-        string s;
-        cin >> s;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N; cin >> N;
+    string forbiddenWords[4] = {"password", "virginia", "cavalier", "code"};
+    while (N--) {
+        string pwd; cin >> pwd;
         bool valid = true;
-
-        if (s.length() < 9 || s.length() > 20) {valid = false;}
-        int lower = 0, upper = 0, digit = 0, special = 0;
-        string alphanum = "";
-        string special_chars = "!@#$%^&*.,;/?";
-        for (char c : s) {
-            if (islower(c)) lower++;
-            if (isupper(c)) upper++;
-            if (isdigit(c)) digit++;
-            if (special_chars.find(c) != string::npos) {special++;}
-            if (isalnum(c)) alphanum += tolower(c);
+        if ((int)pwd.size() < 9 || (int)pwd.size() > 20) {
+            cout << "Invalid Password\n";
+            continue;
         }
-
-        if (lower < 2 || upper < 2 || digit < 1 || special < 2) {valid = false;}
-        for (int i = 2; i < s.length(); i++) if (s[i] == s[i - 1] && s[i] == s[i - 2]) {valid = false; break;}
-        string reversed_alphanum = alphanum;
-        reverse(reversed_alphanum.begin(), reversed_alphanum.end());
-
-        if (alphanum == reversed_alphanum) {valid = false;}
-        string al = alphanum;
-        string ra = reversed_alphanum;
-
-        for (auto &word : forbidden) {
-            if (al.find(word) != string::npos || ra.find(word) != string::npos) {
-                valid = false;
-                break;
+        int lowerCount=0, upperCount=0, digitCount=0, specialCount=0;
+        string specials = "!@#$%^&*.,;/?";
+        for (int i=0; i<(int)pwd.size(); i++) {
+            if (pwd[i]>='a' && pwd[i]<='z') lowerCount++;
+            else if (pwd[i]>='A' && pwd[i]<='Z') upperCount++;
+            else if (pwd[i]>='0' && pwd[i]<='9') digitCount++;
+            else if (specials.find(pwd[i])!=string::npos || pwd[i]==':') specialCount++;
+        }
+        if (lowerCount<2 || upperCount<2 || digitCount<1 || specialCount<2) {
+            cout << "Invalid Password\n";
+            continue;
+        }
+        for (int i=0; i+2<(int)pwd.size(); i++) {
+            if (pwd[i]==pwd[i+1] && pwd[i+1]==pwd[i+2]) {
+                valid = false; break;
             }
         }
-        cout << (valid ? "Valid Password" : "Invalid Password") << "\n";
+        if (!valid) {
+            cout << "Invalid Password\n";
+            continue;
+        }
+        string alnum;
+        for (auto &c: pwd) {
+            if ((c>='a'&&c<='z')||(c>='A'&&c<='Z')||(c>='0'&&c<='9')) {
+                alnum.push_back(tolower((unsigned char)c));
+            }
+        }
+        {
+            string rev = alnum;
+            reverse(rev.begin(), rev.end());
+            if (alnum == rev && !alnum.empty()) {
+                cout << "Invalid Password\n";
+                continue;
+            }
+        }
+        {
+            string revAlnum = alnum;
+            reverse(revAlnum.begin(), revAlnum.end());
+            for (int i=0; i<4; i++) {
+                if (alnum.find(forbiddenWords[i])!=string::npos || revAlnum.find(forbiddenWords[i])!=string::npos) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        if (!valid) {
+            cout << "Invalid Password\n";
+            continue;
+        }
+        cout << "Valid Password\n";
     }
-
-    return 0;
 }
