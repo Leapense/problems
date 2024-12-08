b447183c75d8954828a904ad37b8d2449fbc6e4d
Leapense <iso9661@gmail.com>
2024-12-08 18:52:08 +0900

    `Added code for problem 9342 with regular expression solution`

--- 9342번： 염색체/염색체.py
+++ 9342번： 염색체/염색체.py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 9342                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/9342                           #+#        #+#      #+#     #
-#    Solved: 2024/12/08 18:25:05 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+import re
+
+def main():
+    pattern = re.compile(r'^[ABCDEF]?A+F+C+[ABCDEF]?$')
+    input = sys.stdin.read().splitlines()
+    if not input:
+        return
+    T = int(input[0])
+    lines = input[1:]
+    while len(lines) < T:
+        lines += sys.stdin.read().splitlines()
+    for i in range(T):
+        s = lines[i].strip()
+        if pattern.match(s):
+            print("Infected!")
+        else:
+            print("Good")
+
+if __name__ == '__main__':
+    main()
