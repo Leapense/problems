f58ebb544a1ce1be40d8a953c3fcf72cae25375f
Leapense <iso9661@gmail.com>
2024-12-11 21:18:00 +0900

    `Refactored code for problem 9479: added import statement and modified loop to read input from stdin`

--- 9479번： Ping!/Ping!.py
+++ 9479번： Ping!/Ping!.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 9479                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/9479                           #+#        #+#      #+#     #
-#    Solved: 2024/12/11 21:02:27 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    if line == "0":
+        break
+    P = list(map(int, line))
+    N = len(P)
+
+    S = [0] * N
+    for i in range(1, N):
+        xor_val = 0
+
+        for d in range(1, int(i ** 0.5) + 1):
+            if i % d == 0:
+                if d < i:
+                    xor_val ^= S[d]
+                div = i // d
+                if div != d and div < i:
+                    xor_val ^= S[div]
+
+        S[i] = P[i] ^ xor_val
+    result = [str(i) for i in range(1, N) if S[i] == 1]
+    print(" ".join(result))
