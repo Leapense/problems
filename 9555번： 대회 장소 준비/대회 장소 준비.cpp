93b8b75cd51fef607b06b70fcd9e7dacb77b8d8d
Leapense <iso9661@gmail.com>
2024-12-12 00:14:53 +0900

    `Refactored code for problem 9555 with added comments and improved formatting`

--- 9555번： 대회 장소 준비/대회 장소 준비.cpp
+++ 9555번： 대회 장소 준비/대회 장소 준비.cpp
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 9555                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/9555                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/12 00:04:32 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T; cin >> T;
+
+    int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    while (T--) {
+        int N, M; cin >> N >> M;
+        vector<vector<int>> hall(N, vector<int>(M));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> hall[i][j];
+            }
+        }
+
+        unordered_set<int> violatingSchools;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                int id = hall[i][j];
+                if (id == -1) continue;
+
+                for (int k = 0; k < 8; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                    int nid = hall[nx][ny];
+
+                    if (nid == id && id != -1) {
+                        violatingSchools.insert(id);
+                        break;
+                    }
+                }
+            }
+        }
+
+        cout << (int)violatingSchools.size() << "\n";
+    }
+
+    return 0;
+}
