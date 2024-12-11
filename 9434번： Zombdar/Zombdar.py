c57d2651d814b17c5989bbc5cffe8d6e3d3ff5ad
Leapense <iso9661@gmail.com>
2024-12-11 20:51:33 +0900

    `Added a new function solve() to process input and print logs with timestamps`

--- 9434번： Zombdar/Zombdar.py
+++ 9434번： Zombdar/Zombdar.py
@@ -1,11 +1,33 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 9434                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/9434                           #+#        #+#      #+#     #
-#    Solved: 2024/12/10 22:52:43 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    input = sys.stdin.readline
+    
+    N = int(input().strip())
+    for _ in range(N):
+        buffer = ""
+        timestamp = 0
+        while True:
+            line = input()
+            if not line:
+                # EOF 처리(문제 상 없을 것으로 가정)
+                break
+            line = line.rstrip('\n')
+            
+            # 종료 조건 확인
+            if line == "END OF CASE":
+                break
+            
+            timestamp += 1
+            buffer += line  # 버퍼에 현재 라인 추가
+            
+            # ';'를 발견할 때까지 처리
+            while True:
+                semicolon_index = buffer.find(';')
+                if semicolon_index == -1:
+                    break
+                # 하나의 완성된 로그 추출
+                entry = buffer[:semicolon_index+1]
+                print(f"{timestamp}: {entry}")
+                buffer = buffer[semicolon_index+1:]
+
+solve()
