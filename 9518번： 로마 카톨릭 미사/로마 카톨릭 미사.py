ac9c87fc19e0413818c1d41cf5f3669d5dba195d
Leapense <iso9661@gmail.com>
2024-12-11 21:32:32 +0900

    `Refactor problem 9518 solution to count handshakes and find maximum handshakes by placing 'o' in empty seats`

--- 9518번： 로마 카톨릭 미사/로마 카톨릭 미사.py
+++ 9518번： 로마 카톨릭 미사/로마 카톨릭 미사.py
@@ -1,11 +1,42 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 9518                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/9518                           #+#        #+#      #+#     #
-#    Solved: 2024/12/11 21:19:20 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def count_handshakes(arr, R, S):
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),           (0, 1),
+                  (1, -1), (1, 0), (1, 1)]
+    handshake = 0
+
+    for r in range(R):
+        for c in range(S):
+            if arr[r][c] == 'o':
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < R and 0 <= nc < S and arr[nr][nc] == 'o':
+                        handshake += 1
+
+    handshake //= 2
+    return handshake
+
+def solve():
+    import sys
+    input = sys.stdin.readline
+    R, S = map(int, input().split())
+    arr = [list(input().strip()) for _ in range(R)]
+
+    current_handshakes = count_handshakes(arr, R, S)
+    empty_seats = [(r, c) for r in range(R) for c in range(S) if arr[r][c] == '.']
+
+    if not empty_seats:
+        print(current_handshakes)
+        return
+    max_handshake = current_handshakes
+    for (r, c) in empty_seats:
+        arr[r][c] = 'o'
+        new_handshakes = count_handshakes(arr, R, S)
+        if new_handshakes > max_handshake:
+            max_handshake = new_handshakes
+
+        arr[r][c] = '.'
+
+    print(max_handshake)
+
+if __name__ == '__main__':
+    solve()
