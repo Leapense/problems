022009117f96a737b04ef42be8e5cdecd14f57cb
Leapense <iso9661@gmail.com>
2024-12-14 00:20:12 +0900

    `Refactored code for Another Word Sorting problem (9794) with improved sorting and output`

--- 9794번： Another Word Sorting/Another Word Sorting.cpp
+++ 9794번： Another Word Sorting/Another Word Sorting.cpp
@@ -1,11 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 9794                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/9794                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/13 23:49:03 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Word {
+    string s;
+    long long score;
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    vector<Word> words;
+    string w;
+    while(cin >> w){
+        long long sum = 0;
+        long long bonus = 0;
+        int n = w.size();
+        vector<bool> used(n, false);
+        for(int i=0;i<n;i++) sum += (w[i]-'a'+1);
+        for(int i=0;i<n-1;i++){
+            if(w[i] == w[i+1] && !used[i] && !used[i+1]){
+                bonus += 2*(w[i]-'a'+1);
+                used[i] = used[i+1] = true;
+            }
+        }
+        words.push_back(Word{w, sum + bonus});
+    }
+    sort(words.begin(), words.end(), [&](const Word &a, const Word &b) -> bool{
+        if(a.score != b.score) return a.score > b.score;
+        return a.s < b.s;
+    });
+    for(auto &word : words) cout << word.s << "\n";
+}
