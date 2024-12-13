c137bfc84d2dc623056ef6ec5aeae790186e82c5
Leapense <iso9661@gmail.com>
2024-12-13 21:55:45 +0900

    `Refactored code for problem 9705: updated include, removed comments, and added main function with input handling and calculation logic.`

--- 9705번： CHARITY BOOTH RENTAL/CHARITY BOOTH RENTAL.cpp
+++ 9705번： CHARITY BOOTH RENTAL/CHARITY BOOTH RENTAL.cpp
@@ -1,11 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 9705                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/9705                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/13 21:49:13 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll T;
+    int case_num = 1;
+
+    while (cin >> T && T != 0) {
+        ll term1 = (T - 1) * (T - 2) / 2;
+        ll term2 = 3 * ((T - 1) / 2);
+        ll term3 = (T % 3 == 0) ? 2 : 0;
+        ll M = (term1 - term2 + term3) / 6;
+        cout << "Case #" << case_num++ << ": " << T << ":" << M << "\n";
+    }
+
+    return 0;
+}
