1eec4db448efc8cb5433c2370ada4065c966d3a3
Leapense <iso9661@gmail.com>
2024-12-13 20:46:20 +0900

    `Refactored Julian to Gregorian date conversion code`

--- 9648번： First Date/First Date.cpp
+++ 9648번： First Date/First Date.cpp
@@ -1,55 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
-struct Date {
-    int year, month, day;
-};
-
-Date jdn_to_gregorian(ll jdn){
-    ll a = jdn + 32044;
-    ll b = (4*a +3)/146097;
-    ll c = a - (146097*b)/4;
-    ll d = (4*c +3)/1461;
-    ll e = c - (1461*d)/4;
-    ll m = (5*e +2)/153;
-    int day = e - (153*m +2)/5 +1;
-    int month = m +3 - 12*(m/10);
-    int year = 100*b + d -4800 + m/10;
-    return Date{(int)year, (int)month, (int)day};
+// 줄리안 날짜를 JDN으로 변환 (줄리안 달력)
+int julian_to_jdn(int Y, int M, int D) {
+    int a = (14 - M) / 12;
+    int y = Y + 4800 - a;
+    int m = M + 12*a - 3;
+    return D + (153*m + 2)/5 + 365*y + y/4 - 32083;
 }
 
-ll julian_to_jdn(int year, int month, int day){
-    ll a = (14 - month) / 12;
-    ll y = year + 4800 - a;
-    ll m = month + 12*a -3;
-    ll JDN = day + (153*m +2)/5 + 365*y + y/4 -32083;
-    return JDN;
+// JDN을 그레고리안 날짜로 변환
+void jdn_to_gregorian(int J, int &Y, int &M, int &D) {
+    int a = J + 32044;
+    int b = (4*a+3)/146097;
+    int c = a - (146097*b)/4;
+    int d = (4*c+3)/1461;
+    int e = c - (1461*d)/4;
+    int m = (5*e+2)/153;
+    D = e - (153*m+2)/5 + 1;
+    M = m + 3 - 12*(m/10);
+    Y = 100*b + d - 4800 + m/10;
 }
 
 int main() {
-    string line;
-    while (getline(cin, line)) {
-        if (line.empty()) continue;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-        int year, month, day;
-        sscanf(line.c_str(), "%d-%d-%d", &year, &month, &day);
+    string date;
+    while (true) {
+        if (! (cin >> date)) break;
+        if (date.empty()) break;
 
-        int D;
-        if (year < 1700) {
-            D = 10;
-        } else {
-            D =10 + (year /100) - (year /400) -12;
-            if(year %100 ==0 && year %400 !=0 && month <3){
-                D -=1;
-            }
+        int Y,M,D;
+        {
+            char c;
+            stringstream ss(date);
+            ss >> Y >> c >> M >> c >> D;
         }
 
-        ll JDN_julian = julian_to_jdn(year, month, day);
-        ll JDN_gregorian = JDN_julian + D + 1;
-        Date gregorian = jdn_to_gregorian(JDN_gregorian);
-        printf("%04d-%02d-%02d\n", gregorian.year, gregorian.month, gregorian.day);
+        // 마지막 줄리안 날짜의 다음 날 JDN 구하기
+        int JDN_jul_last = julian_to_jdn(Y,M,D);
+        int JDN_next = JDN_jul_last + 1; // 다음 날 JDN
+
+        // 다음 날 JDN을 그레고리안 날짜로 변환
+        int gY,gM,gD;
+        jdn_to_gregorian(JDN_next, gY, gM, gD);
+
+        cout << setw(4) << setfill('0') << gY << "-" 
+             << setw(2) << setfill('0') << gM << "-"
+             << setw(2) << setfill('0') << gD << "\n";
     }
 
     return 0;
