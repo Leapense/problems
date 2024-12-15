029a89903f94bbb1e9509e60482ed71bd5d1dddb
Leapense <iso9661@gmail.com>
2024-12-15 16:08:11 +0900

    `Added C++ code to solve problem 9996, diff included in commit message`

--- 9996번： 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cpp
+++ 9996번： 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cpp
@@ -1,11 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 9996                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/9996                           #+#        #+#      #+#    */
-/*   Solved: 2024/12/15 15:37:49 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    string pattern;
+    cin >> pattern;
+
+    int startPos = pattern.find('*');
+    string prefix = pattern.substr(0, startPos);
+    string suffix = pattern.substr(startPos + 1);
+
+    for (int i = 0; i < N; i++)
+    {
+        string fileName;
+        cin >> fileName;
+
+        if (fileName.length() < prefix.length() + suffix.length())
+        {
+            cout << "NE" << endl;
+        }
+        else
+        {
+            if (fileName.substr(0, prefix.length()) == prefix && fileName.substr(fileName.length() - suffix.length()) == suffix) {
+                cout << "DA" << endl;
+            } else {
+                cout << "NE" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
