10908b9a93d02c08273daa606db7a9bafb36904f
Leapense <iso9661@gmail.com>
2024-12-15 17:05:49 +0900

    `Added solution for problem 10024 "Reordering the Cows" in C++`

--- 10024번： Reordering the Cows/Reordering the Cows.cpp
+++ 10024번： Reordering the Cows/Reordering the Cows.cpp
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 10024                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/10024                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/15 16:35:41 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    vector<int> b(n);
+    vector<int> pos_in_b(n + 1);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> b[i];
+        pos_in_b[b[i]] = i;
+    }
+
+    vector<bool> visited(n, false);
+    int num_cycles = 0;
+    int max_cycle_len = -1;
+
+    for (int i = 0; i < n; ++i) {
+        if (a[i] != b[i] && !visited[i]) {
+            num_cycles++;
+            int cycle_len = 0;
+            int current_pos = i;
+
+            do {
+                visited[current_pos] = true;
+                cycle_len++;
+                int next_cow = a[current_pos];
+                current_pos = pos_in_b[next_cow];
+            } while (current_pos != i);
+
+            max_cycle_len = max(max_cycle_len, cycle_len);
+        }
+    }
+
+    cout << num_cycles << " " << max_cycle_len << "\n";
+
+    return 0;
+}
