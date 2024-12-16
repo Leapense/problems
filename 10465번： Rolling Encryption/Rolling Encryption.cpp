5c5865fa7ecb2a41927c9510e8f7585d962d2186
Leapense <iso9661@gmail.com>
2024-12-16 21:00:01 +0900

    `Refactored Rolling Encryption code to improve performance and readability`

--- 10465번： Rolling Encryption/Rolling Encryption.cpp
+++ 10465번： Rolling Encryption/Rolling Encryption.cpp
@@ -1,48 +1,43 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int k;
-  cin >> k;
-  string s;
-  cin >> s;
-
-  string encrypted_s = "";
-
-  // First k characters are plain text (corrected boundary condition)
-  for (int i = 0; i < min((int)s.length(), k); ++i) {
-    encrypted_s += s[i];
-  }
-
-  // Encrypt the rest of the characters
-  for (int i = k; i < s.length(); ++i) {
-    // Find the most frequent character in the previous k characters
-    vector<int> counts(26, 0);
-    for (int j = i - k; j < i; ++j) {
-      counts[s[j] - 'a']++;
-    }
-
-    int max_count = 0;
-    char most_frequent = 'a'; // Initialize with the first letter
-
-    for (int j = 0; j < 26; ++j) {
-        if (counts[j] > max_count) {
-            max_count = counts[j];
-            most_frequent = (char)('a' + j);
-        } else if (counts[j] == max_count && (char)('a' + j) < most_frequent) {
-            most_frequent = (char)('a' + j);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int k;
+    cin >> k;
+    string s;
+    cin >> s;
+    int n = s.size();
+    int counts[26] = {0};
+    for (int i = 0; i < k && i < n; i++)
+        counts[s[i] - 'a']++;
+    string result;
+    for (int i = 0; i < n; i++)
+    {
+        if (i < k)
+        {
+            result += s[i];
+        }
+        else
+        {
+            char most_freq = 'a';
+            int max_count = counts[0];
+            for (int j = 1; j < 26; j++)
+            {
+                if (counts[j] > max_count || (counts[j] == max_count && ('a' + j) < most_freq))
+                {
+                    max_count = counts[j];
+                    most_freq = 'a' + j;
+                }
+            }
+            int shift = most_freq - 'a' + 1;
+            char shifted = 'a' + (s[i] - 'a' + shift) % 26;
+            result += shifted;
+            counts[s[i - k] - 'a']--;
+            counts[s[i] - 'a']++;
         }
     }
-
-    // Shift the current character
-    encrypted_s += (char)('a' + (s[i] - 'a' + (most_frequent - 'a' + 1)) % 26);
-  }
-
-  cout << encrypted_s << endl;
-
-  return 0;
+    cout << result;
 }
