b58c3b7ce400d0e65b91a9da077857e711ad6584
Leapense <iso9661@gmail.com>
2024-12-16 20:06:22 +0900

    `Added Rolling Encryption.cpp with implementation details`

--- 10465번： Rolling Encryption/Rolling Encryption.cpp
+++ 10465번： Rolling Encryption/Rolling Encryption.cpp
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 10465                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/10465                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/16 19:57:17 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+
+    string s;
+    cin >> s;
+
+    string encrypted_s = "";
+
+    for (int i = 0; i < min((int)s.length(), k); ++i) {
+        encrypted_s += s[i];
+    }
+
+    for (int i = k; i < s.length(); ++i) {
+        vector<int> counts(26,0);
+
+        for (int j = i - k; j < i; ++j) {
+            counts[s[j] - 'a']++;
+        }
+
+        int max_count = 0;
+
+        char most_frequent = ' ';
+
+        for (int j = 0; j < 26; ++j) {
+            if (counts[j] > max_count) {
+                max_count = counts[j];
+                most_frequent = (char)('a' + j);
+            }
+        }
+
+        encrypted_s += (char)('a' + (s[i] - 'a' + (most_frequent - 'a' + 1)) % 26);
+    }
+
+    cout << encrypted_s << "\n";
+
+    return 0;
+}
