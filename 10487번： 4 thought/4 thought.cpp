81abf46365698d773cc88c13ddba940398e8490b
Leapense <iso9661@gmail.com>
2024-12-16 21:55:57 +0900

    `Refactored code for calculating all possible expressions with four 4's and three operators, and storing the results in a map for efficient lookup.`

--- 10487번： 4 thought/4 thought.cpp
+++ 10487번： 4 thought/4 thought.cpp
@@ -1,59 +1,69 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int calculate(int a, int b, char op) {
-    if (op == '*') return a * b;
-    if (op == '+') return a + b;
-    if (op == '-') return a - b;
-    if (op == '/') {
-        if (b == 0) return 1000001;
-        return a / b;
-    }
-
-    return 1000001;
-}
-
-int main()
-{
-    int m;
-    cin >> m;
-    cin.ignore();
-
-    unordered_map<int, string> results;
-    vector<char> ops = {'*', '+', '-', '/'};
-
-    for (char op1 : ops) {
-        for (char op2 : ops) {
-            for (char op3 : ops) {
-                int result1 = calculate(calculate(4, 4, op1), calculate(4, 4, op3), op2);
-                if (result1 >= -1000000 && result1 <= 1000000) {
-                    string equation1 = "4 " + string(1, op1) + " 4 " + string(1, op2) + " 4 " + string(1, op3) + " 4";
-                    results[result1] = equation1;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    vector<char> operators = {'+', '-', '*', '/'};
+    unordered_map<long long, string> expr_map;
+    for(auto op1: operators){
+        for(auto op2: operators){
+            for(auto op3: operators){
+                vector<long long> nums = {4,4,4,4};
+                vector<char> ops = {op1, op2, op3};
+                vector<long long> values = nums;
+                vector<char> operations = ops;
+                int n_ops = operations.size();
+                for(int i=0;i<operations.size();){
+                    if(operations[i] == '*' || operations[i] == '/'){
+                        long long a = values[i];
+                        long long b = values[i+1];
+                        long long res;
+                        if(operations[i] == '*') res = a * b;
+                        else{
+                            if(b == 0){
+                                res = 0;
+                            }
+                            else{
+                                res = a / b;
+                            }
+                        }
+                        values.erase(values.begin()+i);
+                        values.erase(values.begin()+i);
+                        values.insert(values.begin()+i, res);
+                        operations.erase(operations.begin()+i);
+                    }
+                    else{
+                        i++;
+                    }
                 }
-
-                int result2 = calculate(calculate(calculate(4, 4, op1), 4, op2), 4, op3);
-                if (result2 >= -1000000 && result2 <= 1000000) {
-                    string equation2 = "4 " + string(1, op1) + " 4 " + string(1, op2) + " 4 " + string(1, op3) + " 4";
-                    results[result2] = equation2;
+                while(operations.size()>0){
+                    long long a = values[0];
+                    long long b = values[1];
+                    long long res;
+                    if(operations[0] == '+') res = a + b;
+                    else{
+                        res = a - b;
+                    }
+                    values.erase(values.begin());
+                    values.erase(values.begin());
+                    values.insert(values.begin(), res);
+                    operations.erase(operations.begin());
+                }
+                long long result = values[0];
+                if(expr_map.find(result) == expr_map.end()){
+                    string expr = "4 " + string(1, op1) + " 4 " + string(1, op2) + " 4 " + string(1, op3) + " 4 = " + to_string(result);
+                    expr_map[result] = expr;
                 }
             }
         }
     }
-
-    while (m--) {
-        int n;
-        cin >> n;
-
-        if (results.count(n)) {
-            cout << results[n] << " = " << n << "\n";
-        } else {
-            cout << "no solution" << endl;
-        }
+    int m;
+    cin>>m;
+    while(m--){
+        long long n;
+        cin>>n;
+        if(expr_map.find(n)!=expr_map.end()) cout<<expr_map[n]<<"\n";
+        else cout<<"no solution\n";
     }
-
-    return 0;
 }
