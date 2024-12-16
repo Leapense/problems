89d394b960dcb3e725816da9c3794ce0a1d045ab
Leapense <iso9661@gmail.com>
2024-12-16 20:37:07 +0900

    `Fixed boundary condition in rolling encryption algorithm and added comments for clarity.`

--- 10465번： Rolling Encryption/Rolling Encryption.cpp
+++ 10465번： Rolling Encryption/Rolling Encryption.cpp
@@ -5,42 +5,44 @@
 
 using namespace std;
 
-int main()
-{
-    int k;
-    cin >> k;
+int main() {
+  int k;
+  cin >> k;
+  string s;
+  cin >> s;
 
-    string s;
-    cin >> s;
+  string encrypted_s = "";
 
-    string encrypted_s = "";
+  // First k characters are plain text (corrected boundary condition)
+  for (int i = 0; i < min((int)s.length(), k); ++i) {
+    encrypted_s += s[i];
+  }
 
-    for (int i = 0; i < min((int)s.length(), k); ++i) {
-        encrypted_s += s[i];
+  // Encrypt the rest of the characters
+  for (int i = k; i < s.length(); ++i) {
+    // Find the most frequent character in the previous k characters
+    vector<int> counts(26, 0);
+    for (int j = i - k; j < i; ++j) {
+      counts[s[j] - 'a']++;
     }
 
-    for (int i = k; i < s.length(); ++i) {
-        vector<int> counts(26,0);
+    int max_count = 0;
+    char most_frequent = 'a'; // Initialize with the first letter
 
-        for (int j = i - k; j < i; ++j) {
-            counts[s[j] - 'a']++;
+    for (int j = 0; j < 26; ++j) {
+        if (counts[j] > max_count) {
+            max_count = counts[j];
+            most_frequent = (char)('a' + j);
+        } else if (counts[j] == max_count && (char)('a' + j) < most_frequent) {
+            most_frequent = (char)('a' + j);
         }
-
-        int max_count = 0;
-
-        char most_frequent = ' ';
-
-        for (int j = 0; j < 26; ++j) {
-            if (counts[j] > max_count) {
-                max_count = counts[j];
-                most_frequent = (char)('a' + j);
-            }
-        }
-
-        encrypted_s += (char)('a' + (s[i] - 'a' + (most_frequent - 'a' + 1)) % 26);
     }
 
-    cout << encrypted_s << "\n";
+    // Shift the current character
+    encrypted_s += (char)('a' + (s[i] - 'a' + (most_frequent - 'a' + 1)) % 26);
+  }
 
-    return 0;
+  cout << encrypted_s << endl;
+
+  return 0;
 }
