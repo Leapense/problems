cb4104dbac7b376b0cd5ce9caba0f958f7decbc1
Leapense <iso9661@gmail.com>
2024-12-16 11:30:19 +0900

    `Added solution for problem 10025, including code and commit history`

--- 10025번： 게으른 백곰/게으른 백곰.cpp
+++ 10025번： 게으른 백곰/게으른 백곰.cpp
@@ -1,11 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 10025                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/10025                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/16 11:12:08 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    const int MAX_COORD = 1000000;
+    vector<int> ice(MAX_COORD + 1, 0);
+
+    for (int i = 0; i < N; i++) {
+        int g, x;
+        cin >> g >> x;
+        ice[x] += g;
+    }
+
+    int window_size = 2 * K + 1;
+    long long current_sum = 0;
+    long long max_sum = 0;
+
+    for (int i = 0; i <= min(MAX_COORD, window_size - 1); i++) {
+        current_sum += ice[i];
+    }
+    max_sum = current_sum;
+
+    for (int i = 1; i + window_size - 1 <= MAX_COORD; i++) {
+        current_sum -= ice[i - 1];
+        current_sum += ice[i + window_size - 1];
+        max_sum = max(max_sum, current_sum);
+    }
+
+    cout << max_sum << "\n";
+    return 0;
+}
