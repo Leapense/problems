f4f399fa048eea6d9496955d92d4017322d77d6a
Leapense <iso9661@gmail.com>
2024-12-16 12:01:39 +0900

    `Refactored code for problem 10106: The Geneva Confection`

--- 10106번： The Geneva Confection/The Geneva Confection.cpp
+++ 10106번： The Geneva Confection/The Geneva Confection.cpp
@@ -1,11 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 10106                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/10106                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/16 11:32:44 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T;
+    cin >> T;
+
+    while (T--) 
+    {
+        int N;
+        cin >> N;
+        vector<int> cars(N);
+        for (int &x : cars) cin >> x;
+        int expected = 1;
+
+        vector<int> stack;
+        for (int i = N - 1; i >= 0; i--) {
+            if (cars[i] == expected) {
+                expected++;
+                while(!stack.empty() && stack.back() == expected) {
+                    stack.pop_back();
+                    expected++;
+                }
+            }
+            else
+            {
+                stack.push_back(cars[i]);
+            }
+        }
+        while(!stack.empty() && stack.back() == expected) {
+            stack.pop_back();
+            expected++;
+        }
+
+        if (expected == N + 1) cout << "Y\n";
+        else cout << "N\n";
+    }
+
+    return 0;
+}
