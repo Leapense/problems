191d82ab3090a9ac8021ee8174c2609f8e459756
Leapense <iso9661@gmail.com>
2024-12-19 23:34:06 +0900

    `Refactored code for problem 10751: updated COW.cpp with new implementation`

--- 10751번： COW/COW.cpp
+++ 10751번： COW/COW.cpp
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 10751                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/10751                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/19 22:39:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll N;
+    cin >> N;
+    string S;
+    cin >> S;
+    ll count_C = 0, count_CO = 0, count_COW = 0;
+    for (char c : S)
+    {
+        if (c == 'C')
+            count_C++;
+        else if (c == 'O')
+            count_CO += count_C;
+        else if (c == 'W')
+            count_COW += count_CO;
+    }
+    cout << count_COW;
+
+    return 0;
+}
