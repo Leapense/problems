ba1c61fc146928f43046339dc64dc45a483fe39c
Leapense <iso9661@gmail.com>
2024-12-21 02:00:08 +0900

    `Refactor code for "Trapped in the Haybales" problem, including changes to Bale struct, sorting, and dynamic programming implementation.`

--- 10766번： Trapped in the Haybales/Trapped in the Haybales.cpp
+++ 10766번： Trapped in the Haybales/Trapped in the Haybales.cpp
@@ -1,36 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+static const long long INF = 1000000000000LL; // 10^12
 
-int main(){
+struct Bale {
+    long long s, p;
+};
+
+int N;
+Bale bales[4002];
+bool visited[4002][4002];
+bool dp[4002][4002]; // dp[i][j] = canEscape(i,j)
+
+bool canEscapeMemo(int i, int j) {
+    // If one end is open, we can escape
+    if (i == 0 || j == N+1) return true;
+    if (visited[i][j]) return dp[i][j];
+    visited[i][j] = true;
+    long long dist = bales[j].p - bales[i].p;
+
+    bool res = false;
+    // Try breaking left bale i
+    if (dist > bales[i].s && i > 0) {
+        res = res || canEscapeMemo(i-1, j);
+    }
+    // Try breaking right bale j
+    if (!res && dist > bales[j].s && j < N+1) {
+        res = res || canEscapeMemo(i, j+1);
+    }
+
+    dp[i][j] = res;
+    return res;
+}
+
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    int N;
+    cin.tie(nullptr);
+
     cin >> N;
-    vector<pair<ll, ll>> bales(N);
-    for(int i=0;i<N;i++) cin >> bales[i].first >> bales[i].second;
-    sort(bales.begin(), bales.end(), [&](const pair<ll,ll> &a, const pair<ll,ll> &b)->bool{
-        return a.second < b.second;
+    for (int i=1; i<=N; i++) {
+        cin >> bales[i].s >> bales[i].p;
+    }
+    // Add dummy bales
+    bales[0].s = 0; bales[0].p = -INF;
+    bales[N+1].s = 0; bales[N+1].p = INF;
+
+    sort(bales, bales+N+2, [](const Bale &a, const Bale &b){
+        return a.p < b.p;
     });
-    vector<ll> max_left(N+1, LLONG_MIN);
-    for(int i=1;i<=N;i++) {
-        max_left[i] = max(max_left[i-1], bales[i-1].second + bales[i-1].first);
-    }
-    vector<ll> min_right(N+2, LLONG_MAX);
-    for(int i=N;i>=1;i--){
-        min_right[i] = min(min_right[i+1], bales[i-1].second - bales[i-1].first);
-    }
-    ll total =0;
-    for(int i=1;i<N;i++){
-        if(min_right[i+1] <= bales[i-1].second){
-            ll start = max(bales[i-1].second, min_right[i+1]);
-            ll end = min(bales[i].second, max_left[i]);
-            if(end > bales[i-1].second){
-                total += end - bales[i-1].second;
-            }
+
+    // We have a total of N+2 bales (including dummy)
+    // Check intervals between consecutive bales from 0..N
+    long long result = 0;
+    for (int i=0; i<=N; i++) {
+        // Interval between bale i and bale i+1
+        // If can't escape from this interval, add length
+        if (!canEscapeMemo(i, i+1)) {
+            result += bales[i+1].p - bales[i].p;
         }
     }
-    cout << total;
+
+    cout << result << "\n";
+
     return 0;
 }
