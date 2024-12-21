7b8832be910d703777307557621cf92dd3286c23
Leapense <iso9661@gmail.com>
2024-12-21 19:41:27 +0900

    `Added solution for problem 11029, including necessary includes and main function with input handling and calculation logic.`

--- 11029번： ĐỘI MŨ/ĐỘI MŨ.cpp
+++ 11029번： ĐỘI MŨ/ĐỘI MŨ.cpp
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 11029                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/11029                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/21 18:52:00 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        long long n, p;
+        cin >> n >> p;
+
+        if (n == 1) {
+            cout << 0 << '\n';
+            continue;
+        }
+
+        long long c = __builtin_popcountll(n - 1);
+
+        long long ans = c % p;
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
