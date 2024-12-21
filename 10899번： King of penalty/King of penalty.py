93d81a8667ada4d48b244d78720cab3810ceef92
Leapense <iso9661@gmail.com>
2024-12-21 18:25:21 +0900

    `Refactor: Add binary search to find maximum K for problem 10899`

--- 10899번： King of penalty/King of penalty.py
+++ 10899번： King of penalty/King of penalty.py
@@ -1,11 +1,44 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 10899                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/10899                          #+#        #+#      #+#     #
-#    Solved: 2024/12/21 18:15:55 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+
+    def solve():
+        import sys
+
+        P, N = map(int, sys.stdin.readline().split())
+        Ti = list(map(int, sys.stdin.readline().split()))
+        Ti_sorted = sorted(Ti)
+        prefix_sum = [0] * (N +1)
+        for i in range(N):
+            prefix_sum[i+1] = prefix_sum[i] + Ti_sorted[i]
+            if prefix_sum[i+1] > P -1:
+                # No need to compute further, but continue to compute full prefix_sum
+                pass
+
+        # Binary search to find maximum K where prefix_sum[K] <= P-1
+        left =0
+        right =N
+        while left < right:
+            mid = (left + right +1)//2
+            if prefix_sum[mid] <= P -1:
+                left =mid
+            else:
+                right =mid -1
+        K=left
+        if K ==0:
+            print(0,0)
+            return
+        selected_Ti = Ti_sorted[:K]
+        selected_Ti_sorted_desc = sorted(selected_Ti, reverse=True)
+        sum_Tj = sum(selected_Ti_sorted_desc)
+        S = P - sum_Tj -1
+        if S <0:
+            S =0
+        sum_penalty =0
+        for idx in range(K):
+            multiplier = K - idx
+            sum_penalty += multiplier * selected_Ti_sorted_desc[idx]
+        total_penalty = K * S + sum_penalty
+        print(K, total_penalty)
+
+    solve()
+main()
