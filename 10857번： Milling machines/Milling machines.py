181dcd6bf80c30d92e6ba53ce742c8720ac1d61a
Leapense <iso9661@gmail.com>
2024-12-21 18:15:17 +0900

    `Refactored code for problem 10857: added input parsing and processing`

--- 10857번： Milling machines/Milling machines.py
+++ 10857번： Milling machines/Milling machines.py
@@ -1,11 +1,45 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 10857                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/10857                          #+#        #+#      #+#     #
-#    Solved: 2024/12/21 18:06:51 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.read().split()
+    it = iter(input)
+
+    try:
+        W = int(next(it))
+        S = int(next(it))
+    except StopIteration:
+        W, S = 0, 0
+    try:
+        X = int(next(it))
+        Y = int(next(it))
+    except StopIteration:
+        X, Y = 0, 0
+    workpieces = []
+    for _ in range(W):
+        wp = []
+        for _ in range(X):
+            try:
+                wp.append(int(next(it)))
+            except StopIteration:
+                wp.append(0)
+        workpieces.append(wp)
+    max_si = [0] * X
+    for _ in range(S):
+        for j in range(X):
+            try:
+                s = int(next(it))
+            except StopIteration:
+                s = 0
+            if s > max_si[j]:
+                max_si[j] = s
+    H = [Y - max_si[j] for j in range(X)]
+    output = []
+    for wp in workpieces:
+        rem = []
+        for j in range(X):
+            rem.append(str(wp[j] if wp[j] < H[j] else H[j]))
+        output.append(' '.join(rem))
+    print('\n'.join(output))
+
+if __name__ == '__main__':
+    main()
