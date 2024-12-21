1a7710e83c18a54481638f841f0187ce60467257
Leapense <iso9661@gmail.com>
2024-12-21 20:48:18 +0900

    `Refactored 11035 SNG/SNG.cpp to use modern C++ and improved algorithm`

--- 11035번： SÀNG/SÀNG.cpp
+++ 11035번： SÀNG/SÀNG.cpp
@@ -1,11 +1,44 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 11035                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/11035                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/21 19:43:00 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, K;
+
+    while (true) {
+        if (!(cin >> N >> K)) break;
+
+        vector<bool> crossed(N + 1, false);
+        int countCrossed = 0;
+        int answer = 0;
+
+        for (int i = 2; i <= N; i++) {
+            if (!crossed[i]) {
+                crossed[i] = true;
+                countCrossed++;
+                if (countCrossed == K) {
+                    answer = i;
+                    break;
+                }
+
+                for (int j = 2 * i; j <= N; j += i) {
+                    if (!crossed[j]) {
+                        crossed[j] = true;
+                        countCrossed++;
+                        if (countCrossed == K) {
+                            answer = j;
+                            break;
+                        }
+                    }
+                }
+
+                if (countCrossed == K) break;
+            }
+        }
+
+        cout << answer << "\n";
+    }
+
+    return 0;
+}
