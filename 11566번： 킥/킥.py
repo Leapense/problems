9094de007e48822701c666b1f63bc1a1fefac6b8
Leapense <iso9661@gmail.com>
2024-12-22 21:52:17 +0900

    `Refactor problem solution for 11566: add input parsing and calculate possible values for k`

--- 11566번： 킥/킥.py
+++ 11566번： 킥/킥.py
@@ -1,11 +1,25 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 11566                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11566                          #+#        #+#      #+#     #
-#    Solved: 2024/12/22 21:46:18 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+possible = []
+max_k = (m - n) // (n - 1) if n > 1 else m - n
+
+for k in range(0, max_k + 1):
+    required_length = n + (n - 1) * k
+    if required_length > m:
+        continue
+    for p in range(0, m - required_length + 1):
+        match = True
+        for i in range(n):
+            if b[p + i * (k + 1)] != a[i]:
+                match = False
+                break
+        if match:
+            possible.append(k)
+            break
+if not possible:
+    print(-1)
+else:
+    print(min(possible), max(possible))
