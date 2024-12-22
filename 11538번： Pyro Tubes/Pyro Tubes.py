9fd2750966ffc0af14f506c9bd18fb573f078d65
Leapense <iso9661@gmail.com>
2024-12-22 21:08:32 +0900

    `Added new solve function to Pyro Tubes.py, refactored code and added comments`

--- 11538번： Pyro Tubes/Pyro Tubes.py
+++ 11538번： Pyro Tubes/Pyro Tubes.py
@@ -1,11 +1,33 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 11538                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11538                          #+#        #+#      #+#     #
-#    Solved: 2024/12/22 20:55:51 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    vals = []
+    for line in sys.stdin:
+        n = line.strip()
+        if not n:
+            continue
+        num = int(n)
+        if num == -1:
+            break
+        vals.append(num)
+
+    s = set(vals)
+    vals_len = len(vals)
+    pow2 = [1 << i for i in range(18)]
+    results = []
+
+    for v in vals:
+        count = 0
+        for i in range(18):
+            neighbor = v ^ pow2[i]
+            if neighbor > v and neighbor in s:
+                count += 1
+        for i in range(17):
+            for j in range(i + 1, 18):
+                neighbor = v ^ pow2[i] ^ pow2[j]
+                if neighbor > v and neighbor in s:
+                    count += 1
+        results.append(count)
+    for v, c in zip(vals, results):
+        print(f"{v}:{c}")
+
+solve()
