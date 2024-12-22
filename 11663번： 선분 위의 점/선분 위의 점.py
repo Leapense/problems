57562cf8724ef4406e7c3bdc6d9290068e578356
Leapense <iso9661@gmail.com>
2024-12-22 22:32:06 +0900

    `Refactor problem 11663 solution to use Python and bisect module`

--- 11663번： 선분 위의 점/선분 위의 점.py
+++ 11663번： 선분 위의 점/선분 위의 점.py
@@ -1,11 +1,17 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 11663                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11663                          #+#        #+#      #+#     #
-#    Solved: 2024/12/22 22:11:20 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys, bisect
+def main():
+    input = sys.stdin.read().split()
+    N, M = int(input[0]), int(input[1])
+    points = sorted(map(int, input[2:2+N]))
+    output = []
+    idx = 2 + N
+    for _ in range(M):
+        L, R = int(input[idx]), int(input[idx + 1])
+        idx += 2
+        left = bisect.bisect_left(points, min(L, R))
+        right = bisect.bisect_right(points, max(L, R))
+        output.append(str(right - left))
+
+    print('\n'.join(output))
+if __name__ == '__main__':
+    main()
