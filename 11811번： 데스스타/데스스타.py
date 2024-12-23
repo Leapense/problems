16899089281bf5063a9b972cb13928b1319ef28d
Leapense <iso9661@gmail.com>
2024-12-23 22:28:13 +0900

    `Refactored code for problem 11811: replaced original code with new implementation`

--- 11811번： 데스스타/데스스타.py
+++ 11811번： 데스스타/데스스타.py
@@ -1,11 +1,11 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 11811                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11811                          #+#        #+#      #+#     #
-#    Solved: 2024/12/23 22:13:27 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+result = []
+
+for i in range(n):
+    or_val = 0
+    for j in range(n):
+        if i != j:
+            or_val |= matrix[i][j]
+    result.append(or_val)
+print(' '.join(map(str, result)))
