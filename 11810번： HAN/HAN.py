fcff4047a98f221477bf12fed4ebe730b64df57d
Leapense <iso9661@gmail.com>
2024-12-23 22:09:59 +0900

    `Refactored code for problem 11810 with added comments and improved structure`

--- 11810번： HAN/HAN.py
+++ 11810번： HAN/HAN.py
@@ -1,11 +1,55 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 11810                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11810                          #+#        #+#      #+#     #
-#    Solved: 2024/12/22 23:20:17 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.read().splitlines()
+    Q = int(input[0])
+    orders = input[1:]
+    counts = [0]*26
+    current_step =0
+    current_position = None
+    current_direction =1
+    outputs = []
+    for order in orders:
+        parts = order.split()
+        if parts[0] == "SMJER":
+            n = int(parts[1])
+            delta = n - current_step
+            if current_step ==0 and current_position is None:
+                if delta >=1:
+                    counts[0] +=1
+                    current_position=0
+                    delta -=1
+                    current_step +=1
+            if delta >0:
+                full_cycles = delta //26
+                remainder_steps = delta %26
+                for i in range(26):
+                    counts[i] +=full_cycles
+                for _ in range(remainder_steps):
+                    current_position = (current_position + current_direction) %26
+                    counts[current_position] +=1
+                current_step +=delta
+            current_direction *= -1
+        elif parts[0] == "UPIT":
+            n = int(parts[1])
+            x = parts[2]
+            delta = n - current_step
+            if current_step ==0 and current_position is None:
+                if delta >=1:
+                    counts[0] +=1
+                    current_position=0
+                    delta -=1
+                    current_step +=1
+            if delta >0:
+                full_cycles = delta //26
+                remainder_steps = delta %26
+                for i in range(26):
+                    counts[i] +=full_cycles
+                for _ in range(remainder_steps):
+                    current_position = (current_position + current_direction) %26
+                    counts[current_position] +=1
+                current_step +=delta
+            outputs.append(str(counts[ord(x)-97]))
+    print('\n'.join(outputs))
+
+main()
