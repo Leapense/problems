b89412933bdb843362c2921c406609d106c5b28e
Leapense <iso9661@gmail.com>
2024-12-24 21:09:31 +0900

    `Added solution for problem 11875, implemented in MULTIGRAM.py`

--- 11875번： MULTIGRAM/MULTIGRAM.py
+++ 11875번： MULTIGRAM/MULTIGRAM.py
@@ -1,11 +1,41 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 11875                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11875                          #+#        #+#      #+#     #
-#    Solved: 2024/12/24 20:55:34 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    input = sys.stdin.readline
+
+    S = input().strip()
+    n = len(S)
+
+    divisors = []
+
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+        i += 1
+    divisors.sort()
+
+    for d in divisors:
+        if d == n:
+            break
+        root = S[:d]
+        from collections import Counter
+        root_counter = Counter(root)
+
+        piece_count = n // d
+
+        is_multigram = True
+        for idx in range(1, piece_count):
+            piece = S[idx * d : (idx + 1) * d]
+            piece_counter = Counter(piece)
+            if piece_counter != root_counter:
+                is_multigram = False
+                break
+        if is_multigram:
+            print(root)
+            return
+        
+    print(-1)
+
+solve()
