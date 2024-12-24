13a02acc6514772984b162d4c58a8949578e1724
Leapense <iso9661@gmail.com>
2024-12-24 20:53:00 +0900

    Added Solution of BOJ 32941

--- 32941번： 왜 맘대로 예약하냐고/왜 맘대로 예약하냐고.py
+++ 32941번： 왜 맘대로 예약하냐고/왜 맘대로 예약하냐고.py
@@ -1,11 +1,29 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 32941                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/32941                          #+#        #+#      #+#     #
-#    Solved: 2024/12/24 20:42:41 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    lines = sys.stdin.read().splitlines()
+    pointer = 0
+
+    T_X = lines[pointer].split()
+    T = int(T_X[0])
+    X = int(T_X[1])
+    pointer += 1
+
+    N = int(lines[pointer])
+    pointer += 1
+
+    available_periods = []
+
+    for _ in range(N):
+        K_i = int(lines[pointer])
+        pointer += 1
+        periods = set(map(int, lines[pointer].split()))
+        available_periods.append(periods)
+        pointer += 1
+
+    if all(X in periods for periods in available_periods):
+        print("YES")
+    else:
+        print("NO")
+
+if __name__ == '__main__':
+    main()
