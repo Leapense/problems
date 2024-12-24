387d4d044ceb0a2e1af7352ca379665cf81ce687
Leapense <iso9661@gmail.com>
2024-12-24 21:28:52 +0900

    `Added function to calculate minimum additions to balance parentheses in a string`

--- 11899번： 괄호 끼워넣기/괄호 끼워넣기.py
+++ 11899번： 괄호 끼워넣기/괄호 끼워넣기.py
@@ -1,11 +1,17 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 11899                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/11899                          #+#        #+#      #+#     #
-#    Solved: 2024/12/24 21:12:41 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def min_additions(s):
+    front_add = 0
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                front_add += 1
+    end_add = len(stack)
+    return front_add + end_add
+
+# Read input
+S = input().strip()
+print(min_additions(S))
