7b1af0318b5f99f19e973bcdc595a73e0f95b146
Leapense <iso9661@gmail.com>
2024-12-25 15:46:35 +0900

    `Added code for problem 12186: Sort a scrambled itinerary (Small)`

--- 12186번： Sort a scrambled itinerary (Small)/Sort a scrambled itinerary (Small).cpp
+++ 12186번： Sort a scrambled itinerary (Small)/Sort a scrambled itinerary (Small).cpp
@@ -1,11 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12186                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12186                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 15:35:16 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+    for (int test = 1; test <= T; test++) {
+        int N;
+        cin >> N;
+        cin.ignore();
+        map<string, string> flights;
+        set<string> destinations;
+        for(int i = 0; i < N; i++) {
+            string src, dest;
+            getline(cin, src);
+            getline(cin, dest);
+            flights[src] = dest;
+            destinations.insert(dest);
+        }
+
+        string start;
+        for (auto& pair : flights) {
+            if (destinations.find(pair.first) == destinations.end()) {
+                start = pair.first;
+                break;
+            }
+        }
+
+        string itinerary;
+        string current = start;
+        for (int i = 0; i < N; i++) {
+            itinerary += current + "-" + flights[current];
+            if (i != N - 1) {
+                itinerary += " ";
+            }
+
+            current = flights[current];
+        }
+
+        cout << "Case #" << test << ": " << itinerary << endl;
+    }
+
+    return 0;
+}
