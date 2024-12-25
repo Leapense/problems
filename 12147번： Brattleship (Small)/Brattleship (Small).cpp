b15bc5ef9b0c1ad2029cf886ace5565ab116d187
Leapense <iso9661@gmail.com>
2024-12-25 14:25:15 +0900

    `Added solution for problem 12147 in C++`

--- 12147번： Brattleship (Small)/Brattleship (Small).cpp
+++ 12147번： Brattleship (Small)/Brattleship (Small).cpp
@@ -1,11 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12147                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12147                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 14:18:14 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; ++t) {
+        int R, C, W;
+        cin >> R >> C >> W;
+
+        int option1 = R * (C - W + 1);
+        int option2 = R * W;
+        int minimal_guesses = max(option1, option2);
+        cout << "Case #" << t << ": " << minimal_guesses << endl;
+    }
+
+    return 0;
+}
