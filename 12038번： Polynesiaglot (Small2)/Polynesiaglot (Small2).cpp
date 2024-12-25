9ee6004435f434b0ff54b467d4a6e43c4787f9b5
Leapense <iso9661@gmail.com>
2024-12-25 13:05:21 +0900

    `Refactored Polynesiaglot (Small2) code to reduce memory usage by storing only previous values instead of entire dp table.`

--- 12038번： Polynesiaglot (Small2)/Polynesiaglot (Small2).cpp
+++ 12038번： Polynesiaglot (Small2)/Polynesiaglot (Small2).cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 const long long MOD = 1000000007;
@@ -7,24 +6,25 @@ const long long MOD = 1000000007;
 int main() {
     int T;
     cin >> T;
-
     for (int t = 1; t <= T; ++t) {
         int C, V, L;
         cin >> C >> V >> L;
-
-        vector<vector<long long>> dp(L + 1, vector<long long>(2, 0));
-
-        dp[1][0] = V;
-        dp[1][1] = 0;
-
+        
+        // 이전 단계의 값만 저장
+        long long prev_vowel = V; // dp[i-1][0]
+        long long prev_consonant = 0; // dp[i-1][1]
+        
         for (int i = 2; i <= L; ++i) {
-            dp[i][0] = (dp[i - 1][0] * V + dp[i - 1][1] * V) % MOD;
-            dp[i][1] = (dp[i - 1][0] * C) % MOD;
+            long long current_vowel = (prev_vowel * V + prev_consonant * V) % MOD;
+            long long current_consonant = (prev_vowel * C) % MOD;
+            
+            // 이전 단계 값 업데이트
+            prev_vowel = current_vowel;
+            prev_consonant = current_consonant;
         }
-
-        long long result = (dp[L][0] + dp[L][1]) % MOD;
+        
+        long long result = (prev_vowel + prev_consonant) % MOD;
         cout << "Case #" << t << ": " << result << endl;
     }
-
     return 0;
 }
