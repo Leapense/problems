c84424b7d6e26e387522a32bbac9b1555f1dd30e
Leapense <iso9661@gmail.com>
2024-12-25 20:17:25 +0900

    `Refactor Irregular Expressions (Small) code to split into syllables and improve spell checking logic`

--- 12521번： Irregular Expressions (Small)/Irregular Expressions (Small).cpp
+++ 12521번： Irregular Expressions (Small)/Irregular Expressions (Small).cpp
@@ -4,69 +4,53 @@
 
 using namespace std;
 
-// 음절인지 확인하는 함수
-bool isSyllable(const string& s) {
-    int vowelCount = 0;
-    for (char ch : s) {
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-            vowelCount++;
+// 음절로 구분하는 함수
+vector<string> splitIntoSyllables(const string& s) {
+    vector<string> syllables;
+    int n = s.length();
+    int start = 0;
+    while (start < n) {
+        int end = start + 1;
+        while (end <= n) {
+            string syllable = s.substr(start, end - start);
+            int vowelCount = 0;
+            for (char ch : syllable) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowelCount++;
+                }
+            }
+            if (vowelCount == 1) {
+                syllables.push_back(syllable);
+                start = end;
+                break;
+            }
+            end++;
         }
     }
-    return vowelCount == 1;
+    return syllables;
 }
 
 // 주문인지 확인하는 함수
 bool isSpell(const string& s) {
-    int n = s.length();
+    vector<string> syllables = splitIntoSyllables(s);
+    int n = syllables.size();
     for (int i = 0; i < n; ++i) {
         for (int j = i + 1; j < n; ++j) {
             for (int k = j + 1; k < n; ++k) {
-                string start = s.substr(i, j - i);
-                string middle = s.substr(j, k - j);
-                string end = s.substr(k);
-                
-                // 시작 단어와 끝 단어가 같고, 각 단어가 최소 두 음절인지 확인
-                if (start == end) {
-                    int startSyllables = 0;
-                    int middleSyllables = 0;
-                    int endSyllables = 0;
-                    
-                    // 시작 단어의 음절 수 계산
-                    for (int l = 0; l < start.length(); ) {
-                        for (int m = l + 1; m <= start.length(); ++m) {
-                            if (isSyllable(start.substr(l, m - l))) {
-                                startSyllables++;
-                                l = m;
-                                break;
-                            }
-                        }
-                    }
-                    
-                    // 중간 단어의 음절 수 계산
-                    for (int l = 0; l < middle.length(); ) {
-                        for (int m = l + 1; m <= middle.length(); ++m) {
-                            if (isSyllable(middle.substr(l, m - l))) {
-                                middleSyllables++;
-                                l = m;
-                                break;
-                            }
-                        }
-                    }
-                    
-                    // 끝 단어의 음절 수 계산
-                    for (int l = 0; l < end.length(); ) {
-                        for (int m = l + 1; m <= end.length(); ++m) {
-                            if (isSyllable(end.substr(l, m - l))) {
-                                endSyllables++;
-                                l = m;
-                                break;
-                            }
-                        }
-                    }
-                    
-                    if (startSyllables >= 2 && middleSyllables >= 1 && endSyllables >= 2) {
-                        return true;
-                    }
+                string startWord;
+                for (int l = i; l < j; ++l) {
+                    startWord += syllables[l];
+                }
+                string middleWord;
+                for (int l = j; l < k; ++l) {
+                    middleWord += syllables[l];
+                }
+                string endWord;
+                for (int l = k; l < n; ++l) {
+                    endWord += syllables[l];
+                }
+                if (startWord == endWord && (j - i) >= 2 && (k - j) >= 1 && (n - k) >= 2) {
+                    return true;
                 }
             }
         }
