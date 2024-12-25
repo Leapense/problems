c9716dd813231e7474b61be71b6db5c8ce7fc904
Leapense <iso9661@gmail.com>
2024-12-25 18:40:42 +0900

    `Added solution for problem 12329, Bullseye (Small), in C++`

--- 12329번： Bullseye (Small)/Bullseye (Small).cpp
+++ 12329번： Bullseye (Small)/Bullseye (Small).cpp
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12329                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12329                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 18:35:15 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; ++t) {
+        long long r, t_paint;
+        cin >> r >> t_paint;
+
+        long long count = 0;
+        long long paint_needed = 2 * r + 1;
+
+        while (t_paint >= paint_needed) {
+            t_paint -= paint_needed;
+            count++;
+
+            r += 2;
+            paint_needed = 2 * r + 1;
+        }
+
+        cout << "Case #" << t << ": " << count << endl;
+    }
+
+    return 0;
+}
