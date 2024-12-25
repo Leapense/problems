466178d8718f463719acdccb2cb01db0e44c812d
Leapense <iso9661@gmail.com>
2024-12-25 17:03:49 +0900

    `Added solution for problem 12235, Data Packing (Large), in C++`

--- 12235번： Data Packing (Large)/Data Packing (Large).cpp
+++ 12235번： Data Packing (Large)/Data Packing (Large).cpp
@@ -1,11 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12235                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12235                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 16:56:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        int N, X;
+        cin >> N >> X;
+        vector<int> S(N);
+
+        for (int i = 0; i < N; ++i) {
+            cin >> S[i];
+        }
+
+        sort(S.begin(), S.end());
+        int left = 0;
+        int right = N - 1;
+        int discs = 0;
+
+        while (left <= right) {
+            if (S[left] + S[right] <= X) {
+                ++left;
+            }
+            --right;
+            ++discs;
+        }
+
+        cout << "Case #" << t << ": " << discs << endl;
+    }
+
+    return 0;
+}
