7eaa3a4fde9c3a5d2d7d1c2c2ed12f5e30c9227b
Leapense <iso9661@gmail.com>
2024-12-25 13:01:52 +0900

    `Added new code for problem 12038, Polynesiaglot (Small2), with a dynamic programming solution in C++`

--- 12038번： Polynesiaglot (Small2)/Polynesiaglot (Small2).cpp
+++ 12038번： Polynesiaglot (Small2)/Polynesiaglot (Small2).cpp
@@ -1,11 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12038                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12038                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 12:52:24 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const long long MOD = 1000000007;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; ++t) {
+        int C, V, L;
+        cin >> C >> V >> L;
+
+        vector<vector<long long>> dp(L + 1, vector<long long>(2, 0));
+
+        dp[1][0] = V;
+        dp[1][1] = 0;
+
+        for (int i = 2; i <= L; ++i) {
+            dp[i][0] = (dp[i - 1][0] * V + dp[i - 1][1] * V) % MOD;
+            dp[i][1] = (dp[i - 1][0] * C) % MOD;
+        }
+
+        long long result = (dp[L][0] + dp[L][1]) % MOD;
+        cout << "Case #" << t << ": " << result << endl;
+    }
+
+    return 0;
+}
