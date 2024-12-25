97711138545206ea2aa9755a3ff3fb012c87856e
Leapense <iso9661@gmail.com>
2024-12-25 13:58:17 +0900

    `Added solution for problem 12076, gCube (Small) in C++`

--- 12076번： gCube (Small)/gCube (Small).cpp
+++ 12076번： gCube (Small)/gCube (Small).cpp
@@ -1,11 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12076                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12076                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 13:49:27 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for (int test = 1; test <= T; test++) {
+        int N, M;
+        cin >> N >> M;
+        vector<double> a(N);
+        for (int i = 0; i < N; i++) {
+            cin >> a[i];
+        }
+
+        cout << "Case #" << test << ":" << endl;
+        for (int q = 0; q < M; q++) {
+            int L, R;
+            cin >> L >> R;
+            int D = R - L + 1;
+            double sum_log = 0.0;
+            for (int i = L; i <= R; i++) {
+                sum_log += log(a[i]);
+            }
+            double s = exp(sum_log / D);
+            cout << fixed << setprecision(9) << s << endl;
+        }
+    }
+
+    return 0;
+}
