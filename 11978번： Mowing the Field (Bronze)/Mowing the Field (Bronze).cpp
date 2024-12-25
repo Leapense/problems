40eeab0c5aeaf485e487915e28799dcb331dba8d
Leapense <iso9661@gmail.com>
2024-12-25 12:33:28 +0900

    `Refactored code for problem 11978: Mowing the Field (Bronze) with improved solution`

--- 11978번： Mowing the Field (Bronze)/Mowing the Field (Bronze).cpp
+++ 11978번： Mowing the Field (Bronze)/Mowing the Field (Bronze).cpp
@@ -1,11 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 11978                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/11978                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 00:29:19 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <map>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    map<pair<int, int>, int> visited;
+
+    int x = 0, y = 0;
+    int time = 0;
+    int max_x = -1;
+
+    visited[{x, y}] = time;
+
+    for (int i = 0; i < N; ++i) {
+        char dir;
+        int steps;
+        cin >> dir >> steps;
+
+        for (int s = 1; s <= steps; ++s) {
+            time++;
+            if (dir == 'N') y++;
+            else if (dir == 'S') y--;
+            else if (dir == 'E') x++;
+            else if (dir == 'W') x--;
+
+            pair<int, int> pos = {x, y};
+
+            if (visited.find(pos) != visited.end()) {
+                int last_time = visited[pos];
+                int diff = time - last_time;
+                if (max_x == -1 || diff < max_x) {
+                    max_x = diff;
+                }
+            }
+
+            visited[pos] = time;
+        }
+    }
+
+    cout << max_x << endl;
+
+    return 0;
+}
