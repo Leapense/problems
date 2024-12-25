287b46af76add09db48ce40a4c8706449d3870a3
Leapense <iso9661@gmail.com>
2024-12-25 12:51:51 +0900

    `Refactored code for problem 12018: Yonsei TOTO`

--- 12018번： Yonsei TOTO/Yonsei TOTO.cpp
+++ 12018번： Yonsei TOTO/Yonsei TOTO.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12018                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12018                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 12:34:29 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> min_mileage;
+
+    for (int i = 0; i < n; ++i) {
+        int P, L;
+        cin >> P >> L;
+        vector<int> mileages(P);
+        for (int j = 0; j < P; ++j) {
+            cin >> mileages[j];
+        }
+
+        sort(mileages.begin(), mileages.end(), greater<int>());
+        int required = (P >= L) ? mileages[L - 1] : 1;
+        min_mileage.push_back(required);
+    }
+
+    sort(min_mileage.begin(), min_mileage.end());
+
+    int count = 0;
+    for (int mileage : min_mileage) {
+        if (m >= mileage) {
+            m -= mileage;
+            count++;
+        } else {
+            break;
+        }
+    }
+
+    cout << count << "\n";
+
+    return 0;
+}
