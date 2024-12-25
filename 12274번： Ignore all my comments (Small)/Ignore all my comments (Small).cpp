9b07c774f4c243db4de3fa0c12c769482cdbda53
Leapense <iso9661@gmail.com>
2024-12-25 17:51:10 +0900

    `Refactor: Replace comment block with C++ code to ignore all comments`

--- 12274번： Ignore all my comments (Small)/Ignore all my comments (Small).cpp
+++ 12274번： Ignore all my comments (Small)/Ignore all my comments (Small).cpp
@@ -1,11 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12274                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12274                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 17:42:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string text;
+    string line;
+
+    while (getline(cin, line)) {
+        text += line + "\n";
+    }
+
+    string result;
+    int nesting_level = 0;
+    size_t index = 0;
+    size_t length = text.length();
+
+    while (index < length) {
+        if (text[index] == '/' && index + 1 < length && text[index + 1] == '*') {
+            nesting_level++;
+            index += 2;
+        } else if (text[index] == '*' && index + 1 < length && text[index + 1] == '/' && nesting_level > 0) {
+            nesting_level--;
+            index += 2;
+        } else if (nesting_level == 0) {
+            result += text[index];
+            index++;
+        } else {
+            index++;
+        }
+    }
+    cout << "Case #1:\n" << result;
+
+    return 0;
+}
