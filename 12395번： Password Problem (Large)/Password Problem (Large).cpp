9b09edc502c03df011deac280c4d71890f1d9235
Leapense <iso9661@gmail.com>
2024-12-25 19:02:05 +0900

    `Refactored Password Problem (Large) solution to include expected keystrokes calculation and main function implementation.`

--- 12395번： Password Problem (Large)/Password Problem (Large).cpp
+++ 12395번： Password Problem (Large)/Password Problem (Large).cpp
@@ -1,11 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12395                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12395                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 18:53:25 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double calculateExpectedKeystrokes(int A, int B, const vector<double>& p) {
+    double minExpected = B + 2;
+
+    double probAllCorrect = 1.0;
+    for (int i = 0; i < A; ++i) {
+        probAllCorrect *= p[i];
+    }
+
+    double expected1 = probAllCorrect * (B - A + 1) + (1 - probAllCorrect) * (B - A + 1 + B + 1);
+    minExpected = min(minExpected, expected1);
+
+    for (int backspaces = 1; backspaces <= A; ++backspaces) {
+        double probCorrect = 1.0;
+        for (int i = 0; i < A - backspaces; ++i) {
+            probCorrect *= p[i];
+        }
+
+        double expected2 = probCorrect * (backspaces + B - (A - backspaces) + 1) + (1 - probCorrect) * (backspaces + B - (A - backspaces) + 1 + B + 1);
+        minExpected = min(minExpected, expected2);
+    }
+
+    return minExpected;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        int A, B;
+        cin >> A >> B;
+        vector<double> p(A);
+
+        for (int i = 0; i < A; ++i) {
+            cin >> p[i];
+        }
+
+        double result = calculateExpectedKeystrokes(A, B, p);
+        cout << "Case #" << t << ": " << fixed << setprecision(6) << result << endl;
+    }
+
+    return 0;
+}
