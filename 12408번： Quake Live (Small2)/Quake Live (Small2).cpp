ebf8668fc17dcc69750d1786e2cdc228a9f3235d
Leapense <iso9661@gmail.com>
2024-12-25 19:22:26 +0900

    `Refactored code for problem 12408: Quake Live (Small2) with new implementation and main function`

--- 12408번： Quake Live (Small2)/Quake Live (Small2).cpp
+++ 12408번： Quake Live (Small2)/Quake Live (Small2).cpp
@@ -1,11 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12408                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12408                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 19:19:40 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int calculateMinDifference(const vector<int>& skills, int n) {
+    int minDiff = INT_MAX;
+    vector<bool> v(n, false);
+    fill(v.begin() + n/2, v.end(), true);
+
+    do {
+        int sumA = 0, sumB = 0;
+        for (int i = 0; i < n; ++i) {
+            if (v[i]) {
+                sumA += skills[i];
+            } else {
+                sumB += skills[i];
+            }
+        }
+        int diff = abs(sumA - sumB);
+        if (diff < minDiff) {
+            minDiff = diff;
+        }
+    } while (next_permutation(v.begin(), v.end()));
+
+    return minDiff;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        int N;
+        cin >> N;
+        vector<int> skills(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> skills[i];
+        }
+
+        int minDiff;
+        if (N == 2) {
+            minDiff = abs(skills[0] - skills[1]);
+        } else {
+            minDiff = calculateMinDifference(skills, N);
+        }
+
+        cout << "Case #" << t << ": " << minDiff << endl;
+    }
+    return 0;
+}
