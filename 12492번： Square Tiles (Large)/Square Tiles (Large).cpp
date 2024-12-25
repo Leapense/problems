10fcfa6086e58b067fc793499cfef97d99eebdf1
Leapense <iso9661@gmail.com>
2024-12-25 19:55:04 +0900

    `Refactored code for 12492 Square Tiles (Large) problem, including new includes, namespace usage, and main function implementation.`

--- 12492번： Square Tiles (Large)/Square Tiles (Large).cpp
+++ 12492번： Square Tiles (Large)/Square Tiles (Large).cpp
@@ -1,11 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12492                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12492                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 19:46:43 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void solve(int case_num) {
+    int R, C;
+    cin >> R >> C;
+    vector<string> grid(R);
+
+    for (int i = 0; i < R; ++i) {
+        cin >> grid[i];
+    }
+
+    vector<string> result = grid;
+
+    for (int i = 0; i < R - 1; ++i) {
+        for (int j = 0; j < C - 1; ++j) {
+            if (result[i][j] == '#' && result[i][j+1] == '#' &&
+                result[i+1][j] == '#' && result[i+1][j+1] == '#') {
+                result[i][j] = '/';
+                result[i][j+1] = '\\';
+                result[i+1][j] = '\\';
+                result[i+1][j+1] = '/';
+            }
+        }
+    }
+
+    for (int i = 0; i < R; ++i) {
+        for (int j = 0; j < C; ++j) {
+            if (result[i][j] == '#') {
+                cout << "Case #" << case_num << ":\nImpossible\n";
+                return;
+            }
+        }
+    }
+
+    cout << "Case #" << case_num << ":\n";
+    for (const string& row : result) {
+        cout << row << "\n";
+    }
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        solve(t);
+    }
+
+    return 0;
+}
