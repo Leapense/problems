edd1692485072ae23987b665474ac4cca8554057
Leapense <iso9661@gmail.com>
2024-12-25 19:29:26 +0900

    `Added new code for problem 12457, replacing existing file with new implementation.`

--- 12457번： ビット数 (Small)/ビット数 (Small).cpp
+++ 12457번： ビット数 (Small)/ビット数 (Small).cpp
@@ -1,11 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12457                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12457                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 19:22:51 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int countOnes(int x) {
+    return bitset<32>(x).count();
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; ++t) {
+        int N;
+        cin >> N;
+
+        int maxSum = 0;
+
+        for (int a = 0; a <= N; ++a) {
+            int b = N - a;
+            int sum = countOnes(a) + countOnes(b);
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+
+        cout << "Case #" << t << ": " << maxSum << endl;
+    }
+
+    return 0;
+}
