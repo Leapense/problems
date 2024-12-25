2d4d649eaea70b89b00bbbad1d41fe2dd2e2a701
Leapense <iso9661@gmail.com>
2024-12-25 14:14:37 +0900

    "Update editor settings: change font family to 'JetBrainsMono NF'."

--- 12121번： Pretty Good Proportion (Small)/Pretty Good Proportion (Small).cpp
+++ 12121번： Pretty Good Proportion (Small)/Pretty Good Proportion (Small).cpp
@@ -1,11 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12121                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12121                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/25 14:01:42 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; t++) {
+        int N;
+        double F;
+        cin >> N >> F;
+        string s;
+        cin >> s;
+
+        vector<int> binary_sequence(N);
+        for (int i = 0; i < N; i++) {
+            binary_sequence[i] = s[i] - '0';
+        }
+
+        vector<int> prefix(N + 1, 0);
+        for (int i = 1; i <= N; i++)
+    }
+}
