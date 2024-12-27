9321c9450f8dea0060a6e939126cb50f70d1978d
Leapense <iso9661@gmail.com>
2024-12-28 00:36:44 +0900

    `Added solution for problem 12599, Qualification Round (Small), in C++`

--- 12599번： Qualification Round (Small)/Qualification Round (Small).cpp
+++ 12599번： Qualification Round (Small)/Qualification Round (Small).cpp
@@ -1,11 +1,31 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12599                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12599                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/27 23:58:31 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        int P, C;
+        cin >> P >> C;
+        vector<int> S(P);
+        for (int i = 0; i < P; ++i) {
+            cin >> S[i];
+        }
+        sort(S.begin(), S.end());
+        int sum_S = 0;
+        for (int s : S) {
+            sum_S += s;
+        }
+        int k = P - C + 1;
+        int sum_min = 0;
+        for (int i = 0; i < k; ++i) {
+            sum_min += S[i];
+        }
+        int X = min(sum_S / C, sum_min);
+        cout << "Case #" << t << ": " << X << endl;
+    }
+    return 0;
+}
