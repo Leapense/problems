1356fc86bcc05b53a3615af692956a28ffc8aa39
Leapense <iso9661@gmail.com>
2024-12-28 00:54:29 +0900

    `Refactored code for 12599 Qualification Round (Small) problem`

--- 12599번： Qualification Round (Small)/Qualification Round (Small).cpp
+++ 12599번： Qualification Round (Small)/Qualification Round (Small).cpp
@@ -1,31 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int P, C;
+int S[10];
+vector<vector<int>> subsets;
+int bestAns;
+
+void backtrack(int idx, int current, int sLeft[]) {
+    if (idx == (int)subsets.size()) {
+        bestAns = max(bestAns, current);
+        return;
+    }
+    int sumLeft = 0;
+    for (int i = 0; i < P; i++) sumLeft += sLeft[i];
+    if (current + sumLeft / C <= bestAns) return;
+    int mn = INT_MAX;
+    for (int i : subsets[idx]) mn = min(mn, sLeft[i]);
+    for (int x = mn; x >= 0; x--) {
+        for (int i : subsets[idx]) sLeft[i] -= x;
+        backtrack(idx + 1, current + x, sLeft);
+        for (int i : subsets[idx]) sLeft[i] += x;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int T;
     cin >> T;
-    for (int t = 1; t <= T; ++t) {
-        int P, C;
+    for(int tc=1; tc<=T; tc++){
         cin >> P >> C;
-        vector<int> S(P);
-        for (int i = 0; i < P; ++i) {
-            cin >> S[i];
+        for(int i=0; i<P; i++) cin >> S[i];
+        subsets.clear();
+        for(int mask=0; mask<(1<<P); mask++){
+            int c = __builtin_popcount(mask);
+            if(c == C) {
+                vector<int> sub;
+                for(int i=0; i<P; i++){
+                    if(mask & (1<<i)) sub.push_back(i);
+                }
+                subsets.push_back(sub);
+            }
         }
-        sort(S.begin(), S.end());
-        int sum_S = 0;
-        for (int s : S) {
-            sum_S += s;
-        }
-        int k = P - C + 1;
-        int sum_min = 0;
-        for (int i = 0; i < k; ++i) {
-            sum_min += S[i];
-        }
-        int X = min(sum_S / C, sum_min);
-        cout << "Case #" << t << ": " << X << endl;
+        bestAns = 0;
+        backtrack(0, 0, S);
+        cout << "Case #" << tc << ": " << bestAns << "\n";
     }
     return 0;
 }
