66ded9154c6e099b8aa51f36bed54f924bcc38a4
Leapense <iso9661@gmail.com>
2024-12-28 21:13:17 +0900

    `Added C++ code for problem 12789, utilizing queue and stack data structures to solve the existing file.`

--- 12789번： 도키도키 간식드리미/도키도키 간식드리미.cpp
+++ 12789번： 도키도키 간식드리미/도키도키 간식드리미.cpp
@@ -1,11 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12789                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12789                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/28 20:31:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    queue<int> q;
+    for (int i = 0; i < N; ++i) {
+        int num;
+        cin >> num;
+        q.push(num);
+    }
+    
+    stack<int> s;
+    int expected = 1;
+    
+    while (!q.empty() || !s.empty()) {
+        if (!q.empty() && q.front() == expected) {
+            q.pop();
+            expected++;
+        } else if (!s.empty() && s.top() == expected) {
+            s.pop();
+            expected++;
+        } else if (!q.empty()) {
+            s.push(q.front());
+            q.pop();
+        } else {
+            break;
+        }
+    }
+    
+    if (expected == N + 1) {
+        cout << "Nice" << endl;
+    } else {
+        cout << "Sad" << endl;
+    }
+    
+    return 0;
+}
