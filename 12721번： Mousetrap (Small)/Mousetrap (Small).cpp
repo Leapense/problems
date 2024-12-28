b933c94d223fc0dff04daa2f39b98f033fa07b1d
Leapense <iso9661@gmail.com>
2024-12-28 19:30:52 +0900

    `Refactored code for 12721 problem, added buildPerfectDeck function and main loop for input handling.`

--- 12721번： Mousetrap (Small)/Mousetrap (Small).cpp
+++ 12721번： Mousetrap (Small)/Mousetrap (Small).cpp
@@ -1,11 +1,56 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12721                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12721                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/28 19:23:25 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> buildPerfectDeck(int K) {
+    vector<int> deck(K, 0);
+    queue<int> q;
+
+    for (int i = 1; i <= K; ++i) {
+        q.push(i);
+    }
+
+    int count = 1;
+    while (!q.empty()) {
+        for (int i = 1; i < count; ++i) {
+            q.push(q.front());
+            q.pop();
+        }
+
+        deck[q.front() - 1] = count;
+        q.pop();
+        count++;
+    }
+
+    return deck;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        int K;
+        cin >> K;
+
+        int n;
+        cin >> n;
+
+        vector<int> indices(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> indices[i];
+        }
+
+        vector<int> deck = buildPerfectDeck(K);
+
+        cout << "Case #" << t << ":";
+        for (int i = 0; i < n; ++i) {
+            cout << " " << deck[indices[i] - 1];
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
