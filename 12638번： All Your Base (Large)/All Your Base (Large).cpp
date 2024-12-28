30df468011f5e2f144f02fdb10efddba419612f8
Leapense <iso9661@gmail.com>
2024-12-28 18:41:01 +0900

    `Refactored code for problem 12638: All Your Base (Large) with improved readability and efficiency.`

--- 12638번： All Your Base (Large)/All Your Base (Large).cpp
+++ 12638번： All Your Base (Large)/All Your Base (Large).cpp
@@ -1,11 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12638                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12638                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/28 18:27:20 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    for (int test = 1; test <= T; test++) {
+        string S;
+        cin >> S;
+
+        vector<char> unique_symbols;
+        bool seen[256];
+
+        memset(seen, false, sizeof(seen));
+
+        for (char c : S) {
+            if (!seen[c]) {
+                unique_symbols.push_back(c);
+                seen[c] = true;
+            }
+        }
+
+        int mapping[256];
+        memset(mapping, -1, sizeof(mapping));
+
+        if (unique_symbols.size() >= 1) {
+            mapping[unique_symbols[0]] = 1;
+        }
+
+        if (unique_symbols.size() >= 2) {
+            mapping[unique_symbols[1]] = 0;
+        }
+
+        int next_digit = 2;
+
+        for(int i = 2; i < unique_symbols.size(); i++) {
+            mapping[unique_symbols[i]] = next_digit;
+            next_digit++;
+        }
+
+        int base = max((int)unique_symbols.size(), 2);
+
+        ull value = 0;
+        for (char c : S) {
+            value = value * base + mapping[c];
+        }
+
+        cout << "Case #" << test << ": " << value << "\n";
+    }
+
+    return 0;
+}
