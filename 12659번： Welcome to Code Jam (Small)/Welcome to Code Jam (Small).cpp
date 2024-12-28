83b7801fcbf0c4196f0a006c0b1071c88925217e
Leapense <iso9661@gmail.com>
2024-12-28 19:09:25 +0900

    `Refactored Code Jam (Small) problem solution to use dynamic programming and improved input handling.`

--- 12659번： Welcome to Code Jam (Small)/Welcome to Code Jam (Small).cpp
+++ 12659번： Welcome to Code Jam (Small)/Welcome to Code Jam (Small).cpp
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12659                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12659                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/28 18:59:49 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+const string target = "welcome to code jam";
+const int MOD = 10000;
+
+int countSubsequences(const string& input) {
+    int m = target.length();
+    int n = input.length();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    for (int j = 0; j <= n; ++j) {
+        dp[0][j] = 1;
+    }
+
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (target[i - 1] == input[j - 1]) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i][j - 1]) % MOD;
+            } else {
+                dp[i][j] = dp[i][j - 1] % MOD;
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cin.ignore();
+
+    for (int i = 1; i <= N; ++i) {
+        string input;
+        getline(cin, input);
+        int result = countSubsequences(input);
+
+        cout << "Case #" << i << ": " << setw(4) << setfill('0') << result << endl;
+    }
+
+    return 0;
+}
