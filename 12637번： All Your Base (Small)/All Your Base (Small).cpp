4d19cd18ba0d7d75d31400a6ce6e1d6049e85690
Leapense <iso9661@gmail.com>
2024-12-28 18:25:59 +0900

    `Refactored code for problem 12637 "All Your Base (Small)"`

--- 12637번： All Your Base (Small)/All Your Base (Small).cpp
+++ 12637번： All Your Base (Small)/All Your Base (Small).cpp
@@ -1,11 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12637                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12637                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/28 18:10:37 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    for (int test = 1; test <= T; test++) {
+        string S;
+        cin >> S;
+
+        int mapping[256];
+        for (int i = 0; i < 256; i++) mapping[i] = -1;
+
+        vector<char> unique_symbols;
+
+        for (char c : S) {
+            if (mapping[c] == -1) {
+                unique_symbols.push_back(c);
+                mapping[c] = -2;
+            }
+        }
+
+        mapping[unique_symbols[0]] = 1;
+
+        int next_digit = 0;
+
+        if (unique_symbols.size() > 1) {
+            mapping[unique_symbols[1]] = 0;
+            next_digit = 2;
+        } else {
+            next_digit = 2;
+        }
+
+        for (int i = 2; i < unique_symbols.size(); i++) {
+            while (next_digit == 1 || next_digit == 0) {
+                next_digit++;
+            }
+            mapping[unique_symbols[i]] = next_digit;
+            next_digit++;
+        }
+
+        int base = max(2, (int)unique_symbols.size());
+
+        ll V = 0;
+        for (char c : S) {
+            V = V * base + mapping[c];
+        }
+
+        cout << "Case #" << test << ": " << V << "\n";
+    }
+
+    return 0;
+}
