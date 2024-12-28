4a4d6f420bdf14661639183eee11be329f66ad67
Leapense <iso9661@gmail.com>
2024-12-28 19:22:51 +0900

    `Added solution for problem 12715, Increasing Speed Limits (Small), in C++`

--- 12715번： Increasing Speed Limits (Small)/Increasing Speed Limits (Small).cpp
+++ 12715번： Increasing Speed Limits (Small)/Increasing Speed Limits (Small).cpp
@@ -1,11 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12715                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12715                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/28 19:13:24 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int num_cases;
+    cin >> num_cases;
+
+    for (int case_num = 1; case_num <= num_cases; ++case_num) {
+        int n, m;
+        long long X, Y, Z;
+        cin >> n >> m >> X >> Y >> Z;
+
+        vector<long long> A(m);
+        for (int i = 0; i < m; ++i) {
+            cin >> A[i];
+        }
+        vector<long long> S(n);
+        vector<long long> current_A = A;
+        for (int i = 0; i < n; ++i) {
+            S[i] = current_A[i % m];
+            current_A[i % m] = (X * current_A[i % m] + Y * (i + 1)) % Z;
+        }
+
+        long long MOD = 1000000007;
+        vector<long long> dp(n, 0);
+        long long total_increasing_subsequences = 0;
+
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (S[j] < S[i]) {
+                    dp[i] = (dp[i] + dp[j]) % MOD;
+                }
+            }
+
+            total_increasing_subsequences = (total_increasing_subsequences + dp[i]) % MOD;
+        }
+
+        cout << "Case #" << case_num << ": " << total_increasing_subsequences << endl;
+    }
+
+    return 0;
+}
