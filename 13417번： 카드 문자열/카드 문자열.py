574ce1116d069c9f9a358907fecf07ec7fa9c0e0
Leapense <iso9661@gmail.com>
2024-12-29 21:02:16 +0900

    `Refactor: Update code to solve problem 13417, add main function and import sys`

--- 13417번： 카드 문자열/카드 문자열.py
+++ 13417번： 카드 문자열/카드 문자열.py
@@ -1,11 +1,31 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 13417                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/13417                          #+#        #+#      #+#     #
-#    Solved: 2024/12/29 20:52:18 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    T = int(sys.stdin.readline())
+    for _ in range(T):
+        line = ''
+        while line.strip() == '':
+            line = sys.stdin.readline()
+        N = int(line.strip())
+        cards = []
+        while len(cards) < N:
+            line = sys.stdin.readline()
+            if not line:
+                break
+            parts = line.strip().split()
+            cards.extend(parts)
+        cards = cards[:N]
+        S = []
+        for c in cards:
+            if not S:
+                S.append(c)
+            else:
+                if (c, *S) < (*S, c):
+                    S.insert(0, c)
+                else:
+                    S.append(c)
+
+        print(''.join(S))
+
+if __name__ == '__main__':
+    main()
