fafab29ec1eb967b0ec66e85e6fc0c2e726106b8
Leapense <iso9661@gmail.com>
2024-12-29 19:04:02 +0900

    `Refactored Daydreaming Stockbroker problem solution`

--- 13357번： Daydreaming Stockbroker/Daydreaming Stockbroker.py
+++ 13357번： Daydreaming Stockbroker/Daydreaming Stockbroker.py
@@ -1,11 +1,19 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 13357                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/13357                          #+#        #+#      #+#     #
-#    Solved: 2024/12/29 18:59:08 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+d = int(input())
+p = [int(input()) for _ in range(d)]
+
+money = 100
+shares = 0
+
+for day in range(d):
+    if day < d - 1:
+        if p[day + 1] > p[day]:
+            buy = min(money // p[day], 100000 - shares)
+            money -= buy * p[day]
+            shares += buy
+        elif p[day + 1] < p[day]:
+            money += shares * p[day]
+            shares = 0
+    else:
+        money += shares * p[day]
+        shares = 0
+print(money)
