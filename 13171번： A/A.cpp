722b9e39248a0b7095c9c854542a04e064daef1a
Leapense <iso9661@gmail.com>
2024-12-29 16:36:14 +0900

    `Added powMod function and main program for calculating A^X mod MOD`

--- 13171번： A/A.cpp
+++ 13171번： A/A.cpp
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 13171                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/13171                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/29 16:24:45 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+const long long MOD = 1000000007;
+
+long long powMod(long long A, long long X) {
+    long long result = 1;
+    A = A % MOD;
+
+    while (X > 0) {
+        if (X % 2 == 1) {
+            result = (result * A) % MOD;
+        }
+        A = (A * A) % MOD;
+        X = X / 2;
+    }
+    return result;
+}
+
+int main() {
+    long long A, X;
+    cin >> A >> X;
+
+    cout << powMod(A, X) << "\n";
+
+    return 0;
+}
