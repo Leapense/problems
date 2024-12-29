386ce2607d833cba97bf4b4e14c44de111a471bd
Leapense <iso9661@gmail.com>
2024-12-29 17:36:42 +0900

    `Refactored Harps and Tails problem solution to use C++ standard library and improved code structure.`

--- 13242번： Harps and Tails/Harps and Tails.cpp
+++ 13242번： Harps and Tails/Harps and Tails.cpp
@@ -1,11 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 13242                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/13242                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/29 17:15:32 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> grid(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> grid[i];
+    }
+
+    map<string, int> patternCount;
+
+    for (const string& row : grid) {
+        string pattern = row;
+
+        if (pattern[0] == 'T') {
+            for (char& c : pattern) {
+                c = (c == 'H') ? 'T' : 'H';
+            }
+        }
+
+        patternCount[pattern]++;
+    }
+
+    int maxRows = 0;
+    for (const auto& pair : patternCount) {
+        maxRows = max(maxRows, pair.second);
+    }
+
+    cout << maxRows << "\n";
+
+    return 0;
+}
