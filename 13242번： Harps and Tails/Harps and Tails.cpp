392a73cb82d99eed6cb707542f0c7ccd934e3a0a
Leapense <iso9661@gmail.com>
2024-12-29 17:54:29 +0900

    `Refactored code for Harps and Tails problem: replaced includes, added ios sync, and simplified frequency counting.`

--- 13242번： Harps and Tails/Harps and Tails.cpp
+++ 13242번： Harps and Tails/Harps and Tails.cpp
@@ -1,39 +1,30 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     int n, m;
     cin >> n >> m;
 
-    vector<string> grid(n);
+    unordered_map<string, int> freq;
+    string s;
+
     for (int i = 0; i < n; ++i) {
-        cin >> grid[i];
+        cin >> s;
+        freq[s]++;
     }
 
-    map<string, int> patternCount;
-
-    for (const string& row : grid) {
-        string pattern = row;
-
-        if (pattern[0] == 'T') {
-            for (char& c : pattern) {
-                c = (c == 'H') ? 'T' : 'H';
-            }
+    int max_freq = 0;
+    for (auto &p : freq) {
+        if (p.second > max_freq) {
+            max_freq = p.second;
         }
-
-        patternCount[pattern]++;
     }
 
-    int maxRows = 0;
-    for (const auto& pair : patternCount) {
-        maxRows = max(maxRows, pair.second);
-    }
-
-    cout << maxRows << "\n";
+    cout << max_freq << endl;
 
     return 0;
 }
