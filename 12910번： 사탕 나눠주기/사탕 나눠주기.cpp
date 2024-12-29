ff6718fd71b47a44b2bac54e36a1711445b67015
Leapense <iso9661@gmail.com>
2024-12-29 13:17:26 +0900

    `Added C++ code for problem 12910, including input, processing, and output.`

--- 12910번： 사탕 나눠주기/사탕 나눠주기.cpp
+++ 12910번： 사탕 나눠주기/사탕 나눠주기.cpp
@@ -1,11 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 12910                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/12910                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/29 13:08:41 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> brands(N);
+
+    for (int i = 0; i < N; ++i) {
+        cin >> brands[i];
+    }
+
+    vector<int> count(51, 0);
+    for (int brand : brands) {
+        count[brand]++;
+    }
+
+    int maxK = *max_element(brands.begin(), brands.end());
+
+    int total = 0;
+    for (int K = 1; K <= maxK; ++K) {
+        int ways = 1;
+        for (int i = 1; i <= K; ++i) {
+            if (count[i] == 0) {
+                ways = 0;
+                break;
+            }
+            ways *= count[i];
+        }
+        total += ways;
+    }
+
+    cout << total << "\n";
+
+    return 0;
+}
