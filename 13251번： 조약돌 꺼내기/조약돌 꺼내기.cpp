4a08679aac2e68ff3adcd7de411ecd1b0c953db5
Leapense <iso9661@gmail.com>
2024-12-29 18:20:07 +0900

    `Fixed comb function to return double instead of ull and updated type of comb function return value to match.`

--- 13251번： 조약돌 꺼내기/조약돌 꺼내기.cpp
+++ 13251번： 조약돌 꺼내기/조약돌 꺼내기.cpp
@@ -1,53 +1,40 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-typedef unsigned long long ull;
-
-ull comb(int n, int k) {
-    if (k > n) return 0;
-    if (k == 0 || k == n) return 1;
-    ull res = 1;
-    for (int i = 1; i <= k; i++) {
-        res = res * (n - i + 1) / i;
+double comb(int n, int k){
+    if(k > n) return 0.0;
+    if(k ==0 || k ==n) return 1.0;
+    double res =1.0;
+    for(int i=1;i<=k;i++){
+        res = res * (n - i +1) / i;
     }
-
     return res;
 }
 
-int main() {
+int main(){
     int M;
-    cin >> M;
-
+    cin>>M;
     vector<int> c(M);
-
-    for (auto &x : c) cin >> x;
-
+    for(auto &x:c) cin>>x;
     int K;
-    cin >> K;
-
-    long long N = 0;
-    for (auto x : c) N += x;
-
-    if (K > N) {
+    cin>>K;
+    long long N=0;
+    for(auto x:c) N +=x;
+    if(K > N){
         printf("0.0\n");
         return 0;
     }
-    ull total = comb(N, K);
-    if (total == 0) {
+    double total = comb(N, K);
+    if(total ==0.0){
         printf("0.0\n");
         return 0;
     }
-
-    double sum = 0.0;
-    for (auto x : c) {
-        if (x >= K) {
-            sum += (double)comb(x, K);
+    double sum =0.0;
+    for(auto x:c){
+        if(x >= K){
+            sum += comb(x, K);
         }
     }
-
-    double prob = sum / (double)total;
-
+    double prob = sum / total;
     printf("%.15lf\n", prob);
-
-    return 0;
 }
