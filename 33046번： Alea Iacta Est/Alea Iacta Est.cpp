03d149bd832689ca7ca1076ad1551c7c0d19b320
Leapense <iso9661@gmail.com>
2024-12-29 12:11:30 +0900

    `Refactored Alea Iacta Est problem solution to use C++`

--- 33046번： Alea Iacta Est/Alea Iacta Est.cpp
+++ 33046번： Alea Iacta Est/Alea Iacta Est.cpp
@@ -1,11 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 33046                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/33046                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/29 12:00:16 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A, B, C, D;
+
+int getPlayer(int diceSum, int currentPlayer) {
+    int player = (currentPlayer + diceSum - 1) % 4;
+    if (player == 0) player = 4;
+
+    return player;
+}
+
+int main() {
+    cin >> A >> B;
+    cin >> C >> D;
+
+    int firstSum = A + B;
+    int gaDong = getPlayer(firstSum, 1);
+    int secondSum = C + D;
+    int jinDong = getPlayer(secondSum, gaDong);
+
+    cout << jinDong << endl;
+
+    return 0;
+}
