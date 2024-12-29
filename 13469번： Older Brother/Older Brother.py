e919144bdf18511f942423d73437400745db679e
Leapense <iso9661@gmail.com>
2024-12-29 21:28:17 +0900

    `Added prime power check for problem 13469`

--- 13469번： Older Brother/Older Brother.py
+++ 13469번： Older Brother/Older Brother.py
@@ -1,11 +1,34 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 13469                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/13469                          #+#        #+#      #+#     #
-#    Solved: 2024/12/29 21:16:35 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import math
+
+def is_prime_power(q):
+    if q == 1:
+        return False
+    def smallest_prime_factor(n):
+        if n % 2 == 0:
+            return 2
+        sqrt_n = int(math.isqrt(n)) + 1
+        for i in range(3, sqrt_n, 2):
+            if n % i == 0:
+                return i
+        return n
+    
+    p = smallest_prime_factor(q)
+    while q % p == 0:
+        q = q // p
+
+    return q == 1
+
+def main():
+    import sys
+    input = sys.stdin.read
+    q_str = input().strip()
+    if not q_str:
+        print("no")
+        return
+    q = int(q_str)
+    if is_prime_power(q):
+        print("yes")
+    else:
+        print("no")
+if __name__ == '__main__':
+    main()
