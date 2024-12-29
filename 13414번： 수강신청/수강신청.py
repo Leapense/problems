a41c9d916ac8e475a55d4458d07bcd4946d56d25
Leapense <iso9661@gmail.com>
2024-12-29 20:47:25 +0900

    `Refactored code for problem 13414 with improved input handling and queue management.`

--- 13414번： 수강신청/수강신청.py
+++ 13414번： 수강신청/수강신청.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 13414                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/13414                          #+#        #+#      #+#     #
-#    Solved: 2024/12/29 19:52:12 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    K_L = sys.stdin.readline().split()
+    K = int(K_L[0])
+    L = int(K_L[1])
+
+    clicks = [sys.stdin.readline().strip() for _ in range(L)]
+    seen = set()
+    queue = []
+
+    for click in reversed(clicks):
+        if click not in seen:
+            queue.append(click)
+            seen.add(click)
+
+    queue.reverse()
+
+    registered = queue[:K]
+    for student in registered:
+        print(student)
+
+if __name__ == '__main__':
+    main()
