81009690631001c3d00e4d48a39bea7bed495816
Leapense <iso9661@gmail.com>
2024-12-30 21:16:31 +0900

    `Added input handling and logic for problem 13662`

--- 13662번： Elevador/Elevador.py
+++ 13662번： Elevador/Elevador.py
@@ -1,11 +1,15 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 13662                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/13662                          #+#        #+#      #+#     #
-#    Solved: 2024/12/30 20:54:08 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+for line in sys.stdin:
+    L, C, R1, R2 = map(int, line.strip().split())
+    if L == 0 and C == 0 and R1 == 0 and R2 == 0:
+        break
+    if 2 * R1 > L or 2 * R1 > C or 2 * R2 > L or 2 * R2 > C:
+        print("N")
+    else:
+        distance_sq = (L - R1 - R2) ** 2 + (C - R1 - R2) ** 2
+        if distance_sq >= (R1 + R2) ** 2:
+            print("S")
+        else:
+            print("N")
+            
