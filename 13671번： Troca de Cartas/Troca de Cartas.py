6eee11cb925385596ed02bc8bc6ada3634566c8f
Leapense <iso9661@gmail.com>
2024-12-31 12:35:32 +0900

    `Refactored code for problem 13671: added main function and improved input handling`

--- 13671번： Troca de Cartas/Troca de Cartas.py
+++ 13671번： Troca de Cartas/Troca de Cartas.py
@@ -1,11 +1,35 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 13671                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/13671                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 00:58:20 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    idx = 0
+    while idx < len(data):
+        A = int(data[idx])
+        B = int(data[idx + 1])
+        idx += 2
+        if A == 0 and B == 0:
+            break
+        A_cards = []
+        for _ in range(A):
+            if idx >= len(data):
+                break
+            A_cards.append(int(data[idx]))
+            idx += 1
+        B_cards = []
+        for _ in range(B):
+            if idx >= len(data):
+                break
+            B_cards.append(int(data[idx]))
+            idx += 1
+        
+        set_A = set(A_cards)
+        set_B = set(B_cards)
+
+        A_diff = set_A - set_B
+        B_diff = set_B - set_A
+
+        N = min(len(A_diff), len(B_diff))
+        print(N)
+
+if __name__ == '__main__':
+    main()
