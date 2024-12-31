ae85a6cca031c8bcbadae187507e853949cf8375
Leapense <iso9661@gmail.com>
2024-12-31 16:43:38 +0900

    `Added import statement and main function for problem 14006`

--- 14006번： Large Ping Pong Tournament/Large Ping Pong Tournament.py
+++ 14006번： Large Ping Pong Tournament/Large Ping Pong Tournament.py
@@ -1,11 +1,14 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14006                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14006                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 16:11:12 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.read
+    data = input().split()
+    N = int(data[0])
+    total_players = 1 << N
+    D = int(data[1])
+    other_scores = list(map(int, data[2:]))
+    count = sum(1 for s in other_scores if s <= D)
+    print("YES" if count >= N else "NO")
+
+if __name__ == '__main__':
+    main()
