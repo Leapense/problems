c72fbf4ed0a4fa5923f904b0683ae01dfed15c36
Leapense <iso9661@gmail.com>
2024-12-31 17:13:26 +0900

    `Refactored code for problem 14162 with improved solution`

--- 14162번： Suma/Suma.py
+++ 14162번： Suma/Suma.py
@@ -1,11 +1,12 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14162                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14162                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 17:01:52 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    L, R = map(int, sys.stdin.read().split())
+    total = 0
+    lm = L - 1
+    for d in range(1, R + 1):
+        q = R // d - lm // d
+        total += d * q
+    print(total)
+
+if __name__ == '__main__':
+    main()
