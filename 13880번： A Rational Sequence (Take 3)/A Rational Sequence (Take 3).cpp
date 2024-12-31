a5049a149a68597b14aac0105551e32bada359a9
Leapense <iso9661@gmail.com>
2024-12-31 13:38:10 +0900

    `Added code for problem 13880: A Rational Sequence (Take 3)`

--- 13880번： A Rational Sequence (Take 3)/A Rational Sequence (Take 3).cpp
+++ 13880번： A Rational Sequence (Take 3)/A Rational Sequence (Take 3).cpp
@@ -1,11 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 13880                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/13880                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/31 13:25:05 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int P;
+    cin >> P;
+
+    while (P--) {
+        long long K, N;
+        cin >> K >> N;
+
+        long long p = 1, q = 1;
+        if (N > 1) {
+            int highest = 31 - __builtin_clz(N);
+            for (int i = highest - 1; i >= 0; i--) {
+                if ((N >> i) & 1) {
+                    p += q;
+                } else {
+                    q += p;
+                }
+            }
+        }
+        cout << K << " " << p << "/" << q << "\n";
+    }
+
+    return 0;
+}
