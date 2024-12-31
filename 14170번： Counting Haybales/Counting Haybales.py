b663c728f4d31ee0f53e04149754c7a9b235d8cf
Leapense <iso9661@gmail.com>
2024-12-31 17:31:04 +0900

    `Refactored problem 14170 solution to use sys.stdin and bisect module`

--- 14170번： Counting Haybales/Counting Haybales.py
+++ 14170번： Counting Haybales/Counting Haybales.py
@@ -1,11 +1,19 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14170                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14170                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 17:17:53 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys, bisect
+
+def main():
+    data = sys.stdin.read().split()
+    it = iter(data)
+    N = int(next(it))
+    Q = int(next(it))
+    haybales = sorted(int(next(it)) for _ in range(N))
+    output = []
+    for _ in range(Q):
+        A = int(next(it))
+        B = int(next(it))
+        left = bisect.bisect_left(haybales, A)
+        right = bisect.bisect_right(haybales, B)
+        output.append(str(right - left))
+    print('\n'.join(output))
+
+if __name__ == '__main__':
+    main()
