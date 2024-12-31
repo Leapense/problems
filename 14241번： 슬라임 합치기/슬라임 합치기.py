dd758ced50ed78383f576fda125e49d69e3a64ec
Leapense <iso9661@gmail.com>
2024-12-31 21:29:55 +0900

    `Refactor problem 14241 solution to use dynamic programming`

--- 14241번： 슬라임 합치기/슬라임 합치기.py
+++ 14241번： 슬라임 합치기/슬라임 합치기.py
@@ -1,11 +1,14 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14241                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14241                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 19:33:43 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+N, *rest = map(int, open(0).read().split())
+A = rest[:N]
+prefix = [0] * (N + 1)
+
+for i in range(N):
+    prefix[i + 1] = prefix[i] + A[i]
+
+dp = [[0] * N for _ in range(N)]
+for length in range(2, N + 1):
+    for i in range(N - length + 1):
+        j = i + length - 1
+        for k in range(i, j):
+            dp[i][j] = max(dp[i][j], dp[i][k] + dp[k + 1][j] + (prefix[k + 1] - prefix[i]) * (prefix[j + 1] - prefix[k + 1]))
+print(dp[0][N-1])
