9a97558c6470002f2f568274ed1f7e950a41c258
Leapense <iso9661@gmail.com>
2024-12-31 22:47:08 +0900

    `Update Senate Evacuation (Small) problem solution to use input() and print() for dynamic input and output.`

--- 14363번： Senate Evacuation (Small)/Senate Evacuation (Small).py
+++ 14363번： Senate Evacuation (Small)/Senate Evacuation (Small).py
@@ -1,11 +1,26 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14363                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14363                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 22:35:32 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    P = list(map(int, input().split()))
+    parties = []
+
+    for i in range(N):
+        parties.append([P[i], chr(ord('A') + i)])
+    plan = []
+
+    while sum(p[0] for p in parties) > 0:
+        parties.sort(reverse=True)
+        step = ''
+        parties[0][0] -= 1
+        step += parties[0][1]
+        total = sum(p[0] for p in parties)
+        majority = False
+        for p in parties:
+            if p[0] > total / 2:
+                majority = True
+                break
+        if majority:
+            parties[1][0] -= 1
+            step += parties[1][1]
+        plan.append(step)
+    print(f"Case #{tc}: {' '.join(plan)}")
