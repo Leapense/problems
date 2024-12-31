4f64cf88447d64313b94b91c109c090f499a9482
Leapense <iso9661@gmail.com>
2024-12-31 14:33:18 +0900

    `Refactored Exam Redistribution.cpp with new implementation`

--- 13932번： Exam Redistribution/Exam Redistribution.cpp
+++ 13932번： Exam Redistribution/Exam Redistribution.cpp
@@ -1,11 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 13932                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/13932                          #+#        #+#      #+#    */
-/*   Solved: 2024/12/31 14:23:51 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> s(n);
+    for (auto &x : s)
+        cin >> x;
+    int total = accumulate(s.begin(), s.end(), 0);
+    for (int i = 0; i < n; i++)
+    {
+        bool ok = true;
+        for (int j = 0; j < n; j++)
+            if (j != i && s[i] < s[j])
+            {
+                ok = false;
+                break;
+            }
+        if (ok)
+        {
+            int sum = total - s[i];
+            if (s[i] <= sum)
+            {
+                vector<int> order;
+                order.push_back(i + 1);
+                for (int j = 0; j < n; j++)
+                    if (j != i)
+                        order.push_back(j + 1);
+                for (int k = 0; k < order.size(); k++)
+                    cout << order[k] << (k < order.size() - 1 ? " " : "\n");
+                return 0;
+            }
+        }
+    }
+    cout << "impossible\n";
+
+    return 0;
+}
