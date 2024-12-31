0d42897d0894f2529bdd8d406638340886bfae52
Leapense <iso9661@gmail.com>
2024-12-31 19:33:49 +0900

    `Refactor code to solve problem 14235, add import statements and main function`

--- 14235번： 크리스마스 선물/크리스마스 선물.py
+++ 14235번： 크리스마스 선물/크리스마스 선물.py
@@ -1,11 +1,26 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14235                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14235                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 19:03:20 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys, heapq
+
+def main():
+    data = sys.stdin.read().split()
+    n = int(data[0])
+    idx = 1
+    heap = []
+    output = []
+    for _ in range(n):
+        a = int(data[idx])
+        idx += 1
+        if a == 0:
+            if heap:
+                val = -heapq.heappop(heap)
+                output.append(str(val))
+            else:
+                output.append("-1")
+        else:
+            for _ in range(a):
+                val = int(data[idx])
+                heapq.heappush(heap, -val)
+                idx += 1
+    print("\n".join(output))
+
+if __name__ == '__main__':
+    main()
