86f108d6fb32dfe24c9b662921c11b1358211445
Leapense <iso9661@gmail.com>
2025-01-01 16:37:56 +0900

    `Refactored code for problem 14494 with dynamic programming solution`

--- 14494번： 다이나믹이 뭐예요？/Main.java
+++ 14494번： 다이나믹이 뭐예요？/Main.java
@@ -1,11 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 14494                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/14494                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/01 16:29:48 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nm = br.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+
+        long[][] dp = new long[n + 1][m + 1];
+
+        dp[1][1] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 && j == 1) continue;
+
+                long fromTop = dp[i-1][j];
+                long fromLeft = dp[i][j-1];
+                long fromDiag = dp[i-1][j-1];
+
+                dp[i][j] = (fromTop + fromLeft + fromDiag) % MOD;
+            }
+        }
+
+        System.out.println(dp[n][m] % MOD);
+    }
+}
