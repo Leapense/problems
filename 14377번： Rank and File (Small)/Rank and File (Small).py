b5bfd031a82dd19f0c00e201141c5611234d650a
Leapense <iso9661@gmail.com>
2025-01-01 14:31:17 +0900

    `Update Rank and File (Small) problem solution with new input handling`

--- 14377번： Rank and File (Small)/Rank and File (Small).py
+++ 14377번： Rank and File (Small)/Rank and File (Small).py
@@ -1,11 +1,11 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14377                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14377                          #+#        #+#      #+#     #
-#    Solved: 2024/12/31 22:57:13 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    from collections import defaultdict
+    count = defaultdict(int)
+    for _ in range(2 * N - 1):
+        lst = list(map(int, input().split()))
+        for num in lst:
+            count[num] += 1
+    missing = sorted([num for num, cnt in count.items() if cnt % 2 == 1])
+    print(f"Case #{tc}: {' '.join(map(str, missing))}")
