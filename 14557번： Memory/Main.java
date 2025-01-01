7ebe3c45ae8a90e9ac566033478ff12eedf4e352
Leapense <iso9661@gmail.com>
2025-01-01 19:01:49 +0900

    `Added Java solution for problem 14557, including import statements and main method with BufferedReader and InputStreamReader for input handling.`

--- 14557번： Memory/Main.java
+++ 14557번： Memory/Main.java
@@ -1,11 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 14557                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/14557                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/01 18:41:56 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] tokens = br.readLine().trim().split("\\s+");
+        int R = Integer.parseInt(tokens[0]);
+        int C = Integer.parseInt(tokens[1]);
+
+        int N = (R * C) / 2;
+
+        int minActions = N;
+        int maxActions = 2 * N - 1;
+
+        System.out.println(minActions + " " + maxActions);
+    }
+}
