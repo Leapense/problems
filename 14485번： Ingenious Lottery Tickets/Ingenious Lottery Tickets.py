aa408cb0f9520374bd2d42b5356900aacf429527
Leapense <iso9661@gmail.com>
2025-01-01 16:28:49 +0900

    `Added a new solution for problem 14485, Ingenious Lottery Tickets, in a separate Python script.`

--- 14485번： Ingenious Lottery Tickets/Ingenious Lottery Tickets.py
+++ 14485번： Ingenious Lottery Tickets/Ingenious Lottery Tickets.py
@@ -1,11 +1,25 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14485                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14485                          #+#        #+#      #+#     #
-#    Solved: 2025/01/01 16:01:37 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    input = sys.stdin.readline
+    T = int(input().strip())
+    for _ in range(T):
+        n = int(input().strip())
+        freq = [0] * 50
+        for _ in range(n):
+            nums = list(map(int, input().split()))
+            for x in nums:
+                freq[x] += 1
+
+        def custom_rank(x):
+            return 0 if x == 7 else x
+        
+        candidates = list(range(1, 50))
+        candidates.sort(key=lambda x: (-freq[x], custom_rank(x)))
+
+        chosen = candidates[:6]
+
+        chosen.sort()
+        print(' '.join(map(str, chosen)))
+
+if __name__ == '__main__':
+    solve()
