1ff4fe530e55ceb569d3fd8305f382906e2596c5
Leapense <iso9661@gmail.com>
2025-01-02 21:55:12 +0900

    `diff: updated code for problem 14936 with new solution`

--- 14936번： 엘리베이터 장난/엘리베이터 장난.py
+++ 14936번： 엘리베이터 장난/엘리베이터 장난.py
@@ -1,18 +1,37 @@
-N, m = map(int, input().split())
-t2 = N // 2
-t3 = (N + 1) // 2
-if N <= 2:
-    actions = [t2, t3]
-else:
-    t4 = (N - 1) // 3 + 1
-    actions = [t2, t3, t4]
-count = 0
-num_actions = len(actions)
-for s in range(1 << num_actions):
-    sum_time = 0
-    for i in range(num_actions):
-        if (s >> i) & 1:
-            sum_time += actions[i]
-    if sum_time <= m:
-        count +=1
-print(count)
+def solve():
+    import sys
+    input = sys.stdin.readline
+    N, m = map(int, input().split())
+    cost = [N, N // 2, (N + 1) // 2, (N - 1) // 3 + 1]
+    patterns = []
+    for s in range(16):
+        c = 0
+        for i in range(4):
+            if s & (1 << i):
+                c += cost[i]
+        if c <= m:
+            patterns.append(s)
+    states = set()
+
+    for s in patterns:
+        arr = [False] * N
+        if s & 1:
+            for i in range(N):
+                arr[i] = not arr[i]
+        if s & 2:
+            for i in range(1,N,2):
+                arr[i] = not arr[i]
+        if s & 4:
+            for i in range(0,N,2):
+                arr[i] = not arr[i]
+        if s & 8:
+            i = 0
+            while 3 * i + 1 <= N:
+                arr[3 * i] = not arr[3 * i]
+                i += 1
+        states.add(tuple(arr))
+    print(len(states))
+
+
+if __name__ == '__main__':
+    solve()
