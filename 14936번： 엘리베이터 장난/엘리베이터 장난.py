3b5646ea82618d2703fb5cdd9251438ec9400db9
Leapense <iso9661@gmail.com>
2025-01-02 21:23:01 +0900

    `diff: updated code for problem 14936 with new logic and formatting`

--- 14936번： 엘리베이터 장난/엘리베이터 장난.py
+++ 14936번： 엘리베이터 장난/엘리베이터 장난.py
@@ -1,11 +1,18 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 14936                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/14936                          #+#        #+#      #+#     #
-#    Solved: 2025/01/02 20:40:18 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+N, m = map(int, input().split())
+t2 = N // 2
+t3 = (N + 1) // 2
+if N <= 2:
+    actions = [t2, t3]
+else:
+    t4 = (N - 1) // 3 + 1
+    actions = [t2, t3, t4]
+count = 0
+num_actions = len(actions)
+for s in range(1 << num_actions):
+    sum_time = 0
+    for i in range(num_actions):
+        if (s >> i) & 1:
+            sum_time += actions[i]
+    if sum_time <= m:
+        count +=1
+print(count)
