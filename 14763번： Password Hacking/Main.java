0deeb732d84f4e025e432e0b285745a6611d0c4d
Leapense <iso9661@gmail.com>
2025-01-02 18:59:38 +0900

    `Refactored code for problem 14763: Password Hacking`

--- 14763번： Password Hacking/Main.java
+++ 14763번： Password Hacking/Main.java
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 14763                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/14763                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/02 17:31:38 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Password {
+        String pwd;
+        double prob;
+
+        public Password(String pwd, double prob) {
+            this.pwd = pwd;
+            this.prob = prob;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+        List<Password> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] parts = br.readLine().trim().split("\\s+");
+            String pwd = parts[0];
+            double prob = Double.parseDouble(parts[1]);
+            list.add(new Password(pwd, prob));
+        }
+
+        list.sort((a, b) -> {
+            if (Double.compare(b.prob, a.prob) != 0) {
+                return Double.compare(b.prob, a.prob);
+            }
+            return 0;
+        });
+
+        double expected = 0.0;
+        for (int i = 0; i < N; i++) {
+            expected += (i+1) * list.get(i).prob;
+        }
+        System.out.printf("%.4f", expected);
+
+        br.close();
+    }
+}
