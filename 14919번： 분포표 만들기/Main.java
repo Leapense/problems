69ac3b91c9c4d23046e83d3d163a18545e64d51b
Leapense <iso9661@gmail.com>
2025-01-02 19:39:50 +0900

    `Added Java solution for problem 14919, including import statement, main class, and code to read input, process data, and print output.`

--- 14919번： 분포표 만들기/Main.java
+++ 14919번： 분포표 만들기/Main.java
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 14919                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/14919                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/02 19:20:56 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine().trim());
+        String[] tokens = br.readLine().trim().split("\\s+");
+        int[] counts = new int[m];
+
+        for (String token : tokens) {
+            double a = Double.parseDouble(token);
+            int index = (int)(a * m);
+            if (index >= m) index = m - 1;
+            counts[index]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            sb.append(counts[i]);
+            if (i != m - 1) sb.append(" ");
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+}
