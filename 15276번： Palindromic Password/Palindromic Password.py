ae03039e196e1a868dd08eae675299853be153ae
Leapense <iso9661@gmail.com>
2025-01-03 20:38:24 +0900

    Palindromic Password 문제 해결을 위한 코드 수정

--- 15276번： Palindromic Password/Palindromic Password.py
+++ 15276번： Palindromic Password/Palindromic Password.py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 15276                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/15276                          #+#        #+#      #+#     #
-#    Solved: 2025/01/03 12:38:15 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+N = int(input())
+for _ in range(N):
+    num_str = input()
+    num = int(num_str)
+    if num_str == num_str[::-1]:
+        print(num_str)
+    else:
+        base = int(num_str[:3])
+        candidates = []
+        for b in [base - 1, base, base + 1]:
+            if 100 <= b <= 999:
+                palindrome = int(str(b) + str(b)[::-1])
+                candidates.append(palindrome)
+        min_diff = float('inf')
+        closest = None
+        for p in candidates:
+            diff = abs(num - p)
+            if diff < min_diff or (diff == min_diff and p < closest):
+                min_diff = diff
+                closest = p
+        print("{:06d}".format(closest))
