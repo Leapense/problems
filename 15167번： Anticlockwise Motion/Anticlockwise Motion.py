cd43ffd88fcbe3db49a8d2a35632897393a460a2
Leapense <iso9661@gmail.com>
2025-01-03 12:38:08 +0900

    15167번: Anticlockwise Motion 문제 해결을 위한 코드 수정

--- 15167번： Anticlockwise Motion/Anticlockwise Motion.py
+++ 15167번： Anticlockwise Motion/Anticlockwise Motion.py
@@ -1,11 +1,36 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 15167                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/15167                          #+#        #+#      #+#     #
-#    Solved: 2025/01/03 12:23:05 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import math, sys
+from math import isqrt
+
+def get_coord(n):
+    if n ==1:
+        return (0,0)
+    s = isqrt(n)
+    if s*s <n:
+        s +=1
+    if s%2 ==1 and s*s >=n:
+        k=(s -1)//2
+    else:
+        k=s//2
+    max_num=(2*k +1)**2
+    offset=max_num -n
+    side_len=2*k
+    side=offset // side_len
+    pos=offset % side_len
+    if side ==0:
+        x=-k
+        y=k - pos
+    elif side ==1:
+        x=-k + pos
+        y=-k
+    elif side ==2:
+        x=k
+        y=-k + pos
+    elif side ==3:
+        x=k - pos
+        y=k
+    return (x,y)
+
+a, b = map(int, sys.stdin.read().split())
+xa, ya = get_coord(a)
+xb, yb = get_coord(b)
+print(abs(xa - xb) + abs(ya - yb))
