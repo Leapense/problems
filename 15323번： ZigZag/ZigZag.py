0cc69438a6abd0b124ec52d780319da14d7eae52
Leapense <iso9661@gmail.com>
2025-01-03 20:57:36 +0900

    ZigZag 문제 해결을 위한 코드 구현

--- 15323번： ZigZag/ZigZag.py
+++ 15323번： ZigZag/ZigZag.py
@@ -1,11 +1,37 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 15323                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/15323                          #+#        #+#      #+#     #
-#    Solved: 2025/01/03 20:43:43 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys, heapq
+from collections import defaultdict
+
+def main():
+    data = sys.stdin.read().splitlines()
+    K, N = map(int, data[0].split())
+    words = data[1:1+K]
+    letters = data[1+K:]
+    groups = defaultdict(list)
+    for word in words:
+        groups[word[0]].append(word)
+    word_list = []
+    frequency = []
+    per_letter_heap = {}
+    for c in groups:
+        sorted_words = sorted(groups[c])
+        heap = []
+        for word in sorted_words:
+            word_list.append(word)
+            frequency.append(0)
+            heap.append((0, len(word_list) - 1))
+        heapq.heapify(heap)
+        per_letter_heap[c] = heap
+    output = []
+    for letter in letters:
+        heap = per_letter_heap[letter]
+        while True:
+            freq, idx = heapq.heappop(heap)
+            if freq == frequency[idx]:
+                output.append(word_list[idx])
+                frequency[idx] += 1
+                heapq.heappush(heap, (freq + 1, idx))
+                break
+    print('\n'.join(output))
+
+if __name__ == '__main__':
+    main()
