db0008dbd0d2fd1c609196dc4c0fa97b474e5752
Leapense <iso9661@gmail.com>
2025-01-03 12:22:02 +0900

    Boolean Satisfiability 문제 해결을 위한 코드 수정

--- 15132번： Boolean Satisfiability/Boolean Satisfiability.py
+++ 15132번： Boolean Satisfiability/Boolean Satisfiability.py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 15132                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/15132                          #+#        #+#      #+#     #
-#    Solved: 2025/01/03 12:15:36 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+formula = sys.stdin.read().strip()
+literals = formula.split('|')
+positive_vars = set()
+negative_vars = set()
+
+for lit in literals:
+    if lit.startswith('~'):
+        negative_vars.add(lit[1])
+    else:
+        positive_vars.add(lit[0])
+
+all_vars = positive_vars.union(negative_vars)
+m = len(all_vars)
+conflict = any(var in positive_vars and var in negative_vars for var in all_vars)
+if conflict:
+    result = 2 ** m
+else:
+    result = 2 ** m - 1
+
+print(result)
