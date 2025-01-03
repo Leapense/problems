bc9aabfae78117ca56ed46d6fe937304635756a0
Leapense <iso9661@gmail.com>
2025-01-03 12:06:39 +0900

    Added solution for problem 15045: Ginástica

--- 15045번： Ginástica/Ginástica.py
+++ 15045번： Ginástica/Ginástica.py
@@ -1,11 +1,16 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 15045                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/15045                          #+#        #+#      #+#     #
-#    Solved: 2025/01/03 11:54:58 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+MOD = 10 ** 9 + 7
+T, M, N = map(int, input().split())
+size = N - M + 1
+prev = [1] * size
+
+for _ in range(T - 1):
+    current = [0] * size
+
+    for i in range(size):
+        if i > 0:
+            current[i] += prev[i - 1]
+        if i < size - 1:
+            current[i] += prev[i + 1]
+        current[i] %= MOD
+    prev = current
+print(sum(prev) % MOD)
