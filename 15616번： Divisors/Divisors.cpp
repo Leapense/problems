6888830c35f32061f9e2f85c6a016a08e8934a38
Leapense <iso9661@gmail.com>
2025-01-04 20:35:28 +0900

    15616번 Divisors 문제 해결을 위한 C++ 코드 구현

--- 15616번： Divisors/Divisors.cpp
+++ 15616번： Divisors/Divisors.cpp
@@ -1,11 +1,35 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15616                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15616                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/04 20:03:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    int max_a = 0;
+    for (auto &x : a) {
+        cin >> x;
+        if (x > max_a) max_a = x;
+    }
+
+    vector<int> freq(max_a + 1, 0);
+    for (auto x : a) {
+        freq[x]++;
+    }
+    vector<ll> total(max_a + 1, 0);
+    for (int x = 1; x <= max_a; x++) {
+        for (int m = x; m <= max_a; m += x) {
+            total[x] += freq[m];
+        }
+    }
+
+    ll res = 0;
+    for (int x = 1; x <= max_a; x++) res += (ll)freq[x] * (total[x] - 1);
+    cout << res << "\n";
+
+    return 0;
+}
