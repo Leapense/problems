dd097fb5030c5475feb09098b041bffc0d50d572
Leapense <iso9661@gmail.com>
2025-01-04 21:00:58 +0900

    15728번 문제 해결을 위한 C++ 코드 구현

--- 15728번： 에리 - 카드/에리 - 카드.cpp
+++ 15728번： 에리 - 카드/에리 - 카드.cpp
@@ -1,11 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15728                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15728                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/04 20:37:23 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> shared(N);
+    for (auto &x : shared) cin >> x;
+
+    vector<int> team(N);
+    for (auto &x : team) cin >> x;
+
+    int s_max = *max_element(shared.begin(), shared.end());
+    int s_min = *min_element(shared.begin(), shared.end());
+
+    vector<long long> C;
+
+    for (auto t : team) {
+        if (t > 0) {
+            C.push_back((long long)t * s_max);
+        } else if (t < 0) {
+            C.push_back((long long)t * s_min);
+        } else {
+            C.push_back(0);
+        }
+    }
+
+    sort(C.begin(), C.end(), greater<long long>());
+
+    if (K >= N) {
+        cout << 0;
+        return 0;
+    }
+
+    cout << C[K];
+    return 0;
+}
