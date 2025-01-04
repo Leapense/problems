150f0c2d085c5efebc6e490937a96cc49026e708
Leapense <iso9661@gmail.com>
2025-01-04 22:03:24 +0900

    15815번 문제 해결을 위한 C++ 코드 구현 및 주석 추가

--- 15815번： 천재 수학자 성필/천재 수학자 성필.cpp
+++ 15815번： 천재 수학자 성필/천재 수학자 성필.cpp
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15815                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15815                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/04 21:53:23 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+// infix 연산으로 바꾸는 문제
+int main() {
+    string s;
+    cin >> s;
+    stack<long long> st;
+
+    for (char c : s) {
+        if (isdigit(c)) {
+            st.push(c - '0');
+        } else {
+            long long b = st.top(); st.pop();
+            long long a = st.top(); st.pop();
+            long long res;
+            if (c == '+') res = a + b;
+            else if (c == '-') res = a - b;
+            else if (c == '*') res = a * b;
+            else if (c == '/') res = a / b;
+            st.push(res);
+        }
+    }
+    cout << st.top() << endl;
+
+    return 0;
+}
