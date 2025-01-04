5666c472462bf83a52906a8c947ce25233746033
Leapense <iso9661@gmail.com>
2025-01-04 18:30:42 +0900

    15594번 Out of Place 문제 해결을 위한 C++ 코드 수정 및 최적화

--- 15594번： Out of Place/Out of Place.cpp
+++ 15594번： Out of Place/Out of Place.cpp
@@ -1,11 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15594                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15594                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/04 12:37:45 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> current(N);
+    for (auto &x : current) cin >> x;
+    vector<int> sorted = current;
+
+    sort(sorted.begin(), sorted.end());
+    int cnt = 0;
+    for (int i = 0; i < N; i++) if (current[i] != sorted[i]) cnt++;
+    cout << (cnt == 0 ? 0 : cnt - 1);
+
+    return 0;
+}
