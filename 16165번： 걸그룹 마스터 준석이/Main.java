260b5efe6ebb5416a62a833525315656a9a0dfd4
Leapense <iso9661@gmail.com>
2025-01-05 21:34:29 +0900

    16165번 문제 해결을 위한 Java 코드 구현: 그룹과 멤버 관리 로직 추가

--- 16165번： 걸그룹 마스터 준석이/Main.java
+++ 16165번： 걸그룹 마스터 준석이/Main.java
@@ -1,11 +1,47 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 16165                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/16165                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/05 21:28:07 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, ArrayList<String>> groupToMembers = new HashMap<>();
+        HashMap<String, String> memberToGroup = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String groupName = br.readLine();
+            int memberCount = Integer.parseInt(br.readLine());
+            ArrayList<String> members = new ArrayList<>();
+
+            for (int j = 0; j < memberCount; j++) {
+                String memberName = br.readLine();
+                members.add(memberName);
+                memberToGroup.put(memberName, groupName);
+            }
+            groupToMembers.put(groupName, members);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            String query = br.readLine();
+            int type = Integer.parseInt(br.readLine());
+
+            if (type == 0) {
+                ArrayList<String> members = groupToMembers.get(query);
+                Collections.sort(members);
+                for (String member : members) {
+                    sb.append(member).append("\n");
+                }
+            } else if (type == 1) {
+                String group = memberToGroup.get(query);
+                sb.append(group).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
