a01081114bfe1f94231ad3b272c0cb1d9ab159a2
Leapense <iso9661@gmail.com>
2025-01-05 19:35:08 +0900

    16034번 문제 해결을 위한 Java 코드 구현: 스카이스크래퍼 문제의 메인 로직 추가

--- 16034번： Skyscraper “MinatoHarukas”/Main.java
+++ 16034번： Skyscraper “MinatoHarukas”/Main.java
@@ -1,11 +1,35 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 16034                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/16034                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/05 18:38:44 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            int b = Integer.parseInt(br.readLine());
+            if (b == 0) break;
+
+            int maxFloors = 0;
+            int startFloor = 0;
+
+            for (int k = 1; k * (k - 1) / 2 < b; k++) {
+                int temp = b - (k * (k - 1)) / 2;
+
+                if (temp % k == 0) {
+                    int n = temp / k;
+
+                    if (n > 0 && k > maxFloors) {
+                        maxFloors = k;
+                        startFloor = n;
+                    }
+                }
+            }
+
+            sb.append(startFloor).append(" ").append(maxFloors).append("\n");
+        }
+
+        System.out.print(sb);
+
+        br.close();
+    }
+}
