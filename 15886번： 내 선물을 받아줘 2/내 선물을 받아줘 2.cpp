6f683ea08c2c58a63e7b2bf14cf3509f339404f5
Leapense <iso9661@gmail.com>
2025-01-05 16:38:52 +0900

    15886번 문제 해결을 위한 C++ 코드 구현: 입력 처리 및 사이클 계산 로직 추가

--- 15886번： 내 선물을 받아줘 2/내 선물을 받아줘 2.cpp
+++ 15886번： 내 선물을 받아줘 2/내 선물을 받아줘 2.cpp
@@ -1,11 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15886                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15886                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/05 16:12:32 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+
+    vector<int> next_pos(N + 1, 0);
+    for (int i = 1; i <= N; ++i) {
+        if (S[i-1] == 'E') {
+            next_pos[i] = i + 1;
+        } else {
+            next_pos[i] = i - 1;
+        }
+    }
+
+    vector<int> color(N + 1, 0);
+
+    int cycle_count = 0;
+
+    for (int i = 1; i <= N; ++i) {
+        if (color[i] == 0) {
+            int current = i;
+            vector<int> path;
+            while (true) {
+                if (color[current] == 1) {
+                    cycle_count++;
+                    break;
+                }
+                if (color[current] == 2) {
+                    break;
+                }
+
+                color[current] = 1;
+                path.push_back(current);
+                current = next_pos[current];
+            }
+
+            for (auto node : path) {
+                color[node] = 2;
+            }
+        }
+    }
+
+    cout << cycle_count;
+
+    return 0;
+}
