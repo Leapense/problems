afa1fe5b4317a0f79ad0549e01481b6ba475413b
Leapense <iso9661@gmail.com>
2025-01-05 15:29:22 +0900

    15860번 문제를 해결하기 위한 C++ 코드 구현

--- 15860번： Ninety-nine/Ninety-nine.cpp
+++ 15860번： Ninety-nine/Ninety-nine.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15860                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15860                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/05 15:18:14 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int total = 0;
+    int my_move = 1;
+    cout << my_move << "\n" << flush;
+    total += my_move;
+
+    while (total < 99) {
+        int opponent_move;
+
+        if (!(cin >> opponent_move)) {
+            return 0;
+        }
+
+        int add;
+        int remainder = total % 3;
+        if (remainder == 0) {
+            add = 1;
+        } else {
+            add = 3 - remainder;
+            if (add > 2) {
+                add = 1;
+            }
+        }
+
+        if (add < 1 || add > 2) {
+            add = 1;
+        }
+
+        my_move = add;
+        cout << my_move << "\n" << flush;
+        total += my_move;
+    }
+
+    return 0;
+}
