600f740810aa085a9be00b2457a36619d5eaefff
Leapense <iso9661@gmail.com>
2025-01-05 16:35:47 +0900

    15860번 문제 해결을 위한 코드 수정: 입력 처리 방식 개선 및 불필요한 코드 제거, 15886번 문제 추가 및 README 작성

--- 15860번： Ninety-nine/Ninety-nine.cpp
+++ 15860번： Ninety-nine/Ninety-nine.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include <cstdlib>
-
 using namespace std;
 
-int main() {
+int main()
+{
     int num = 0;
     cout << 1 << endl;
-    while (cin >> num && num != 99) {
-        int r;
-        if (num % 3 == 0) r = num + 1 + rand() % 2;
-        else r = (num / 3 * 3 + 3);
-        cout << r << endl;
 
-        if (r == 99) break;
+    while (num != 99 && cin >> num && num != 99) {
+        ++num;
+        cout << num << endl;
     }
+
+    return 0;
 }
