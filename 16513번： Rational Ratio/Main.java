00077a18a4763158f6b5ce53a9027327ddef3fe5
Leapense <iso9661@gmail.com>
2025-01-07 19:35:36 +0900

    16513번 문제 해결을 위한 Java 코드 수정: 유리 비율 계산 로직 개선 및 출력 형식 수정

--- 16513번： Rational Ratio/Main.java
+++ 16513번： Rational Ratio/Main.java
@@ -1,19 +1,34 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-    static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
+        int k = sc.nextInt();
+        String[] parts = number.split("\\.");
+        String A_str = parts[0];
+        String D_str = parts[1];
+        int total_decimal = D_str.length();
+        int n = total_decimal - k;
+        String B_str = n > 0 ? D_str.substring(0, n) : "0";
+        String C_str = D_str.substring(total_decimal - k);
+        BigInteger A = new BigInteger(A_str);
+        BigInteger B = new BigInteger(B_str);
+        BigInteger C = new BigInteger(C_str);
+        BigInteger ten = BigInteger.TEN;
+        BigInteger ten_n = ten.pow(n);
+        BigInteger ten_k = ten.pow(k);
+        BigInteger ten_nk = ten.pow(n + k);
+        BigInteger D = ten_nk.subtract(ten_n);
+        BigInteger term1 = A.multiply(D);
+        BigInteger term2 = B.multiply(ten_k.subtract(BigInteger.ONE));
+        BigInteger N = term1.add(term2).add(C);
+        BigInteger gcd = N.gcd(D);
 
-        return a;
-    }
+        N = N.divide(gcd);
+        D = D.divide(gcd);
 
-    public static String rationalRatio(String input) {
-        String[] parts = input.split(" ");
-        String decimalStr = parts[0];
-        int repeatLen = Integer.parseInt(parts[1]);
+        System.out.println(N + "/" + D);
     }
 }
--- 31244번： СИМЕТРИЧНИ ЧИСЛА/Main.java
+++ 31244번： СИМЕТРИЧНИ ЧИСЛА/Main.java
@@ -1,11 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31244                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31244                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/07 18:51:39 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        String result;
+        if (A == C) {
+            result = "" + A + B + A;
+        } else if (C == B) {
+            result = "" + A + B + C + A;
+        } else {
+            result = "" + A + B + C + B + A;
+        }
+
+        System.out.println(result);
+    }
+}
