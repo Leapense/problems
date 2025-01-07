08c8d7ebc387ef4fb20e414ead1b8b7d8c02ef6b
Leapense <iso9661@gmail.com>
2025-01-07 21:42:56 +0900

    16520번 문제 해결을 위한 Python 코드 구현: Kaleidoscopic Palindromes 문제 해결 로직 추가

--- 16520번： Kaleidoscopic Palindromes/Kaleidoscopic Palindromes.py
+++ 16520번： Kaleidoscopic Palindromes/Kaleidoscopic Palindromes.py
@@ -1,11 +1,20 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 16520                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/16520                          #+#        #+#      #+#     #
-#    Solved: 2025/01/07 21:38:04 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    a, b, k = map(int, sys.stdin.readline().split())
+    if k >= 3:
+        ans = 0
+        if a <= 0 <= b:
+            ans += 1
+        if a <= 1 <= b:
+            ans += 1
+        print(ans)
+    else:
+        ans = 0
+        for n in range(a, b + 1):
+            s = bin(n)[2:]
+            if s == s[::-1]:
+                ans += 1
+        print(ans)
+
+if __name__ == "__main__":
+    solve()
