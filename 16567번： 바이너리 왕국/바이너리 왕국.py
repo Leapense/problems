c495e3736ab4ea545a9b737c729f5a612f94e225
Leapense <iso9661@gmail.com>
2025-01-08 15:14:20 +0900

    16567번 문제 해결을 위한 Python 코드 추가: 상태 변화 카운팅 로직 구현

--- 16567번： 바이너리 왕국/바이너리 왕국.py
+++ 16567번： 바이너리 왕국/바이너리 왕국.py
@@ -1,11 +1,31 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 16567                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/16567                          #+#        #+#      #+#     #
-#    Solved: 2025/01/08 15:02:39 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+input = sys.stdin.read
+data = input().splitlines()
+
+N, M = map(int, data[0].split())
+state = list(map(int, data[1].split()))
+
+count = 0
+for i in range(N):
+    if state[i] == 1 and (i == 0 or state[i - 1] == 0):
+        count += 1
+
+result = []
+for line in data[2:]:
+    command = line.split()
+    if command[0] == "0":
+        result.append(str(count))
+    else:
+        idx = int(command[1]) - 1
+        if state[idx] == 0:
+            state[idx] = 1
+
+            left = idx > 0 and state[idx - 1] == 1
+            right = idx < N - 1 and state[idx + 1] == 1
+
+            if not left and not right:
+                count += 1
+            elif left and right:
+                count -= 1
+
+sys.stdout.write("\n".join(result) + "\n")
