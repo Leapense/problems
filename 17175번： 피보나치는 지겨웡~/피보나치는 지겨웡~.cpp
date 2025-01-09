759192caf85602553f69b5c412867b9d8a32523d
Leapense <iso9661@gmail.com>
2025-01-09 22:03:39 +0900

    17175번 문제 해결을 위한 C++ 코드 수정: 피보나치 호출 횟수 계산 함수 구현

--- 17175번： 피보나치는 지겨웡~/피보나치는 지겨웡~.cpp
+++ 17175번： 피보나치는 지겨웡~/피보나치는 지겨웡~.cpp
@@ -1,11 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 17175                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/17175                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/09 22:00:30 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+const int MOD = 1'000'000'007;
+
+int fibonacci_calls(int n) {
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+
+    int prev2 = 1, prev1 = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        int current = (prev1 + prev2 + 1) % MOD;
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci_calls(n) << endl;
+    return 0;
+}
