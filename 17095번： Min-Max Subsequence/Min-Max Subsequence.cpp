7aa89ec1da7d2f8af23e4144e69d4f18a25f6b4f
Leapense <iso9661@gmail.com>
2025-01-09 21:27:31 +0900

    17124번 문제 해결을 위한 C++ 코드 및 README 추가: 두 개의 배열 문제 구현

--- 17095번： Min-Max Subsequence/Min-Max Subsequence.cpp
+++ 17095번： Min-Max Subsequence/Min-Max Subsequence.cpp
@@ -1,11 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 17095                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/17095                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/09 18:51:03 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    
+    cin >> N;
+    vector<int> A(N);
+
+    for (auto &x : A) cin >> x;
+
+    int maxA = *max_element(A.begin(), A.end());
+    int minA = *min_element(A.begin(), A.end());
+
+    if (maxA == minA) {
+        cout << 1;
+        return 0;
+    }
+
+    int last_max = -1, last_min = -1;
+    int res = N;
+    for (int i = 0; i < N; i++) {
+        if (A[i] == maxA) last_max = i;
+        if (A[i] == minA) last_min = i;
+
+        if (last_max != -1 && last_min != -1) {
+            res = min(res, abs(last_max - last_min) + 1);
+        }
+    }
+
+    cout << res << "\n";
+
+    return 0;
+}
