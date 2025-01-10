0ec58021b3777bb25d85763c7b67f008c627587b
Leapense <iso9661@gmail.com>
2025-01-10 20:18:47 +0900

    `Added C++ code for problem 17393 with binary search solution`

--- 17393번： 다이나믹 롤러/다이나믹 롤러.cpp
+++ 17393번： 다이나믹 롤러/다이나믹 롤러.cpp
@@ -1,11 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 17393                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/17393                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/10 20:15:39 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll N;
+    cin >> N;
+
+    vector<ll> A(N), B(N);
+
+    for (auto &x : A) cin >> x;
+    for (auto &x : B) cin >> x;
+
+    vector<ll> res(N, 0);
+
+    for (ll i = 0; i < N; i++) {
+        ll pos = upper_bound(B.begin() + i + 1, B.end(), A[i]) - B.begin();
+        res[i] = pos - (i + 1);
+        if (res[i] < 0) res[i] = 0;
+    }
+
+    for (ll i = 0; i < N; i++) cout << res[i] << (i < N - 1 ? " " : "\n");
+
+    return 0;
+}
