49b439618eeeac793c4d945cde987dd5803b712f
Leapense <iso9661@gmail.com>
2025-01-11 13:18:19 +0900

    `Added new solution for problem 17484 using dynamic programming`

--- 17484번： 진우의 달 여행 (Small)/진우의 달 여행 (Small).py
+++ 17484번： 진우의 달 여행 (Small)/진우의 달 여행 (Small).py
@@ -1,11 +1,33 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 17484                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/17484                          #+#        #+#      #+#     #
-#    Solved: 2025/01/11 13:07:26 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def solve():
+    N, M = map(int, sys.stdin.readline().split())
+    grid = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+    INF = float('inf')
+    dp = [[[INF] * 4 for _ in range(M)] for _ in range(N)]
+
+    for j in range(M):
+        dp[0][j][3] = grid[0][j]
+
+    directions = [(1, 0), (1, -1), (1, 1)]
+
+    for i in range(N - 1):
+        for j in range(M):
+            for k in range(4):
+                if dp[i][j][k] == INF:
+                    continue
+                for d in range(3):
+                    if d == k:
+                        continue
+                    ni, nj = i + directions[d][0], j + directions[d][1]
+                    if 0 <= ni < N and 0 <= nj < M:
+                        dp[ni][nj][d] = min(dp[ni][nj][d], dp[i][j][k] + grid[ni][nj])
+
+    result = INF
+    for j in range(M):
+        for k in range(3):
+            result = min(result, dp[N-1][j][k])
+    print(result)
+
+solve()
