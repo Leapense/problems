507bc1e4551943183fdba9ada874f95cae5da688
Leapense <iso9661@gmail.com>
2025-01-11 18:08:43 +0900

    17649번 문제 해결을 위한 C++ 코드 구현: 점과 쿼리 간 거리 계산 및 쿼리 처리 로직 추가

--- 17649번： Triangle/Triangle.cpp
+++ 17649번： Triangle/Triangle.cpp
@@ -1,11 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 17649                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/17649                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/11 18:03:41 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, Q;
+    cin >> N >> Q;
+    vector<pair<double, double>> points(N);
+    for (auto &p : points) cin >> p.first >> p.second;
+
+    while (Q--) {
+        double qx, qy;
+        cin >> qx >> qy;
+
+        vector<double> distances(N);
+        for (int i = 0; i < N; i++) {
+            distances[i] = sqrt((points[i].first - qx) * (points[i].first - qx) + (points[i].second - qy) * (points[i].second - qy));
+        }
+
+        sort(distances.begin(), distances.end());
+
+        int total = 0;
+        int start = 0;
+
+        while (start < N) {
+            int end = start + 1;
+            while (end < N && abs(distances[end] - distances[start]) < 0.0001) end++;
+
+            int k = end - start;
+
+            if (k >= 2) total += k * (k - 1) / 2;
+
+            start = end;
+        }
+
+        cout << total << "\n";
+    }
+
+
+    return 0;
+}
