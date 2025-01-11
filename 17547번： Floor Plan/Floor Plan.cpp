b2a6222470cb90aff98372b418483165e4b8ac7b
Leapense <iso9661@gmail.com>
2025-01-11 16:45:40 +0900

    17585번 문제 해결을 위한 README 및 소스코드 추가: 회로 수학 구현

--- 17547번： Floor Plan/Floor Plan.cpp
+++ 17547번： Floor Plan/Floor Plan.cpp
@@ -1,11 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 17547                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/17547                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/11 15:30:15 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    bool found = false;
+
+    long long m, k;
+
+    for (long long b = 1; b * b <= n; b++) {
+        if (n % b == 0) {
+            long long a = n / b;
+            if (a > b && (a + b) % 2 == 0 && (a - b) % 2 == 0) {
+                m = (a + b) / 2;
+                k = (a - b) / 2;
+
+                cout << m << " " << k;
+                return 0;
+            }
+        }
+    }
+
+    if (n == 1) {
+        cout << 1 << " " << 0;
+        return 0;
+    }
+
+    cout << "impossible\n";
+
+    return 0;
+}
