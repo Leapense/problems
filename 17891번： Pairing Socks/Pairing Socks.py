ddf8cef41c0f5a85da98d0439b17d33243925120
Leapense <iso9661@gmail.com>
2025-01-11 20:48:12 +0900

    17891번 문제 해결을 위한 코드 변경: Pairing Socks 문제의 파이썬 구현 추가 및 로직 최적화

--- 17891번： Pairing Socks/Pairing Socks.py
+++ 17891번： Pairing Socks/Pairing Socks.py
@@ -1,11 +1,27 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 17891                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/17891                          #+#        #+#      #+#     #
-#    Solved: 2025/01/11 20:30:59 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+from collections import defaultdict
+
+n, *rest = map(int, sys.stdin.read().split())
+socks = rest
+count = defaultdict(int)
+for sock in socks:
+    count[sock] += 1
+for v in count.values():
+    if v % 2 != 0:
+        print("impossible")
+        sys.exit()
+
+aux = []
+moves = 0
+
+for sock in socks:
+    if aux and aux[-1] == sock:
+        aux.pop()
+        moves += 1
+    else:
+        aux.append(sock)
+        moves += 1
+if not aux:
+    print(moves)
+else:
+    print("impossible")
