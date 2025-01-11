62bec20df98ceb586f2de591370351e4f6f45587
Leapense <iso9661@gmail.com>
2025-01-11 16:57:09 +0900

    17585번 문제 해결을 위한 C++ 코드 구현: 회로 수학 계산기 추가

--- 17585번： Circuit Math/Circuit Math.cpp
+++ 17585번： Circuit Math/Circuit Math.cpp
@@ -1,11 +1,47 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 17585                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/17585                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/11 15:37:41 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<char> values(n);
+
+    for (auto &c : values) cin >> c;
+    string s;
+    getline(cin, s);
+    while (s.empty()) getline(cin, s);
+
+    vector<string> tokens;
+    string token;
+
+    stringstream ss(s);
+    while (ss >> token) tokens.push_back(token);
+
+    unordered_map<char, char> var_map;
+
+    for (int i = 0; i < n; i++) var_map['A' + i] = values[i];
+
+    stack<char> st;
+
+    for (auto &tok : tokens) {
+        if (var_map.find(tok[0]) != var_map.end()) {
+            st.push(var_map[tok[0]]);
+        } else {
+            if (tok[0] == '*') {
+                char b = st.top(); st.pop();
+                char a = st.top(); st.pop();
+                st.push((a == 'T' && b == 'T') ? 'T' : 'F');
+            } else if (tok[0] == '+') {
+                char b = st.top(); st.pop();
+                char a = st.top(); st.pop();
+                st.push((a == 'T' || b == 'T') ? 'T' : 'F');
+            } else if (tok[0] == '-') {
+                char a = st.top(); st.pop();
+                st.push((a == 'T') ? 'F' : 'T');
+            }
+        }
+    }
+
+    cout << st.top();
+}
