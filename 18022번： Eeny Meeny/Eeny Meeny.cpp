64b26970f099b0a30d6dc06f754ff68003ae1a57
Leapense <iso9661@gmail.com>
2025-01-13 17:08:12 +0900

    `Refactored Eeny Meeny Miny Moe problem solution in C++`

--- 18022번： Eeny Meeny/Eeny Meeny.cpp
+++ 18022번： Eeny Meeny/Eeny Meeny.cpp
@@ -1,11 +1,62 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18022                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18022                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/13 17:00:33 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string word;
+    vector<string> rhyme_words;
+
+    while (cin >> word && word != "\n") {
+        rhyme_words.push_back(word);
+        if (cin.peek() == '\n') break;
+    }
+
+    int m = rhyme_words.size();
+
+    int n;
+    cin >> n;
+
+    vector<string> kids(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> kids[i];
+    }
+
+    vector<string> team1, team2;
+
+    int current_index = 0;
+    bool isTeam1Turn = true;
+
+    while (!kids.empty()) {
+        int selected_index = (current_index + m - 1) % kids.size();
+        string selected_kid = kids[selected_index];
+
+        if (isTeam1Turn) {
+            team1.push_back(selected_kid);
+        } else {
+            team2.push_back(selected_kid);
+        }
+
+        kids.erase(kids.begin() + selected_index);
+
+        current_index = selected_index;
+        if (current_index >= kids.size()) {
+            current_index = 0;
+        }
+
+        isTeam1Turn = !isTeam1Turn;
+    }
+
+    cout << team1.size() << "\n";
+    for (auto &kid : team1) {
+        cout << kid << "\n";
+    }
+
+    cout << team2.size() << "\n";
+    for (auto &kid : team2) {
+        cout << kid << "\n";
+    }
+
+    return 0;
+}
