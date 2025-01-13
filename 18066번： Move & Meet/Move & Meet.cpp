6f4f34d0b6491dec787edd8d2e950d95771b2b75
Leapense <iso9661@gmail.com>
2025-01-13 21:36:38 +0900

    `Refactored Move & Meet problem solution to improve code readability and efficiency.`

--- 18066번： Move & Meet/Move & Meet.cpp
+++ 18066번： Move & Meet/Move & Meet.cpp
@@ -1,67 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long abs_val(long long x) {
-    return x < 0 ? -x : x;
-}
-
-int sign_func(long long x) {
-    if (x > 0) return 1;
-    if (x < 0) return -1;
-    return 0;
-}
-
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(NULL);
 
-    long long x1, y1, d1;
-    long long x2, y2, d2;
+    long long x1, y1, d1, x2, y2, d2;
+    cin >> x1 >> y1 >> d1 >> x2 >> y2 >> d2;
 
-    cin >> x1 >> y1 >> d1;
-    cin >> x2 >> y2 >> d2;
-
-    long long initial_distance = abs_val(x1 - x2) + abs_val(y1 - y2);
-
-    if (initial_distance > d1 + d2) {
-        cout << "impossible";
+    long long dx = llabs(x1 - x2), dy = llabs(y1 - y2), dist = dx + dy;
+    if (dist > d1 + d2 || (d1 + d2 - dist) % 2 != 0) {
+        cout << "impossible\n";
         return 0;
     }
 
-    if ((d1 + d2 - initial_distance) % 2 != 0) {
-        cout << "impossible";
+    long long L = max(0LL, dist - d2), U = min(d1, dist);
+    if (L > U) {
+        cout << "impossible\n";
         return 0;
     }
 
-    long long t = (d1 + initial_distance - d2) / 2;
+    long long t = L;
+    long long px = x1, py = y1;
+    long long mx = min((long long) llabs(x2 - x1), t);
 
-    long long dx = x2 - x1;
-    long long dy = y2 - y1;
-
-    long long step_x = min(t, abs_val(dx));
-    int direction_x = sign_func(dx);
-    long long xm = x1 + step_x * direction_x;
-
-    long long remaining_steps = t - step_x;
-
-    long long step_y = min(remaining_steps, abs_val(dy));
-    int direction_y = sign_func(dy);
-    long long ym = y1 + step_y * direction_y;
-
-    long long distance1 = abs_val(xm - x1) + abs_val(ym - y1);
-    long long distance2 = abs_val(xm - x2) + abs_val(ym - y2);
-
-    if (distance1 > d1 || distance2 > d2) {
-        cout << "impossible" << endl;
-        return 0;
-    }
-
-    if ((d1 - distance1) % 2 != 0 || (d2 - distance2) % 2 != 0) {
-        cout << "impossible" << endl;
-        return 0;
-    }
-
-    cout << xm << " " << ym << endl;
+    if (x2 < x1) px -= mx; else px += mx;
+    t -= mx;
+    long long my = min((long long) llabs(y2 - y1), t);
+    if (y2 < y1) py -= my; else py += my;
+    cout << px << " " << py << "\n";
 
     return 0;
 }
