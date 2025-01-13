bcf05ed8728c0b2ce627b91d7dd6b4f1d3978c2d
Leapense <iso9661@gmail.com>
2025-01-13 17:36:40 +0900

    `Refactored code for problem 18044: updated Polygon.cpp with new implementation`

--- 18044번： Polygon/Polygon.cpp
+++ 18044번： Polygon/Polygon.cpp
@@ -1,11 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18044                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18044                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/13 17:25:25 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int z;
+    cin >> z;
+
+    while (z--) {
+        int n;
+        cin >> n;
+
+        vector<long long> l(n);
+        for (int i = 0; i < n; i++) {
+            cin >> l[i];
+        }
+
+        sort(l.begin(), l.end(), greater<long long>());
+        
+        long long sum = 0;
+        for (auto s : l) {
+            sum += s;
+        }
+
+        int current_n = n;
+        long long current_sum = sum;
+
+        while (current_n >= 3) {
+            long long longest_s = l[0];
+            long long tmp_sum = current_sum - longest_s;
+
+            if (longest_s < tmp_sum) {
+                cout << current_sum << "\n";
+                break;
+            } else {
+                current_sum -= l[0];
+                l.erase(l.begin());
+                current_n--;
+            }
+        }
+
+        if (current_n < 3) {
+            cout << "0\n";
+        }
+    }
+
+    return 0;
+}
