b7e9445a02e40f6168536d998e205cba7e51d47e
Leapense <iso9661@gmail.com>
2025-01-13 22:24:22 +0900

    `Added function expeditious_cubing to calculate the minimum time required to achieve a target score in a cubing problem.`

--- 18084번： Expeditious Cubing/Expeditious Cubing.py
+++ 18084번： Expeditious Cubing/Expeditious Cubing.py
@@ -1,11 +1,27 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 18084                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/18084                          #+#        #+#      #+#     #
-#    Solved: 2025/01/13 21:59:21 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def expeditious_cubing(t1, t2, t3, t4, target):
+    times = [t1, t2, t3, t4]
+    times.sort()
+
+    current_best_score = sum(times[1:]) / 3
+    if current_best_score <= target:
+        return "infinite"
+    
+    lo, hi = 1, 20
+    result = "impossible"
+
+    while hi - lo > 1e-6:
+        mid = (lo + hi) / 2
+        new_times = sorted(times + [mid])
+        new_score = sum(new_times[1:4]) / 3
+
+        if new_score <= target:
+            result = f"{mid:.2f}"
+            lo = mid
+        else:
+            hi = mid
+
+    return result
+
+t1, t2, t3, t4 = map(float, input().strip().split())
+target = float(input().strip())
+print(expeditious_cubing(t1, t2, t3, t4, target))
