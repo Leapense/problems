896b6f0fd38b84a640b36de1f707dbd46f37a16e
Leapense <iso9661@gmail.com>
2025-01-13 17:01:38 +0900

    18022번 문제 해결을 위한 코드 추가: Eeny Meeny 문제의 C++ 구현 및 README 문서 작성

--- 17982번： Climbing Stairs/Climbing Stairs.cpp
+++ 17982번： Climbing Stairs/Climbing Stairs.cpp
@@ -1,11 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 17982                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/17982                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/13 15:07:59 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, r, k;
+    cin >> n >> r >> k;
+    vector<vector<vector<int>>> dist(1001, vector<vector<int>>(2, vector<int>(1001, -1)));
+
+    queue<array<int, 3>> q;
+
+    int startVisited = (k == 0 ? 1 : 0);
+
+    dist[0][startVisited][0] = 0;
+    q.push({0, startVisited, 0});
+    int ans = INT_MAX;
+
+    while (!q.empty()) {
+        auto [f, v, s] = q.front();
+        q.pop();
+
+        int cost = dist[f][v][s];
+
+        if (f == r && v == 1 && s >= n) {
+            ans = min(ans, cost + r);
+            break;
+        }
+
+        for (int nf : {f - 1, f + 1}) {
+            if (nf < 0 || nf > 1000) continue;
+            int ns = (s < n ? s + 1 : n);
+            int nv = (v == 1 ? 1 : (nf == k ? 1 : 0));
+
+            if (dist[nf][nv][ns] == -1) {
+                dist[nf][nv][ns] = cost + 1;
+                q.push({nf, nv, ns});
+            }
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
