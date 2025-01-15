ddd02a0c90ca398a7a059d6fc06dfb49bc379979
Leapense <iso9661@gmail.com>
2025-01-15 19:06:56 +0900

    `Added transform_string function to swap J, O, I characters in input string`

--- 33163번： OIJ (OIJ)/OIJ (OIJ).py
+++ 33163번： OIJ (OIJ)/OIJ (OIJ).py
@@ -1,11 +1,15 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 33163                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/33163                          #+#        #+#      #+#     #
-#    Solved: 2025/01/15 18:45:36 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def transform_string():
+    N = int(input().strip())
+    S = input().strip()
+
+    transformation = {
+        'J' : 'O',
+        'O' : 'I',
+        'I' : 'J'
+    }
+
+    result = ''.join(transformation[char] for char in S)
+    print(result)
+
+if __name__ == '__main__':
+    transform_string()
