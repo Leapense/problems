6a2f87ce48d90b3d6fabdd24be7ad5c11b2d12df
Leapense <iso9661@gmail.com>
2025-01-15 20:47:51 +0900

    문제 18101의 C++ 솔루션 구현

--- 18101번： Правильный подмногоугольник/Правильный подмногоугольник.cpp
+++ 18101번： Правильный подмногоугольник/Правильный подмногоугольник.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18101                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18101                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/15 20:32:48 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long N;
+    cin >> N;
+
+    vector<long long> divisors;
+
+    if (N == 3) {
+        cout << 3 << "\n";
+        return 0;
+    }
+
+    long long limit = (long long)floor(sqrt((long double) N));
+
+    for (long long i = 1; i <= limit; i++) {
+        if (N % i == 0) {
+            if (i >= 3) {
+                divisors.push_back(i);
+            }
+
+            long long paired = N / i;
+            if (paired >= 3 && paired != i) {
+                divisors.push_back(paired);
+            }
+        }
+    }
+
+    if (divisors.empty()) {
+        cout << N << "\n";
+        return 0;
+    }
+
+    sort(divisors.begin(), divisors.end());
+    cout << divisors.front() << "\n";
+
+    return 0;
+}
