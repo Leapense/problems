85acb97c230aac8bb4a841eb1d7db22016b8661e
Leapense <iso9661@gmail.com>
2025-01-16 21:25:56 +0900

    `Refactored code for problem 18979: Triangle Partition`

--- 18979번： Triangle Partition/Triangle Partition.cpp
+++ 18979번： Triangle Partition/Triangle Partition.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18979                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18979                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/16 21:11:56 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Point {
+    long long x, y;
+    int index;
+};
+
+bool comparePoints(const Point &a, const Point &b) {
+    if (a.x != b.x) return a.x < b.x;
+    return a.y < b.y;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+
+        int total = 3 * n;
+        vector<Point> points(total);
+        for (int i = 0; i < total; ++i) {
+            cin >> points[i].x >> points[i].y;
+            points[i].index = i + 1;
+        }
+
+        sort(points.begin(), points.end(), comparePoints);
+
+        for (int i = 0; i < n; ++i) {
+            int start = 3 * i;
+            cout << points[start].index << " " << points[start + 1].index << " " << points[start + 2].index << "\n";
+        }
+    }
+
+    return 0;
+}
