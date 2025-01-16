6cff59a8b70382ef84134bdeeec5583904d20891
Leapense <iso9661@gmail.com>
2025-01-16 18:38:43 +0900

    `Refactored code for problem 18325, added includes and namespace, and implemented solution with sorting and dynamic assignment of liquid.`

--- 18325번： Pod starim krovovima/Pod starim krovovima.cpp
+++ 18325번： Pod starim krovovima/Pod starim krovovima.cpp
@@ -1,11 +1,57 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18325                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18325                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/15 21:41:46 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Glass {
+    int index;
+    long long Ti;
+    long long Zi;
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<Glass> glasses(N);
+
+    long long total_liquid = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> glasses[i].Ti >> glasses[i].Zi;
+        glasses[i].index = i;
+        total_liquid += glasses[i].Ti;
+    }
+
+    sort(glasses.begin(), glasses.end(), [&](const Glass &a, const Glass &b) -> bool {
+        if (a.Zi != b.Zi) {
+            return a.Zi > b.Zi;
+        }
+        return a.index < b.index;
+    });
+
+    vector<long long> final_amounts(N, 0);
+    long long remaining_liquid = total_liquid;
+    for (auto &glass : glasses) {
+        if (remaining_liquid == 0) {
+            break;
+        }
+
+        long long assign = min(glass.Zi, remaining_liquid);
+        final_amounts[glass.index] = assign;
+        remaining_liquid -= assign;
+    }
+
+    int used_glasses = 0;
+    for (int i = 0; i < N; i++) if (final_amounts[i] > 0) used_glasses++;
+    int empty_glasses = N - used_glasses;
+
+    cout << empty_glasses << "\n";
+    for (int i = 0; i < N; i++) {
+        cout << final_amounts[i];
+        if (i != N - 1) cout << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
