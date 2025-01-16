1f37286b2d50bb529754bcb352976ec32579701b
Leapense <iso9661@gmail.com>
2025-01-16 21:09:34 +0900

    `Refactored code for problem 18917 with improved performance and readability`

--- 18917번： 수열과 쿼리 38/수열과 쿼리 38.cpp
+++ 18917번： 수열과 쿼리 38/수열과 쿼리 38.cpp
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18917                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18917                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/16 21:03:52 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int M;
+    cin >> M;
+
+    unordered_map<int, int> count_map;
+    count_map.reserve(600000);
+    count_map[0] = 1;
+    ll total_sum = 0;
+    ll total_xor = 0;
+
+    for (int i = 0; i < M; i++) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            int x;
+            cin >> x;
+            count_map[x]++;
+            total_sum += x;
+            total_xor ^= x;
+        }
+        else if (type == 2) {
+            int x;
+            cin >> x;
+            count_map[x]--;
+            total_sum -= x;
+            total_xor ^= x;
+        }
+        else if (type == 3) {
+            cout << total_sum << "\n";
+        }
+        else if (type == 4) {
+            cout << total_xor << "\n";
+        }
+    }
+
+    return 0;
+}
