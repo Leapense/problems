2667ccac01450709a865bb7b654112987989c922
Leapense <iso9661@gmail.com>
2025-01-16 19:56:10 +0900

    `Refactored The Minions Quiz solution to use C++ and added input handling`

--- 18674번： The Minions Quiz/The Minions Quiz.cpp
+++ 18674번： The Minions Quiz/The Minions Quiz.cpp
@@ -1,11 +1,35 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18674                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18674                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/16 19:45:28 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+
+        int N = A + B + 1;
+        vector<ull> numbers(N);
+
+        for (int i = 0; i < N; i++) cin >> numbers[i];
+
+        ull current = numbers[0];
+
+        for (int i = 1; i <= A; i++) {
+            current &= numbers[i];
+        }
+        for (int i = A + 1; i < N; i++) {
+            current |= numbers[i];
+        }
+
+        cout << current << "\n";
+    }
+
+    return 0;
+}
