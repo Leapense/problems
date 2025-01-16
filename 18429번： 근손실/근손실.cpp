da7e454c25f5c0fce8b40d31a21c387cbbb73368
Leapense <iso9661@gmail.com>
2025-01-16 19:28:13 +0900

    `Added solution for problem 18429 using backtracking and dynamic programming`

--- 18429번： 근손실/근손실.cpp
+++ 18429번： 근손실/근손실.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18429                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18429                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/16 19:25:08 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, K;
+vector<int> A;
+int count_valid = 0;
+
+bool visited[8] = {false};
+
+void backtrack(int day, int current_weight) {
+    if (day == N) {
+        count_valid++;
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (!visited[i]) {
+            visited[i] = true;
+            int new_weight = current_weight + A[i] - K;
+
+            if (new_weight >= 500) {
+                backtrack(day + 1, new_weight);
+            }
+
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> N >> K;
+    A.resize(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    backtrack(0, 500);
+
+    cout << count_valid << endl;
+    return 0;
+}
