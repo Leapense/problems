4e9817fccf3073ddf1d186117ce136cf0a14bb5a
Leapense <iso9661@gmail.com>
2025-01-16 21:03:44 +0900

    `Added solution for problem 18729, Wizards Unite, in Wizards Unite.cpp`

--- 18729번： Wizards Unite/Wizards Unite.cpp
+++ 18729번： Wizards Unite/Wizards Unite.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 18729                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/18729                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/16 20:06:31 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int z;
+    cin >> z;
+
+    while (z--) {
+        int n, k;
+        cin >> n >> k;
+        vector<ll> t(n);
+        for (auto &x : t) cin >> x;
+
+        sort(t.begin(), t.end(), [&](const ll &a, const ll &b) -> bool {
+            return a > b;
+        });
+
+        ll max_silver = 0;
+        if (k > 0) {
+            max_silver = t[0];
+            for (int i = 1; i < k; ++i) {
+                max_silver = max(max_silver, t[i]);
+            }
+        }
+
+        ll sum_gold = 0;
+        for (int i = k; i < n; ++i) {
+            sum_gold += t[i];
+        }
+
+        ll total_time = max(sum_gold, max_silver);
+        cout << total_time << "\n";
+    }
+
+    return 0;
+}
