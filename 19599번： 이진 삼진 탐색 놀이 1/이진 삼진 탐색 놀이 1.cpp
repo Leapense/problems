46d5e5a83f0a349ebf254def7aa9eb0045dd9972
Leapense <iso9661@gmail.com>
2025-01-17 13:04:42 +0900

    문제 19599: 이진 삼진 탐색 놀이 1의 C++ 코드 구현 추가

--- 19599번： 이진 삼진 탐색 놀이 1/이진 삼진 탐색 놀이 1.cpp
+++ 19599번： 이진 삼진 탐색 놀이 1/이진 삼진 탐색 놀이 1.cpp
@@ -1,11 +1,77 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 19599                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/19599                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/17 12:59:14 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<int> B(N, 0);
+    vector<int> T(N, 0);
+
+    for (int i = 0; i < N; ++i) {
+        int left = 0, right = N - 1;
+        int count = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == i) {
+                B[i] = count;
+                break;
+            }
+            if (i < mid) {
+                count += 1;
+                right = mid - 1;
+            } else {
+                count += 1;
+                left = mid + 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; ++i) {
+        int left = 0, right = N - 1;
+        int count = 0;
+
+        while (left <= right) {
+            int delta = (right - left) / 3;
+            int left_third = left + delta;
+            int right_third = right - delta;
+
+            if (left_third == i) {
+                T[i] = count;
+                break;
+            }
+            if (right_third == i) {
+                T[i] = count + 1;
+                break;
+            }
+
+            count += 2;
+
+            if (i < left_third) {
+                right = left_third - 1;
+            } else if (i < right_third) {
+                left = left_third + 1;
+                right = right_third - 1;
+            } else {
+                left = right_third + 1;
+            }
+        }
+    }
+
+    int less = 0, equal = 0, greater = 0;
+
+    for (int i = 0; i < N; ++i) {
+        if (B[i] < T[i]) less += 1;
+        else if (B[i] == T[i]) equal += 1;
+        else greater += 1;
+    }
+
+    cout << less << "\n" << equal << "\n" << greater;
+
+    return 0;
+}
