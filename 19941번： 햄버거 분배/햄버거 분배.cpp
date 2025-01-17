5dc198b2cb6c0e1128553d8a7e074b915e9056ea
Leapense <iso9661@gmail.com>
2025-01-17 20:44:22 +0900

    `Refactored code for problem 19941 with improved readability and functionality`

--- 19941번： 햄버거 분배/햄버거 분배.cpp
+++ 19941번： 햄버거 분배/햄버거 분배.cpp
@@ -1,11 +1,35 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 19941                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/19941                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/17 20:25:14 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    string S;
+    cin >> S;
+
+    vector<int> people, hamburgers;
+
+    for (int i = 0; i < N; i++) {
+        if (S[i] == 'P') people.push_back(i + 1);
+        else if (S[i] == 'H') hamburgers.push_back(i + 1);
+    }
+
+    int i = 0, j = 0, count = 0;
+
+    while (i < people.size() && j < hamburgers.size()) {
+        if (abs(people[i] - hamburgers[j]) <= K) {
+            count++;
+            i++;
+            j++;
+        }
+        else if (hamburgers[j] < people[i] - K) {
+            j++;
+        } else {
+            i++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
