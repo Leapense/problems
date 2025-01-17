fdbad01f99a49e41f771ae86f6e2424314a6d17c
Leapense <iso9661@gmail.com>
2025-01-17 13:18:13 +0900

    `Added new code for problem 19637, replacing previous diff output with actual code`

--- 19637번： IF문 좀 대신 써줘/IF문 좀 대신 써줘.cpp
+++ 19637번： IF문 좀 대신 써줘/IF문 좀 대신 써줘.cpp
@@ -1,11 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 19637                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/19637                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/17 13:08:10 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<long long> upper_limits(N);
+    vector<string> titles(N);
+
+    for (int i = 0; i < N; ++i) {
+        cin >> titles[i] >> upper_limits[i];
+    }
+
+    for (int i = 0; i < M; ++i) {
+        long long power;
+        cin >> power;
+
+        int idx = lower_bound(upper_limits.begin(), upper_limits.end(), power) - upper_limits.begin();
+
+        cout << titles[idx] << "\n";
+    }
+
+    return 0;
+}
