f7cf4f17ac14abc56e72117ad9103098d56d2cea
Leapense <iso9661@gmail.com>
2025-01-17 19:50:49 +0900

    `Added solution for problem 19780, a C++ program that sorts an array and calculates the minimum number of elements to remove to make the product of the remaining elements not exceed the sum of the array elements.`

--- 19780번： Мерлин/Мерлин.cpp
+++ 19780번： Мерлин/Мерлин.cpp
@@ -1,11 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 19780                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/19780                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/17 19:34:52 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<long long> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    long long S = 0;
+
+    for (int i = 0; i < n; i++) {
+        S += a[i];
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (a[i - 1] * i <= S) ans = i;
+        else break;
+    }
+
+    cout << n - ans << "\n";
+
+    return 0;
+}
