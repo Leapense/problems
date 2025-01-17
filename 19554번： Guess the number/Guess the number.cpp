9af82583d3d107b7e4edac943300067b3243489e
Leapense <iso9661@gmail.com>
2025-01-17 12:59:06 +0900

    `Refactored Guess the Number problem solution to use binary search with a fixed number of queries.`

--- 19554번： Guess the number/Guess the number.cpp
+++ 19554번： Guess the number/Guess the number.cpp
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 19554                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/19554                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/17 12:07:23 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long N;
+    cin >> N;
+
+    long long low = 1;
+    long long high = N;
+    int query_count = 0;
+
+    const int MAX_QUERIES = 50;
+
+    while (low <= high && query_count < MAX_QUERIES) {
+        long long mid = low + (high - low) / 2;
+
+        cout << "? " << mid << "\n";
+        cout.flush();
+        query_count++;
+
+        int response;
+        cin >> response;
+
+        if (response == 0) {
+            cout << "= " << mid << "\n";
+            cout.flush();
+            return 0;
+        }
+        else if (response == -1) {
+            low = mid + 1;
+        }
+        else if (response == 1) {
+            high = mid - 1;
+        }
+        else {
+            cerr << "Unexpected response: " << response << "\n";
+            return 1;
+        }
+    }
+
+    cout << "= " << low << "\n";
+    cout.flush();
+
+    return 0;
+}
