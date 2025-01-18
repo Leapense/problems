b86b15124ee625e35db869b1fe0a1709a5e1c472
Leapense <iso9661@gmail.com>
2025-01-18 21:20:39 +0900

    `Refactored code for problem 20300 with improved solution`

--- 20300번： 서강근육맨/서강근육맨.cpp
+++ 20300번： 서강근육맨/서강근육맨.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20300                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20300                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 21:14:15 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<ll> t(N);
+    for (auto &x : t) cin >> x;
+
+    sort(t.begin(), t.end());
+    ll left = t.back();
+    ll right = t.back() * 2;
+
+    auto possible = [&](ll M) -> bool {
+        int start = 0, end = N - 1;
+        int cnt = 0;
+        while (start <= end) {
+            if (t[start] + t[end] <= M) {
+                start++;
+            }
+            end--;
+            cnt++;
+        }
+        return cnt <= (N + 1) / 2;
+    };
+
+    while (left < right) {
+        ll mid = left + (right - left) / 2;
+        if (possible(mid)) right = mid;
+        else left = mid + 1;
+    }
+
+    cout << left << "\n";
+    return 0;
+}
