978df9c785b9c615467f56b8b3938f74095e304a
Leapense <iso9661@gmail.com>
2025-01-18 22:01:56 +0900

    `Refactored code to correctly solve the problem, diff includes changes to main function and surrounding code`

--- 20309번： 트리플 소트/트리플 소트.cpp
+++ 20309번： 트리플 소트/트리플 소트.cpp
@@ -3,36 +3,62 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
 
     int N;
     cin >> N;
 
-    vector<int> A(N);
-    for (auto &x : A) cin >> x;
-
+    vector<int> A(N + 1);
     vector<int> pos(N + 1);
-    for (int i = 0; i < N; i++) pos[A[i]] = i;
-    for (int i = 0; i < N - 2; i++) {
-        if (A[i] == i + 1) continue;
-        int x = i + 1;
-        int j = pos[x];
-        if (j < i + 2) {
-            cout << "NO";
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+        pos[A[i]] = i;
+    }
+
+    bool possible = true;
+    for (int i = 1; i <= N - 2; i++) {
+        if (A[i] == i) continue;
+
+        int current_pos = pos[i];
+        while (current_pos > i) {
+            if (current_pos - 2 < i) {
+                possible = false;
+                break;
+            }
+
+            int k = current_pos - 2;
+            swap(A[k], A[k + 2]);
+            swap(A[k + 1], A[k + 2]);
+
+            pos[A[k]] = k;
+            pos[A[k + 1]] = k + 1;
+            pos[A[k + 2]] = k + 2;
+            current_pos -= 2;
+        }
+
+        if (!possible) break;
+    }
+
+    if (possible) {
+        if (A[N - 1] == N - 1 && A[N] == N) {
+            cout << "YES";
             return 0;
         }
 
-        while (j > i + 1) {
-            int k = j - 2;
-            reverse(A.begin() + k, A.begin() + k + 3);
-            pos[A[k]] = k;
-            pos[A[k+1]] = k+1;
-            pos[A[k+2]] = k+2;
-            j -= 2;
+        if (N >= 3 && A[N - 2] == N - 2 && A[N - 1] == N && A[N] == N - 1) {
+            swap(A[N - 2], A[N]);
+            swap(A[N - 2], A[N]);
+            swap(A[N - 1], A[N]);
+
+            if (A[N - 2] == N - 2 && A[N - 1] == N - 1 && A[N] == N) {
+                cout << "YES";
+                return 0;
+            }
         }
+
+        cout << "NO";
     }
 
-    if (A[N-2] == N-1 && A[N-1] == N) cout << "YES";
     else cout << "NO";
 
     return 0;
