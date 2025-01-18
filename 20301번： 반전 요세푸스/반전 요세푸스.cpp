be165eb60ac995020e8cd7b60118eadce40e74bc
Leapense <iso9661@gmail.com>
2025-01-18 21:34:34 +0900

    `Added solution for problem 20301, including code and commit message in the diff`

--- 20301번： 반전 요세푸스/반전 요세푸스.cpp
+++ 20301번： 반전 요세푸스/반전 요세푸스.cpp
@@ -1,11 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20301                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20301                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 21:21:47 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, K, M;
+    cin >> N >> K >> M;
+
+    list<int> people;
+
+    for (int i = 1; i <= N; i++) people.push_back(i);
+    vector<int> result;
+
+    int direction = 1;
+    int removed = 0;
+
+    auto it = people.begin();
+    for (int i = 1; i < K; i++) {
+        if (it == people.end()) it = people.begin();
+        ++it;
+    }
+
+    while (!people.empty()) {
+        result.push_back(*it);
+        it = people.erase(it);
+        removed++;
+        if (people.empty()) break;
+        if (removed % M == 0) {
+            direction *= -1;
+        }
+        if (direction == 1) {
+            if (it == people.end()) it = people.begin();
+            for (int i = 1; i < K; i++) {
+                if (it == people.end()) it = people.begin();
+                ++it;
+            }
+        } else {
+            if (it == people.begin()) it = people.end();
+            for (int i = 1; i < K; i++) {
+                if (it == people.begin()) it = people.end();
+                --it;
+            }
+
+            if (it == people.end()) it = people.begin();
+        }
+    }
+
+    for (auto num : result) cout << num << "\n";
+
+    return 0;
+}
