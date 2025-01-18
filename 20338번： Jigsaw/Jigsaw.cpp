5e6027b807cbc418f5c87979558885d91f9eae86
Leapense <iso9661@gmail.com>
2025-01-18 22:42:57 +0900

    `Added solution for problem 20338, including necessary includes and main function implementation.`

--- 20338번： Jigsaw/Jigsaw.cpp
+++ 20338번： Jigsaw/Jigsaw.cpp
@@ -1,11 +1,47 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20338                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20338                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 22:25:48 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll c, e, m;
+    cin >> c >> e >> m;
+
+    if (c != 4) {
+        cout << "impossible";
+        return 0;
+    }
+
+    if (m > 0) {
+        ll sqrtm = sqrt((double)m);
+
+        for (ll d = 1; d <= sqrtm; d++) {
+            if (m % d == 0) {
+                ll h = d + 2;
+                ll w = m / d + 2;
+                if (w >= h && 2 <= h && 2 <= w && 2 * w + 2 * h - 8 == e) {
+                    cout << w << " " << h;
+
+                    return 0;
+                }
+            }
+        }
+
+        cout << "impossible";
+    } else {
+        if ((e + 4) % 2 != 0) {
+            cout << "impossible";
+            return 0;
+        }
+
+        ll w = (e + 4) / 2;
+        if (w >= 2) {
+            cout << w << " " << 2;
+        } else {
+            cout << "impossible";
+        }
+    }
+
+    return 0;
+}
