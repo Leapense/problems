08f8e423112fa63487fc8a191accd023bd3645b3
Leapense <iso9661@gmail.com>
2025-01-18 22:06:44 +0900

    `Refactored code for 20309 problem, updated cin.tie to nullptr`

--- 20309번： 트리플 소트/트리플 소트.cpp
+++ 20309번： 트리플 소트/트리플 소트.cpp
@@ -3,63 +3,23 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
 
     int N;
     cin >> N;
 
-    vector<int> A(N + 1);
-    vector<int> pos(N + 1);
-    for (int i = 1; i <= N; i++) {
-        cin >> A[i];
-        pos[A[i]] = i;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
     }
 
-    bool possible = true;
-    for (int i = 1; i <= N - 2; i++) {
-        if (A[i] == i) continue;
-
-        int current_pos = pos[i];
-        while (current_pos > i) {
-            if (current_pos - 2 < i) {
-                possible = false;
-                break;
-            }
-
-            int k = current_pos - 2;
-            swap(A[k], A[k + 2]);
-            swap(A[k + 1], A[k + 2]);
-
-            pos[A[k]] = k;
-            pos[A[k + 1]] = k + 1;
-            pos[A[k + 2]] = k + 2;
-            current_pos -= 2;
-        }
-
-        if (!possible) break;
-    }
-
-    if (possible) {
-        if (A[N - 1] == N - 1 && A[N] == N) {
-            cout << "YES";
+    for (int i = 0; i < N; i++) {
+        if(((i + 1) & 1) != (arr[i] & 1)) {
+            cout << "NO\n";
             return 0;
         }
-
-        if (N >= 3 && A[N - 2] == N - 2 && A[N - 1] == N && A[N] == N - 1) {
-            swap(A[N - 2], A[N]);
-            swap(A[N - 2], A[N]);
-            swap(A[N - 1], A[N]);
-
-            if (A[N - 2] == N - 2 && A[N - 1] == N - 1 && A[N] == N) {
-                cout << "YES";
-                return 0;
-            }
-        }
-
-        cout << "NO";
     }
 
-    else cout << "NO";
-
+    cout << "YES\n";
     return 0;
 }
