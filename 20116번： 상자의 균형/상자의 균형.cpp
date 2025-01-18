3937ec9c0d181b5d988c65aa2b7481cf1d238e2e
Leapense <iso9661@gmail.com>
2025-01-18 16:29:57 +0900

    `Added code for problem 20116, includes new file with solution and diff from empty file`

--- 20116번： 상자의 균형/상자의 균형.cpp
+++ 20116번： 상자의 균형/상자의 균형.cpp
@@ -1,11 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20116                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20116                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 16:02:19 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    ll L;
+
+    cin >> n >> L;
+    vector<ll> x(n);
+
+    for (auto &e : x) cin >> e;
+
+    vector<ll> suffix_sum(n + 1, 0);
+    for (int i = n - 1; i >= 0; i--) suffix_sum[i] = suffix_sum[i + 1] + x[i];
+    bool stable = true;
+
+    for (int i = 0; i < n - 1 && stable; i++) {
+        ll sum = suffix_sum[i + 1];
+        int cnt = n - i - 1;
+
+        ll lower = (x[i] - L) * cnt;
+        ll upper = (x[i] + L) * cnt;
+
+        if (!(sum > lower && sum < upper)) {
+            stable = false;
+        }
+    }
+
+    cout << (stable ? "stable" : "unstable");
+
+    return 0;
+}
