1066c18f16c2d9326ca01902fb642a1ffd127bf2
Leapense <iso9661@gmail.com>
2025-01-18 15:10:00 +0900

    `Added code for problem 20011, including input parsing and unique pattern counting.`

--- 20011번： Рекламный щит/Рекламный щит.cpp
+++ 20011번： Рекламный щит/Рекламный щит.cpp
@@ -1,11 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20011                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20011                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 14:58:14 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k, n, m;
+    cin >> k >> n >> m;
+
+    vector<vector<string>> hieroglyphs(k, vector<string>(n));
+    for (int h = 0; h < k; h++) {
+        for (int r = 0; r < n; r++) {
+            cin >> hieroglyphs[h][r];
+        }
+    }
+
+    vector<string> patterns(n * m, "");
+    for (int h = 0; h < k; h++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                patterns[i * m + j] += (hieroglyphs[h][i][j] == '*' ? '1' : '0');
+            }
+        }
+    }
+    
+    unordered_set<string> unique_patterns(patterns.begin(), patterns.end());
+    cout << unique_patterns.size() << endl;
+
+    return 0;
+}
