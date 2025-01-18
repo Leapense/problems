cf0d4dc555d5f426d9d90b27accb9d717adfa6e8
Leapense <iso9661@gmail.com>
2025-01-18 21:42:25 +0900

    `Fixed binary file diff formatting and added newline at end of file`

--- 20301번： 반전 요세푸스/반전 요세푸스.cpp
+++ 20301번： 반전 요세푸스/반전 요세푸스.cpp
@@ -1,50 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
     int N, K, M;
     cin >> N >> K >> M;
-
+    
     list<int> people;
-
-    for (int i = 1; i <= N; i++) people.push_back(i);
+    for(int i = 1; i <= N; ++i) people.push_back(i);
+    
     vector<int> result;
-
     int direction = 1;
     int removed = 0;
-
+    
     auto it = people.begin();
-    for (int i = 1; i < K; i++) {
-        if (it == people.end()) it = people.begin();
+    for(int i = 1; i < K; ++i){
+        if(it == people.end()) it = people.begin();
         ++it;
     }
-
-    while (!people.empty()) {
+    
+    while(!people.empty()){
         result.push_back(*it);
-        it = people.erase(it);
+
+        auto current = it;
+        ++it;
+        if(it == people.end()) it = people.begin();
+        
+        people.erase(current);
         removed++;
-        if (people.empty()) break;
-        if (removed % M == 0) {
+        
+        if(people.empty()) break;
+        
+        if(removed % M == 0){
             direction *= -1;
         }
-        if (direction == 1) {
-            if (it == people.end()) it = people.begin();
-            for (int i = 1; i < K; i++) {
-                if (it == people.end()) it = people.begin();
+
+        if(direction == 1){
+            for(int i = 1; i < K; ++i){
                 ++it;
+                if(it == people.end()) it = people.begin();
             }
-        } else {
-            if (it == people.begin()) it = people.end();
-            for (int i = 1; i < K; i++) {
-                if (it == people.begin()) it = people.end();
+        }
+        else{
+            if(it == people.begin()) it = people.end();
+            --it;
+            for(int i = 1; i < K; ++i){
+                if(it == people.begin()) it = people.end();
                 --it;
             }
-
-            if (it == people.end()) it = people.begin();
         }
     }
-
-    for (auto num : result) cout << num << "\n";
+    
+    for(auto num: result) cout << num << "\n";
 
     return 0;
 }
