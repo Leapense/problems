547e7a18d82673cc1b3be87434fdc1dbd6fee6ac
Leapense <iso9661@gmail.com>
2025-01-18 17:49:52 +0900

    `Added solution for problem 20126, including code and commit message in the diff`

--- 20126번： 교수님의 기말고사/교수님의 기말고사.cpp
+++ 20126번： 교수님의 기말고사/교수님의 기말고사.cpp
@@ -1,11 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20126                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20126                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 17:38:52 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    long long N, M, S;
+    cin >> N >> M >> S;
+
+    vector<pair<long long, long long>> exams(N);
+    for (auto &p : exams) cin >> p.first >> p.second;
+
+    sort(exams.begin(), exams.end());
+
+    long long prev_end = 0;
+    for (auto &[x, y] : exams) {
+        if (x - prev_end >= M) {
+            cout << prev_end;
+            return 0;
+        }
+        prev_end = max(prev_end, x + y);
+    }
+
+    if (S - prev_end >= M) {
+        cout << prev_end;
+    }
+    else {
+        cout << -1;
+    }
+
+    return 0;
+}
