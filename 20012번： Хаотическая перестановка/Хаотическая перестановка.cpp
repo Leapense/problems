246f10784a658d6697ac0cdaca211945fa56ca2a
Leapense <iso9661@gmail.com>
2025-01-18 15:28:46 +0900

    `Added code to solve problem 20012, includes main function and comments`

--- 20012번： Хаотическая перестановка/Хаотическая перестановка.cpp
+++ 20012번： Хаотическая перестановка/Хаотическая перестановка.cpp
@@ -1,11 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20012                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20012                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 15:18:02 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> perm(n);
+
+    for (auto &x : perm) cin >> x;
+
+    vector<int> swaps;
+
+    bool changed = true;
+    while (changed && swaps.size() <= n) {
+        changed = false;
+        for (int i = 0; i <= n - 3; i++) {
+            if ((perm[i] < perm[i + 1] && perm[i + 1] < perm[i + 2]) || (perm[i] > perm[i + 1] && perm[i + 1] > perm[i + 2])) {
+                swap(perm[i + 1], perm[i + 2]);
+                swaps.push_back(i + 2);
+                changed = true;
+                if (swaps.size() > n) break;
+            }
+        }
+    }
+
+    bool chaotic = true;
+
+    for (int i = 0; i <= n - 3; i++) {
+        if ((perm[i] < perm[i + 1] && perm[i + 1] < perm[i + 2]) || (perm[i] > perm[i + 1] && perm[i + 1] > perm[i + 2])) {
+            chaotic = false;
+            break;
+        }
+    }
+
+    if (chaotic && swaps.size() <= n) {
+        cout << swaps.size() << "\n";
+        for (auto x : swaps) cout << x << " ";
+    } else {
+        cout << -1;
+    }
+
+    return 0;
+}
