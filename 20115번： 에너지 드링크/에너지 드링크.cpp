eaca1d143c258079d6ec145dac81e162dc3143d0
Leapense <iso9661@gmail.com>
2025-01-18 15:57:06 +0900

    `Added code for problem 20115 with diff from a to b`

--- 20115번： 에너지 드링크/에너지 드링크.cpp
+++ 20115번： 에너지 드링크/에너지 드링크.cpp
@@ -1,11 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20115                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20115                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/18 15:37:38 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<long long> x(N);
+    long long total = 0;
+    long long max_x = 0;
+
+    for (int i = 0; i < N; i++) {
+        cin >> x[i];
+        total += x[i];
+        if (x[i] > max_x) max_x = x[i];
+    }
+    double final_amount = max_x + (double)(total - max_x) / 2.0;
+
+    cout << fixed << setprecision(10) << final_amount << endl;
+
+    return 0;
+}
