46f52f083ccd8bb46e26d2910307081cdeba550b
Leapense <iso9661@gmail.com>
2025-01-19 18:31:45 +0900

    `Refactored solution for problem 21282, updated code with new implementation`

--- 21282번： Kulramen/Kulramen.py
+++ 21282번： Kulramen/Kulramen.py
@@ -1,11 +1,31 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21282                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21282                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 18:22:22 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+
+    input = sys.stdin.readline
+    R = int(input().strip())
+    Ls = []
+    Rs = []
+
+    for _ in range(R):
+        l, r = map(int, input().split())
+        Ls.append(l)
+        Rs.append(r)
+
+    N = int(input().strip())
+    A = [Ls[i] + Rs[i] for i in range(R)]
+    base = [A[i] + 1 for i in range(R)]
+    value = 0
+    mult = 1
+
+    for i in range(R - 1, -1, -1):
+        value += Ls[i] * mult
+        mult *= base[i]
+    value = (value + N) % mult
+    newL = [0] * R
+    for i in range(R - 1, -1, -1):
+        newL[i] = value % base[i]
+        value //= base[i]
+    for i in range(R):
+        print(newL[i], A[i] - newL[i])
+
+solve()
