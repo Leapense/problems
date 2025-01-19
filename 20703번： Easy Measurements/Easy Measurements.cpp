9c135a35be0549ccae6e565e70cbb756b0354f68
Leapense <iso9661@gmail.com>
2025-01-19 14:17:25 +0900

    `Added solution for problem 20703, Easy Measurements, in C++`

--- 20703번： Easy Measurements/Easy Measurements.cpp
+++ 20703번： Easy Measurements/Easy Measurements.cpp
@@ -1,11 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20703                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20703                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/19 14:03:32 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        ll b, d;
+        cin >> b >> d;
+
+        ll g = gcd(b, d);
+        ll t = b * g - 1;
+        ll res = t / d;
+        
+        cout << res << "\n";
+    }
+
+    return 0;
+}
