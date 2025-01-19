7173f534c500773878fb6a8beef877ae4e36b05e
Leapense <iso9661@gmail.com>
2025-01-19 17:51:36 +0900

    Add main function and implementation for problem 21149: Unread Messages

--- 21149번： Unread Messages/Unread Messages.py
+++ 21149번： Unread Messages/Unread Messages.py
@@ -1,11 +1,20 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21149                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21149                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 17:45:21 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.read().split()
+    n = int(input[0])
+    m = int(input[1])
+    senders = list(map(int, input[2:2+m]))
+    total_messages = 0
+    total_last_sent_sum = 0
+    last_sent = {}
+    for i, s in enumerate(senders, 1):
+        total_messages += 1
+        prev = last_sent.get(s, 0)
+        total_last_sent_sum += i - prev
+        last_sent[s] = i
+        total_unread = n * total_messages - total_last_sent_sum
+        print(total_unread)
+
+if __name__ == "__main__":
+    main()
