26ee24ecd09718db3587b91e29b66923f4487fce
Leapense <iso9661@gmail.com>
2025-01-19 20:23:00 +0900

    `Refactored phi function to inline calculations`

--- 21495번： Кольцевая линия/Кольцевая линия.py
+++ 21495번： Кольцевая линия/Кольцевая линия.py
@@ -1,18 +1,15 @@
 import sys
 n = int(sys.stdin.readline())
+r = n
+x = n
+p = 2
 
-def phi(x):
-    r = x
-    p = 2
-
-    while p * p <= x:
-        if x % p == 0:
-            while x % p == 0:
-                x //= p
-            r -= r // p
-        p += 1 if p == 2 else 2
-    if x > 1:
-        r -= r // x
-    return r
-
-print(n * phi(n))
+while p * p <= x:
+    if x % p == 0:
+        while x % p == 0:
+            x //= p
+        r -= r // p
+    p += 1 if p == 2 else 2
+if x > 1:
+    r -= r // x
+print(n * r)
