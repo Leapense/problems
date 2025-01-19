db701d66266e2f2d82b8c021036d1a936562b0b0
Leapense <iso9661@gmail.com>
2025-01-19 17:35:43 +0900

    `Added function to find earliest time to turn off lights in problem 21113`

--- 21113번： Returning Lights To Box/Returning Lights To Box.py
+++ 21113번： Returning Lights To Box/Returning Lights To Box.py
@@ -1,11 +1,28 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21113                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21113                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 17:27:02 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def earliest_time_to_off(N, M, a, b):
+    count = sum(a)
+    if count == 0:
+        return 0
+    
+    c = a.copy()
+    for t in range(1, M + 1):
+        i = b[t - 1] - 1
+        if c[i] == 1:
+            count -= 1
+        else:
+            count += 1
+
+        c[i] ^= 1
+        if count <= t:
+            return t
+        
+    return count
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    N = int(data[0])
+    M = int(data[1])
+    a = list(map(int, data[2:2+N]))
+    b = list(map(int, data[2+N:2+N+M]))
+    print(earliest_time_to_off(N, M, a, b))
