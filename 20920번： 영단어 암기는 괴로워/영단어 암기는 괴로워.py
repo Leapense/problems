640808a6a14bf42d7e7a6efcdd5e280c64335886
Leapense <iso9661@gmail.com>
2025-01-19 16:05:31 +0900

    `Refactor: Update code to solve problem 20920, add import statements and main function`

--- 20920번： 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 20920번： 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,11 +1,18 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 20920                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/20920                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 15:57:54 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+from collections import defaultdict
+
+def main():
+    input = sys.stdin.readline
+    N, M = map(int, input().split())
+    word_count = defaultdict(int)
+
+    for _ in range(N):
+        word = input().rstrip()
+        if len(word) >= M:
+            word_count[word] += 1
+    words = list(word_count.keys())
+    words.sort(key=lambda x: (-word_count[x], -len(x), x))
+    print('\n'.join(words))
+
+if __name__ == '__main__':
+    main()
