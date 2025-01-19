486d489b6ed08fb4eac2a59190a23caad3021756
Leapense <iso9661@gmail.com>
2025-01-19 18:52:30 +0900

    `Added solution for problem 21311: Evenly Separated Strings`

--- 21311번： Evenly Separated Strings/Evenly Separated Strings.py
+++ 21311번： Evenly Separated Strings/Evenly Separated Strings.py
@@ -1,11 +1,18 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21311                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21311                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 18:40:28 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    s = input().strip()
+    positions = [[] for _ in range(26)]
+    for i, ch in enumerate(s):
+        positions[ord(ch) - ord('a')].append(i)
+
+    for p in positions:
+        if len(p) > 2:
+            print("NO")
+            return
+        if len(p) == 2:
+            if (p[1] - p[0]) % 2 == 0:
+                print("NO")
+                return
+            
+    print("YES")
+
+solve()
