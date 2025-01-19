128eac7bbe0c64d6e9d6acb274c8afd07a3a72c7
Leapense <iso9661@gmail.com>
2025-01-19 15:34:15 +0900

    `Added solution for problem 20865 in C++`

--- 20865번： Byäldsten/Byäldsten.cpp
+++ 20865번： Byäldsten/Byäldsten.cpp
@@ -1,11 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20865                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20865                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/19 15:25:16 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Person {
+    long long f;
+    long long d;
+    int idx;
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+
+    vector<Person> people(n);
+    for (int i = 0; i < n; i++) cin >> people[i].f >> people[i].d, people[i].idx = i;
+    sort(people.begin(), people.end(), [&](const Person& a, const Person& b) -> bool {
+        return a.f < b.f;
+    });
+
+    vector<long long> counts(n, 0);
+    long long prev_d = -1;
+
+    for (auto &p : people) {
+        long long start = max(p.f, prev_d);
+        long long end = p.d;
+
+        if (start < end) counts[p.idx] += end - start;
+        prev_d = max(prev_d, end);
+    }
+
+    for (auto c : counts) cout << c << "\n";
+
+    return 0;
+}
