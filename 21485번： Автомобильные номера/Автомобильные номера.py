0ffd7e90ea14ce475da50ff22886cd5dda103ea7
Leapense <iso9661@gmail.com>
2025-01-19 19:58:06 +0900

    `Refactored code for problem 21485 with improved solution`

--- 21485번： Автомобильные номера/Автомобильные номера.py
+++ 21485번： Автомобильные номера/Автомобильные номера.py
@@ -1,11 +1,16 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21485                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21485                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 19:48:18 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import itertools
+s = input().strip()
+
+letters = [s[i] for i in [0,4,5]]
+digits = [s[i] for i in [1,2,3]]
+letter_perms = set(itertools.permutations(letters))
+digit_perms = set(itertools.permutations(digits))
+results = []
+
+for lp in letter_perms:
+    for dp in digit_perms:
+        results.append(lp[0] + dp[0] + dp[1] + dp[2] + lp[1] + lp[2])
+
+print(len(results))
+for r in results:
+    print(r)
