25f9336d8c23777b06d3ed19aca217da24908df9
Leapense <iso9661@gmail.com>
2025-01-19 19:11:55 +0900

    Add initial implementation and README for problem 21427: Красивые номера

--- 21316번： 스피카/스피카.py
+++ 21316번： 스피카/스피카.py
@@ -1,11 +1,14 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21316                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21316                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 19:01:00 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+g = {i: set() for i in range(1, 13)}
+for _ in range(12):
+    x, y = map(int, sys.stdin.readline().split())
+    g[x].add(y)
+    g[y].add(x)
+
+d = {i: len(g[i]) for i in range(1, 13)}
+for i in range(1, 13):
+    s = sorted(d[n] for n in g[i])
+    if d[i] == 3 and s == [1, 2, 3]:
+        print(i)
+        break
