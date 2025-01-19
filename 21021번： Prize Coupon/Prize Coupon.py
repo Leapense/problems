7f919cc313fca0aabe6c34045b01438ce72a05d7
Leapense <iso9661@gmail.com>
2025-01-19 17:23:05 +0900

    `Added import statement and main function for problem 21021`

--- 21021번： Prize Coupon/Prize Coupon.py
+++ 21021번： Prize Coupon/Prize Coupon.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21021                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21021                          #+#        #+#      #+#     #
-#    Solved: 2025/01/19 16:09:02 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.read
+    data = input().split()
+    N = int(data[0])
+    C = list(map(int, data[1:N+1]))
+    covered = [False] * N
+
+    for i in range(N - 1, -1, -1):
+        if not covered[i]:
+            if i < N - 1 and C[i + 1] > 0:
+                C[i + 1] -= 1
+                covered[i] = True
+            elif C[i] > 0:
+                C[i] -= 1
+                covered[i] = True
+            elif i > 0 and C[i - 1] > 0:
+                C[i - 1] -= 1
+                covered[i] = True
+    print(sum(covered))
+
+if __name__ == '__main__':
+    main()
