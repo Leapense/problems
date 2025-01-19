71833c9ea477b7d86507c0fe9f6ce20e4949fc98
Leapense <iso9661@gmail.com>
2025-01-19 14:54:33 +0900

    `Refactored Skolavslutningen problem solution with improved code organization and efficiency.`

--- 20841번： Skolavslutningen/Skolavslutningen.cpp
+++ 20841번： Skolavslutningen/Skolavslutningen.cpp
@@ -1,11 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 20841                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/20841                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/19 14:41:46 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    vector<string> grid(N);
+    for (auto &s : grid) cin >> s;
+
+    int total = K + M;
+    vector<vector<int>> adj(total, vector<int>());
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            int class_idx = grid[i][j] - 'A';
+            int col_idx = K + j;
+            adj[class_idx].push_back(col_idx);
+            adj[col_idx].push_back(class_idx);
+        }
+    }
+
+    vector<bool> visited(total, false);
+    int components = 0;
+
+    for (int i = 0; i < total; i++) {
+        if (!visited[i]) {
+            components++;
+            queue<int> q;
+            q.push(i);
+
+            visited[i] = true;
+
+            while(!q.empty()) {
+                int u = q.front(); q.pop();
+                for (auto &v : adj[u]) {
+                    if (!visited[v]) {
+                        visited[v] = true;
+                        q.push(v);
+                    }
+                }
+            }
+        }
+    }
+
+    cout << components << endl;
+    return 0;
+}
