a9baf56bcc75823e47963c60925962f3bee0a142
Leapense <iso9661@gmail.com>
2025-01-20 16:50:51 +0900

    `Added solution for problem 21979 using dynamic programming and memoization`

--- 21979번： 16진수 쪼개기/16진수 쪼개기.py
+++ 21979번： 16진수 쪼개기/16진수 쪼개기.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21979                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21979                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 16:36:01 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+from functools import lru_cache
+
+def main():
+    T = int(sys.stdin.readline())
+    for _ in range(T):
+        S = sys.stdin.readline().strip()
+        n = len(S)
+        @lru_cache(None)
+        def dfs(i, prev):
+            if i == n:
+                return 1
+            total = 0
+
+            for j in range(i + 1, n + 1):
+                substr = S[i:j]
+                current = int(substr, 16)
+                if prev is None or current >= prev:
+                    total += dfs(j, current)
+            return total
+        print(dfs(0, None))
+
+if __name__ == "__main__":
+    main()
