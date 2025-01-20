6b5d822abaa07258a3749ee2a47991c37b3bc977
Leapense <iso9661@gmail.com>
2025-01-20 21:55:41 +0900

    `Refactored Moonlight Farm problem solution to use C++ and added input validation`

--- 22542번： Moonlight Farm/Moonlight Farm.cpp
+++ 22542번： Moonlight Farm/Moonlight Farm.cpp
@@ -1,11 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 22542                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/22542                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/20 21:42:06 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Crop {
+    string name;
+    double efficiency;
+};
+
+int main() {
+    int N;
+    while (cin >> N && N != 0) {
+        vector<Crop> crops;
+        for (int i = 0; i < N; i++) {
+            string Li;
+            int Pi, Ai, Bi, Ci, Di, Ei, Fi, Si, Mi;
+            cin >> Li >> Pi >> Ai >> Bi >> Ci >> Di >> Ei >> Fi >> Si >> Mi;
+
+            long long total_revenue = (long long)Fi * Si * Mi - Pi;
+            long long total_time = Ai + Bi + Ci + Di + Ei + (Mi - 1) * (Di + Ei);
+            double efficiency = (double)total_revenue / total_time;
+            crops.push_back(Crop{Li, efficiency});
+        }
+        sort(crops.begin(), crops.end(), [&](const Crop &a, const Crop &b) -> bool {
+            if (abs(a.efficiency - b.efficiency) > 1e-9) {
+                return a.efficiency > b.efficiency;
+            }
+            return a.name < b.name;
+        });
+        for (auto &c : crops) cout << c.name << "\n";
+        cout << "#\n";
+    }
+
+    return 0;
+}
