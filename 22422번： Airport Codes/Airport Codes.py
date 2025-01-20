e6c7460af7ef572b6d5e97672054e0f7721a4dda
Leapense <iso9661@gmail.com>
2025-01-20 20:09:57 +0900

    추가: 22422번 문제 '공항 코드'의 코드 구현 및 메인 함수 추가

--- 22422번： Airport Codes/Airport Codes.py
+++ 22422번： Airport Codes/Airport Codes.py
@@ -1,11 +1,46 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 22422                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/22422                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 20:01:09 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def extract_code(name, k):
+    code = []
+    code.append(name[0])
+    for i in range(1, len(name)):
+        if name[i-1] in "aiueo":
+            code.append(name[i])
+    return "".join(code)[:k]
+
+def is_unique_code(names, k):
+    seen = set()
+    for name in names:
+        code = extract_code(name, k)
+        if code in seen:
+            return False
+        seen.add(code)
+    return True
+
+def find_minimum_k(names):
+    for k in range(1, 51):
+        if is_unique_code(names, k):
+            return k
+    return -1
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    i = 0
+    results = []
+    while i < len(data):
+        if data[i] == "0":
+            break
+    
+        n = int(data[i])
+        names = data[i+1:i+1+n]
+        
+        result = find_minimum_k(names)
+        results.append(result)
+
+        i += 1 + n
+    for res in results:
+        print(res)
+
+if __name__ == "__main__":
+    main()
