6b2cf8d86c6e5ee0fc3fb866f0242d85594ec217
Leapense <iso9661@gmail.com>
2025-01-20 15:37:25 +0900

    `Added code for problem 21764: Histogram Sequence 2`

--- 21764번： Histogram Sequence 2/Histogram Sequence 2.py
+++ 21764번： Histogram Sequence 2/Histogram Sequence 2.py
@@ -1,11 +1,17 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21764                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21764                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 15:12:23 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+input = sys.stdin.readline
+
+mod = 10**9 + 7
+
+N = int(input())
+h = list(map(int, input().split()))
+
+if N == 1:
+    print(h[0] % mod)
+else:
+    ans = 1
+    ans = (ans * min(h[0], h[1])) % mod
+    for i in range(1, N-1):
+        ans = (ans * min(min(h[i-1], h[i]), min(h[i], h[i+1]))) % mod
+    ans = (ans * min(h[N-2], h[N-1])) % mod
+    print(ans % mod)
