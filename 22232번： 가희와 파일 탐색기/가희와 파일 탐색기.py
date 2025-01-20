3a4a78597ee64a4b82322602724953f9981bfcca
Leapense <iso9661@gmail.com>
2025-01-20 17:43:09 +0900

    `Refactor diff: Add Python script to sort files based on name and extension`

--- 22232번： 가희와 파일 탐색기/가희와 파일 탐색기.py
+++ 22232번： 가희와 파일 탐색기/가희와 파일 탐색기.py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 22232                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/22232                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 17:40:29 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    N, M = map(int, sys.stdin.readline().split())
+    files = []
+
+    for _ in range(N):
+        full_name = sys.stdin.readline().strip()
+        filename, extension = full_name.split('.')
+        files.append((filename, extension, full_name))
+    recognized = set()
+    for _ in range(M):
+        ext = sys.stdin.readline().strip()
+        recognized.add(ext)
+
+    files.sort(key=lambda x: (x[0], 0 if x[1] in recognized else 1, x[1]))
+    for file in files:
+        print(file[2])
+
+if __name__ == "__main__":
+    main()
