666521ef0e25fd705c82ffc397747e8bab8f0716
Leapense <iso9661@gmail.com>
2025-01-20 16:04:05 +0900

    `Refactor code for problem 21919 with sieve function and main logic`

--- 21919번： 소수 최소 공배수/소수 최소 공배수.py
+++ 21919번： 소수 최소 공배수/소수 최소 공배수.py
@@ -1,11 +1,26 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21919                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21919                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 15:56:42 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys, math
+
+def sieve(n):
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(math.isqrt(n)) + 1):
+        if sieve[i]:
+            sieve[i*i:n+1:i] = [False] * len(range(i*i, n+1, i))
+    return sieve
+
+def main():
+    sieve_list = sieve(1000000)
+    data = list(map(int, sys.stdin.read().split()))
+    N = data[0]
+    A = data[1:]
+    primes = set(x for x in A if sieve_list[x])
+    if not primes:
+        print(-1)
+    else:
+        lcm = 1
+        for p in primes:
+            lcm *= p
+        print(lcm)
+
+if __name__ == "__main__":
+    main()
