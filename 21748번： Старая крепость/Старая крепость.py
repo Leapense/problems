1c8c90c916863534174dfab05a1a21f13c6e74dc
Leapense <iso9661@gmail.com>
2025-01-20 15:11:05 +0900

    `Refactored code for problem 21748 with improved solution`

--- 21748번： Старая крепость/Старая крепость.py
+++ 21748번： Старая крепость/Старая крепость.py
@@ -1,11 +1,33 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21748                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21748                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 15:04:57 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import math, sys
+
+n, r = map(int, sys.stdin.readline().split())
+fragments = []
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    if b <= a:
+        b += 360
+    fragments.append((a, b))
+fragments.sort()
+
+arc_length = 0
+
+for a, b in fragments:
+    arc_length += math.radians(b - a) * r
+
+gaps = []
+for i in range(n):
+    a1, b1 = fragments[i]
+    a2, b2 = fragments[(i + 1) % n]
+    gap = a2 - b1
+    if gap < 0:
+        gap += 360
+    gaps.append(gap)
+
+chord_length = 0
+for gap in gaps:
+    theta = math.radians(gap)
+    chord_length += 2 * r * math.sin(theta / 2)
+
+total_length = arc_length + chord_length
+print(total_length)
