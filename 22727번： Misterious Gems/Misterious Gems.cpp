8accc65507571d32a1c7f9aaf1c3391c675b4bb9
Leapense <iso9661@gmail.com>
2025-01-20 22:51:40 +0900

    `Added solution for problem 22727, implementing a set to track gems and a vector to store commands.`

--- 22727번： Misterious Gems/Misterious Gems.cpp
+++ 22727번： Misterious Gems/Misterious Gems.cpp
@@ -1,11 +1,35 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 22727                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/22727                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/20 22:27:43 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    while(1){
+        int N;
+        cin>>N;
+        if(N==0) break;
+        set<pair<int,int>> gems;
+        for(int i=0;i<N;i++){
+            int x,y;
+            cin>>x>>y;
+            gems.emplace(x,y);
+        }
+        int M;
+        cin>>M;
+        vector<pair<char,int>> commands(M);
+        for(auto &p:commands) cin>>p.first>>p.second;
+        int x=10, y=10;
+        for(auto &[d,l]:commands){
+            int dx=0, dy=0;
+            if(d=='N') dy=1;
+            if(d=='S') dy=-1;
+            if(d=='E') dx=1;
+            if(d=='W') dx=-1;
+            for(int i=0;i<l;i++){
+                x += dx;
+                y += dy;
+                gems.erase({x,y});
+            }
+        }
+        if(gems.empty()) cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+}
