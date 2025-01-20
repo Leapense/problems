20fdec2635dea79d1a2a710303ef5689f3f38607
Leapense <iso9661@gmail.com>
2025-01-20 16:13:03 +0900

    `Added new code for problem 21921, replacing original solution with a new implementation.`

--- 21921번： 블로그/블로그.py
+++ 21921번： 블로그/블로그.py
@@ -1,11 +1,28 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21921                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21921                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 16:05:30 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.readline
+
+    N, X = map(int, sys.stdin.readline().split())
+    visitors = list(map(int, sys.stdin.readline().split()))
+
+    current_sum = sum(visitors[:X])
+    max_sum = current_sum
+    count = 1
+
+    for i in range(X, N):
+        current_sum += visitors[i] - visitors[i - X]
+        if current_sum > max_sum:
+            max_sum = current_sum
+            count = 1
+        elif current_sum == max_sum:
+            count += 1
+
+    if max_sum > 0:
+        print(max_sum)
+        print(count)
+    else:
+        print("SAD")
+
+if __name__ == "__main__":
+    main()
