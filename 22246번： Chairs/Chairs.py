3bc5d9fea6cbdcbe26699927f54f5f80a8db9807
Leapense <iso9661@gmail.com>
2025-01-20 18:42:02 +0900

    `Added time_to_seconds function and event sorting optimization added to Chairs.py`

--- 22246번： Chairs/Chairs.py
+++ 22246번： Chairs/Chairs.py
@@ -1,11 +1,25 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 22246                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/22246                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 18:36:09 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def time_to_seconds(t):
+    h, m, s = map(int, t.split(":"))
+    return h * 3600 + m * 60 + s
+
+import sys
+N, *lines = sys.stdin.read().splitlines()
+N = int(N)
+events = []
+for line in lines:
+    entry, exit = line.split()
+    events.append((time_to_seconds(entry), 0))
+    events.append((time_to_seconds(exit), 1))
+
+events.sort()
+current = max_count = 0
+
+for time, typ in events:
+    if typ == 0:
+        current += 1
+        if current > max_count:
+            max_count = current
+    else:
+        current -= 1
+
+print(max_count)
