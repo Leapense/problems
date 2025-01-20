eff24b133f06fa745d04960a1705f6ce23f3d358
Leapense <iso9661@gmail.com>
2025-01-20 16:34:58 +0900

    `Added Python solution for problem 21968, replacing diff output with actual code`

--- 21968번： 선린의 터를/선린의 터를.py
+++ 21968번： 선린의 터를/선린의 터를.py
@@ -1,11 +1,18 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21968                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21968                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 16:27:52 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    T, *rest = sys.stdin.read().split()
+    T = int(T)
+    for i in range(T):
+        N = int(rest[i])
+        result = 0
+        power_of_three = 1
+        while N > 0:
+            if N % 2 == 1:
+                result += power_of_three
+            power_of_three *= 3
+            N //= 2
+        print(result)
+
+if __name__ == "__main__":
+    main()
