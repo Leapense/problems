41860b9e2dd1bfd4b8d5dc705d72f2db8bafcfa3
Leapense <iso9661@gmail.com>
2025-01-20 15:01:50 +0900

    `diff: updated code for problem 21735 with dynamic programming solution`

--- 21735번： 눈덩이 굴리기/눈덩이 굴리기.py
+++ 21735번： 눈덩이 굴리기/눈덩이 굴리기.py
@@ -1,11 +1,20 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 21735                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/21735                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 14:54:45 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
+dp = [[-1] * (N+1) for _ in range(M+1)]
+dp[0][0] = 1
+
+for t in range(M):
+    for i in range(N+1):
+        if dp[t][i] == -1:
+            continue
+        if i+1 <= N:
+            dp[t+1][i+1] = max(dp[t+1][i+1], dp[t][i] + a[i])
+        if i+2 <= N:
+            new_size = dp[t][i]//2 + a[i+1]
+            dp[t+1][i+2] = max(dp[t+1][i+2], new_size)
+result = 0
+for t in range(M+1):
+    for i in range(N+1):
+        if dp[t][i] > result:
+            result = dp[t][i]
+print(result)
