2946c02e3e563aa57627470a339df556286030d3
Leapense <iso9661@gmail.com>
2025-01-20 17:16:22 +0900

    `Added Python solution for problem 22017, Illumination 2, with diff included in commit`

--- 22017번： イルミネーション 2 (Illumination 2)/イルミネーション 2 (Illumination 2).py
+++ 22017번： イルミネーション 2 (Illumination 2)/イルミネーション 2 (Illumination 2).py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 22017                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/22017                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 17:08:41 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.read().split()
+    N = int(input[0])
+    A = list(map(int, input[1:N+1]))
+    zero_prefix = [0] * (N+1)
+    for i in range(1, N+1):
+        zero_prefix[i] = zero_prefix[i-1] + (1 if A[i-1] == 0 else 0)
+    one_suffix = [0] * (N+1)
+    for i in range(N-1, -1, -1):
+        one_suffix[i] = one_suffix[i+1] + (1 if A[i] == 1 else 0)
+    min_toggles = float('inf')
+    for r in range(N+1):
+        toggles = zero_prefix[r] + one_suffix[r]
+        if toggles < min_toggles:
+            min_toggles = toggles
+    print(min_toggles)
+
+if __name__ == "__main__":
+    main()
