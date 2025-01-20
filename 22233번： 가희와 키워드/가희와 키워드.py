5f4760576af953807c699854adefb92cea53e3d7
Leapense <iso9661@gmail.com>
2025-01-20 17:54:31 +0900

    `diff: updated code for problem 22233 with new logic and import statement`

--- 22233번： 가희와 키워드/가희와 키워드.py
+++ 22233번： 가희와 키워드/가희와 키워드.py
@@ -1,11 +1,23 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 22233                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/22233                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 17:48:14 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    input = sys.stdin.readline
+
+    N, M = map(int, sys.stdin.readline().split())
+    notepad = set()
+    for _ in range(N):
+        keyword = sys.stdin.readline().strip()
+        notepad.add(keyword)
+
+    remaining = N
+    for _ in range(M):
+        line = sys.stdin.readline().strip()
+        keywords = line.split(',')
+        for kw in keywords:
+            if kw in notepad:
+                notepad.remove(kw)
+                remaining -= 1
+        print(remaining)
+
+if __name__ == "__main__":
+    main()
