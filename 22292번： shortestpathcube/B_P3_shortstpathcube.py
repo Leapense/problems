48036db1975ddf5a6d91242f79419c10023f4a83
Leapense <iso9661@gmail.com>
2025-01-20 19:03:01 +0900

    추가: 22292번 문제 '최단경로 큐브'의 P3 함수 구현 및 B_P3_shortestpathcube.py 파일 추가

---
+++ 22292번： shortestpathcube/B_P3_shortstpathcube.py
@@ -0,0 +1,15 @@
+def side(x, y, z):
+    # ADD ADDITIONAL CODE HERE!
+    pass
+
+def P3(x, y, z):
+    sideOn = side(x, y, z)
+    # ADD ADDITIONAL CODE HERE!
+    return
+
+############### SUBMIT THE CODE ABOVE ONLY ###############
+
+def truncate(x):
+    return int(100*x)/100
+
+print(truncate(P3(0, 100, 20))) # 128.06
--- 22292번： shortestpathcube/shortestpathcube.py
+++ 22292번： shortestpathcube/shortestpathcube.py
@@ -1,11 +1,58 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 22292                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/22292                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 18:44:54 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import math
+
+def P3(x, y, z):
+    min_dist = float('inf')
+    cube_size = 100
+    faces = []
+
+    if z == 0 or z == cube_size:
+        faces.append('z')
+    if x == 0 or x == cube_size:
+        faces.append('x')
+    if y == 0 or y == cube_size:
+        faces.append('y')
+
+    for face in faces:
+        if face == 'z':
+            if z == 0:
+                unfolded = []
+                unfolded.append(((x, y), (cube_size, cube_size, cube_size)))
+                for direction in ['x+', 'x-', 'y+', 'y-']:
+                    if direction == 'x+':
+                        px, py = x, y
+                        tx, ty, tz = cube_size, cube_size, cube_size
+                        dist = math.sqrt((cube_size - px) ** 2 + (cube_size - py) ** 2)
+                        min_dist = min(min_dist, dist)
+                    elif direction == 'x-':
+                        px, py = x, y
+                        tx, ty, tz = cube_size, cube_size, cube_size
+                        dist = math.sqrt((cube_size - px) ** 2 + (cube_size - py) ** 2)
+                        min_dist = min(min_dist, dist)
+                    elif direction == 'y+':
+                        px, py = x, y
+                        tx, ty, tz = cube_size, cube_size, cube_size
+                        dist = math.sqrt((cube_size - px) ** 2 + (cube_size - py) ** 2)
+                        min_dist = min(min_dist, dist)
+                    elif direction == 'y-':
+                        px, py = x, y
+                        tx, ty, tz = cube_size, cube_size, cube_size
+                        dist = math.sqrt((cube_size - px) ** 2 + (cube_size - py) ** 2)
+                        min_dist = min(min_dist, dist)
+            else:
+                dist = math.sqrt((cube_size - x) ** 2 + (cube_size - y) ** 2)
+                min_dist = min(min_dist, dist)
+        elif face == 'x':
+            if x == 0:
+                dist = math.sqrt((cube_size - y) ** 2 + (cube_size - z) ** 2)
+                min_dist = min(min_dist, dist)
+            else:
+                dist = math.sqrt((cube_size - y) ** 2 + (cube_size - z) ** 2)
+                min_dist = min(min_dist, dist)
+        elif face == 'y':
+            if y == 0:
+                dist = math.sqrt((cube_size - x) ** 2 + (cube_size - z) ** 2)
+                min_dist = min(min_dist, dist)
+            else:
+                dist = math.sqrt((cube_size - x) ** 2 + (cube_size - z) ** 2)
+                min_dist = min(min_dist, dist)
+    return min_dist
