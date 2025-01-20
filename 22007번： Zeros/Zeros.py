55e31f6db06e398e9cb769e7e28c9a25759764f5
Leapense <iso9661@gmail.com>
2025-01-20 17:07:41 +0900

    `Added function to calculate max k for powers of 2 and 5, updated main logic to use it`

--- 22007번： Zeros/Zeros.py
+++ 22007번： Zeros/Zeros.py
@@ -1,11 +1,16 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 22007                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/22007                          #+#        #+#      #+#     #
-#    Solved: 2025/01/20 17:02:14 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def get_max_k(p, a, b):
+    max_k = 0
+    k = 1
+    while True:
+        power = p ** k
+        if power > b:
+            break
+        if b // power - (a - 1) // power >= 1:
+            max_k = k
+        k += 1
+    return max_k
+
+a, b = map(int, input().split())
+k2 = get_max_k(2, a, b)
+k5 = get_max_k(5, a, b)
+print(min(k2, k5))
