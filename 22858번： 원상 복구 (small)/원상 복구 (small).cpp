1096c8455a1613c8df70e43de29610d7a0993207
Leapense <iso9661@gmail.com>
2025-01-21 18:58:21 +0900

    `Refactored code for problem 22858 with added comments and improved formatting`

--- 22858번： 원상 복구 (small)/원상 복구 (small).cpp
+++ 22858번： 원상 복구 (small)/원상 복구 (small).cpp
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 22858                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/22858                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/21 18:35:10 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> S(N);
+    for (auto &x : S) cin >> x;
+
+    vector<int> D(N);
+    for (auto &x : D) {
+        cin >> x;
+        x--;
+    }
+
+    vector<int> Dk(N, 0);
+    vector<bool> visited(N, false);
+
+    for (int i = 0; i < N; i++) {
+        if (!visited[i]) {
+            vector<int> cycle;
+            int j = i;
+            while (!visited[j]) {
+                visited[j] = true;
+                cycle.push_back(j);
+                j = D[j];
+            }
+            int L = cycle.size();
+            for (int idx = 0; idx < L; idx++) {
+                Dk[cycle[idx]] = cycle[(idx + K) % L];
+            }
+        }
+    }
+
+    vector<int> invDk(N, 0);
+    for (int i = 0; i < N; i++) invDk[Dk[i]] = i;
+    vector<int> P(N);
+    for (int i = 0; i < N; i++) P[i] = S[invDk[i]];
+    for (int i = 0; i < N; i++) cout << P[i] << (i < N - 1 ? " " : "\n");
+
+    return 0;
+}
