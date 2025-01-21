a19b4e6ca423c0df5f49c7a3dc92ea1b7e0950b1
Leapense <iso9661@gmail.com>
2025-01-21 18:22:57 +0900

    `Added implementation for problem 22821: Multi-column List`

--- 22821번： Multi-column List/Multi-column List.cpp
+++ 22821번： Multi-column List/Multi-column List.cpp
@@ -1,11 +1,75 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 22821                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/22821                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/21 18:08:23 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    while (1) {
+        string s;
+        cin >> s;
+
+        if (s == "0") break;
+        int plen = stoi(s);
+        int cnum, width, cspace;
+        cin >> cnum >> width >> cspace;
+
+        vector<string> input;
+        while (cin >> ws, getline(cin, s) && s != "?") {
+            input.push_back(s);
+        }
+
+        vector<string> processed;
+        for (auto &line : input) {
+            if (line.empty()) {
+                processed.push_back(string(width, '.'));
+                continue;
+            }
+            int len = line.size();
+            int start = 0;
+
+            while (start < len) {
+                string part = line.substr(start, width);
+                if (part.size() < width) {
+                    part += string(width - part.size(), '.');
+                }
+
+                processed.push_back(part);
+                start += width;
+            }
+        }
+
+        if (!processed.empty()) {
+            int total_columns = ceil((double)processed.size() / plen);
+            int total_pages = ceil((double)total_columns / cnum);
+
+            for (int p = 0; p < total_pages; p++) {
+                for (int l = 0; l < plen; l++) {
+                    string out = "";
+                    for (int c = 0; c < cnum; c++) {
+                        int col = p * cnum + c;
+                        if (col < total_columns) {
+                            int idx = col * plen + l;
+                            if (idx < processed.size()) {
+                                out += processed[idx];
+                            } else {
+                                out += string(width, '.');
+                            }
+                        } else {
+                            out += string(width, '.');
+                        }
+                        if (c != cnum - 1) {
+                            out += string(cspace, '.');
+                        }
+                    }
+
+                    cout << out << "\n";
+                }
+                cout << "#\n";
+            }
+        }
+        cout << "?\n";
+    }
+
+    return 0;
+}
