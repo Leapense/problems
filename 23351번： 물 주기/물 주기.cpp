5124575586923a07ccfde32c6b116aef8825055f
Leapense <iso9661@gmail.com>
2025-01-21 21:43:46 +0900

    `Added solution for problem 23351, including new code and diff from previous version.`

--- 23351번： 물 주기/물 주기.cpp
+++ 23351번： 물 주기/물 주기.cpp
@@ -1,11 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 23351                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/23351                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/21 21:37:13 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, K, A, B;
+    cin >> N >> K >> A >> B;
+
+    vector<int> pots(N, K);
+    int groups = N / A;
+
+    vector<vector<int>> groupList(groups, vector<int>());
+    for (int i = 0; i < N; i++) groupList[i / A].push_back(i);
+
+    int day = 0;
+
+    while (1) {
+        day++;
+        int currentGroup = (day-1) % groups;
+        for (auto &idx : groupList[currentGroup]) pots[idx] += B;
+        for (auto &pot : pots) pot -= 1;
+        bool dead = false;
+        for (auto &pot : pots) if (pot <= 0) {dead = true; break;}
+        if (dead) {
+            cout << day;
+            break;
+        }
+    }
+
+    return 0;
+}
