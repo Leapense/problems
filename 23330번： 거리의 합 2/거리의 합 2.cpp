e4a382ccc4785b5c3b338a45f4d6a5c2372d84b9
Leapense <iso9661@gmail.com>
2025-01-21 21:34:36 +0900

    `Added solution for problem 23330, including necessary includes and main function implementation.`

--- 23330번： 거리의 합 2/거리의 합 2.cpp
+++ 23330번： 거리의 합 2/거리의 합 2.cpp
@@ -1,11 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 23330                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/23330                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/21 21:17:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<long long> x(n);
+
+    for (auto &e : x) cin >> e;
+
+    sort(x.begin(), x.end());
+    vector<long long> prefix_sum(n);
+    prefix_sum[0] = x[0];
+
+    for (int i = 1; i < n; i++) prefix_sum[i] = prefix_sum[i-1] + x[i];
+    long long total = 0;
+    for (int i = 1; i < n; i++) total += x[i] * i - prefix_sum[i-1];
+    cout << total * 2 << endl;
+
+    return 0;
+}
