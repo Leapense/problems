a1688e8199e65dff513ba32c5d90a62f755f5e60
Leapense <iso9661@gmail.com>
2025-01-21 22:25:37 +0900

    `Refactored code for problem 23365: added includes, removed comments, and modified main function`

--- 23365번： Buffered Buffet/Buffered Buffet.cpp
+++ 23365번： Buffered Buffet/Buffered Buffet.cpp
@@ -1,11 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 23365                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/23365                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/21 22:15:04 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<long long> d(n);
+    for (auto &x : d)
+        cin >> x;
+
+    sort(d.begin(), d.end());
+    long long sum = 0;
+
+    for (auto x : d)
+        sum += x;
+    long long C = sum + d[n - 1] - d[0];
+
+    cout << C << endl;
+    return 0;
+}
