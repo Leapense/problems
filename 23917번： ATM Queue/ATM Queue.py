752c4c412ad5cb4775e79c1c7aed1312ff1f32b5
Leapense <iso9661@gmail.com>
2025-01-22 23:12:24 +0900

    `Refactored ATM Queue code: optimized Person class and queue processing`

--- 23917번： ATM Queue/ATM Queue.py
+++ 23917번： ATM Queue/ATM Queue.py
@@ -1,43 +1,30 @@
-from collections import deque
 import sys
 
 class Person:
-    def __init__(self, number, amount):
+    __slots__ = ['rounds', 'number']
+    def __init__(self, rounds, number):
+        self.rounds = rounds
         self.number = number
-        self.amount = amount
-
-class ATMQueue:
-    def __init__(self, N, X, amounts):
-        self.queue = deque()
-        for i in range(N):
-            self.queue.append(Person(i + 1, amounts[i]))
-        self.X = X
-        self.result = []
-
-    def process_queue(self):
-        while self.queue:
-            person = self.queue.popleft()
-            if person.amount <= self.X:
-                self.result.append(str(person.number))
-            else:
-                person.amount -= self.X
-                self.queue.append(person)
-        return ' '.join(self.result)
 
 def main():
     input = sys.stdin.read().split()
     idx = 0
     T = int(input[idx])
-    idx += 1
-    for tc in range(1, T + 1):
+    idx +=1
+    for tc in range(1, T+1):
         N = int(input[idx])
-        X = int(input[idx + 1])
-        idx += 2
+        X = int(input[idx+1])
+        idx +=2
         amounts = list(map(int, input[idx:idx+N]))
-        idx += N
-        atm = ATMQueue(N, X, amounts)
-        order = atm.process_queue()
-        print(f"Case #{tc}: {order}")
+        idx +=N
+        people = []
+        for i in range(N):
+            rounds = (amounts[i] + X -1) // X
+            people.append(Person(rounds, i+1))
+        # 정렬: rounds 오름차순, 번호 오름차순
+        people.sort(key=lambda p: (p.rounds, p.number))
+        order = [str(p.number) for p in people]
+        print(f"Case #{tc}: {' '.join(order)}")
 
 if __name__ == "__main__":
     main()
