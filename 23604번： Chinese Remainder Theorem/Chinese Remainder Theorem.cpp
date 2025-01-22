f9f9216480252a4922d76c54e18e792be84fd42f
Leapense <iso9661@gmail.com>
2025-01-22 20:16:15 +0900

    `Refactored Chinese Remainder Theorem solution to use a more straightforward approach`

--- 23604번： Chinese Remainder Theorem/Chinese Remainder Theorem.cpp
+++ 23604번： Chinese Remainder Theorem/Chinese Remainder Theorem.cpp
@@ -1,11 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 23604                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/23604                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/22 20:05:45 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+
+    vector<ll> a(n);
+    for (auto &x : a)
+        cin >> x;
+
+    vector<ll> b(n);
+    for (auto &x : b)
+        cin >> x;
+
+    ll res = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        ll diff = a[i] - b[i];
+        res = gcd(res, diff);
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
