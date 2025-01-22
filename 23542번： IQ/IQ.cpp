0d3d76273003d185954afe3cf3f8fe7ce49e13e0
Leapense <iso9661@gmail.com>
2025-01-22 20:04:36 +0900

    `Added solution for problem 23542, IQ, with C++ code with diff included`

--- 23542번： IQ/IQ.cpp
+++ 23542번： IQ/IQ.cpp
@@ -1,11 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 23542                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/23542                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/22 19:48:09 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(2 * n);
+    for (auto &x : a)
+        cin >> x;
+
+    sort(a.begin(), a.end());
+    int max_sum = INT32_MIN, min_sum = INT32_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        int s = a[i] + a[2 * n - 1 - i];
+        max_sum = max(max_sum, s);
+        min_sum = min(min_sum, s);
+    }
+    cout << max_sum - min_sum << endl;
+
+    return 0;
+}
