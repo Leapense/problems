a3d1db92a5dced45698e55693d084f33e6f44fc7
Leapense <iso9661@gmail.com>
2025-01-22 21:25:03 +0900

    `Added solution for problem 23814, including necessary includes, variable declarations, and main function implementation.`

--- 23814번： 아 저는 볶음밥이요/아 저는 볶음밥이요.cpp
+++ 23814번： 아 저는 볶음밥이요/아 저는 볶음밥이요.cpp
@@ -1,11 +1,65 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 23814                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/23814                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/22 21:06:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll D;
+    cin >> D;
+
+    ll N, M, K;
+    cin >> N >> M >> K;
+
+    ll total_dumplings = (N + M + K) / D;
+    ll N_mod = N % D;
+    ll M_mod = M % D;
+    ll required_x = (D - N_mod) % D;
+    ll required_y = (D - M_mod) % D;
+    ll max_z = 0;
+
+    if (required_x <= K)
+    {
+        ll x = required_x;
+        ll y = 0;
+        ll z = K - x;
+
+        ll dumplings = (N + x) / D + (M + y) / D + z / D;
+        if (dumplings >= total_dumplings)
+        {
+            max_z = max(max_z, z);
+        }
+    }
+
+    if (required_y <= K)
+    {
+        ll x = 0;
+        ll y = required_y;
+        ll z = K - y;
+        ll dumplings = (N + x) / D + (M + y) / D + z / D;
+        if (dumplings >= total_dumplings)
+        {
+            max_z = max(max_z, z);
+        }
+    }
+
+    if (required_x + required_y <= K)
+    {
+        ll x = required_x;
+        ll y = required_y;
+        ll z = K - x - y;
+
+        ll dumplings = (N + x) / D + (M + y) / D + z / D;
+        if (dumplings >= total_dumplings)
+        {
+            max_z = max(max_z, z);
+        }
+    }
+
+    cout << max_z << endl;
+
+    return 0;
+}
