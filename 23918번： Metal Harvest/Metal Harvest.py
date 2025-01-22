c80b67eedd631af652e8f868c7af71bc88003458
Leapense <iso9661@gmail.com>
2025-01-22 23:33:42 +0900

    `Refactored Metal Harvest problem solution to use a class-based approach`

--- 23918번： Metal Harvest/Metal Harvest.py
+++ 23918번： Metal Harvest/Metal Harvest.py
@@ -1,11 +1,32 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 23918                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/23918                          #+#        #+#      #+#     #
-#    Solved: 2025/01/22 23:17:02 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class MetalHarvestSolver:
+    def __init__(self):
+        import sys
+        self.input = sys.stdin.read().split()
+        self.index = 0
+
+    def read_int(self):
+        val = int(self.input[self.index])
+        self.index += 1
+        return val
+    
+    def solve(self):
+        T = self.read_int()
+        for tc in range(1, T + 1):
+            N = self.read_int()
+            K = self.read_int()
+            intervals = []
+            for _ in range(N):
+                S = self.read_int()
+                E = self.read_int()
+                intervals.append((E, S))
+            intervals.sort()
+            count = 0
+            current_end = -1
+            for E, S in intervals:
+                if S >= current_end:
+                    D = max(S, E - K)
+                    current_end = D + K
+                    count += 1
+            print(f"Case #{tc}: {count}")
+if __name__ == "__main__":
+    MetalHarvestSolver().solve()
