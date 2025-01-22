ccfe16fa1b538ebf15dd05dcc2bd5d6a58a5361f
Leapense <iso9661@gmail.com>
2025-01-22 21:44:01 +0900

    `Refactored code for problem 23910: Alien Piano`

--- 23910번： Alien Piano/Alien Piano.cpp
+++ 23910번： Alien Piano/Alien Piano.cpp
@@ -1,11 +1,68 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 23910                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/23910                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/22 21:33:47 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    for (int tc = 1; tc <= T; tc++)
+    {
+        int K;
+        cin >> K;
+
+        vector<int> A(K);
+        for (auto &x : A)
+            cin >> x;
+
+        vector<int> prev_cost(5, 0);
+        for (int i = 1; i < K; i++)
+        {
+            vector<int> curr_cost(5, INT32_MAX);
+            for (int k = 1; k <= 4; k++)
+            {
+                for (int kp = 1; kp <= 4; kp++)
+                {
+                    int cost = prev_cost[kp];
+                    if (A[i] > A[i - 1])
+                    {
+                        if (k <= kp)
+                            cost += 1;
+                    }
+                    else if (A[i] < A[i - 1])
+                    {
+                        if (k >= kp)
+                            cost += 1;
+                    }
+                    else
+                    {
+                        if (k != kp)
+                            cost += 1;
+                    }
+
+                    curr_cost[k] = min(curr_cost[k], cost);
+                }
+            }
+
+            prev_cost = curr_cost;
+        }
+
+        int res;
+
+        if (K == 1)
+        {
+            res = 0;
+        }
+        else
+        {
+            res = *min_element(prev_cost.begin() + 1, prev_cost.end());
+        }
+
+        cout << "Case #" << tc << ": " << res << "\n";
+    }
+
+    return 0;
+}
