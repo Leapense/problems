35fb0d7d89475847be440736579f1a94157ade14
Leapense <iso9661@gmail.com>
2025-01-23 17:22:42 +0900

    `Added new solution for problem 23929 with input handling and prefix sum optimization`

--- 23929번： Training/Training.py
+++ 23929번： Training/Training.py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 23929                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/23929                          #+#        #+#      #+#     #
-#    Solved: 2025/01/23 17:16:32 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+class Solution:
+    def run(self):
+        input = sys.stdin.read().split()
+        idx = 0
+        T = int(input[idx]); idx += 1
+        for tc in range(1, T + 1):
+            N, P = int(input[idx]), int(input[idx + 1]); idx += 2
+            S = list(map(int, input[idx:idx+N])); idx += N
+            S.sort()
+            prefix = [0] * (N + 1)
+            for i in range(N):
+                prefix[i + 1] = prefix[i] + S[i]
+            min_coach = float('inf')
+            for i in range(P, N + 1):
+                total = P * S[i - 1] - (prefix[i] - prefix[i - P])
+                if total < min_coach:
+                    min_coach = total
+            print(f"Case #{tc}: {min_coach}")
+Solution().run()
