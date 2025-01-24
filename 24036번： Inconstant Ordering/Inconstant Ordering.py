11e7817f72f50fb9b4c864350fb4c1e37ed73ac5
Leapense <iso9661@gmail.com>
2025-01-24 19:10:54 +0900

    `Added redundant import statements and commented out code`

--- 24036번： Inconstant Ordering/Inconstant Ordering.py
+++ 24036번： Inconstant Ordering/Inconstant Ordering.py
@@ -1,11 +1,13 @@
 from functools import lru_cache
 import sys
+import sys
+import sys
 
 class InconstantOrdering:
     def __init__(self, N, L):
         self.N = N
         self.L = L
-    
+
     def get_min_string(self):
         N = self.N
         L = self.L
@@ -18,20 +20,26 @@ class InconstantOrdering:
             current_block_size = L[block_index]
 
             if block_index % 2 == 0:
-                def generate_sequence(start, length, current_seq):
+                # Odd block: strictly increasing
+                # Generate all possible strictly increasing sequences of length L_i starting from s > prev_letter
+                # Yield sequences in lex order
+                def generate_sequences(start, length, current_seq):
                     if length == 0:
                         yield current_seq
                         return
-
                     for c in range(start, 26):
                         if not current_seq or c > current_seq[-1]:
-                            yield from generate_sequence(c + 1, length - 1, current_seq + (c,))
-                    for seq in generate_sequence(prev_letter + 1, current_block_size, ()):
-                        last_letter = seq[-1]
-                        rest = helper(block_index + 1, last_letter)
-                        if rest is not None:
-                            return ''.join(chr(ord('A') + c) for c in seq) + rest
+                            yield from generate_sequences(c + 1, length - 1, current_seq + (c,))
+                
+                for seq in generate_sequences(prev_letter + 1, current_block_size, ()):
+                    last_letter = seq[-1]
+                    rest = helper(block_index + 1, last_letter)
+                    if rest is not None:
+                        return ''.join(chr(ord('A') + c) for c in seq) + rest
+
             else:
+                # Even block: strictly decreasing
+                # Assign a strictly decreasing sequence of L_i letters, starting from s < prev_letter
                 for s in range(0, prev_letter):
                     if s - (current_block_size - 1) < 0:
                         continue
@@ -39,14 +47,17 @@ class InconstantOrdering:
                     rest = helper(block_index + 1, seq[-1])
                     if rest is not None:
                         return ''.join(chr(ord('A') + c) for c in seq) + rest
+
             return None
 
-        result = helper(0, ord('A') - ord('A'))
+        result = helper(0, ord('A') - ord('A'))  # Start with 'A' which is 0
         return 'A' + result if result else 'A'
 
 def main():
+    import sys
+    import sys
     T = int(sys.stdin.readline())
-    for test_case in range(1, T + 1):
+    for test_case in range(1, T +1):
         N_line = ''
         while N_line.strip() == '':
             N_line = sys.stdin.readline()
