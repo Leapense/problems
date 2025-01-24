a6314c8573707bd7326f7123f2b09b5bebc13133
Leapense <iso9661@gmail.com>
2025-01-24 21:57:58 +0900

    `Added new code for problem 24306, including a Clock class and main function.`

--- 24306번： ЧАСОВНИК/ЧАСОВНИК.py
+++ 24306번： ЧАСОВНИК/ЧАСОВНИК.py
@@ -1,11 +1,19 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24306                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24306                          #+#        #+#      #+#     #
-#    Solved: 2025/01/24 21:49:43 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import math
+
+class Clock:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def count_overlaps(self):
+        gcd_value = math.gcd(self.a - 1, self.a * self.b - 1)
+        return max(gcd_value - 1, 0)
+    
+def main():
+    a, b, c = map(int, input().split())
+    clock = Clock(a, b, c)
+    print(clock.count_overlaps())
+
+if __name__ == "__main__":
+    main()
