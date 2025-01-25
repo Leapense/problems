5c6aea5442beda23071d2d35545c8492150e643b
Leapense <iso9661@gmail.com>
2025-01-25 12:37:37 +0900

    `Added implementation for problem 24460, including SpecialAwardSelector class and main function.`

--- 24460번： 특별상이라도 받고 싶어/특별상이라도 받고 싶어.cpp
+++ 24460번： 특별상이라도 받고 싶어/특별상이라도 받고 싶어.cpp
@@ -1,11 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 24460                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/24460                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/25 12:15:19 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+class SpecialAwardSelector
+{
+public:
+    int N;
+    vector<vector<int>> grid;
+    SpecialAwardSelector(int size) : N(size), grid(size, vector<int>(size)) {}
+    int select_special(int start_row, int start_col, int size)
+    {
+        if (size == 1)
+            return grid[start_row][start_col];
+        int half = size / 2;
+        vector<int> candidates;
+        candidates.push_back(select_special(start_row, start_col, half));
+        candidates.push_back(select_special(start_row, start_col + half, half));
+        candidates.push_back(select_special(start_row + half, start_col, half));
+        candidates.push_back(select_special(start_row + half, start_col + half, half));
+
+        sort(candidates.begin(), candidates.end());
+        return candidates[1];
+    }
+
+    int get_special_award()
+    {
+        return select_special(0, 0, N);
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    SpecialAwardSelector selector(N);
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> selector.grid[i][j];
+        }
+    }
+    cout << selector.get_special_award() << endl;
+    return 0;
+}
