93599d6eaa2511f6f2dc39580d356bd99569c5b6
Leapense <iso9661@gmail.com>
2025-01-25 19:26:24 +0900

    `Added solution for problem 24653, implementing Billboard class and sorting billboards by removal interval.`

--- 24653번： Announcements/Announcements.cpp
+++ 24653번： Announcements/Announcements.cpp
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 24653                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/24653                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/25 19:14:28 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Billboard
+{
+public:
+    long long S;
+    long long remove_i;
+    Billboard(long long s, long long T) : S(s)
+    {
+        remove_i = ((s / T) + 1) * T;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    cin >> N;
+
+    long long T;
+    vector<long long> S(N);
+    for (auto &x : S)
+        cin >> x;
+    cin >> T;
+    vector<Billboard> billboards;
+    billboards.reserve(N);
+
+    for (auto s : S)
+        billboards.emplace_back(s, T);
+    sort(billboards.begin(), billboards.end(), [&](const Billboard &a, const Billboard &b) -> bool
+         { return a.remove_i < b.remove_i; });
+
+    long long visits = 0;
+    long long last_visit = -1;
+    for (auto &b : billboards)
+    {
+        if (b.S > last_visit)
+        {
+            visits++;
+            last_visit = b.remove_i - 1;
+        }
+    }
+    cout << visits;
+
+    return 0;
+}
