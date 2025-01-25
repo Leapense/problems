6be7e331f6d09aacfd122f6263863b6fd7c958f8
Leapense <iso9661@gmail.com>
2025-01-25 19:11:14 +0900

    `Added code for problem 24625, including input reading, computation of maximum trace, and result printing.`

--- 24625번： Взять след!/Взять след!.cpp
+++ 24625번： Взять след!/Взять след!.cpp
@@ -1,11 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 24625                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/24625                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/25 19:01:55 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+class MatrixTraceMaximizer
+{
+public:
+    int n;
+    vector<long long> a;
+    long long maxTrace;
+
+    void readInput()
+    {
+        cin >> n;
+        a.resize(n);
+        for (auto &x : a)
+            cin >> x;
+    }
+
+    void computeMaxTrace()
+    {
+        sort(a.begin(), a.end(), greater<long long>());
+        int mMax = floor(sqrt(n));
+        maxTrace = LLONG_MIN;
+        long long currentSum = 0;
+        for (int m = 1; m <= mMax; m++)
+        {
+            currentSum += a[m - 1];
+            if (1LL * m * m > n)
+                continue;
+            maxTrace = max(maxTrace, currentSum);
+        }
+    }
+
+    void printResult()
+    {
+        cout << maxTrace;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    MatrixTraceMaximizer mtm;
+    mtm.readInput();
+    mtm.computeMaxTrace();
+    mtm.printResult();
+
+    return 0;
+}
