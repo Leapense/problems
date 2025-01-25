24f823135b489e81034f5be8ca42993c4e743d58
Leapense <iso9661@gmail.com>
2025-01-25 10:33:55 +0900

    `Refactored PencilHider class to PencilCaseBundle with improved solution`

--- 24344번： СКРИЙ МОЛИВА/СКРИЙ МОЛИВА.py
+++ 24344번： СКРИЙ МОЛИВА/СКРИЙ МОЛИВА.py
@@ -1,67 +1,46 @@
-import sys
-
-class PencilHider:
-    def __init__(self):        
-        self.read_input()
-
-    def read_input(self):
-        input = sys.stdin.read
-        data = input().split()
+class PencilCaseBundle:
+    def __init__(self):
+        import sys
+        data = sys.stdin.read().split()
         self.N = int(data[0])
         self.M = int(data[1])
         self.H = int(data[2])
         self.grid = []
-        index = 3
-
+        idx = 3
         for _ in range(self.N):
-            row = list(map(int, data[index:index+self.M]))
+            row = list(map(int, data[idx:idx+self.M]))
             self.grid.append(row)
-            index += self.M
-
-    def has_tall_enough_left(self, i, j):
-        for col in range(j-1, -1, -1):
-            if self.grid[i][col] >= self.H:
-                return True
-        return False
-
-    def has_tall_enough_right(self, i, j):
-        for col in range(j+1, self.M):
-            if self.grid[i][col] >= self.H:
-                return True
-        return False
-    
-    def has_tall_enough_up(self, i, j):
-        for row in range(i-1, -1, -1):
-            if self.grid[row][j] >= self.H:
-                return True
-        return False
-    
-    def has_tall_enough_down(self, i, j):
-        for row in range(i+1, self.N):
-            if self.grid[row][j] >= self.H:
-                return True
-        return False
-    
-    def count_hidden_columns(self):
+            idx += self.M
+        self.solve()
+        
+    def solve(self):
+        left_max = [[0]*self.M for _ in range(self.N)]
+        right_max = [[0]*self.M for _ in range(self.N)]
+        up_max = [[0]*self.M for _ in range(self.N)]
+        down_max = [[0]*self.M for _ in range(self.N)]
+        for i in range(self.N):
+            current_max = 0
+            for j in range(self.M):
+                current_max = max(current_max, self.grid[i][j])
+                left_max[i][j] = current_max
+            current_max = 0
+            for j in range(self.M-1, -1, -1):
+                current_max = max(current_max, self.grid[i][j])
+                right_max[i][j] = current_max
+        for j in range(self.M):
+            current_max = 0
+            for i in range(self.N):
+                current_max = max(current_max, self.grid[i][j])
+                up_max[i][j] = current_max
+            current_max = 0
+            for i in range(self.N-1, -1, -1):
+                current_max = max(current_max, self.grid[i][j])
+                down_max[i][j] = current_max
         count = 0
         for i in range(self.N):
             for j in range(self.M):
-                current_height = self.grid[i][j]
-                if self.H <= current_height:
-                    count += 1
-                else:
-                    left = self.has_tall_enough_left(i, j)
-                    right = self.has_tall_enough_right(i, j)
-                    up = self.has_tall_enough_up(i, j)
-                    down = self.has_tall_enough_down(i, j)
-                    if left and right and up and down:
-                        count += 1
-        return count
-    
-    def process(self):
-        result = self.count_hidden_columns()
-        print(result)
+                if left_max[i][j] >= self.H and right_max[i][j] >= self.H and up_max[i][j] >= self.H and down_max[i][j] >= self.H:
+                    count +=1
+        print(count)
 
-if __name__ == "__main__":
-    hider = PencilHider()
-    hider.process()
+PencilCaseBundle()
