f7ba074708c324d256986b0b19b8cf704b2615b8
Leapense <iso9661@gmail.com>
2025-01-25 12:59:14 +0900

    `Added C++ solution for problem 24469 with improved formatting and code organization.`

--- 24469번： Autići/Autići.cpp
+++ 24469번： Autići/Autići.cpp
@@ -1,11 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 24469                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/24469                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/25 12:52:19 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        long long n;
+        cin >> n;
+
+        if (n == 1)
+        {
+            cout << "0\n";
+            return;
+        }
+
+        vector<long long> d(n);
+        for (auto &x : d)
+            cin >> x;
+        long long d_min = *min_element(d.begin(), d.end());
+        long long sum = accumulate(d.begin(), d.end(), 0LL);
+        long long total = sum + (n - 2) * d_min;
+        cout << total << endl;
+    }
+};
+
+int main()
+{
+    Solution s;
+    s.solve();
+
+    return 0;
+}
