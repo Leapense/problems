fa25d13ab4aa42fb469c450bc873147dc011f4e7
Leapense <iso9661@gmail.com>
2025-01-25 11:16:53 +0900

    `Refactor: Update code to process shuffles with new logic and import sys for reading input`

--- 24393번： 조커 찾기/조커 찾기.py
+++ 24393번： 조커 찾기/조커 찾기.py
@@ -1,11 +1,40 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24393                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24393                          #+#        #+#      #+#     #
-#    Solved: 2025/01/25 11:01:00 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+class ShuffleProcessor:
+    def __init__(self):
+        self.position = 1
+        self.process_shuffles()
+    def process_shuffles(self):
+        input = sys.stdin.read().split()
+        N = int(input[0])
+        idx = 1
+        for _ in range(N):
+            A = []
+            while idx < len(input) and len(A) < 27:
+                A.append(int(input[idx]))
+                idx += 1
+                if sum(A) == 27:
+                    break
+            left_total = 13
+            right_total = 14
+            if self.position <= left_total:
+                side = 'left'
+                pos = self.position
+            else:
+                side = 'right'
+                pos = self.position - left_total
+            total_taken = 0
+            for j, a in enumerate(A):
+                if j % 2 == 0:
+                    current_side = 'right'
+                else:
+                    current_side = 'left'
+                if current_side == side:
+                    if a >= pos:
+                        self.position = total_taken + pos
+                        break
+                    else:
+                        pos -= a
+                total_taken += a
+        print(self.position)
+ShuffleProcessor()
