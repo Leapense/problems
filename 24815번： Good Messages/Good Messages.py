47aee05e448cb28f040e1701271e4c4073afca54
Leapense <iso9661@gmail.com>
2025-01-26 18:48:09 +0900

    `Refactored code for problem 24815: updated input handling and vowel counting logic`

--- 24815번： Good Messages/Good Messages.py
+++ 24815번： Good Messages/Good Messages.py
@@ -1,11 +1,23 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24815                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24815                          #+#        #+#      #+#     #
-#    Solved: 2025/01/26 18:38:18 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+O = int(input())
+message = input().strip()
+N = int(input())
+
+vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
+t = len(message)
+good_steps = 0
+bad_steps = 0
+
+for i in range(1, N + 1):
+    shift = (O * i) % 26
+    shifted = []
+    for c in message:
+        new_ord = (ord(c) - ord('a') + shift) % 26
+        shifted_char = chr(new_ord + ord('a'))
+        shifted.append(shifted_char)
+    count = sum(1 for char in shifted if char in vowels)
+    if 3 * count >= t:
+        bad_steps += 1
+    else:
+        good_steps += 1
+
+print("Boris" if good_steps > bad_steps else "Colleague")
