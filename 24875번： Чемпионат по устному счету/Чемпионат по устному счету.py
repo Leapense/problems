01961d7b83628261871dba1c28b6932c2d7b82f9
Leapense <iso9661@gmail.com>
2025-01-26 19:39:46 +0900

    `Refactored code for problem 24875 with improved input handling and output formatting`

--- 24875번： Чемпионат по устному счету/Чемпионат по устному счету.py
+++ 24875번： Чемпионат по устному счету/Чемпионат по устному счету.py
@@ -1,11 +1,25 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24875                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24875                          #+#        #+#      #+#     #
-#    Solved: 2025/01/26 19:31:40 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def main():
+    n = int(sys.stdin.readline())
+    arr = list(map(int, sys.stdin.readline().split()))
+    total = sum(arr)
+    q = int(sys.stdin.readline())
+    shift = 0
+    for _ in range(q):
+        parts = sys.stdin.readline().split()
+        if parts[0] == '2':
+            k = int(parts[1])
+            shift = (shift + k) % n
+            print(total)
+        else:
+            i = int(parts[1])
+            x = int(parts[2])
+            original_idx = (i - 1 - shift) % n
+            old_val = arr[original_idx]
+            total += x - old_val
+            arr[original_idx] = x
+            print(total)
+
+if __name__ == "__main__":
+    main()
