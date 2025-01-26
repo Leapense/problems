f52b45acf5491ae2eb723ae3f621c2f07d4cffc1
Leapense <iso9661@gmail.com>
2025-01-26 22:02:33 +0900

    `Added import statement and modified code to read input from sys.stdin`

--- 24919번： Inversions Organize/Inversions Organize.py
+++ 24919번： Inversions Organize/Inversions Organize.py
@@ -1,11 +1,23 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24919                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24919                          #+#        #+#      #+#     #
-#    Solved: 2025/01/26 21:45:36 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+class Solution:
+    def __init__(self):
+        self.input = sys.stdin.read().splitlines()
+        self.idx = 0
+
+    def solve(self):
+        T = int(self.input[self.idx]);self.idx += 1
+        for tc in range(1, T + 1):
+            N = int(self.input[self.idx]);self.idx += 1
+            grid = [self.input[self.idx+i] for i in range(2 * N)];self.idx += 2 * N
+            sum_top = sum(row.count('I') for row in grid[:N])
+            sum_bottom = sum(row.count('I') for row in grid[N:])
+            sum_left = sum(row[:N].count('I') for row in grid)
+            sum_right = sum(row[N:].count('I') for row in grid)
+            D_row = sum_top - sum_bottom
+            D_col = sum_left - sum_right
+            a = (-D_row - D_col) // 2
+            b = (-D_row + D_col) // 2
+            flips = abs(a) + abs(b)
+            print(f"Case #{tc}: {flips}")
+Solution().solve()
