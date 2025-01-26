6b210856d51e076229a635bb7e35e11c9c10ccac
Leapense <iso9661@gmail.com>
2025-01-26 18:31:55 +0900

    `Refactor: Update The Grand Adventure.py with new class-based implementation`

--- 24813번： The Grand Adventure/The Grand Adventure.py
+++ 24813번： The Grand Adventure/The Grand Adventure.py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24813                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24813                          #+#        #+#      #+#     #
-#    Solved: 2025/01/26 17:59:21 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Adventure:
+    def __init__(self, path):
+        self.path = path
+        self.stack = []
+        self.demand = {'t': '|', 'j': '*', 'b': '$'}
+
+    def can_complete(self):
+        for char in self.path:
+            if char in {'$', '|', '*'}:
+                self.stack.append(char)
+            elif char in {'t', 'j', 'b'}:
+                if not self.stack or self.stack[-1] != self.demand[char]:
+                    return "NO"
+                self.stack.pop()
+        return "YES" if not self.stack else "NO"
+    
+n = int(input())
+for _ in range(n):
+    path = input().strip()
+    adventure = Adventure(path)
+    print(adventure.can_complete())
