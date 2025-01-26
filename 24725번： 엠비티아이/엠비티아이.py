f4b23a089731785f4b5ae2a4bc57437a3a211a92
Leapense <iso9661@gmail.com>
2025-01-26 15:17:17 +0900

    `Added code for MBTI finder problem 24725`

--- 24725번： 엠비티아이/엠비티아이.py
+++ 24725번： 엠비티아이/엠비티아이.py
@@ -1,11 +1,46 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24725                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24725                          #+#        #+#      #+#     #
-#    Solved: 2025/01/26 15:09:23 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class MBTIFinder:
+    def __init__(self, grid, n, m):
+        self.grid = grid
+        self.n = n
+        self.m = m
+        self.mbti_types = {
+            "ISTJ", "ISFJ", "INFJ", "INTJ",
+            "ISTP", "ISFP", "INFP", "INTP",
+            "ESTP", "ESFP", "ENFP", "ENTP",
+            "ESTJ", "ESFJ", "ENFJ", "ENTJ"
+        }
+        self.directions = [
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, -1),
+            (1, 1)
+        ]
+
+    def count_mbti(self):
+        count = 0
+        for i in range(self.n):
+            for j in range(self.m):
+                for dr, dc in self.directions:
+                    ni = i + 3 * dr
+                    nj = j + 3 * dc
+                    if 0 <= ni < self.n and 0 <= nj < self.m:
+                        s = ''.join(self.grid[i + k * dr][j + k * dc] for k in range(4))
+                        if s in self.mbti_types:
+                            count += 1
+        return count
+    
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    N, M = int(data[0]), int(data[1])
+    grid = data[2:N+2]
+    finder = MBTIFinder(grid, N, M)
+    print(finder.count_mbti())
+
+if __name__ == "__main__":
+    main()
