258ffc47077a3e1e7510538ef45423dc60ba4362
Leapense <iso9661@gmail.com>
2025-01-26 17:51:49 +0900

    `Added Ornaments.py solution for problem 24772 with input processing input from stdin and calculating final length of ornaments.`

--- 24772번： Ornaments/Ornaments.py
+++ 24772번： Ornaments/Ornaments.py
@@ -1,11 +1,16 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 24772                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/24772                          #+#        #+#      #+#     #
-#    Solved: 2025/01/26 17:36:36 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import math, sys
+
+class Ornaments:
+    def process(self):
+        for line in sys.stdin:
+            r, h, s = map(int, line.strip().split())
+            if r == 0 and h == 0 and s == 0:
+                break
+            l = math.sqrt(h * h - r * r)
+            theta = math.pi - math.acos(r / h)
+            c = 2 * r * theta
+            basic_length = 2 * l + c
+            final_length = basic_length * (1 + s / 100)
+            print("{0:.2f}".format(final_length))
+if __name__ == "__main__":
+    Ornaments().process()
