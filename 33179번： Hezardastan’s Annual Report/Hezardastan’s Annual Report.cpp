1bdc7a7e70b06a1f360d161b6194d1bbdf1a61a1
Leapense <iso9661@gmail.com>
2025-01-30 22:28:41 +0900

    `Added solution for problem 33179, Hezardastan's Annual Report, in C++`

--- 33179번： Hezardastan’s Annual Report/Hezardastan’s Annual Report.cpp
+++ 33179번： Hezardastan’s Annual Report/Hezardastan’s Annual Report.cpp
@@ -1,11 +1,31 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 33179                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/33179                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/30 22:05:46 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> pages(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> pages[i];
+        if (pages[i] % 2 == 1)
+        {
+            pages[i]++;
+        }
+    }
+
+    int total_sheets = 0;
+    for (int i = 0; i < n; i++)
+    {
+        total_sheets += pages[i] / 2;
+    }
+
+    cout << total_sheets << endl;
+    return 0;
+}
