3e12740e7043bef17226efe5f0e6b9aee5535d0c
Leapense <iso9661@gmail.com>
2025-01-30 16:01:07 +0900

    `Added solution for problem 25090 with a main function and input handling`

--- 25090번： d1000000/d1000000.py
+++ 25090번： d1000000/d1000000.py
@@ -1,11 +1,20 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 25090                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/25090                          #+#        #+#      #+#     #
-#    Solved: 2025/01/30 15:49:13 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    T = int(input[idx])
+    idx += 1
+    for case in range(1, T + 1):
+        N = int(input[idx])
+        idx += 1
+        S = list(map(int, input[idx:idx + N]))
+        idx += N
+        S.sort()
+        current = 0
+        for s in S:
+            if s >= current + 1:
+                current += 1
+        print(f"Case #{case}: {current}")
+
+if __name__ == "__main__":
+    main()
