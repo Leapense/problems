6443fc1dae756c45da261dc6734b037d4a8a6180
Leapense <iso9661@gmail.com>
2025-01-30 14:35:17 +0900

    `Refactor code to use object-oriented programming and improve readability`

--- 25045번： 비즈마켓/비즈마켓.py
+++ 25045번： 비즈마켓/비즈마켓.py
@@ -1,16 +1,43 @@
-n, m = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+import sys
 
-a.sort(reverse=True)
-b.sort(reverse=True)
+class BizMarket:
+    def __init__(self):
+        self.N = 0
+        self.M = 0
+        self.A = []
+        self.B = []
+    def read_input(self):
+        input = sys.stdin.read().split()
+        ptr = 0
+        self.N = int(input[ptr])
+        self.M = int(input[ptr + 1])
+        ptr += 2
+        self.A = list(map(int, input[ptr:ptr + self.N]))
+        ptr += self.N
+        self.B = list(map(int, input[ptr:ptr + self.M]))
 
-total = 0
-i = 0
+    def compute_max_satisfaction(self):
+        self.A.sort(reverse=True)
+        self.B.sort()
+        prefix_A = [0] * (self.N + 1)
+        for i in range(1, self.N + 1):
+            prefix_A[i] = prefix_A[i - 1] + self.A[i - 1]
+        prefix_B = [0] * (self.M + 1)
+        for i in range(1, self.M + 1):
+            prefix_B[i] = prefix_B[i - 1] + self.B[i - 1]
+        
+        max_sum = 0
+        max_k = min(self.N, self.M)
+        for k in range(1, max_k + 1):
+            if self.A[k - 1] >= self.B[k - 1]:
+                current_sum = prefix_A[k] - prefix_B[k]
+                if current_sum > max_sum:
+                    max_sum = current_sum
+        return max_sum
+    def solve(self):
+        self.read_input()
+        result = self.compute_max_satisfaction()
+        print(result)
 
-for payment in b:
-    if i < n and a[i] >= payment:
-        total += a[i] - payment
-        i += 1
-
-print(total)
+if __name__ == "__main__":
+    BizMarket().solve()
