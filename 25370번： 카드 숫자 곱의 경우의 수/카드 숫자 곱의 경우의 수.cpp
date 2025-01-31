df551bce748693ae6e53b11ef231ca9011a676e7
Leapense <iso9661@gmail.com>
2025-01-31 13:04:12 +0900

    `Added CardMultiplier class to solve problem 25370, generating unique products of numbers 1-9 for a given depth.`

--- 25370번： 카드 숫자 곱의 경우의 수/카드 숫자 곱의 경우의 수.cpp
+++ 25370번： 카드 숫자 곱의 경우의 수/카드 숫자 곱의 경우의 수.cpp
@@ -1,11 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25370                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25370                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/31 12:58:15 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <set>
+#include <vector>
+
+class CardMultiplier
+{
+private:
+    int n;
+    std::set<int> results;
+    std::vector<int> numbers;
+
+    void generateProducts(int depth, int product)
+    {
+        if (depth == n)
+        {
+            results.insert(product);
+            return;
+        }
+
+        for (int num : numbers)
+        {
+            generateProducts(depth + 1, product * num);
+        }
+    }
+
+public:
+    CardMultiplier(int num) : n(num), numbers({1, 2, 3, 4, 5, 6, 7, 8, 9}) {}
+
+    int countUniqueProducts()
+    {
+        generateProducts(0, 1);
+        return results.size();
+    }
+};
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    CardMultiplier solver(n);
+
+    std::cout << solver.countUniqueProducts() << std::endl;
+
+    return 0;
+}
