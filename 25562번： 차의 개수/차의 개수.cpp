67b716bc53ce43cb0784a6f889273b44db18c301
Leapense <iso9661@gmail.com>
2025-01-31 18:48:49 +0900

    `Added new code for problem 25562, including a CarDifference class and main function.`

--- 25562번： 차의 개수/차의 개수.cpp
+++ 25562번： 차의 개수/차의 개수.cpp
@@ -1,11 +1,57 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25562                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25562                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/31 18:42:56 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+class CarDifference
+{
+public:
+    int N;
+    CarDifference(int n) : N(n) {}
+
+    void solve()
+    {
+        long long max_diff = (long long)N * (N - 1) / 2;
+        cout << max_diff << "\n";
+        vector<long long> max_set;
+
+        for (int i = 0; i < N; i++)
+        {
+            max_set.push_back(1LL << i);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            if (i > 0)
+                cout << " ";
+            cout << max_set[i];
+        }
+        cout << "\n";
+
+        long long min_diff = N - 1;
+        cout << min_diff << "\n";
+        vector<long long> min_set;
+        for (int i = 0; i < N; i++)
+        {
+            min_set.push_back(1 + i);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            if (i > 0)
+                cout << " ";
+            cout << min_set[i];
+        }
+
+        cout << "\n";
+    }
+};
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    CarDifference cd(N);
+    cd.solve();
+
+    return 0;
+}
