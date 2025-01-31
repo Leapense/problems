c8634fd3c31169ff4ed5ec449ae67d383e86a9f4
Leapense <iso9661@gmail.com>
2025-01-31 11:08:12 +0900

    `Added TypoFixer class to calculate minimum key presses for two strings`

--- 25276번： Sperhling/Sperhling.cpp
+++ 25276번： Sperhling/Sperhling.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25276                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25276                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/31 10:55:35 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+class TypoFixer
+{
+public:
+    TypoFixer(const std::string &s1, const std::string &s2) : S1(s1), S2(s2) {}
+
+    int minKeyPresses()
+    {
+        int commonLength = longestCommonSuffixPrefix();
+        int deleteCount = S1.size() - commonLength;
+        int addCount = S2.size() - commonLength;
+        return deleteCount + addCount;
+    }
+
+private:
+    std::string S1, S2;
+
+    int longestCommonSuffixPrefix()
+    {
+        int len = std::min(S1.size(), S2.size());
+        int i = 0;
+        while (i < len && S1[i] == S2[i])
+        {
+            i++;
+        }
+
+        return i;
+    }
+};
+
+int main()
+{
+    std::string s1, s2;
+    std::cin >> s1 >> s2;
+    TypoFixer fixer(s1, s2);
+    std::cout << fixer.minKeyPresses() << std::endl;
+
+    return 0;
+}
