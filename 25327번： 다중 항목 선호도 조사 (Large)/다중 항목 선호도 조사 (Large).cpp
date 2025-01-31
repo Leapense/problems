faa7e3019e8370f4371af6568d7ef770adeb8b49
Leapense <iso9661@gmail.com>
2025-01-31 11:23:37 +0900

    `Refactored code for Problem Number: 25327, added C++ implementation for PreferenceQuery class`

--- 25327번： 다중 항목 선호도 조사 (Large)/다중 항목 선호도 조사 (Large).cpp
+++ 25327번： 다중 항목 선호도 조사 (Large)/다중 항목 선호도 조사 (Large).cpp
@@ -1,11 +1,72 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25327                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25327                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/31 11:09:59 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class PreferenceQuery
+{
+private:
+    unordered_map<string, int> preferenceMap;
+    int n, m;
+
+    string createKey(const string &subject, const string &fruit, const string &color)
+    {
+        return subject + " " + fruit + " " + color;
+    }
+
+public:
+    PreferenceQuery(int n, int m) : n(n), m(m) {}
+
+    void addPreference(const string &subject, const string &fruit, const string &color)
+    {
+        string key = createKey(subject, fruit, color);
+        preferenceMap[key]++;
+    }
+
+    int query(const string &subject, const string &fruit, const string &color)
+    {
+        int count = 0;
+        for (const auto &entry : preferenceMap)
+        {
+            stringstream ss(entry.first);
+            string s, f, c;
+            ss >> s >> f >> c;
+
+            if ((subject == "-" || subject == s) &&
+                (fruit == "-" || fruit == f) &&
+                (color == "-" || color == c))
+            {
+                count += entry.second;
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    PreferenceQuery pq(n, m);
+
+    for (int i = 0; i < n; ++i)
+    {
+        string subject, fruit, color;
+        cin >> subject >> fruit >> color;
+        pq.addPreference(subject, fruit, color);
+    }
+
+    for (int i = 0; i < m; ++i)
+    {
+        string subject, fruit, color;
+        cin >> subject >> fruit >> color;
+        cout << pq.query(subject, fruit, color) << '\n';
+    }
+
+    return 0;
+}
