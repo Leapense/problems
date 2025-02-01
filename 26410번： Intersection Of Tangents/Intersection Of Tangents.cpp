2b1a91de1f94ab27b4a7cdb17020f83d25c78c1d
Leapense <iso9661@gmail.com>
2025-02-01 21:50:11 +0900

    `Refactored code for problem 26410: Intersection Of Tangents/Intersection Of Tangents.cpp`

--- 26410번： Intersection Of Tangents/Intersection Of Tangents.cpp
+++ 26410번： Intersection Of Tangents/Intersection Of Tangents.cpp
@@ -1,11 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 26410                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/26410                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 21:16:28 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+
+    long long x, y;
+    long long xmin = 1e9, ymin = 1e9;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x >> y;
+        xmin = min(xmin, x);
+        ymin = min(ymin, y);
+    }
+
+    cout << xmin << " " << ymin;
+
+    return 0;
+}
