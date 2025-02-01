96b22fb3aa98a5ac036e64012e0a64283c3c0f3d
Leapense <iso9661@gmail.com>
2025-02-01 13:17:39 +0900

    `Added new implementation for problem 25803, replacing original code with a C++ solution using the next permutation algorithm.`

--- 25803번： Soccer Standing Table/Soccer Standing Table.cpp
+++ 25803번： Soccer Standing Table/Soccer Standing Table.cpp
@@ -1,11 +1,31 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25803                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25803                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 13:13:40 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solve
+{
+public:
+    void run()
+    {
+        vector<int> v(5);
+        for (int i = 0; i < 5; i++)
+            cin >> v[i];
+        sort(v.begin(), v.end());
+
+        do
+        {
+            int MP = v[0], W = v[1], D = v[2], L = v[3], Pts = v[4];
+            if (MP == W + D + L && Pts == 3 * W + D)
+            {
+                cout << MP << " " << W << " " << D << " " << L << " " << Pts << endl;
+                break;
+            }
+        } while (next_permutation(v.begin(), v.end()));
+    }
+};
+
+int main()
+{
+    Solve s;
+    s.run();
+    return 0;
+}
