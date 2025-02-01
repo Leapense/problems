664cd322e739b3487f5a0e73ef69e93be18b460b
Leapense <iso9661@gmail.com>
2025-02-01 15:32:00 +0900

    `Refactored code for problem 25984: Extended Braille`

--- 25984번： Extended Braille/Extended Braille.cpp
+++ 25984번： Extended Braille/Extended Braille.cpp
@@ -1,11 +1,61 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25984                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25984                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 15:01:15 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    unordered_set<string> shapes;
+
+    for (int i = 0; i < n; i++)
+    {
+        int m;
+        cin >> m;
+        vector<pair<int, int>> dots(m);
+        for (int j = 0; j < m; j++)
+        {
+            cin >> dots[j].first >> dots[j].second;
+        }
+
+        pair<int, int> base = dots[0];
+        for (int j = 1; j < m; j++)
+        {
+            if (dots[j] < base)
+            {
+                base = dots[j];
+            }
+        }
+
+        for (int j = 0; j < m; j++)
+        {
+            dots[j].first -= base.first;
+            dots[j].second -= base.second;
+        }
+
+        sort(dots.begin(), dots.end());
+
+        ostringstream oss;
+
+        for (auto &p : dots)
+        {
+            oss << p.first << ":" << p.second << ",";
+        }
+
+        string shapeStr = oss.str();
+        shapes.insert(shapeStr);
+    }
+
+    cout << shapes.size() << endl;
+
+    return 0;
+}
