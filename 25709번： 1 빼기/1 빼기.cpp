618a93f65b3ecd01755eeed641894bea50755790
Leapense <iso9661@gmail.com>
2025-02-01 13:02:38 +0900

    `Added new solution for problem 25709 with detailed step-by-step logic and refactored code for better readability.`

--- 25709번： 1 빼기/1 빼기.cpp
+++ 25709번： 1 빼기/1 빼기.cpp
@@ -1,11 +1,65 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25709                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25709                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 12:28:28 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+class Solution
+{
+public:
+    long long solve()
+    {
+        long long N;
+        cin >> N;
+
+        long long steps = 0;
+
+        while (N > 0)
+        {
+            string s = to_string(N);
+            if (s.find('1') != string::npos)
+            {
+                long long candidate = LLONG_MAX;
+                for (int i = 0; i < s.size(); i++)
+                {
+                    if (s[i] == '1')
+                    {
+                        string tmp = s.substr(0, i) + s.substr(i + 1);
+                        long long num = (tmp == "" ? 0 : stoll(tmp));
+                        if (num < candidate)
+                            candidate = num;
+                    }
+                }
+                N = candidate;
+                steps++;
+            }
+            else
+            {
+                long long d = 1;
+                while (true)
+                {
+                    long long next = N - d;
+                    if (next == 0)
+                        break;
+                    if (to_string(next).find('1') != string::npos)
+                    {
+                        break;
+                    }
+                    d++;
+                }
+                N -= d;
+                steps += d;
+            }
+        }
+
+        return steps;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    cout << sol.solve();
+
+    return 0;
+}
