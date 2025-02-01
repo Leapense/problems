e60bf7222b170fb9e15bb697eb454c372ca9d6fd
Leapense <iso9661@gmail.com>
2025-02-01 12:26:45 +0900

    `Added solution for problem 25706, including code and main function`

--- 25706번： 자전거 묘기/자전거 묘기.cpp
+++ 25706번： 자전거 묘기/자전거 묘기.cpp
@@ -1,11 +1,59 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25706                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25706                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 12:16:01 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class BikeStunts
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int N;
+        cin >> N;
+
+        vector<int> jump(N + 1);
+        for (int i = 1; i <= N; i++)
+        {
+            cin >> jump[i];
+        }
+
+        vector<int> dp(N + 2, 0);
+
+        dp[N] = 1;
+
+        for (int i = N - 1; i >= 1; i--)
+        {
+            if (jump[i] == 0)
+            {
+                dp[i] = 1 + dp[i + 1];
+            }
+            else
+            {
+                int landing = i + jump[i] + 1;
+
+                if (landing <= N)
+                {
+                    dp[i] = 1 + dp[landing];
+                }
+                else
+                {
+                    dp[i] = 1;
+                }
+            }
+        }
+        for (int i = 1; i <= N; i++)
+        {
+            cout << dp[i] << (i == N ? "\n" : " ");
+        }
+    }
+};
+
+int main()
+{
+    BikeStunts bs;
+    bs.solve();
+
+    return 0;
+}
