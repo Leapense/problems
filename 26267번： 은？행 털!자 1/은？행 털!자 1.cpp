6b686fa92ac75949bca6cf03297f36349f193dc6
Leapense <iso9661@gmail.com>
2025-02-01 20:00:55 +0900

    `Refactored code for problem 26267 with improved formatting and functionality`

--- 26267번： 은？행 털!자 1/은？행 털!자 1.cpp
+++ 26267번： 은？행 털!자 1/은？행 털!자 1.cpp
@@ -1,11 +1,31 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 26267                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/26267                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 19:55:04 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    unordered_map<long long, long long> mp;
+    long long ans = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        long long X, T, C;
+        cin >> X >> T >> C;
+        long long key = X - T;
+        mp[key] += C;
+    }
+
+    for (auto &p : mp)
+    {
+        ans = max(ans, p.second);
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
