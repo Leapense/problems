1e35540b31c8afc7ca33375de58e89d344ce9e98
Leapense <iso9661@gmail.com>
2025-02-01 14:05:35 +0900

    `Added SGA President problem solution in C++`

--- 25873번： SGA President/SGA President.cpp
+++ 25873번： SGA President/SGA President.cpp
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25873                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25873                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 13:41:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class SGACandidate
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n;
+        cin >> n;
+
+        unordered_map<char, unordered_map<string, long long>> groups;
+        for (int i = 0; i < n; i++)
+        {
+            string name;
+            cin >> name;
+            groups[name[0]][name]++;
+        }
+
+        long long result = 0;
+        for (auto &group : groups)
+        {
+            long long total = 0, sumSquares = 0;
+            for (auto &entry : group.second)
+            {
+                total += entry.second;
+                sumSquares += entry.second * entry.second;
+            }
+
+            result += total * total - sumSquares;
+        }
+
+        cout << result << "\n";
+    }
+};
+
+int main()
+{
+    SGACandidate solver;
+    solver.solve();
+    return 0;
+}
