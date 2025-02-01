57277a7f90f5775fc9af722b4ddcc6ebe4bc8a22
Leapense <iso9661@gmail.com>
2025-02-01 12:15:46 +0900

    `Added Ramen.cpp solution for problem 25694 with input handling and logic implementation.`

--- 25694번： Ramen/Ramen.cpp
+++ 25694번： Ramen/Ramen.cpp
@@ -1,11 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25694                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25694                          #+#        #+#      #+#    */
-/*   Solved: 2025/01/31 21:53:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    vector<long long> a(N);
+    for (int i = 0; i < N; i++)
+        cin >> a[i];
+    if (N == 1)
+    {
+        cout << "YES";
+        return 0;
+    }
+    long long s = 0;
+    bool ok = true;
+    for (int k = 1; k < N; k++)
+    {
+        s += a[k - 1];
+        if (s <= 0)
+        {
+            ok = false;
+            break;
+        }
+    }
+    if (ok)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+    return 0;
+}
