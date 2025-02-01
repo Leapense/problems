2193e3923d3684936a48b7a6a7f599b26bb71173
Leapense <iso9661@gmail.com>
2025-02-01 22:48:00 +0900

    `Refactored code for problem 26449: Harvest`

--- 26449번： Harvest/Harvest.cpp
+++ 26449번： Harvest/Harvest.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 26449                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/26449                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 21:50:22 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> plants(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> plants[i].first >> plants[i].second;
+    }
+
+    int K;
+    cin >> K;
+
+    vector<int> h(K);
+
+    for (int i = 0; i < K; i++)
+    {
+        cin >> h[i];
+    }
+
+    int ans = 0;
+    for (int i = 0; i < N; i++)
+    {
+        auto it = lower_bound(h.begin(), h.end(), plants[i].first);
+        if (it != h.end() && *it <= plants[i].second)
+            ans++;
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
