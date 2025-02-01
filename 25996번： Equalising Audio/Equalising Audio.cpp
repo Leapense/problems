6ebc48c24a040a4140c8fe40849613e88091eda5
Leapense <iso9661@gmail.com>
2025-02-01 16:02:28 +0900

    `Added code for problem 25996: Equalising Audio, including input, calculation and output of equalised audio values.`

--- 25996번： Equalising Audio/Equalising Audio.cpp
+++ 25996번： Equalising Audio/Equalising Audio.cpp
@@ -1,11 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 25996                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/25996                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/01 15:36:42 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    double x;
+    cin >> n >> x;
+
+    double *a = new double[n];
+    double sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i] * a[i];
+    }
+
+    if (sum == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << fixed << setprecision(10) << a[i] << (i == n - 1 ? "\n" : " ");
+        }
+    }
+    else
+    {
+        double factor = sqrt(x * n / sum);
+        for (int i = 0; i < n; i++)
+        {
+            cout << fixed << setprecision(10) << a[i] * factor << (i == n - 1 ? "\n" : " ");
+        }
+    }
+
+    delete[] a;
+    return 0;
+}
