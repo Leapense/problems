5ca36f9f924567c72da1f594b5a744d68b7921f4
Leapense <iso9661@gmail.com>
2025-02-01 19:05:55 +0900

    `Refactored Multidimensional Hangman solution to read input from stdin and calculate the best word based on given patterns.`

--- 26206번： Multidimensional Hangman/Multidimensional Hangman.py
+++ 26206번： Multidimensional Hangman/Multidimensional Hangman.py
@@ -1,11 +1,26 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 26206                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/26206                          #+#        #+#      #+#     #
-#    Solved: 2025/02/01 18:55:34 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+input_data = sys.stdin.read().strip().split()
+N = int(input_data[0])
+C = int(input_data[1])
+patterns = input_data[2:]
+
+from collections import defaultdict
+counts = defaultdict(int)
+
+for pattern in patterns:
+    unknown_idx = pattern.find('*')
+    base = list(pattern)
+    for c in range(ord('a'), ord('z') + 1):
+        base[unknown_idx] = chr(c)
+        word = ''.join(base)
+        counts[word] += 1
+    base[unknown_idx] = '*'
+
+max_count = 0
+best_word = None
+for w, cnt in counts.items():
+    if cnt > max_count or (cnt == max_count and w < best_word):
+        max_count = cnt
+        best_word = w
+
+sys.stdout.write(best_word + ' ' + str(max_count) + '\n')
