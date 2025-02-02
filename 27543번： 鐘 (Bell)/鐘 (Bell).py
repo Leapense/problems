49416cea489f568fcacd9022f98560bf4f2fdcb4
Leapense <iso9661@gmail.com>
2025-02-02 22:37:58 +0900

    `Refactor: Update Bell problem solution to use sys.stdin and bisect`

--- 27543번： 鐘 (Bell)/鐘 (Bell).py
+++ 27543번： 鐘 (Bell)/鐘 (Bell).py
@@ -1,11 +1,31 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27543                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27543                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 22:29:58 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+import bisect
+
+class BellSolution:
+    def __init__(self):
+        data = sys.stdin.read().strip().split()
+        self.N = int(data[0])
+        self.M = int(data[1])
+        self.K = int(data[2])
+        self.A = list(map(int, data[3:3+self.N]))
+        self.B = list(map(int, data[3+self.N:3+self.N+self.M]))
+    def solve(self):
+        ans = []
+        for b in self.B:
+            i = bisect.bisect_left(self.A, b)
+            c = []
+            if i > 0:
+                c.append(abs(b - self.A[i-1]))
+            if i < self.N:
+                c.append(abs(b - self.A[i]))
+            d = min(c) if c else float('inf')
+            ans.append(self.K - d if d < self.K else 0)
+        return ans
+    
+def main():
+    s = BellSolution()
+    res = s.solve()
+    print('\n'.join(map(str, res)))
+
+if __name__ == "__main__":
+    main()
