c53ed4517da20ba8fdec6e06f75157a4615b88f8
Leapense <iso9661@gmail.com>
2025-02-02 20:47:02 +0900

    `Added DeliverySolver class to solve problem 27370`

--- 27370번： 친구와 배달하기/친구와 배달하기.py
+++ 27370번： 친구와 배달하기/친구와 배달하기.py
@@ -1,11 +1,50 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27370                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27370                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 20:36:54 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class DeliverySolver:
+    def solve(self):
+        import sys
+        input = sys.stdin.readline
+        T = int(input())
+        for _ in range(T):
+            N, PA, PB = map(int, input().split())
+            Xs = list(map(int, input().split()))
+            sumA = 0
+            sumB = 0
+            tieCount = 0
+            costTie = 0
+            for x in Xs:
+                distA = 2 * abs(x - PA)
+                distB = 2 * abs(x - PB)
+                if distA < distB:
+                    sumA += distA
+                elif distA > distB:
+                    sumB += distB
+                else:
+                    tieCount += 1
+                    costTie = distA
+
+            baseSum = sumA + sumB
+            totalSum = baseSum + tieCount * costTie
+            diff0 = sumA - sumB
+            if tieCount == 0:
+                print(totalSum, abs(diff0))
+                continue
+            def f(k):
+                return diff0 + costTie * (2 * k - tieCount)
+            crossNum = tieCount * costTie - diff0
+            crossDen = costTie
+            kFloat = crossNum / (2 * crossDen)
+            kFloor = int(kFloat)
+            candidates = [kFloor, kFloor + 1]
+            bestDiff = None
+
+            for cand in candidates:
+                if cand < 0:
+                    cand = 0
+                if cand > tieCount:
+                    cand = tieCount
+                val = abs(f(cand))
+                if bestDiff is None or val < bestDiff:
+                    bestDiff = val
+            print(totalSum, bestDiff)
+
+if __name__ == "__main__":
+    DeliverySolver().solve()
