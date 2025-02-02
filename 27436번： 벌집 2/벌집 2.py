9dd9309d7cb8ce06073abc11bf47e96cb28582a4
Leapense <iso9661@gmail.com>
2025-02-02 21:25:30 +0900

    `Refactored code for problem 27436 with a new class-based solution`

--- 27436번： 벌집 2/벌집 2.py
+++ 27436번： 벌집 2/벌집 2.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27436                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27436                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 20:55:14 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Beehive:
+    def __init__(self, N):
+        self.N = N
+
+    def solve(self):
+        if self.N == 1:
+            return 1
+        left, right = 1, 2000000000
+        while left < right:
+            mid = (left + right) // 2
+            if 1 + 3 * mid * (mid - 1) >= self.N:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+    
+def main():
+    import sys
+    N = int(sys.stdin.readline().strip())
+    beehive = Beehive(N)
+    print(beehive.solve())
+
+if __name__ == "__main__":
+    main()
