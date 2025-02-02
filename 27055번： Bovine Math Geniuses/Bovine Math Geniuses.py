c6fadd4b5c431c33336efea0d4c577c8eed3448a
Leapense <iso9661@gmail.com>
2025-02-02 19:49:45 +0900

    `Refactor: Bovine Math Geniuses problem solution`

--- 27055번： Bovine Math Geniuses/Bovine Math Geniuses.py
+++ 27055번： Bovine Math Geniuses/Bovine Math Geniuses.py
@@ -1,11 +1,30 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27055                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27055                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 19:40:51 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class BovineMath:
+    def __init__(self, start):
+        self.start = start
+
+    def next_num(self, x):
+        s = f"{x:06d}"
+        mid = int(s[1:5])
+        sq = mid * mid
+        return sq % 1000000
+    
+    def solve(self):
+        visited = [-1] * 1000000
+        n = self.start
+        visited[n] = 0
+        iteration = 0
+        while True:
+            iteration += 1
+            n = self.next_num(n)
+            if visited[n] == -1:
+                visited[n] = iteration
+            else:
+                first_loop_num = n
+                loop_length = iteration - visited[n]
+                print(first_loop_num, loop_length, iteration)
+                break
+
+if __name__ == "__main__":
+    start_number = int(input().strip())
+    solver = BovineMath(start_number)
+    solver.solve()
