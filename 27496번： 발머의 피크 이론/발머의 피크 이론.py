c43c048e7789785679dd07daef9458e898baab9a
Leapense <iso9661@gmail.com>
2025-02-02 22:16:37 +0900

    `Added new solution for problem 27496 in Python`

--- 27496번： 발머의 피크 이론/발머의 피크 이론.py
+++ 27496번： 발머의 피크 이론/발머의 피크 이론.py
@@ -1,11 +1,20 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27496                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27496                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 22:11:33 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Solution:
+    def solve(self):
+        import sys
+        input = sys.stdin.readline
+        N, L = map(int, input().split())
+        arr = list(map(int, input().split()))
+        prefix = [0] * (N + 1)
+        for i in range(N):
+            prefix[i + 1] = prefix[i] + arr[i]
+        count = 0
+        for i in range(1, N + 1):
+            start = i - L if i - L >= 0 else 0
+            s = prefix[i] - prefix[start]
+            c = s * 0.001
+            if 0.129 <= c <= 0.138:
+                count += 1
+        print(count)
+
+if __name__ == "__main__":
+    Solution().solve()
