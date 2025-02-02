7aa88949145c032c7ef185b6fcb4876c43c57877
Leapense <iso9661@gmail.com>
2025-02-02 17:46:25 +0900

    `Added Uniform Maker class to calculate minimum changes for uniform words`

--- 26853번： Uniform Maker/Uniform Maker.py
+++ 26853번： Uniform Maker/Uniform Maker.py
@@ -1,11 +1,30 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 26853                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/26853                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 17:34:56 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class UniformMaker:
+    def __init__(self, N, M, words):
+        self.N = N
+        self.M = M
+        self.words = words
+
+    def get_minimum_changes(self):
+        total_changes = 0
+        for i in range(self.M):
+            freq = [0] * 26
+
+            for word in self.words:
+                char_index = ord(word[i]) - ord('a')
+                freq[char_index] += 1
+
+            max_freq = max(freq)
+
+            changes_for_position = self.N - max_freq
+            total_changes += changes_for_position
+
+        return total_changes
+    
+if __name__ == "__main__":
+    import sys
+    input_data = sys.stdin.read().strip().split()
+    N, M = map(int, input_data[:2])
+    words = input_data[2:]
+
+    uniform_maker = UniformMaker(N, M, words)
+    print(uniform_maker.get_minimum_changes())
