fcdd2d760c6d38e13e2c1b3131763da107e1a8bd
Leapense <iso9661@gmail.com>
2025-02-02 15:02:20 +0900

    `Refactor Greedy Scheduler code to use priority queue and improve formatting`

--- 26567번： Greedy Scheduler/Greedy Scheduler.cpp
+++ 26567번： Greedy Scheduler/Greedy Scheduler.cpp
@@ -1,11 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 26567                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/26567                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/02 14:53:49 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        int n, c;
+        cin >> n >> c;
+        vector<long long> times(c);
+        for (int i = 0; i < c; i++)
+        {
+            cin >> times[i];
+        }
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+
+        for (int i = 1; i <= n; i++)
+        {
+            pq.push({0, i});
+        }
+
+        vector<int> result(c);
+        for (int i = 0; i < c; i++)
+        {
+            auto [finishTime, cashier] = pq.top();
+            pq.pop();
+            result[i] = cashier;
+            pq.push({finishTime + times[i], cashier});
+        }
+
+        for (int i = 0; i < c; i++)
+        {
+            cout << result[i];
+            if (i < c - 1)
+                cout << " ";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
