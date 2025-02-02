3a6ed241763fd14ed2d3db5751092750bc8c87ec
Leapense <iso9661@gmail.com>
2025-02-02 15:29:58 +0900

    `Added solution for problem 26762, Pionki, in C++`

--- 26762번： Pionki/Pionki.cpp
+++ 26762번： Pionki/Pionki.cpp
@@ -1,11 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 26762                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/26762                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/02 15:22:34 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<long long> rowCount(N, 0), colCount(M, 0);
+    long long totalPawns = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        string row;
+        cin >> row;
+        for (int j = 0; j < M; j++)
+        {
+            if (row[j] == '#')
+            {
+                rowCount[i]++;
+                colCount[j]++;
+                totalPawns++;
+            }
+        }
+    }
+
+    if (totalPawns == 0)
+    {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    long long maxRow = 0, maxCol = 0;
+    for (int i = 0; i < N; i++)
+    {
+        maxRow = max(maxRow, rowCount[i]);
+    }
+    for (int j = 0; j < M; j++)
+    {
+        maxCol = max(maxCol, colCount[j]);
+    }
+
+    long long answer = 2LL * totalPawns - (maxRow + maxCol);
+    cout << answer << "\n";
+
+    return 0;
+}
