e08b0088dd68e54b2c2c1962d309b5fd3b0344e6
Leapense <iso9661@gmail.com>
2025-02-02 23:04:20 +0900

    `Refactored Boredom Buster (Easy) problem solution to use a class-based approach.`

--- 27690번： Boredom buster (Easy)/Boredom buster (Easy).py
+++ 27690번： Boredom buster (Easy)/Boredom buster (Easy).py
@@ -1,11 +1,17 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27690                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27690                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 22:59:44 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class BoredomBuster:
+    def __init__(self):
+        import sys
+        self.input_data = sys.stdin.read().split()
+        self.t = int(self.input_data[0])
+        self.results = []
+
+    def solve(self):
+        index = 1
+        for _ in range(self.t):
+            n = int(self.input_data[index])
+            index += 1
+            self.results.append(str((n * (n - 1)) // 2))
+        print("\n".join(self.results))
+
+if __name__ == "__main__":
+    BoredomBuster().solve()
