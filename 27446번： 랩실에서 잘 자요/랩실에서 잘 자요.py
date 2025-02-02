f1cc6c713fccaeb3d93dd6d017c3f2d1648c58df
Leapense <iso9661@gmail.com>
2025-02-02 21:44:06 +0900

    `Added solution for problem 27446 in Python`

--- 27446번： 랩실에서 잘 자요/랩실에서 잘 자요.py
+++ 27446번： 랩실에서 잘 자요/랩실에서 잘 자요.py
@@ -1,11 +1,28 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27446                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27446                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 21:25:39 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Solution:
+    def solve(self):
+        import sys
+        input_data = sys.stdin.read().strip().split()
+        N, M = map(int, input_data[:2])
+        if M > 0:
+            pages = list(map(int, input_data[2:2+M]))
+        else:
+            pages = []
+
+        have = set(pages)
+        missing = [i for i in range(1, N + 1) if i not in have]
+        if not missing:
+            print(0)
+            return
+        
+        missing.sort()
+        k = len(missing)
+        dp = [0] * (k + 1)
+        for i in range(k - 1, -1, -1):
+            dp[i] = float('inf')
+            for j in range(i, k):
+                cost = 5 + 2 * (missing[j] - missing[i] + 1)
+                dp[i] = min(dp[i], cost + dp[j+1])
+        print(dp[0])
+
+if __name__ == "__main__":
+    Solution().solve()
