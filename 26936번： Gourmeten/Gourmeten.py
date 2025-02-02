941c10acab5647f24ea5087e2e456c8aa0e28a56
Leapense <iso9661@gmail.com>
2025-02-02 19:01:38 +0900

    `Refactor Gourmeten class to Gourmet class, simplify solve method and input handling.`

--- 26936번： Gourmeten/Gourmeten.py
+++ 26936번： Gourmeten/Gourmeten.py
@@ -1,34 +1,17 @@
-class Gourmeten:
-    def __init__(self, M, N, times):
-        self.M = M
-        self.N = N
-        self.times = times
-
+class Gourmet:
+    def __init__(self):
+        import sys
+        self.data = sys.stdin.read().split()
+        self.M = int(self.data[0])
+        self.N = int(self.data[1])
+        self.times = list(map(int, self.data[2:2+self.N]))
     def solve(self):
-        M = self.M
-        dp = [0] * (M + 1)
+        dp = [0] * (self.M + 1)
         dp[0] = 1
-        multiples = []
-        for t in self.times:
-            mult = []
-            k = 1
-            while k * t <= M:
-                mult.append(k * t)
-                k += 1
-            multiples.append(mult)
-        for m in range(1, M + 1):
-            for mult in multiples:
-                for time in mult:
-                    if time > m:
-                        break
-                    dp[m] += dp[m - time]
-        return dp[M]
-
-if __name__ == '__main__':
-    import sys
-    input_data = sys.stdin.read().split()
-    M = int(input_data[0])
-    N = int(input_data[1])
-    times = list(map(int, input_data[2:2+N]))
-    solver = Gourmeten(M, N, times)
-    print(solver.solve())
+        for m in range(1, self.M + 1):
+            for t in self.times:
+                if m >= t:
+                    dp[m] += dp[m - t]
+        print(dp[self.M])
+if __name__ == "__main__":
+    Gourmet().solve()
