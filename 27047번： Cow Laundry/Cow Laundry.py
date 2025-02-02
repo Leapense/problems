7001e6a8e57563ac6383c18ce6b049a0cfc8d7ce
Leapense <iso9661@gmail.com>
2025-02-02 19:29:44 +0900

    `Added Cow Laundry problem solution with a class-based approach`

--- 27047번： Cow Laundry/Cow Laundry.py
+++ 27047번： Cow Laundry/Cow Laundry.py
@@ -1,11 +1,35 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27047                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27047                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 19:21:37 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class CowLaundrySolver:
+    def __init__(self, n, wires):
+        self.n = n
+        self.top = []
+        self.bottom = []
+        for t, b in wires:
+            self.top.append(t)
+            self.bottom.append(b)
+
+    def solve(self):
+        pos_in_bottom = [0] * (self.n + 1)
+        for i in range(self.n):
+            pos_in_bottom[self.bottom[i]] = i + 1
+
+        sequence = []
+        for i in range(self.n):
+            sequence.append(pos_in_bottom[self.top[i]])
+        return self.count_inversions(sequence)
+    
+    def count_inversions(self, arr):
+        inversions = 0
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                if arr[i] > arr[j]:
+                    inversions += 1
+        return inversions
+    
+if __name__ == "__main__":
+    n = int(input())
+    wires = []
+    for _ in range(n):
+        t, b = map(int, input().split())
+        wires.append((t, b))
+    solver = CowLaundrySolver(n, wires)
+    print(solver.solve())
