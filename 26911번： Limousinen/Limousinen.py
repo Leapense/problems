d71b285fbc9533d62f7812151653ee53e84c9ee4
Leapense <iso9661@gmail.com>
2025-02-02 18:12:52 +0900

    `Added new code for problem 26911, replacing existing Limousinen.py file with a new implementation using a Solver class and reading input from stdin.`

--- 26911번： Limousinen/Limousinen.py
+++ 26911번： Limousinen/Limousinen.py
@@ -1,11 +1,37 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 26911                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/26911                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 17:55:31 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Solver:
+    def __init__(self):
+        pass
+
+    def solve(self):
+        import sys
+        input_data = sys.stdin.read().strip().split()
+        N = int(input_data[0])
+        T = int(input_data[1])
+        coords = input_data[2:]
+
+        costs = []
+        index = 0
+
+        for _ in range(N):
+            x = int(coords[index]); y = int(coords[index + 1])
+            index += 2
+            cost = 2 * (abs(x) + abs(y))
+            costs.append(cost)
+
+        costs.sort()
+
+        time_sum = 0
+        count = 0
+        for c in costs:
+            time_sum += c
+            if time_sum > T:
+                break
+            count += 1
+        print(count)
+
+def main():
+    solver = Solver()
+    solver.solve()
+
+if __name__ == '__main__':
+    main()
