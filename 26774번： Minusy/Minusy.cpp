740684bab77274f1bc3cfba93cd929bee9c2acf6
Leapense <iso9661@gmail.com>
2025-02-02 17:11:57 +0900

    `Refactored code with improved performance and readability`

--- 26774번： Minusy/Minusy.cpp
+++ 26774번： Minusy/Minusy.cpp
@@ -4,75 +4,100 @@
 #include <algorithm>
 using namespace std;
 
+struct Segment
+{
+    char ch;
+    int cnt;
+};
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     string s;
     cin >> s;
-    vector<pair<char, int>> seg;
     int n = s.size();
-    int i = 0;
-    while (i < n)
+    vector<Segment> seg;
+    seg.push_back({s[0], 1});
+    for (int i = 1; i < n; i++)
     {
-        char cur = s[i];
-        int cnt = 0;
-        while (i < n && s[i] == cur)
-        {
-            cnt++;
-            i++;
-        }
-        seg.push_back({cur, cnt});
-    }
-    vector<int> runs;
-    vector<int> oddGaps;
-    int curr = 0;
-    if (!seg.empty())
-    {
-        if (seg[0].first == '+')
-            curr = seg[0].second;
+        if (s[i] == seg.back().ch)
+            seg.back().cnt++;
         else
-        {
-            if (seg[0].second % 2 == 0)
-                curr = seg[0].second / 2;
-            else
-                curr = 0;
-        }
+            seg.push_back({s[i], 1});
     }
-    for (int j = 1; j < seg.size(); j++)
-    {
-        if (seg[j].first == '+')
-        {
-            curr += seg[j].second;
-        }
-        else
-        {
-            if (seg[j].second % 2 == 0)
-            {
-                curr += seg[j].second / 2;
-            }
-            else
-            {
-                runs.push_back(curr);
-                oddGaps.push_back(seg[j].second);
-                curr = 0;
-            }
-        }
-    }
-    runs.push_back(curr);
+    int m = seg.size();
     int ans = 0;
-    for (int r : runs)
-        ans = max(ans, r);
-    for (int k = 0; k < oddGaps.size(); k++)
+    bool hasPlus = false;
+    for (auto &p : seg)
     {
-        int gapVal = oddGaps[k] / 2;
-        int cand = 0;
-        if (k < runs.size() && k + 1 < runs.size())
+        if (p.ch == '+')
         {
-            cand = max(runs[k] + gapVal, gapVal + runs[k + 1]);
+            hasPlus = true;
+            ans = max(ans, p.cnt);
         }
-        ans = max(ans, cand);
     }
-    cout << ans;
+    if (!hasPlus)
+    {
+        cout << (n / 2) << "\n";
+        return 0;
+    }
+    vector<int> left_merged(m, 0), right_merged(m, 0);
+    for (int i = 0; i < m; i++)
+    {
+        if (seg[i].ch == '+')
+        {
+            if (i - 1 >= 0 && seg[i - 1].ch == '-' && seg[i - 1].cnt % 2 == 0)
+            {
+                int prev = (i - 2 >= 0 ? left_merged[i - 2] : 0);
+                left_merged[i] = prev + (seg[i - 1].cnt / 2) + seg[i].cnt;
+            }
+            else
+            {
+                left_merged[i] = seg[i].cnt;
+            }
+            ans = max(ans, left_merged[i]);
+        }
+    }
+    for (int i = m - 1; i >= 0; i--)
+    {
+        if (seg[i].ch == '+')
+        {
+            if (i + 1 < m && seg[i + 1].ch == '-' && seg[i + 1].cnt % 2 == 0)
+            {
+                int nxt = (i + 2 < m ? right_merged[i + 2] : 0);
+                right_merged[i] = seg[i].cnt + (seg[i + 1].cnt / 2) + nxt;
+            }
+            else
+            {
+                right_merged[i] = seg[i].cnt;
+            }
+            ans = max(ans, right_merged[i]);
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        if (seg[i].ch == '-')
+        {
+            int conv = seg[i].cnt / 2;
+            if (i > 0 && i < m - 1 && seg[i - 1].ch == '+' && seg[i + 1].ch == '+')
+            {
+                ans = max(ans, max(left_merged[i - 1] + conv, right_merged[i + 1] + conv));
+            }
+            if (i == 0 && m > 1 && seg[i + 1].ch == '+')
+            {
+                ans = max(ans, conv + right_merged[i + 1]);
+            }
+            if (i == m - 1 && m > 1 && seg[i - 1].ch == '+')
+            {
+                ans = max(ans, left_merged[i - 1] + conv);
+            }
+            if (m == 1)
+            {
+                ans = max(ans, conv);
+            }
+        }
+    }
+    cout << ans << "\n";
     return 0;
 }
