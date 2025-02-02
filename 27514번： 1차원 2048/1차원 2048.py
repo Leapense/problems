0bd29fc8d622d79a990ec24103071cc2e3345e57
Leapense <iso9661@gmail.com>
2025-02-02 22:23:13 +0900

    `Added solution for problem 27514, refactored code with improved readability and structure.`

--- 27514번： 1차원 2048/1차원 2048.py
+++ 27514번： 1차원 2048/1차원 2048.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27514                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27514                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 22:18:02 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Solution:
+    def solve(self):
+        import sys
+        input = sys.stdin.readline
+        N = int(input().strip())
+        arr = list(map(int, input().split()))
+        count = [0] * 63
+        for val in arr:
+            if val > 0:
+                k = val.bit_length() - 1
+                count[k] += 1
+
+        for i in range(63):
+            pairs = count[i] // 2
+            if i < 62:
+                count[i + 1] += pairs
+            count[i] %= 2
+        for i in range(62, -1, -1):
+            if count[i] > 0:
+                print(2 ** i)
+                return
+            
+if __name__ == "__main__":
+    Solution().solve()
