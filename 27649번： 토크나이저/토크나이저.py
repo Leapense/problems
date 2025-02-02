9f05ec07a5d12e912c47b3e8821dae2a40830231
Leapense <iso9661@gmail.com>
2025-02-02 22:58:26 +0900

    `Added Tokenizer class to parse and filter input string`

--- 27649번： 토크나이저/토크나이저.py
+++ 27649번： 토크나이저/토크나이저.py
@@ -1,11 +1,48 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27649                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27649                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 22:50:31 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Tokenizer:
+    def __init__(self, s):
+        self.s = s
+
+    def tokenize(self):
+        tokens = []
+        i = 0
+        n = len(self.s)
+        current = ""
+        while i < n:
+            ch = self.s[i]
+            if ch == ' ':
+                if current:
+                    tokens.append(current)
+                    current = ""
+                while i < n and self.s[i] == ' ':
+                    i += 1
+                continue
+            if ch in "<>()":
+                if current:
+                    tokens.append(current)
+                    current = ""
+                tokens.append(ch)
+                i += 1
+                continue
+            if ch == '&' or ch == '|':
+                if current:
+                    tokens.append(current)
+                    current = ""
+                tokens.append(self.s[i:i+2])
+                i += 2
+                continue
+            current += ch
+            i += 1
+        if current:
+            tokens.append(current)
+        return tokens
+
+    def get_result(self):
+        tokens = self.tokenize()
+        filtered = [t for t in tokens if t != " "]
+        return " ".join(filtered)
+
+if __name__ == "__main__":
+    import sys
+    s = sys.stdin.readline().rstrip('\n')
+    tokenizer = Tokenizer(s)
+    print(tokenizer.get_result())
