697e716c831e926f9d955e11578a1df3878cf720
Leapense <iso9661@gmail.com>
2025-02-02 20:18:15 +0900

    `Added Python code for solving problem 27260, including a class-based solution and a main execution block.`

--- 27260번： Красивые перестановки/Красивые перестановки.py
+++ 27260번： Красивые перестановки/Красивые перестановки.py
@@ -1,11 +1,19 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27260                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27260                          #+#        #+#      #+#     #
-#    Solved: 2025/02/02 20:13:10 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class Solver:
+    def solve(self):
+        import sys
+        data = sys.stdin.read().strip().split()
+        n = int(data[0])
+        k = int(data[1])
+        from itertools import permutations
+        arr = range(1, n + 1)
+        ans = 0
+        for perm in permutations(arr):
+            s = 0
+            for i in range(n - 1):
+                s += perm[i] * perm[i + 1]
+            if s % k == 0:
+                ans += 1
+        print(ans)
+
+if __name__ == "__main__":
+    Solver().solve()
