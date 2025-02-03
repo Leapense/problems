1bb149b450124f933422f5e109db659bc249a112
Leapense <iso9661@gmail.com>
2025-02-03 09:36:00 +0900

    `Added code for problem 27920, implementing a CardFlip class to solve the problem.`

--- 27920번： 카드 뒤집기/카드 뒤집기.py
+++ 27920번： 카드 뒤집기/카드 뒤집기.py
@@ -1,11 +1,48 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27920                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27920                          #+#        #+#      #+#     #
-#    Solved: 2025/02/03 09:27:30 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class CardFlip:
+    def solve(self):
+        import sys
+        input_data = sys.stdin.read().split()
+        if not input_data:
+            return
+        
+        n = int(input_data[0])
+        p = [0] * n
+        if n % 2 == 0:
+            l, r, idx = 1, n, 0
+            while l < r:
+                p[idx] = l
+                p[idx + 1] = r
+                l += 1
+                r -= 1
+                idx += 2
+        else:
+            m = (n + 1) // 2
+            p[0] = m
+            left, right, idx = m - 1, m + 1, 1
+            while left >= 1 or right <= n:
+                if right <= n:
+                    p[idx] = right
+                    idx += 1
+                    right += 1
+                if left >= 1:
+                    p[idx] = left
+                    idx += 1
+                    left -= 1
+        b = [0] * n
+        used = [False] * (n + 1)
+        for i in range(n - 1):
+            diff = abs(p[i + 1] - p[i])
+            b[p[i] - 1] = diff
+            used[diff] = True
+        for num in range(1, n + 1):
+            if not used[num]:
+                b[p[n - 1] - 1] = num
+                break
+        out = []
+        out.append("YES")
+        out.append(" ".join(map(str, b)))
+        out.append(" ".join(map(str, p)))
+        sys.stdout.write("\n".join(out))
+
+if __name__ == "__main__":
+    CardFlip().solve()
