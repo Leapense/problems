2833d49e7f9aea14a33b297fcf49670993652c80
Leapense <iso9661@gmail.com>
2025-02-03 16:41:55 +0900

    `Added logic to read additional input and perform DFS traversal
    This commit adds new code to read two integers N and M, and three integers for each of two tasks. It then calls a DFS function with initial parameters. The DFS function is modified to

--- 28075번： 스파이/Main.java
+++ 28075번： 스파이/Main.java
@@ -7,5 +7,35 @@ public class Main {
 
     public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        for (int i = 0; i < 3; i++) {
+            score[0][i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            score[1][i] = sc.nextInt();
+        }
+
+        dfs(0, -1, 0);
+        System.out.println(count);
+    }
+
+    static void dfs(int day, int prev, int sum) {
+        if (day == N) {
+            if (sum >= M)
+                count++;
+            return;
+        }
+
+        for (int task = 0; task < 2; task++) {
+            for (int loc = 0; loc < 3; loc++) {
+                int add = score[task][loc];
+                if (loc == prev)
+                    add /= 2;
+                dfs(day + 1, loc, sum + add);
+            }
+        }
     }
 }
