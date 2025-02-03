46a8ba93cbd1e293c2acbf48550c93ca252753d2
Leapense <iso9661@gmail.com>
2025-02-03 22:17:13 +0900

    `Refactored code for problem 28941 with new logic and formatting`

--- 28941번： Цифровая загадка/Цифровая загадка.py
+++ 28941번： Цифровая загадка/Цифровая загадка.py
@@ -1,11 +1,16 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 28941                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/28941                          #+#        #+#      #+#     #
-#    Solved: 2025/02/03 22:07:58 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+increments = []
+
+for num in nums:
+    s = str(num)
+    length = len(s)
+    for i, ch in enumerate(s):
+        d = int(ch)
+        if d < 9:
+            pos = length - i - 1
+            inc = (9 - d) * (10 ** pos)
+            increments.append(inc)
+increments.sort(reverse=True)
+result = sum(increments[:k])
+print(result)
