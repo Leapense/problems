e04d904ac9c1070d99e05131b1c2080e171ac1d9
Leapense <iso9661@gmail.com>
2025-02-03 18:53:09 +0900

    `Added C++ solution for problem 28517 with diff and commit message`

--- 28517번： Стрельба из пушки/Стрельба из пушки.cpp
+++ 28517번： Стрельба из пушки/Стрельба из пушки.cpp
@@ -1,11 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28517                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28517                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 18:41:55 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+class Solution
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        long long d, h, w, k;
+        cin >> d >> h >> w >> k;
+
+        double dd = d, hh = h, ww = w, kk = k;
+        double ans = 1e18;
+        bool possible = false;
+        double thetaDirect = atan(ww / dd);
+        if (2 * ww <= hh)
+        {
+            double distHouse = 2 * dd / cos(thetaDirect);
+            if (distHouse <= kk + 1e-9)
+            {
+                ans = thetaDirect;
+                possible = true;
+            }
+        }
+
+        if (kk >= 2 * dd)
+        {
+            double thetaVertical = acos(2 * dd / kk);
+            if (dd * tan(thetaVertical) + 1e-9 >= ww)
+            {
+                if (!possible || thetaVertical < ans)
+                {
+                    ans = thetaVertical;
+                    possible = true;
+                }
+            }
+        }
+        if (!possible)
+            cout << -1;
+        else
+            cout << fixed << setprecision(4) << (ans * 180.0 / M_PI);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    sol.solve();
+
+    return 0;
+}
