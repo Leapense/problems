3ff8566c7e4440beeb127b869be1e0bb50e15895
Leapense <iso9661@gmail.com>
2025-02-03 16:04:42 +0900

    `Refactored code for problem 27966 with added import statement, main method, and output handling.`

--- 27966번： △/Main.java
+++ 27966번： △/Main.java
@@ -1,11 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 27966 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/27966 #+# #+# #+# */
-/* Solved: 2025/02/03 15:57:51 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+
+public class Main {
+    public static void main(String... args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int N = Integer.parseInt(br.readLine());
+        long result = (long) (N - 1) * (N - 1);
+        out.println(result);
+
+        for (int i = 2; i <= N; i++) {
+            out.println("1 " + i);
+        }
+        out.flush();
+    }
+}
