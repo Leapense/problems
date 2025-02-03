0410e5956c0dad84d2ef889189472c843ae74518
Leapense <iso9661@gmail.com>
2025-02-03 18:39:40 +0900

    `Added C++ code for solving problem 28438, including a MatrixOperator class and main function for processing operations and printing results.`

--- 28438번： 행렬 연산 (행렬 계산하기)/행렬 연산 (행렬 계산하기).cpp
+++ 28438번： 행렬 연산 (행렬 계산하기)/행렬 연산 (행렬 계산하기).cpp
@@ -1,11 +1,61 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28438                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28438                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 18:29:22 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MatrixOperator
+{
+public:
+    int N, M, Q;
+    vector<long long> row, col;
+
+    MatrixOperator(int n, int m, int q) : N(n), M(m), Q(q)
+    {
+        row.assign(n, 0);
+        col.assign(m, 0);
+    }
+
+    void processOperation(int type, int index, int value)
+    {
+        if (type == 1)
+        {
+            row[index - 1] += value;
+        }
+        else
+        {
+            col[index - 1] += value;
+        }
+    }
+
+    void printResult()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                cout << row[i] + col[j] << (j == M - 1 ? "\n" : " ");
+            }
+        }
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M, Q;
+    cin >> N >> M >> Q;
+
+    MatrixOperator solver(N, M, Q);
+    int type, index, value;
+
+    for (int i = 0; i < Q; i++)
+    {
+        cin >> type >> index >> value;
+        solver.processOperation(type, index, value);
+    }
+
+    solver.printResult();
+
+    return 0;
+}
