29ee3ba53926a4d2bb70a109ad5b6dfa91db6fca
Leapense <iso9661@gmail.com>
2025-02-03 18:25:50 +0900

    `Added C++ code for solving a problem, including a class Solution with a solve method and a main function, along with a diff of the code changes.`

--- 28418번： 회장님께 바치는 합성함수/회장님께 바치는 합성함수.cpp
+++ 28418번： 회장님께 바치는 합성함수/회장님께 바치는 합성함수.cpp
@@ -1,11 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28418                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28418                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 18:15:18 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+
+using namespace std;
+class Solution
+{
+public:
+    void solve()
+    {
+        int a, b, c, d, e;
+        cin >> a >> b >> c;
+        cin >> d >> e;
+
+        long long A = (long long)a * d * (d - 1);
+        long long B = 2LL * a * d * e;
+        long long C = (long long)a * e * e + b * e + c - (long long)c * d - e;
+
+        if (A == 0 && B == 0 && C == 0)
+        {
+            cout << "Nice";
+            return;
+        }
+        if (A == 0)
+        {
+            if (B != 0)
+            {
+                cout << "Remember my character";
+            }
+            else
+            {
+                cout << "Head on";
+            }
+
+            return;
+        }
+
+        long long D = B * B - 4LL * A * C;
+        if (D > 0)
+            cout << "Go ahead";
+        else if (D == 0)
+            cout << "Remember my character";
+        else
+            cout << "Head on";
+    }
+};
+
+int main()
+{
+    Solution sol;
+    sol.solve();
+
+    return 0;
+}
