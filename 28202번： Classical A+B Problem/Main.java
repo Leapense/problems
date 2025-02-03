f30ec2a08aa48a0338180cf1d5a17c438506d170
Leapense <iso9661@gmail.com>
2025-02-03 17:04:54 +0900

    `Refactor: Update Main.java to solve Classical A+B Problem with BigInteger and repdigit check`

--- 28202번： Classical A+B Problem/Main.java
+++ 28202번： Classical A+B Problem/Main.java
@@ -1,11 +1,56 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 28202 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/28202 #+# #+# #+# */
-/* Solved: 2025/02/03 16:52:54 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String nStr = br.readLine().trim();
+            BigInteger n = new BigInteger(nStr);
+            int L = nStr.length();
+            boolean found = false;
+            if (L == 1) {
+                out.println("1 1");
+                continue;
+            }
+            if (nStr.charAt(0) != '1') {
+                StringBuilder aBuilder = new StringBuilder();
+                for (int i = 0; i < L; i++) {
+                    aBuilder.append(nStr.charAt(0));
+                }
+                BigInteger aCandidate = new BigInteger(aBuilder.toString());
+                BigInteger bCandidate = n.subtract(aCandidate);
+                if (isRepdigit(bCandidate.toString())) {
+                    out.println(aCandidate + " " + bCandidate);
+                    continue;
+                }
+            }
+            for (int d = 1; d <= 9 && !found; d++) {
+                StringBuilder aBuilder = new StringBuilder();
+                for (int i = 0; i < L - 1; i++) {
+                    aBuilder.append((char) (d + '0'));
+                }
+                BigInteger aCandidate = new BigInteger(aBuilder.toString());
+                BigInteger bCandidate = n.subtract(aCandidate);
+                if (bCandidate.compareTo(BigInteger.ZERO) > 0 && isRepdigit(bCandidate.toString())) {
+                    out.println(aCandidate + " " + bCandidate);
+                    found = true;
+                    break;
+                }
+            }
+        }
+        out.flush();
+        out.close();
+    }
+
+    private static boolean isRepdigit(String s) {
+        char c = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != c)
+                return false;
+        }
+        return true;
+    }
+}
