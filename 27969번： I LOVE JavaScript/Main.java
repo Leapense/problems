c44a2f934572d79cb2206d5158112309d03d073c
Leapense <iso9661@gmail.com>
2025-02-03 16:11:31 +0900

    Change font size in VSCode settings from 15 to 12

--- 27969번： I LOVE JavaScript/Main.java
+++ 27969번： I LOVE JavaScript/Main.java
@@ -1,11 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 27969 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/27969 #+# #+# #+# */
-/* Solved: 2025/02/03 16:05:51 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String... args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, " ");
+        Stack<Long> stack = new Stack<>();
+
+        long result = 0;
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            if (token.equals("[")) {
+                stack.push(0L);
+            } else if (token.equals("]")) {
+                long cur = stack.pop()
+            }
+        }
+    }
+}
