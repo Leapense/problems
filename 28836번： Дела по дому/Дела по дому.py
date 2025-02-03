0507e4bec570f511b01d150c14b1e258fc086a35
Leapense <iso9661@gmail.com>
2025-02-03 20:44:20 +0900

    "Removed unused time calculations and updated minimum time calculation in Solution class."

--- 28836번： Дела по дому/Дела по дому.py
+++ 28836번： Дела по дому/Дела по дому.py
@@ -7,12 +7,11 @@ class Solution:
         T1 = a / v0 + c / v1 + b / v2
         T2 = b / v0 + c / v1 + a / v2
         T3 = a / v0 + a / v1 + b / v0 + b / v1
-        T4 = a / v0 + c / v1 + c / v2 + a / v2
-        T5 = a / v0 + c / v1 + c / v2 + b / v2
-        T6 = b / v0 + c / v1 + c / v2 + a / v2
+        T5 = a / v0 + c / v1 + c / v2 + a / v2
+        T6 = b / v0 + c / v1 + c / v2 + b / v2
         T7 = a / v0 + c / v0 + c / v1 + a / v2
         T8 = b / v0 + c / v0 + c / v1 + b / v2
-        ans = min(T1, T2, T3, T4, T5, T6, T7, T8)
+        ans = min(T1, T2, T3, T5, T6, T7, T8)
         sys.stdout.write("{:.15f}".format(ans))
 
 if __name__ == "__main__":
