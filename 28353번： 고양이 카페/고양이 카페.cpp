ba602408c668d714a292aa8728d92e918952055f
Leapense <iso9661@gmail.com>
2025-02-03 17:46:35 +0900

    `Added solution for problem 28353 in C++`

--- 28353번： 고양이 카페/고양이 카페.cpp
+++ 28353번： 고양이 카페/고양이 카페.cpp
@@ -1,11 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28353                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28353                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 17:35:08 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class CatCafe
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n;
+        long long k;
+
+        cin >> n >> k;
+        vector<long long> w(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> w[i];
+        sort(w.begin(), w.end());
+
+        int left = 0, right = n - 1, ans = 0;
+
+        while (left < right)
+        {
+            if (w[left] + w[right] <= k)
+            {
+                ans++;
+                left++;
+                right--;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        cout << ans;
+    }
+};
+
+int main()
+{
+    CatCafe cc;
+    cc.solve();
+
+    return 0;
+}
