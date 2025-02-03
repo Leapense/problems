5245c3ebf7860cf5aa3c6eff335e2bd154b26a9f
Leapense <iso9661@gmail.com>
2025-02-03 17:33:56 +0900

    `Added solution for problem 28298, including code and main function`

--- 28298번： 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cpp
+++ 28298번： 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cpp
@@ -1,11 +1,81 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28298                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28298                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 17:25:14 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int N, M, K;
+        cin >> N >> M >> K;
+
+        vector<string> grid(N);
+        for (int i = 0; i < N; i++)
+        {
+            cin >> grid[i];
+        }
+
+        int blockRows = N / K, blockCols = M / K;
+        vector<vector<char>> best(K, vector<char>(K));
+
+        long long changes = 0;
+
+        for (int r = 0; r < K; r++)
+        {
+            for (int c = 0; c < K; c++)
+            {
+                vector<int> freq(26, 0);
+                for (int bi = 0; bi < blockRows; bi++)
+                {
+                    for (int bj = 0; bj < blockCols; bj++)
+                    {
+                        int i = bi * K + r;
+                        int j = bj * K + c;
+                        freq[grid[i][j] - 'A']++;
+                    }
+                }
+
+                int maxFreq = 0;
+                int bestColor = 0;
+
+                for (int k = 0; k < 26; k++)
+                {
+                    if (freq[k] > maxFreq)
+                    {
+                        maxFreq = freq[k];
+                        bestColor = k;
+                    }
+                }
+                best[r][c] = 'A' + bestColor;
+                changes += (blockRows * blockCols - maxFreq);
+            }
+        }
+
+        cout << changes << "\n";
+
+        for (int i = 0; i < N; i++)
+        {
+            string row = "";
+            for (int j = 0; j < M; j++)
+            {
+                row.push_back(best[i % K][j % K]);
+            }
+
+            cout << row << "\n";
+        }
+    }
+};
+
+int main()
+{
+    Solution sol;
+    sol.solve();
+    return 0;
+}
