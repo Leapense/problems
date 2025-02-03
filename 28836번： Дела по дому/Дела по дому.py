fed6c75dfe33474aa9005f9bf7ce5f05aa2286c9
Leapense <iso9661@gmail.com>
2025-02-03 20:50:20 +0900

    `Refactor code to use integers instead of floats for input values and simplify calculations.`

--- 28836번： Дела по дому/Дела по дому.py
+++ 28836번： Дела по дому/Дела по дому.py
@@ -2,17 +2,16 @@ class Solution:
     def solve(self):
         import sys
         data = sys.stdin.read().split()
-        a, b, c = map(float, data[:3])
-        v0, v1, v2 = map(float, data[3:6])
-        T1 = a / v0 + c / v1 + b / v2
-        T2 = b / v0 + c / v1 + a / v2
-        T3 = a / v0 + a / v1 + b / v0 + b / v1
-        T5 = a / v0 + c / v1 + c / v2 + a / v2
-        T6 = b / v0 + c / v1 + c / v2 + b / v2
-        T7 = a / v0 + c / v0 + c / v1 + a / v2
-        T8 = b / v0 + c / v0 + c / v1 + b / v2
-        ans = min(T1, T2, T3, T5, T6, T7, T8)
-        sys.stdout.write("{:.15f}".format(ans))
+        if not data:
+            return
+        a, b, c, v0, v1, v2 = map(int, data)
+        t1 = a / v0 + c / v1 + b / v2
+        t2 = b / v0 + c / v1 + a / v2
+        t3 = a / v0 + a / v1 + b / v0 + b / v1
+        t4 = a / v0 + c / v0 + c / v1 + a / v2
+        t5 = b / v0 + c / v0 + c / v1 + b / v2
+        ans = min(t1, t2, t3, t4, t5)
+        sys.stdout.write(f"{ans:.15f}")
 
 if __name__ == "__main__":
     Solution().solve()
