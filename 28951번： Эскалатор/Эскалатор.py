308708d98d19cfb6ed3058981b9ee8113cfac665
Leapense <iso9661@gmail.com>
2025-02-03 22:48:32 +0900

    `Refactored code for problem 28951 with new sum_digits_count function and input handling`

--- 28951번： Эскалатор/Эскалатор.py
+++ 28951번： Эскалатор/Эскалатор.py
@@ -1,11 +1,17 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 28951                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/28951                          #+#        #+#      #+#     #
-#    Solved: 2025/02/03 22:42:35 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def sum_digits_count(m):
+    total = 0
+    d = 1
+    lower = 1
+    while lower <= m:
+        upper = min(m, 10 ** d - 1)
+        total += (upper - lower + 1) * d
+        d += 1
+        lower = 10 ** (d - 1) * 10
+    return total
+
+n = int(input().strip())
+m = n // 10
+result = sum_digits_count(m) + m + 1
+if n != 1 and n % 10 != 0:
+    result += len(str(n))
+print(result)
