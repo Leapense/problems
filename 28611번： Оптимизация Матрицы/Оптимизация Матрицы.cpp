0fd8d105eae5429edc94fa39a49d47f179a03965
Leapense <iso9661@gmail.com>
2025-02-03 19:27:51 +0900

    `Added new code for problem 28611, including a class Solver with a solve method and a main function to run it.`

--- 28611번： Оптимизация Матрицы/Оптимизация Матрицы.cpp
+++ 28611번： Оптимизация Матрицы/Оптимизация Матрицы.cpp
@@ -1,11 +1,44 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28611                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28611                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 19:16:20 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solver
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int n, k;
+        cin >> n >> k;
+        vector<long long> w(n + 1);
+        for (int i = 1; i <= n; i++)
+            cin >> w[i];
+        vector<long long> P(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            P[i] = P[i - 1] + w[i];
+        }
+        long long total = P[n];
+        long long ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int leftBound = max(1, i - k);
+            int rightBound = min(n, i + k);
+            long long leftSum = P[i - 1] - P[leftBound - 1];
+            long long rightSum = P[rightBound] - P[i];
+            int d = (i - leftBound) + (rightBound - i);
+            long long current = total - (leftSum + rightSum) + (long long)d * w[i];
+            ans = max(ans, current);
+        }
+        cout << ans;
+    }
+};
+
+int main()
+{
+    Solver solver;
+    solver.solve();
+    return 0;
+}
