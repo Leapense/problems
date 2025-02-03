6ebee8441b9bece63d235290e10557c7af062083
Leapense <iso9661@gmail.com>
2025-02-03 19:16:09 +0900

    `Added C++ solution for problem 28586, including code and main function`

--- 28586번： Воссоединение c Веномом/Воссоединение c Веномом.cpp
+++ 28586번： Воссоединение c Веномом/Воссоединение c Веномом.cpp
@@ -1,11 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28586                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28586                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 19:09:35 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+class Solution
+{
+public:
+    void solve()
+    {
+        ll t, a, s;
+        cin >> t >> a >> s;
+
+        if (((a - t) & 1LL) || ((s - t) & 1LL))
+        {
+            cout << -1;
+            return;
+        }
+
+        ll diff1 = (a - t) / 2;
+        ll diff2 = (s - t) / 2;
+        ll k0 = max({0LL, -diff1, -diff2});
+        ll m = 3 * k0 + (diff1 + diff2);
+
+        cout << m;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Solution sol;
+    sol.solve();
+    return 0;
+}
