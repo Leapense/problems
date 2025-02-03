e0c9169612a4bfe00d139d1c8740e1223aba31d7
Leapense <iso9661@gmail.com>
2025-02-03 09:46:11 +0900

    `Added new code for problem 27922, replacing existing file with a Python script that reads input from stdin and solves the problem using a MobisProject class.`

--- 27922번： 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.py
+++ 27922번： 현대모비스 입사 프로젝트/현대모비스 입사 프로젝트.py
@@ -1,11 +1,23 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 27922                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/27922                          #+#        #+#      #+#     #
-#    Solved: 2025/02/03 09:37:47 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+class MobisProject:
+    def __init__(self):
+        import sys
+        self.input = sys.stdin.readline
+    
+    def solve(self):
+        N, K = map(int, self.input().split())
+        ab, ac, bc = [], [], []
+        for _ in range(N):
+            a, b, c = map(int, self.input().split())
+            ab.append(a + b)
+            ac.append(a + c)
+            bc.append(b + c)
+        ab.sort(reverse=True)
+        ac.sort(reverse=True)
+        bc.sort(reverse=True)
+        res_ab = sum(ab[:K])
+        res_ac = sum(ac[:K])
+        res_bc = sum(bc[:K])
+        print(max(res_ab, res_ac, res_bc))
+
+if __name__ == "__main__":
+    MobisProject().solve()
