c5d4dd8ab07ac8a930873226daeeb55f3344a2b0
Leapense <iso9661@gmail.com>
2025-02-03 19:07:54 +0900

    `Added code for problem 28527, including input/output handling and problem-specific logic`

--- 28527번： Незваные гости/Незваные гости.cpp
+++ 28527번： Незваные гости/Незваные гости.cpp
@@ -1,11 +1,66 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 28527                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/28527                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/03 18:55:05 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class UninvitedGuests
+{
+public:
+    void solve()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int n, m;
+        cin >> n >> m;
+
+        vector<int> plusCount(n + 1, 0), minusCount(n + 1, 0);
+        for (int i = 0; i < m; i++)
+        {
+            int x;
+            char ch;
+            cin >> x >> ch;
+
+            if (ch == '+')
+            {
+                plusCount[x]++;
+            }
+            else
+            {
+                minusCount[x]++;
+            }
+        }
+
+        vector<int> minVisitors(n + 1, 0), maxVisitors(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+        {
+            if (plusCount[i] == 0)
+            {
+                minVisitors[i] = 0;
+                maxVisitors[i] = 0;
+            }
+            else
+            {
+                int diff = plusCount[i] - minusCount[i];
+                minVisitors[i] = (diff > 0 ? diff : 1);
+                maxVisitors[i] = plusCount[i];
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            cout << minVisitors[i] << (i == n ? "\n" : " ");
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            cout << maxVisitors[i] << (i == n ? "\n" : " ");
+        }
+    }
+};
+
+int main()
+{
+    UninvitedGuests solver;
+    solver.solve();
+    return 0;
+}
