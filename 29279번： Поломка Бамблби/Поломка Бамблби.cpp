ef7c8b19f985b6bda76ba4aeb51fe68fac860070
Leapense <iso9661@gmail.com>
2025-02-04 20:32:36 +0900

    `Added solution for problem 29279, a C++ program that reads input, calculates prefix sums, and outputs results for queries.`

--- 29279번： Поломка Бамблби/Поломка Бамблби.cpp
+++ 29279번： Поломка Бамблби/Поломка Бамблби.cpp
@@ -1,11 +1,44 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29279                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29279                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 20:25:44 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> prefix(n + 1, 0);
+    for (int i = 0; i < n; i++)
+    {
+        prefix[i + 1] = prefix[i] + (a[i] == 1 ? 0 : 1);
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        int cnt = prefix[r] - prefix[l - 1];
+
+        if (cnt == 0)
+        {
+            cout << 2 << "\n";
+        }
+        else
+        {
+            cout << 1 << "\n";
+        }
+    }
+
+    return 0;
+}
