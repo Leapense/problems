433d10d9871a2967dfc3259a482442148b537e4a
Leapense <iso9661@gmail.com>
2025-02-04 17:56:12 +0900

    `Refactored calculation of triangular number and adjusted print statement`

--- 28985번： Новый чемодан/Новый чемодан.py
+++ 28985번： Новый чемодан/Новый чемодан.py
@@ -1,15 +1,8 @@
 n = int(input())
-if n < 7:
+T = n * (n + 1) // 2
+if n < 4:
     print(0)
+elif n % 4 == 0 or n % 4 == 3:
+    print(T)
 else:
-    S = n * (n + 1) // 2
-    mod8 = n % 8
-    if mod8 == 0 or mod8 == 7:
-        r = 0
-    elif mod8 == 1 or mod8 == 6:
-        r = 1
-    elif mod8 == 2 or mod8 == 5:
-        r = 3
-    else:
-        r = 6
-    print(S - r)
+    print(0)
