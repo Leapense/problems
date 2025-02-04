aff2828975de958982963c3fa5a69f0e59f6d91d
Leapense <iso9661@gmail.com>
2025-02-04 21:50:14 +0900

    `Refactored code for problem 29411 with improved sorting and grouping logic`

--- 29411번： k-сортировка/k-сортировка.cpp
+++ 29411번： k-сортировка/k-сортировка.cpp
@@ -1,11 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29411                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29411                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 21:44:20 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    int n;
+    long long k;
+    cin >> n >> k;
+
+    vector<long long> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    unordered_map<long long, vector<long long>> groups;
+
+    for (int i = 0; i < n; i++)
+    {
+        long long r = arr[i] % k;
+        groups[r].push_back(arr[i]);
+    }
+
+    for (auto &g : groups)
+    {
+        sort(g.second.begin(), g.second.end());
+    }
+
+    vector<long long> sortedArr(n);
+
+    unordered_map<long long, int> indexPtr;
+
+    for (int i = 0; i < n; i++)
+    {
+        long long r = arr[i] % k;
+        sortedArr[i] = groups[r][indexPtr[r]++];
+    }
+
+    if (is_sorted(sortedArr.begin(), sortedArr.end()))
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
