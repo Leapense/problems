722332b5d3a2c06f4feb457dbf86a0c047aa8fba
Leapense <iso9661@gmail.com>
2025-02-04 22:01:23 +0900

    `Added solution for problem 29425, including code and diff explanation`

--- 29425번： Кубики/Кубики.cpp
+++ 29425번： Кубики/Кубики.cpp
@@ -1,11 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29425                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29425                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 21:51:02 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> prefixMax(n);
+    prefixMax[0] = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        prefixMax[i] = max(prefixMax[i - 1], a[i]);
+    }
+
+    vector<int> suffixMin(n);
+    suffixMin[n - 1] = a[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffixMin[i] = min(suffixMin[i + 1], a[i]);
+    }
+
+    vector<int> possible;
+
+    for (int p = 1; p < n; p++)
+    {
+        if (prefixMax[p - 1] < suffixMin[p])
+        {
+            possible.push_back(p);
+        }
+    }
+
+    cout << possible.size() << "\n";
+
+    for (int p : possible)
+    {
+        cout << p << " ";
+    }
+
+    cout << "\n";
+    return 0;
+}
