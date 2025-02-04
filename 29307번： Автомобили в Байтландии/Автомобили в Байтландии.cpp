8eb9f70dadb8b8342ff83c90e7f9081e6d04e716
Leapense <iso9661@gmail.com>
2025-02-04 20:51:41 +0900

    `Refactor: Update code for problem 29307 with new implementation`

--- 29307번： Автомобили в Байтландии/Автомобили в Байтландии.cpp
+++ 29307번： Автомобили в Байтландии/Автомобили в Байтландии.cpp
@@ -1,11 +1,60 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29307                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29307                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 20:33:13 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    queue<long long> que;
+    unordered_map<long long, long long> freq;
+    freq.reserve(n * 2);
+
+    long long sum = 0;
+    long long countModels = 0;
+
+    while (n--)
+    {
+        char op;
+        cin >> op;
+
+        if (op == '+')
+        {
+            long long k;
+            cin >> k;
+            que.push(k);
+
+            sum += k;
+            countModels++;
+            freq[k]++;
+        }
+        else if (op == '-')
+        {
+            long long x = que.front();
+            que.pop();
+
+            sum -= x;
+            countModels--;
+            freq[x]--;
+        }
+        else if (op == '?')
+        {
+            if (sum % countModels != 0)
+            {
+                cout << 0 << "\n";
+            }
+            else
+            {
+                long long avg = sum / countModels;
+                cout << (freq.count(avg) ? freq[avg] : 0) << endl;
+            }
+        }
+    }
+
+    return 0;
+}
