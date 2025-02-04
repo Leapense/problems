a860eef92cb7cbac802c4c69fcadbeaca8ab039c
Leapense <iso9661@gmail.com>
2025-02-04 21:13:38 +0900

    `Refactored code for problem 29384 with added comments and improved formatting`

--- 29384번： Вилки и ножи/Вилки и ножи.cpp
+++ 29384번： Вилки и ножи/Вилки и ножи.cpp
@@ -1,11 +1,77 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29384                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29384                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 21:04:15 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    int forks = n, knives = m;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> eventPQ;
+
+    vector<string> ans;
+
+    for (int i = 0; i < k; i++)
+    {
+        int t, l, a;
+        cin >> t >> l >> a;
+
+        while (!eventPQ.empty() && eventPQ.top().first <= t)
+        {
+            auto ev = eventPQ.top();
+            eventPQ.pop();
+
+            if (ev.second == 0)
+            {
+                forks++;
+            }
+            else
+            {
+                forks++;
+                knives++;
+            }
+        }
+
+        if (a == 0)
+        {
+            if (forks >= 1)
+            {
+                ans.push_back("Yes");
+                forks--;
+                eventPQ.push({t + l, 0});
+            }
+            else
+            {
+                ans.push_back("No");
+            }
+        }
+        else
+        {
+            if (forks >= 1 && knives >= 1)
+            {
+                ans.push_back("Yes");
+                forks--;
+                knives--;
+                eventPQ.push({t + l, 1});
+            }
+            else
+            {
+                ans.push_back("No");
+            }
+        }
+    }
+
+    for (const auto &s : ans)
+    {
+        cout << s << "\n";
+    }
+
+    return 0;
+}
