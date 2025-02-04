8cf228505f60565bf274d64613c078431b5eb707
Leapense <iso9661@gmail.com>
2025-02-04 20:18:05 +0900

    `Added C++ code for problem 29198, including input parsing, sorting, and frequency calculation.`

--- 29198번： 이번에는 C번이 문자열/이번에는 C번이 문자열.cpp
+++ 29198번： 이번에는 C번이 문자열/이번에는 C번이 문자열.cpp
@@ -1,11 +1,62 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29198                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29198                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 20:08:19 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct StringInfo
+{
+    string s;
+    array<int, 26> freq;
+};
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    vector<StringInfo> arr(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i].s;
+        arr[i].freq.fill(0);
+        for (char c : arr[i].s)
+        {
+            arr[i].freq[c - 'A']++;
+        }
+    }
+
+    sort(arr.begin(), arr.end(), [](const StringInfo &a, const StringInfo &b)
+         {
+        for (int i = 0; i < 26; i++) {
+            if (a.freq[i] != b.freq[i]) {
+                return a.freq[i] > b.freq[i];
+            }
+        }
+
+        return false; });
+
+    array<int, 26> totalFreq;
+    totalFreq.fill(0);
+
+    for (int i = 0; i < K; i++)
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            totalFreq[j] += arr[i].freq[j];
+        }
+    }
+
+    string ans;
+    for (int c = 0; c < 26; c++)
+    {
+        ans.append(totalFreq[c], 'A' + c);
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
