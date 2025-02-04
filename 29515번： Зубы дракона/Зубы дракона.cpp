2a36562364a718994f7afce905d58e99a21fb2bb
Leapense <iso9661@gmail.com>
2025-02-04 23:19:29 +0900

    `Added solution for problem 29515, includes C++ code and comments`

--- 29515번： Зубы дракона/Зубы дракона.cpp
+++ 29515번： Зубы дракона/Зубы дракона.cpp
@@ -1,11 +1,65 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29515                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29515                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 22:51:21 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+
+    int n;
+    long long d;
+    cin >> n >> d;
+
+    vector<long long> top(n), bot(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> top[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> bot[i];
+    }
+
+    for (int offset = 0; offset <= n; offset++)
+    {
+        bool valid = true;
+        for (int i = offset; i < n; i++)
+        {
+            if (top[i] + bot[i - offset] > d)
+            {
+                valid = false;
+                break;
+            }
+        }
+
+        if (valid)
+        {
+            cout << offset;
+            return 0;
+        }
+
+        if (offset > 0)
+        {
+            valid = true;
+            for (int i = 0; i < n - offset; i++)
+            {
+                if (top[i] + bot[i + offset] > d)
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+            {
+                cout << offset;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
