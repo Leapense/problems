9de311cdf5682b6d5bc6695a217b35a2a2239651
Leapense <iso9661@gmail.com>
2025-02-04 22:06:53 +0900

    `Added code for problem 29430, implementing a solution to find the k-th square-free number in a given range.`

--- 29430번： Бесквадратные числа/Бесквадратные числа.cpp
+++ 29430번： Бесквадратные числа/Бесквадратные числа.cpp
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29430                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29430                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 22:02:08 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long k;
+    cin >> k;
+
+    int a, b;
+    cin >> a >> b;
+    int range = b - a + 1;
+
+    vector<bool> isSquarefree(range, true);
+
+    for (int i = 2; (long long)i * i <= b; i++)
+    {
+        int sq = i * i;
+        int start = ((a + sq - 1) / sq) * sq;
+        for (int j = start; j <= b; j += sq)
+        {
+            isSquarefree[j - a] = false;
+        }
+    }
+
+    long long count = 0;
+    for (int i = 0; i < range; i++)
+    {
+        if (isSquarefree[i])
+        {
+            count++;
+            if (count == k)
+            {
+                cout << a + i;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
