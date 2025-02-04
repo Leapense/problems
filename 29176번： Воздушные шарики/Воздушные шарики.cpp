4b056cf10613ed466880424f296341e7614d2c63
Leapense <iso9661@gmail.com>
2025-02-04 20:07:32 +0900

    `Added code for problem 29176, including input, processing, and output.`

--- 29176번： Воздушные шарики/Воздушные шарики.cpp
+++ 29176번： Воздушные шарики/Воздушные шарики.cpp
@@ -1,11 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29176                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29176                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 20:01:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> colors(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> colors[i];
+    }
+
+    vector<bool> used(n, false);
+    unordered_set<int> chosenColors;
+    vector<int> answer;
+
+    for (int i = 0; i < n && answer.size() < k; i++)
+    {
+        if (chosenColors.find(colors[i]) == chosenColors.end())
+        {
+            chosenColors.insert(colors[i]);
+            answer.push_back(colors[i]);
+            used[i] = true;
+        }
+    }
+
+    for (int i = 0; i < n && answer.size() < k; i++)
+    {
+        if (!used[i])
+        {
+            answer.push_back(colors[i]);
+            used[i] = true;
+        }
+    }
+
+    for (int i = 0; i < answer.size(); i++)
+    {
+        cout << answer[i] << (i + 1 == answer.size() ? "\n" : " ");
+    }
+
+    return 0;
+}
