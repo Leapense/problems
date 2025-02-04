d4051135315dae5cb01db70bd34522f0e796912a
Leapense <iso9661@gmail.com>
2025-02-04 22:26:47 +0900

    `Added solution for problem 29488, including C++ code and diff output.`

--- 29488번： Резать!/Резать!.cpp
+++ 29488번： Резать!/Резать!.cpp
@@ -1,11 +1,62 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29488                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29488                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 22:19:34 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    int total = n * m;
+
+    if (total % k != 0 || k > total)
+    {
+        cout << -1;
+        return 0;
+    }
+
+    vector<vector<int>> ans(n, vector<int>(m, 0));
+    vector<pair<int, int>> path;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                path.push_back({i, j});
+            }
+        }
+        else
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                path.push_back({i, j});
+            }
+        }
+    }
+
+    int pieces = total / k;
+    int pieceId = 1;
+
+    for (int i = 0; i < total; i++)
+    {
+        ans[path[i].first][path[i].second] = pieceId;
+        if ((i + 1) % k == 0 && pieceId < pieces)
+        {
+            pieceId++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << ans[i][j] << (j == m - 1 ? "\n" : " ");
+        }
+    }
+
+    return 0;
+}
