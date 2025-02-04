6eecd506f63359f94e7ef8d8256159244e1f241b
Leapense <iso9661@gmail.com>
2025-02-04 19:14:09 +0900

    `Added solution for problem 29075, including palindrome check and character swapping logic.`

--- 29075번： Пароль от сейфа/Пароль от сейфа.cpp
+++ 29075번： Пароль от сейфа/Пароль от сейфа.cpp
@@ -1,11 +1,79 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29075                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29075                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 18:54:52 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+bool isPalindrome(const string &s)
+{
+    int n = s.size();
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (s[i] != s[n - 1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    int n = s.size();
+
+    if (isPalindrome(s))
+    {
+        cout << "YES";
+        return 0;
+    }
+
+    int mismatchCount = 0;
+
+    unordered_set<int> candidates;
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (s[i] != s[n - 1 - i])
+        {
+            mismatchCount++;
+            candidates.insert(i);
+            candidates.insert(n - 1 - i);
+        }
+    }
+
+    if (mismatchCount > 2)
+    {
+        cout << "NO";
+        return 0;
+    }
+
+    if (n % 2 == 1)
+    {
+        candidates.insert(n / 2);
+    }
+
+    vector<int> cand(candidates.begin(), candidates.end());
+    for (int i = 0; i < (int)cand.size(); i++)
+    {
+        for (int j = i + 1; j < (int)cand.size(); j++)
+        {
+            string t = s;
+            swap(t[cand[i]], t[cand[j]]);
+            if (isPalindrome(t))
+            {
+                cout << "YES";
+                return 0;
+            }
+        }
+    }
+
+    cout << "NO";
+    return 0;
+}
