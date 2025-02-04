40bbe97b14629270ab968bd2bac90ec2496f32fa
Leapense <iso9661@gmail.com>
2025-02-04 17:59:50 +0900

    `Refactor logic in file 28985... to handle n < 7 and adjust calculation for S`

--- 28985번： Новый чемодан/Новый чемодан.py
+++ 28985번： Новый чемодан/Новый чемодан.py
@@ -1,8 +1,6 @@
 n = int(input())
-T = n * (n + 1) // 2
-if n < 4:
+if n < 7:
     print(0)
-elif n % 4 == 0 or n % 4 == 3:
-    print(T)
 else:
-    print(0)
+    S = n * (n + 1) // 2
+    print(S - (S % 4))
