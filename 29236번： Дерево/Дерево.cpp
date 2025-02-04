828e42a19e76fc0fdc8c5bcae98c7a955908a187
Leapense <iso9661@gmail.com>
2025-02-04 20:24:57 +0900

    `Added solution for problem 29236, including code and commit metadata`

--- 29236번： Дерево/Дерево.cpp
+++ 29236번： Дерево/Дерево.cpp
@@ -1,11 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29236                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29236                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 20:19:33 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    unsigned long long N;
+    cin >> N;
+
+    unsigned long long half = N / 2;
+    __int128 ans;
+
+    if (N % 2 == 0)
+    {
+        ans = (__int128)half * half;
+    }
+    else
+    {
+        ans = (__int128)half * (half + 1);
+    }
+
+    if (ans == 0)
+    {
+        cout << 0;
+        return 0;
+    }
+
+    string s;
+    while (ans > 0)
+    {
+        int digit = (int)(ans % 10);
+        s.push_back('0' + digit);
+        ans /= 10;
+    }
+
+    reverse(s.begin(), s.end());
+    cout << s;
+
+    return 0;
+}
