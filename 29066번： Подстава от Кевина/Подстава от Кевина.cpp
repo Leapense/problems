dc1d242b7a44d1fefb8f471c1505b3514a452b47
Leapense <iso9661@gmail.com>
2025-02-04 18:53:57 +0900

    `Added C++ code for problem 29066, including input handling, sorting, and searching for a pair of boards with specific weight ranges.`

--- 29066번： Подстава от Кевина/Подстава от Кевина.cpp
+++ 29066번： Подстава от Кевина/Подстава от Кевина.cpp
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29066                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29066                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 18:40:46 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    vector<pair<int, int>> boards(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int weight;
+        cin >> weight;
+        boards[i] = {weight, i + 1};
+    }
+
+    sort(boards.begin(), boards.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        int a = boards[i].first;
+        int lowerBound = a + y;
+        int upperBound = x - a;
+
+        if (lowerBound > upperBound)
+        {
+            continue;
+        }
+
+        auto it = lower_bound(boards.begin() + i + 1, boards.end(), make_pair(lowerBound, -1));
+
+        if (it != boards.end() && it->first <= upperBound)
+        {
+            cout << boards[i].second << " " << it->second;
+            return 0;
+        }
+    }
+
+    cout << 0;
+    return 0;
+}
