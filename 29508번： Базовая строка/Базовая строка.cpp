761a909fd1aeba4bda7d1a166ff581591cb05b25
Leapense <iso9661@gmail.com>
2025-02-04 22:50:50 +0900

    `Added solution for problem 29508, including code and commit history`

--- 29508번： Базовая строка/Базовая строка.cpp
+++ 29508번： Базовая строка/Базовая строка.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29508                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29508                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/04 22:34:30 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+bool check(const string &str, const string &candidate)
+{
+    int lenStr = str.size(), lenCandidate = candidate.size();
+    if (lenStr % lenCandidate != 0)
+        return false;
+
+    for (int i = 0; i < lenStr; i++)
+    {
+        if (str[i] != candidate[i % lenCandidate])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s, t;
+    cin >> s >> t;
+
+    int g = gcd(s.size(), t.size());
+
+    string candidate = s.substr(0, g);
+
+    if (check(s, candidate) && check(t, candidate))
+        cout << candidate;
+    else
+        cout << "NO";
+
+    return 0;
+}
