126a3dfe938b8bed3a780f8bbc9df3b1eb0aa1bd
Leapense <iso9661@gmail.com>
2025-02-05 19:15:24 +0900

    `Added code to solve problem 29583, diff includes changes to a Python script`

--- 29583번： Расстановка книг/Расстановка книг.py
+++ 29583번： Расстановка книг/Расстановка книг.py
@@ -1,11 +1,26 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 29583                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/29583                          #+#        #+#      #+#     #
-#    Solved: 2025/02/05 19:08:08 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+from itertools import permutations
+
+def solve():
+    input = sys.stdin.read
+    data = input().split()
+
+    n = int(data[0])
+    p = list(map(int, data[1:n+1]))
+    a = list(map(int, data[n+1:]))
+
+    count = 0
+
+    for perm in permutations(p):
+        valid = True
+        for i in range(n - 1):
+            if perm[i] + perm[i+1] > a[i]:
+                valid = False
+                break
+        if valid:
+            count += 1
+
+    sys.stdout.write(str(count))
+
+if __name__ == "__main__":
+    solve()
