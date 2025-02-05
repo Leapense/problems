fd6e3c51b38d853818f17a1c5b2b01175e4fb85f
Leapense <iso9661@gmail.com>
2025-02-05 18:39:27 +0900

    `Fixed binary search and set usage in pyramid_candies problem solution`

--- 29571번： Конфеты Дяди Федора/Конфеты Дяди Федора.cpp
+++ 29571번： Конфеты Дяди Федора/Конфеты Дяди Федора.cpp
@@ -1,55 +1,51 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <set>
+
 using namespace std;
+
+long long pyramid_candies(int h)
+{
+    if (h < 0)
+        return 0;
+    return (long long)h * (h + 1) * (h + 2) / 6;
+}
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int n;
     cin >> n;
 
-    // n==0인 경우 (즉, 모든 캔디가 먹힌 경우)는 정답으로 YES
     if (n == 0)
     {
-        cout << "YES";
+        cout << "YES" << endl;
         return 0;
     }
 
-    // n의 최댓값(10^9)을 고려하여 충분한 b의 최대값을 설정 (대략 2000면 충분)
-    const int B_MAX = 2000;
-    vector<long long> tetra(B_MAX + 1, 0);
-
-    // 테트라헤드럴 수 T(m) = m*(m+1)*(m+2)/6 을 0 <= m <= B_MAX까지 계산
-    for (int m = 0; m <= B_MAX; m++)
+    set<int> possible_sums;
+    for (int h = 2; h <= 2000; ++h)
     {
-        tetra[m] = (long long)m * (m + 1) * (m + 2) / 6;
-    }
-
-    // b: 전체 피라미드 층 수, a: 먹힌 층의 수 (1 <= a < b)
-    // T(b) - T(a) = n 이 되는 쌍 (a,b)가 있는지 검사합니다.
-    for (int b = 2; b <= B_MAX; b++)
-    {
-        // candidate = T(b) - n. candidate가 T(a)여야 함.
-        long long candidate = tetra[b] - n;
-
-        // candidate가 음수이면 a가 존재할 수 없음
-        if (candidate < 0)
-            continue;
-        // candidate가 0이면 a==0인 경우인데, n>0에서는 먹힌 층이 0이면 안 됨.
-        if (candidate == 0)
-            continue;
-
-        // a는 1 이상, b-1 이하여야 하므로 tetra[1]부터 tetra[b-1] 범위에서 candidate를 찾습니다.
-        if (binary_search(tetra.begin() + 1, tetra.begin() + b, candidate))
+        for (int r = 1; r < h; ++r)
         {
-            cout << "YES";
-            return 0;
+            long long sum = pyramid_candies(h) - pyramid_candies(r);
+            if (sum <= 1000000000)
+            {
+                possible_sums.insert(sum);
+            }
+            else
+            {
+                break; // Sum will only increase for larger r or h, so no need to continue for this h.
+            }
         }
     }
 
-    cout << "NO";
+    if (possible_sums.count(n))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
