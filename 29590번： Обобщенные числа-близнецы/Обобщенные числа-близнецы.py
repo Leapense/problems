8cee3de13ebda3a1125172d4dfd27d856ce58884
Leapense <iso9661@gmail.com>
2025-02-05 19:23:00 +0900

    `Added code for problem 29590 with prime number check and count of prime pairs`

--- 29590번： Обобщенные числа-близнецы/Обобщенные числа-близнецы.py
+++ 29590번： Обобщенные числа-близнецы/Обобщенные числа-близнецы.py
@@ -1,11 +1,23 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 29590                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/29590                          #+#        #+#      #+#     #
-#    Solved: 2025/02/05 19:16:19 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    input = sys.stdin.readline
+
+    n, k = map(int, input().split())
+
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+
+    count = 0
+    for p in range(2, n - k + 1):
+        if is_prime[p] and (p + k) <= n and is_prime[p + k]:
+            count += 1
+
+    sys.stdout.write(str(count))
+
+if __name__ == "__main__":
+    solve()
