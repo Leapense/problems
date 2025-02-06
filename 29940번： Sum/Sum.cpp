e1f93149aa579a4cced55f4731705e6fab3b79f5
Leapense <iso9661@gmail.com>
2025-02-06 18:39:58 +0900

    `Refactored code for problem 29940: added includes, main function, and logic for finding pairs of numbers in an array that sum to a given value.`

--- 29940번： Sum/Sum.cpp
+++ 29940번： Sum/Sum.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29940                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29940                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/06 18:35:06 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n, s;
+    std::cin >> n >> s;
+    std::vector<int> a(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> a[i];
+    }
+
+    int left = 0;
+    int right = n - 1;
+    int count = 0;
+
+    while (left < right)
+    {
+        int current_sum = a[left] + a[right];
+        if (current_sum == s)
+        {
+            count++;
+            left++;
+            right--;
+        }
+        else if (current_sum < s)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
+}
