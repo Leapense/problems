b74051ee102e25c53ffadf4b846a4cb91ecda805
Leapense <iso9661@gmail.com>
2025-02-06 18:31:30 +0900

    `Added code for problem 29882: Ranking`

--- 29882번： Ranking/Ranking.cpp
+++ 29882번： Ranking/Ranking.cpp
@@ -1,11 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29882                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29882                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/06 18:17:42 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    unordered_map<string, unordered_map<string, int>> scoreBoard;
+
+    for (int i = 0; i < N; i++)
+    {
+        string account, task;
+        int score;
+        cin >> account >> task >> score;
+
+        if (scoreBoard[account].find(task) == scoreBoard[account].end())
+        {
+            scoreBoard[account][task] = score;
+        }
+        else
+        {
+            scoreBoard[account][task] = max(scoreBoard[account][task], score);
+        }
+    }
+
+    vector<pair<string, int>> ranking;
+    for (auto &entry : scoreBoard)
+    {
+        int total = 0;
+        for (auto &taskScore : entry.second)
+        {
+            total += taskScore.second;
+        }
+        ranking.push_back({entry.first, total});
+    }
+
+    sort(ranking.begin(), ranking.end(), [](const pair<string, int> &a, const pair<string, int> &b)
+         { return a.second > b.second; });
+
+    for (auto &p : ranking)
+    {
+        cout << p.first << " " << p.second << "\n";
+    }
+
+    return 0;
+}
