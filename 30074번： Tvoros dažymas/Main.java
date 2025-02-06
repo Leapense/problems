75d80a5cddfe117f5b3ca69ba495063ae3d840e5
Leapense <iso9661@gmail.com>
2025-02-06 21:15:58 +0900

    `Added Java solution for problem 30074 with input handling and calculation of total wait time.`

--- 30074번： Tvoros dažymas/Main.java
+++ 30074번： Tvoros dažymas/Main.java
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30074 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30074 #+# #+# #+# */
-/* Solved: 2025/02/06 20:42:39 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int V = sc.nextInt();
+        int Z = sc.nextInt();
+        int T = sc.nextInt();
+
+        sc.close();
+
+        int barboraTime = 0;
+        int totalWait = 0;
+
+        for (int i = 1; i <= N; i++) {
+            int tomasFinishTime = -T + i * V;
+            if (tomasFinishTime > barboraTime) {
+                totalWait += tomasFinishTime - barboraTime;
+                barboraTime = tomasFinishTime;
+            }
+            barboraTime += Z;
+        }
+
+        System.out.println(totalWait);
+    }
+}
