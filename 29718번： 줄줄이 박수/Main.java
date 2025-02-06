d77a31bf1b276f2bbc13e0fb30807acf4d4da6bd
Leapense <iso9661@gmail.com>
2025-02-06 12:42:06 +0900

    `Added Java solution for problem 29718, including code for reading input, calculating column sums, and finding the maximum sum of A consecutive columns.`

--- 29718번： 줄줄이 박수/Main.java
+++ 29718번： 줄줄이 박수/Main.java
@@ -1,11 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 29718 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/29718 #+# #+# #+# */
-/* Solved: 2025/02/06 12:34:25 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] applause = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                applause[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int A = Integer.parseInt(br.readLine().trim());
+
+        int[] colSum = new int[M];
+        for (int j = 0; j < M; j++) {
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                sum += applause[i][j];
+            }
+            colSum[j] = sum;
+        }
+
+        long currentSum = 0;
+        for (int j = 0; j < A; j++) {
+            currentSum += colSum[j];
+        }
+
+        long maxSum = currentSum;
+
+        for (int j = 1; j <= M - A; j++) {
+            currentSum = currentSum - colSum[j - 1] + colSum[j + A - 1];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        out.println(maxSum);
+        out.flush();
+        out.close();
+    }
+}
