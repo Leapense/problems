76ef27ee6882ce34326c12d4b53ba246bc5ca653
Leapense <iso9661@gmail.com>
2025-02-06 18:04:37 +0900

    `Added solution for problem 29813, including C++ code and diff of changes`

--- 29813번： 최애의 팀원/최애의 팀원.cpp
+++ 29813번： 최애의 팀원/최애의 팀원.cpp
@@ -1,11 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 29813                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/29813                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/06 13:19:23 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+struct Student
+{
+    string initial;
+    int number;
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    deque<Student> dq;
+
+    for (int i = 0; i < N; i++)
+    {
+        Student s;
+        cin >> s.initial >> s.number;
+        dq.push_back(s);
+    }
+
+    while (dq.size() > 1)
+    {
+        Student curr = dq.front();
+        dq.pop_front();
+
+        int passes = curr.number - 1;
+        for (int i = 0; i < passes; i++)
+        {
+            Student temp = dq.front();
+            dq.pop_front();
+            dq.push_back(temp);
+        }
+
+        dq.pop_front();
+    }
+
+    cout << dq.front().initial << "\n";
+
+    return 0;
+}
