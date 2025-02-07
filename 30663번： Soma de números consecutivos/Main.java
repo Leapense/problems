45ce5a084716898d3899d3654745cc317b2c3538
Leapense <iso9661@gmail.com>
2025-02-07 21:08:48 +0900

    Implement Java solution for problem 30663: Soma de números consecutivos

--- 30663번： Soma de números consecutivos/Main.java
+++ 30663번： Soma de números consecutivos/Main.java
@@ -1,11 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30663 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30663 #+# #+# #+# */
-/* Solved: 2025/02/07 21:04:15 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0)
+                break;
+
+            if ((n & (n - 1)) == 0)
+                System.out.println("NAO");
+            else
+                System.out.println("SIM");
+        }
+
+        sc.close();
+    }
+}
