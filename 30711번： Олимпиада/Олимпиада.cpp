bc114f67356e0664f67061494c978f1578e4b1c8
Leapense <iso9661@gmail.com>
2025-02-07 22:08:05 +0900

    30711번: 문제 30711의 C++ 구현 추가

--- 30711번： Олимпиада/Олимпиада.cpp
+++ 30711번： Олимпиада/Олимпиада.cpp
@@ -1,11 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 30711                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/30711                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/07 21:57:16 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, p;
+    long long c;
+
+    cin >> n >> p >> c;
+
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<long long> sorted = a;
+    sort(sorted.begin(), sorted.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        long long score = a[i];
+
+        int cnt = sorted.end() - upper_bound(sorted.begin(), sorted.end(), score - c);
+        cnt--;
+
+        int cnt_fail = sorted.end() - upper_bound(sorted.begin(), sorted.end(), score + c);
+
+        if (cnt < p)
+            cout << "Pass\n";
+        else if (cnt_fail >= p)
+            cout << "Fail\n";
+        else
+            cout << "Short\n";
+    }
+
+    return 0;
+}
