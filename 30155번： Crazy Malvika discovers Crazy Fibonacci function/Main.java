eccf906b7913bf3c413e2cf1a24e6408956a3bf5
Leapense <iso9661@gmail.com>
2025-02-07 17:56:20 +0900

    Add Java solution for problem 30155: Implement Crazy Fibonacci function logic

--- 30155번： Crazy Malvika discovers Crazy Fibonacci function/Main.java
+++ 30155번： Crazy Malvika discovers Crazy Fibonacci function/Main.java
@@ -1,1 +1,44 @@
 import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        final long MOD = 1000000007;
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long A = Long.parseLong(st.nextToken());
+            long B = Long.parseLong(st.nextToken());
+            long N = Long.parseLong(st.nextToken());
+
+            long result = 0;
+
+            long r = N % 6;
+            if (r < 0) {
+                r += 6;
+            }
+
+            if (r == 1)
+                result = A;
+            else if (r == 2)
+                result = B;
+            else if (r == 3)
+                result = B - A;
+            else if (r == 4)
+                result = -A;
+            else if (r == 5)
+                result = -B;
+            else if (r == 0)
+                result = A - B;
+
+            result %= MOD;
+            if (result < 0) {
+                result += MOD;
+            }
+
+            System.out.println(result);
+        }
+    }
+}
