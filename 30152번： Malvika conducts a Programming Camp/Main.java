e938f8c51401e8637b9a5f7d8f9f3e7b8d0c98e7
Leapense <iso9661@gmail.com>
2025-02-07 17:47:34 +0900

    Add Java solution for problem 30152: Malvika conducts a Programming Camp

--- 30152번： Malvika conducts a Programming Camp/Main.java
+++ 30152번： Malvika conducts a Programming Camp/Main.java
@@ -1,11 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30152 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30152 #+# #+# #+# */
-/* Solved: 2025/02/07 17:38:53 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            if (n == 1) {
+                System.out.println(0);
+            } else if (n == 2) {
+                System.out.println(m);
+            } else {
+                System.out.println(2 * m + n - 3);
+            }
+        }
+
+        sc.close();
+    }
+}
