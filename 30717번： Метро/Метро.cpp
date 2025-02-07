9380ca04f9c21f604ed08d62309447f43cd14718
Leapense <iso9661@gmail.com>
2025-02-07 22:25:21 +0900

    문제 30717의 C++ 구현 추가

--- 30717번： Метро/Метро.cpp
+++ 30717번： Метро/Метро.cpp
@@ -1,11 +1,59 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 30717                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/30717                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/07 22:16:52 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    ll p, k;
+    cin >> n >> p >> k;
+
+    vector<ll> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    ll base_rides = 0;
+    vector<ll> deficiencies;
+
+    for (int i = 0; i < n; i++)
+    {
+        base_rides += a[i] / k;
+        ll r = a[i] % k;
+        if (r > 0)
+        {
+            deficiencies.push_back(k - r);
+        }
+    }
+
+    sort(deficiencies.begin(), deficiencies.end());
+
+    ll extra_rides = 0;
+
+    for (ll cost : deficiencies)
+    {
+        if (p >= cost)
+        {
+            p -= cost;
+            extra_rides++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    extra_rides += p / k;
+
+    cout << base_rides + extra_rides << "\n";
+
+    return 0;
+}
