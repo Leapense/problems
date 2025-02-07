bb91f2e2ec9d4da103f8c047e667e3376ace878a
Leapense <iso9661@gmail.com>
2025-02-07 20:31:40 +0900

    Implement Java solution for problem 30571: Naming Wine Bottles

--- 30571번： Naming Wine Bottles/Main.java
+++ 30571번： Naming Wine Bottles/Main.java
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30571 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30571 #+# #+# #+# */
-/* Solved: 2025/02/07 20:17:18 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+
+        Map<String, String> nameMap = new HashMap<>();
+        int counter = 0;
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine().trim();
+            String volumeStr = line.substring(0, line.length() - 1);
+
+            BigDecimal volume = new BigDecimal(volumeStr).stripTrailingZeros();
+            String key = volume.toPlainString();
+
+            if (!nameMap.containsKey(key)) {
+                String generatedName = generateName(counter);
+                nameMap.put(key, generatedName);
+                counter++;
+            }
+
+            output.append(nameMap.get(key)).append("\n");
+        }
+
+        System.out.println(output);
+    }
+
+    private static String generateName(int num) {
+        StringBuilder sb = new StringBuilder();
+        do {
+            char c = (char) ('a' + (num % 26));
+            sb.append(c);
+            num /= 26;
+        } while (num > 0);
+
+        return sb.reverse().toString();
+    }
+}
