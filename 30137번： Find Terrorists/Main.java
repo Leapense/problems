3155d73c48abbaee0c60ebaacb34166ce0328967
Leapense <iso9661@gmail.com>
2025-02-07 12:18:47 +0900

    Add Java solution for problem 30137: Find Terrorists with input handling and prime counting logic

--- 30137번： Find Terrorists/Main.java
+++ 30137번： Find Terrorists/Main.java
@@ -1,11 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30137 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30137 #+# #+# #+# */
-/* Solved: 2025/02/07 12:10:37 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int k = 0; k < t; k++) {
+            int L = sc.nextInt();
+            int H = sc.nextInt();
+
+            boolean found = false;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = L; i <= H; i++) {
+                if (i <= 0)
+                    continue;
+                int cnt = countDiv(i);
+                if (isPrime(cnt)) {
+                    sb.append(i).append(" ");
+                    found = true;
+                }
+            }
+
+            if (!found)
+                System.out.println(-1);
+            else
+                System.out.println(sb.toString().trim());
+        }
+    }
+
+    static int countDiv(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i * i == n)
+                    count++;
+                else
+                    count += 2;
+            }
+        }
+
+        return count;
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
