5a3eb6de64596c3cccca70fb86b2397f377f6e1e
Leapense <iso9661@gmail.com>
2025-02-07 20:09:26 +0900

    Add Java solution for problem 30389: Suffi$＼otimes$

--- 30389번： Suffi$＼otimes$/Main.java
+++ 30389번： Suffi$＼otimes$/Main.java
@@ -1,11 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30389 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30389 #+# #+# #+# */
-/* Solved: 2025/02/07 19:53:56 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet<String> result = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+
+            for (int j = 0; j < s.length(); j++) {
+                String suffix = s.substring(j);
+
+                if (result.contains(suffix)) {
+                    result.remove(suffix);
+                } else {
+                    result.add(suffix);
+                }
+            }
+        }
+
+        System.out.println(result.size());
+    }
+}
