32c026c7083bc72612f493bfb7bd43725b464dd0
Leapense <iso9661@gmail.com>
2025-02-07 19:30:14 +0900

    Add Java solution for problem 30226: Snailography

--- 30226번： Snailography/Main.java
+++ 30226번： Snailography/Main.java
@@ -1,11 +1,71 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30226 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30226 #+# #+# #+# */
-/* Solved: 2025/02/07 19:14:43 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+        String message = sc.nextLine();
+
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '#';
+            }
+        }
+
+        int mid = n / 2;
+        int r = mid, c = mid;
+        int idx = 0;
+
+        if (idx < message.length()) {
+            board[r][c] = message.charAt(idx++);
+        } else {
+            board[r][c] = '#';
+        }
+
+        int total = n * n;
+        int count = 1;
+
+        int[][] dirs = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+        int dirIndex = 0;
+        int steps = 1;
+
+        while (count < total) {
+            for (int d = 0; d < 2; d++) {
+                int dr = dirs[dirIndex][0];
+                int dc = dirs[dirIndex][1];
+                for (int s = 0; s < steps; s++) {
+                    if (count >= total)
+                        break;
+                    r += dr;
+                    c += dc;
+                    if (idx < message.length()) {
+                        board[r][c] = message.charAt(idx++);
+                    } else {
+                        board[r][c] = '#';
+                    }
+
+                    count++;
+                }
+
+                dirIndex = (dirIndex + 1) % 4;
+            }
+
+            steps++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != '#') {
+                    sb.append(board[i][j]);
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
