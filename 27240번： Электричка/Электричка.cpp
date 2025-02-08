8a6e47920f40266e1cdeb35f8ff9fdde72de1558
Leapense <iso9661@gmail.com>
2025-02-08 14:47:55 +0900

    문제 27240의 C++ 구현 추가 및 README.md 파일 생성

--- 27240번： Электричка/Электричка.cpp
+++ 27240번： Электричка/Электричка.cpp
@@ -1,11 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 27240                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/27240                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/08 13:31:36 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n, a, b;
+    long long s, t;
+
+    cin >> n >> a >> b;
+    cin >> s >> t;
+
+    if (s > a && s < b && t > a && t < b)
+    {
+        cout << "City";
+    }
+    else if ((s <= a && t <= a) || (s >= b && t >= b))
+    {
+        cout << "Outside";
+    }
+    else
+    {
+        cout << "Full";
+    }
+
+    return 0;
+}
