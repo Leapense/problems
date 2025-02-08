dc5b10b058964bfe8cd42c93d3e6ab38e89be5a3
Leapense <iso9661@gmail.com>
2025-02-08 19:07:55 +0900

    문제 30884: Java 메인 함수 추가 및 괄호 처리 로직 구현

--- 30884번： Yeah, but How？/Main.java
+++ 30884번： Yeah, but How？/Main.java
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 30884 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/30884 #+# #+# #+# */
-/* Solved: 2025/02/08 18:30:14 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        System.out.println(solve(S));
+    }
+
+    static String solve(String S) {
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (c == '(') {
+                if (stack.peek() > 0) {
+                    sb.append('+');
+                }
+
+                sb.append('(');
+                stack.push(0);
+            } else if (c == ')') {
+                if (stack.peek() == 0) {
+                    sb.append("1");
+                }
+
+                sb.append(')');
+                stack.pop();
+
+                if (!stack.isEmpty()) {
+                    int cnt = stack.pop();
+                    stack.push(cnt + 1);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
