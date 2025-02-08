ccb0ae3faf10dfbf6c1315dc99ef39886f424f7b
Leapense <iso9661@gmail.com>
2025-02-08 18:21:33 +0900

    문제 30847: C++ 소스코드를 Python으로 구현, 메인 함수 추가

--- 30847번： Нечетный ним/Нечетный ним.py
+++ 30847번： Нечетный ним/Нечетный ним.py
@@ -1,11 +1,31 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 30847                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/30847                          #+#        #+#      #+#     #
-#    Solved: 2025/02/08 18:09:16 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    n = int(input().strip())
+    piles = list(map(int, input().split()))
+    odd_count = sum(1 for a in piles if a % 2 == 1)
+
+    if odd_count % 2 == 1:
+        normal_winner = "Misha"
+    else:
+        normal_winner = "Gleb"
+
+    all_ones = all(a == 1 for a in piles)
+
+    if all_ones:
+        if n % 2 == 0:
+            misere_winner = "Misha"
+        else:
+            misere_winner = "Gleb"
+    else:
+        if odd_count % 2 == 0:
+            misere_winner = "Misha"
+        else:
+            misere_winner = "Gleb"
+
+    sys.stdout.write(normal_winner + "\n")
+    sys.stdout.write(misere_winner + "\n")
+
+if __name__ == "__main__":
+    main()
