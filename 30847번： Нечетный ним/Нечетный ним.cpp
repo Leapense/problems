4c88b1aca1ad22c4224f444a6c82d25e02d7e986
Leapense <iso9661@gmail.com>
2025-02-08 17:56:44 +0900

    홀수 돌 문제의 C++ 구현 수정 및 주석 추가

--- 30847번： Нечетный ним/Нечетный ним.cpp
+++ 30847번： Нечетный ним/Нечетный ним.cpp
@@ -9,34 +9,29 @@ int main()
     int N;
     cin >> N;
 
-    long long oddCount = 0;
+    // 홀수 돌 더미의 개수를 센다.
+    int oddCount = 0;
     for (int i = 0; i < N; i++)
     {
         int a;
         cin >> a;
         if (a % 2 == 1)
-        {
             oddCount++;
-        }
     }
 
+    // [일반 님]
+    // 미샤(첫 번째 플레이어)는 홀수 돌 더미의 개수가 홀수일 때 승리
     if (oddCount % 2 == 1)
-    {
-        cout << "Misha\n";
-    }
+        cout << "Misha" << "\n";
     else
-    {
-        cout << "Gleb\n";
-    }
+        cout << "Gleb" << "\n";
 
+    // [미저 님]
+    // 미샤는 홀수 돌 더미의 개수가 짝수일 때 승리
     if (oddCount % 2 == 0)
-    {
-        cout << "Misha\n";
-    }
+        cout << "Misha" << "\n";
     else
-    {
-        cout << "Gleb\n";
-    }
+        cout << "Gleb" << "\n";
 
     return 0;
 }
