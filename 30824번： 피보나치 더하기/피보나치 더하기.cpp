9ed0310c48ece75fe53f10f5506e37f1f95a579c
Leapense <iso9661@gmail.com>
2025-02-08 15:02:32 +0900

    문제 30824의 C++ 구현 추가

--- 30824번： 피보나치 더하기/피보나치 더하기.cpp
+++ 30824번： 피보나치 더하기/피보나치 더하기.cpp
@@ -1,11 +1,80 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 30824                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/30824                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/08 14:30:12 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<unsigned long long> fib;
+    fib.push_back(1);
+    fib.push_back(1);
+
+    while (true)
+    {
+        unsigned long long nxt = fib[fib.size() - 1] + fib[fib.size() - 2];
+        if (nxt > 10000000000000000ULL)
+            break;
+        fib.push_back(nxt);
+    }
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int k;
+        unsigned long long x;
+        cin >> k >> x;
+        bool possible = false;
+
+        if (k == 1)
+        {
+            if (binary_search(fib.begin(), fib.end(), x))
+                possible = true;
+        }
+        else if (k == 2)
+        {
+            for (auto a : fib)
+            {
+                if (a > x)
+                    break;
+                unsigned long long remain = x - a;
+                if (binary_search(fib.begin(), fib.end(), remain))
+                {
+                    possible = true;
+                    break;
+                }
+            }
+        }
+        else if (k == 3)
+        {
+            for (auto a : fib)
+            {
+                if (a > x)
+                    break;
+                for (auto b : fib)
+                {
+                    if (a + b > x)
+                        break;
+                    unsigned long long remain = x - a - b;
+                    if (remain < 1)
+                        continue;
+                    if (binary_search(fib.begin(), fib.end(), remain))
+                    {
+                        possible = true;
+                        break;
+                    }
+                }
+
+                if (possible)
+                    break;
+            }
+        }
+
+        cout << (possible ? "YES" : "NO") << "\n";
+    }
+    return 0;
+}
