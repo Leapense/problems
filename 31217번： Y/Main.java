f804ec92bb4377eb4950f37cca8cf5e9da2db9cf
Leapense <iso9661@gmail.com>
2025-02-10 00:21:28 +0900

    문제 31217: Java 메인 함수 구현 및 그래프 차수 계산 로직 추가

--- 31217번： Y/Main.java
+++ 31217번： Y/Main.java
@@ -1,11 +1,35 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 31217 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/31217 #+# #+# #+# */
-/* Solved: 2025/02/10 00:04:13 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] degree = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            degree[u]++;
+            degree[v]++;
+        }
+
+        long result = 0;
+        long MOD = 1000000007;
+
+        for (int i = 1; i <= n; i++) {
+            if (degree[i] >= 3) {
+                long d = degree[i];
+                long count = d * (d - 1) * (d - 2) / 6;
+                result = (result + count) % MOD;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
