652a5e59a2eda982eb590537f1442d43326f322d
Leapense <iso9661@gmail.com>
2025-02-10 13:02:34 +0900

    문제 31263: Java 메인 함수 구현 및 동적 프로그래밍을 통한 최소 분할 수 계산 로직 추가

--- 31263번： 대한민국을 지키는 가장 긴 힘/Main.java
+++ 31263번： 대한민국을 지키는 가장 긴 힘/Main.java
@@ -1,11 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 31263 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/31263 #+# #+# #+# */
-/* Solved: 2025/02/10 12:50:10 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String S = sc.next();
+        int len = S.length();
+
+        int[] dp = new int[len + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (dp[i] == Integer.MAX_VALUE)
+                continue;
+
+            for (int j = 1; j <= 3; j++) {
+                if (i + j > len)
+                    break;
+                String sub = S.substring(i, i + j);
+                if (sub.charAt(0) == '0' && sub.length() > 1)
+                    continue;
+
+                int num = Integer.parseInt(sub);
+
+                if (num >= 1 && num <= 641) {
+                    dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[len]);
+        sc.close();
+    }
+}
