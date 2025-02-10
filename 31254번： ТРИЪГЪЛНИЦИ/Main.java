e5573f78620a9ec81157337b9288ff5818417f64
Leapense <iso9661@gmail.com>
2025-02-10 12:49:08 +0900

    문제 31254: Java 메인 함수 및 점의 삼각형 여부 판단 로직 추가

--- 31254번： ТРИЪГЪЛНИЦИ/Main.java
+++ 31254번： ТРИЪГЪЛНИЦИ/Main.java
@@ -1,11 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 31254 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/31254 #+# #+# #+# */
-/* Solved: 2025/02/10 12:34:58 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    static class Point {
+        String name;
+        int x, y;
+
+        Point(String name, int x, int y) {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static boolean isCollinear(Point a, Point b, Point c) {
+        int area2 = a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
+        return area2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        Point[] points = new Point[n];
+
+        for (int i = 0; i < n; i++) {
+            String name = scanner.next();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            points[i] = new Point(name, x, y);
+        }
+
+        boolean triangleFound = false;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (!isCollinear(points[i], points[j], points[k])) {
+                        System.out.println(points[i].name + " " + points[j].name + " " + points[k].name);
+                        triangleFound = true;
+                    }
+                }
+            }
+        }
+
+        if (!triangleFound) {
+            System.out.println("No triangles.");
+        }
+
+        scanner.close();
+    }
+}
