626105efba1d3caa1fc41b4722edd301a66cdd87
Leapense <iso9661@gmail.com>
2025-02-10 13:16:48 +0900

    문제 31409: Java 메인 함수 구현 및 배열 변경 수 계산 로직 추가

--- 31409번： 착신 전환 소동/Main.java
+++ 31409번： 착신 전환 소동/Main.java
@@ -1,11 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 31409 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/31409 #+# #+# #+# */
-/* Solved: 2025/02/10 13:03:41 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] a = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int[] b = new int[N + 1];
+        int changes = 0;
+
+        int candidate = -1;
+        for (int i = 1; i <= N; i++) {
+            if (a[i] != i) {
+                candidate = i;
+                break;
+            }
+        }
+
+        if (candidate != -1) {
+            for (int i = 1; i <= N; i++) {
+                if (a[i] == i) {
+                    b[i] = candidate;
+                    changes++;
+                } else {
+                    b[i] = a[i];
+                }
+            }
+        } else {
+            for (int i = 1; i < N; i++) {
+                b[i] = i + 1;
+            }
+            b[N] = 1;
+            changes = N;
+        }
+
+        System.out.println(changes);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(b[i]).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+
+        sc.close();
+    }
+}
