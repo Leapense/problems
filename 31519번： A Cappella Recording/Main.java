58cd7f696350c117c05867abcef9877f6620c388
Leapense <iso9661@gmail.com>
2025-02-10 13:49:27 +0900

    문제 31519: A Cappella Recording의 메인 함수 구현 및 음성 녹음 수 계산 로직 추가

--- 31519번： A Cappella Recording/Main.java
+++ 31519번： A Cappella Recording/Main.java
@@ -1,11 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 31519 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/31519 #+# #+# #+# */
-/* Solved: 2025/02/10 13:40:55 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+
+        int[] pitches = new int[n];
+        for (int i = 0; i < n; i++) {
+            pitches[i] = sc.nextInt();
+        }
+
+        Arrays.sort(pitches);
+
+        int recordings = 0;
+        int i = 0;
+
+        while (i < n) {
+            int startPitch = pitches[i];
+            int limit = startPitch + d;
+
+            while (i < n && pitches[i] <= limit) {
+                i++;
+            }
+            recordings++;
+        }
+
+        System.out.println(recordings);
+        sc.close();
+    }
+}
