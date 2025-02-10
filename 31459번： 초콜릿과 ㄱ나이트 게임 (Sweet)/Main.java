91db21118013b6b31f932484850e645a4131cd7e
Leapense <iso9661@gmail.com>
2025-02-10 13:32:04 +0900

    문제 31459: Java 메인 함수 구현 및 입력 처리 로직 추가

--- 31459번： 초콜릿과 ㄱ나이트 게임 (Sweet)/Main.java
+++ 31459번： 초콜릿과 ㄱ나이트 게임 (Sweet)/Main.java
@@ -1,11 +1,48 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 31459 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/31459 #+# #+# #+# */
-/* Solved: 2025/02/10 13:25:50 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int t = 0; t < T; t++) {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int moveX = sc.nextInt();
+            int moveY = sc.nextInt();
+
+            boolean[][] visited = new boolean[Y][X];
+            int answer = 0;
+
+            for (int r = 0; r < Y; r++) {
+                for (int c = 0; c < X; c++) {
+                    if (!visited[r][c]) {
+                        int startR = r, startC = c;
+                        while (startR - moveY >= 0 && startC - moveX >= 0) {
+                            startR -= moveY;
+                            startC -= moveX;
+                        }
+
+                        int chainLength = 0;
+                        int cr = startR, cc = startC;
+                        while (cr < Y && cc < X) {
+                            if (!visited[cr][cc]) {
+                                visited[cr][cc] = true;
+                            }
+
+                            chainLength++;
+                            cr += moveY;
+                            cc += moveX;
+                        }
+
+                        answer += (chainLength + 1) / 2;
+                    }
+                }
+            }
+
+            System.out.println(answer);
+        }
+
+        sc.close();
+    }
+}
