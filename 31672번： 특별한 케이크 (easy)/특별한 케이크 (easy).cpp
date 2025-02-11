33a116e45bd228792369b000cf09da82f6e9aed0
Leapense <iso9661@gmail.com>
2025-02-11 20:37:35 +0900

    문제 31672: 특별한 케이크 문제의 C++ 솔루션 구현

--- 31672번： 특별한 케이크 (easy)/특별한 케이크 (easy).cpp
+++ 31672번： 특별한 케이크 (easy)/특별한 케이크 (easy).cpp
@@ -1,11 +1,116 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31672                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31672                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/11 20:08:58 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Testimony
+{
+    vector<int> S;
+    int B;
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string dummy;
+    getline(cin, dummy);
+
+    int N;
+    cin >> N;
+
+    vector<Testimony> testimonies(N + 1);
+    for (int i = 1; i <= N; i++)
+    {
+        int M;
+        cin >> M;
+        testimonies[i].S.resize(M);
+
+        for (int j = 0; j < M; j++)
+        {
+            cin >> testimonies[i].S[j];
+        }
+
+        cin >> testimonies[i].B;
+    }
+
+    vector<int> possibleCandidates;
+
+    for (int culprit = 1; culprit <= N; culprit++)
+    {
+        bool valid = true;
+        for (int i = 1; i <= N; i++)
+        {
+            bool contains = false;
+            for (int num : testimonies[i].S)
+            {
+                if (num == culprit)
+                {
+                    contains = true;
+                    break;
+                }
+            }
+            if (i == culprit)
+            {
+                if (testimonies[i].B == 1)
+                {
+                    if (contains)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (!contains)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                if (testimonies[i].B == 1)
+                {
+                    if (!contains)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (contains)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (valid)
+        {
+            possibleCandidates.push_back(culprit);
+        }
+    }
+
+    if (possibleCandidates.empty())
+    {
+        cout << "swi";
+    }
+    else
+    {
+        for (int i = 0; i < possibleCandidates.size(); i++)
+        {
+            cout << possibleCandidates[i];
+
+            if (i != possibleCandidates.size() - 1)
+            {
+                cout << " ";
+            }
+        }
+    }
+
+    return 0;
+}
