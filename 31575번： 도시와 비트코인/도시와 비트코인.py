a0135abaf51de18cdcf3979f958bfc015cc60ea4
Leapense <iso9661@gmail.com>
2025-02-11 18:55:12 +0900

    문제 31575: 메인 함수 구현 및 DP를 이용한 경로 탐색 로직 추가

--- 31575번： 도시와 비트코인/도시와 비트코인.py
+++ 31575번： 도시와 비트코인/도시와 비트코인.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 31575                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/31575                          #+#        #+#      #+#     #
-#    Solved: 2025/02/11 18:52:26 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+    grid = [list(map(int, input().split())) for _ in range(M)]
+    dp = [[False] * N for _ in range(M)]
+    dp[0][0] = True
+    
+    for i in range(M):
+        for j in range(N):
+            if grid[i][j] != 1:
+                continue
+            if i == 0 and j == 0:
+                continue
+            if i > 0 and dp[i - 1][j]:
+                dp[i][j] = True
+            if j > 0 and dp[i][j - 1]:
+                dp[i][j] = True
+
+    print("Yes" if dp[M - 1][N - 1] else "No")
+
+if __name__ == "__main__":
+    main()
