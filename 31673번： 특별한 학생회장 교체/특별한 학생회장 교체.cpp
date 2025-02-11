088bfce6c013f9e54d9abcaef40561caf686b8fd
Leapense <iso9661@gmail.com>
2025-02-11 21:19:39 +0900

    문제 31673: C++ 솔루션 구현

--- 31673번： 특별한 학생회장 교체/특별한 학생회장 교체.cpp
+++ 31673번： 특별한 학생회장 교체/특별한 학생회장 교체.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31673                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31673                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/11 20:58:04 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    ll M;
+    cin >> N >> M;
+
+    vector<ll> V(N);
+    ll total = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> V[i];
+        total += V[i];
+    }
+
+    ll req = (total + 1) / 2;
+    sort(V.begin(), V.end(), greater<ll>());
+
+    ll sum = 0;
+    int k = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += V[i];
+        k++;
+        if (sum >= req)
+            break;
+    }
+
+    ll ans = M / (k + 1);
+    cout << ans << "\n";
+
+    return 0;
+}
