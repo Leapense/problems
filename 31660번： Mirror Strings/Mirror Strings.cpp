06cd9f4f108a36eb6bbf12736af6f13648485fe8
Leapense <iso9661@gmail.com>
2025-02-11 20:07:54 +0900

    문제 31660: Mirror Strings 문제의 C++ 솔루션 구현

--- 31660번： Mirror Strings/Mirror Strings.cpp
+++ 31660번： Mirror Strings/Mirror Strings.cpp
@@ -1,11 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31660                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31660                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/11 19:38:21 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const long long MOD = 1000000007;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int L, R;
+    cin >> L >> R;
+
+    int maxK = (R + 1) / 2;
+    vector<long long> pow5(maxK + 1, 0), pow2(maxK + 1, 0);
+
+    pow5[0] = 1;
+    pow2[0] = 1;
+
+    for (int i = 1; i <= maxK; i++)
+    {
+        pow5[i] = (pow5[i - 1] * 5LL) % MOD;
+        pow2[i] = (pow2[i - 1] * 2LL) % MOD;
+    }
+
+    long long ans = 0;
+
+    for (int m = L; m <= R; m++)
+    {
+        int k = (m + 1) / 2;
+        long long cur = (pow5[k] + pow2[k]) % MOD;
+        ans = (ans + cur) % MOD;
+    }
+
+    cout << ans % MOD << "\n";
+
+    return 0;
+}
