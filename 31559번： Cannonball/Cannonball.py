e4976d56ac67f2cce94f817025d7235cbc443db1
Leapense <iso9661@gmail.com>
2025-02-11 18:52:43 +0900

    문제 31575: 도시와 비트코인 문제 설명 및 README.md 추가, 초기 코드 파일 생성

--- 31559번： Cannonball/Cannonball.py
+++ 31559번： Cannonball/Cannonball.py
@@ -1,11 +1,48 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 31559                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/31559                          #+#        #+#      #+#     #
-#    Solved: 2025/02/10 13:50:46 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    input = sys.stdin.readline
+
+    N, S = map(int, input().split())
+    info = [None] * (N + 1)
+    for i in range(1, N + 1):
+        q, v = map(int, input().split())
+        info[i] = (q, v)
+
+    pos = S
+    power = 1
+    direction = 1
+    broken_targets = set()
+    
+    start_type, start_val = info[pos]
+    if start_type == 1:
+        if power >= start_val:
+            broken_targets.add(pos)
+    else:
+        power += start_val
+        direction *= -1
+    visited = set()
+    while True:
+        next_pos = pos + direction * power
+
+        if not (1 <= next_pos <= N):
+            break
+        state = (next_pos, power, direction)
+        if state in visited:
+            break
+
+        visited.add(state)
+
+        pos = next_pos
+        loc_type, loc_val = info[pos]
+
+        if loc_type == 1:
+            if power >= loc_val:
+                broken_targets.add(pos)
+
+        else:
+            power += loc_val
+            direction *= -1
+
+    print(len(broken_targets))
+
+solve()
