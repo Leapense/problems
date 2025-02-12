3cab8dc2403f9a852c6a2867401bdef06e689e80
Leapense <iso9661@gmail.com>
2025-02-12 22:59:06 +0900

    Implement C++ solution for problem 32173: 새치기

--- 32173번： 새치기/새치기.cpp
+++ 32173번： 새치기/새치기.cpp
@@ -1,11 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32173                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32173                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 22:05:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<long long> s(N + 1);
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> s[i];
+    }
+
+    if (N == 1)
+    {
+        cout << s[1] << "\n";
+        return 0;
+    }
+
+    long long ans = s[1];
+    long long prefix = s[1];
+
+    for (int m = 2; m <= N; m++)
+    {
+        long long candidate = s[m] - prefix;
+        ans = max(ans, candidate);
+        prefix += s[m];
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
