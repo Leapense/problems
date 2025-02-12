7863ac95d066daa20d5511265016afa82dad2aae
Leapense <iso9661@gmail.com>
2025-02-12 20:31:11 +0900

    Implement C++ solution for problem 31872: Calculate total after removing gaps

--- 31872번： 강의실/강의실.cpp
+++ 31872번： 강의실/강의실.cpp
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31872                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31872                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 19:57:20 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<long long> A(N);
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    sort(A.begin(), A.end());
+
+    long long total = A[N - 1];
+
+    vector<long long> gaps;
+
+    gaps.push_back(A[0] - 0);
+
+    for (int i = 1; i < N; i++)
+    {
+        gaps.push_back(A[i] - A[i - 1]);
+    }
+
+    sort(gaps.begin(), gaps.end(), greater<long long>());
+
+    for (int i = 0; i < K && i < gaps.size(); i++)
+    {
+        total -= gaps[i];
+    }
+
+    if (total < 0)
+    {
+        total = 0;
+    }
+
+    cout << total << "\n";
+
+    return 0;
+}
