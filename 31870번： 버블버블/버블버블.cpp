444ae2ffba317fc0d4462fe5268b7476ac1f6032
Leapense <iso9661@gmail.com>
2025-02-12 19:55:51 +0900

    Implement C++ solution for problem 31870: Calculate minimum inversions in an array

--- 31870번： 버블버블/버블버블.cpp
+++ 31870번： 버블버블/버블버블.cpp
@@ -1,11 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31870                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31870                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 19:37:11 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    long long invAsc = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (A[i] > A[j])
+            {
+                invAsc++;
+            }
+        }
+    }
+
+    long long total = (long long)N * (N - 1) / 2;
+    long long invDesc = total - invAsc;
+    long long costWithReverse = 1 + invDesc;
+
+    long long answer = min(invAsc, costWithReverse);
+
+    cout << answer << "\n";
+    return 0;
+}
