e2ee81dced4f02c23b26e6407e203e6fa6bb0838
Leapense <iso9661@gmail.com>
2025-02-12 19:34:27 +0900

    Implement C++ solution for problem 31869: Add appointment management and calculate maximum streak of valid days

--- 31869번： 선배님 밥 사주세요!/선배님 밥 사주세요!.cpp
+++ 31869번： 선배님 밥 사주세요!/선배님 밥 사주세요!.cpp
@@ -1,11 +1,81 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31869                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31869                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 19:16:05 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+struct Appointment
+{
+    int absDay;
+    int cost;
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    unordered_map<string, Appointment> appointmentMap;
+
+    for (int i = 0; i < N; i++)
+    {
+        string name;
+        int week, day, cost;
+        cin >> name >> week >> day >> cost;
+
+        int absDay = (week - 1) * 7 + day;
+        appointmentMap[name] = {absDay, cost};
+    }
+
+    vector<int> validDays;
+
+    for (int i = 0; i < N; i++)
+    {
+        string name;
+        int money;
+        cin >> name >> money;
+
+        Appointment app = appointmentMap[name];
+        if (money >= app.cost)
+        {
+            validDays.push_back(app.absDay);
+        }
+    }
+
+    sort(validDays.begin(), validDays.end());
+    validDays.erase(unique(validDays.begin(), validDays.end()), validDays.end());
+
+    int maxStreak = 0;
+    int currentStreak = 0;
+
+    if (validDays.empty())
+    {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    currentStreak = 1;
+    maxStreak = 1;
+
+    for (size_t i = 1; i < validDays.size(); i++)
+    {
+        if (validDays[i] == validDays[i - 1] + 1)
+        {
+            currentStreak++;
+        }
+        else
+        {
+            currentStreak = 1;
+        }
+
+        maxStreak = max(maxStreak, currentStreak);
+    }
+
+    cout << maxStreak << "\n";
+    return 0;
+}
