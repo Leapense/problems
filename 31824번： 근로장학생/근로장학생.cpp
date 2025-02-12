cf6c7de4001587a59fde0fcfba961e6951924332
Leapense <iso9661@gmail.com>
2025-02-12 13:45:33 +0900

    문제 31824: C++ 솔루션 구현

--- 31824번： 근로장학생/근로장학생.cpp
+++ 31824번： 근로장학생/근로장학생.cpp
@@ -1,11 +1,62 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31824                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31824                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 13:37:39 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<pair<string, string>> info(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> info[i].first >> info[i].second;
+    }
+
+    while (M--)
+    {
+        string S;
+        cin >> S;
+        string result = "";
+        int len = S.size();
+
+        for (int pos = 0; pos < len; pos++)
+        {
+            vector<pair<string, string>> matches;
+
+            for (auto &p : info)
+            {
+                const string &Q = p.first;
+                const string &A = p.second;
+                if (pos + Q.size() <= S.size() && S.substr(pos, Q.size()) == Q)
+                {
+                    matches.push_back({Q, A});
+                }
+            }
+
+            sort(matches.begin(), matches.end(), [](const pair<string, string> &a, const pair<string, string> &b)
+                 { return a.first < b.first; });
+
+            for (auto &match : matches)
+            {
+                result += match.second;
+            }
+        }
+
+        if (result.empty())
+        {
+            cout << "-1\n";
+        }
+        else
+        {
+            cout << result << "\n";
+        }
+    }
+
+    return 0;
+}
