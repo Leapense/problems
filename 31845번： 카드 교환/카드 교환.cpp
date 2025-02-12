19c6f909741a100bead4f5e07598ab82599b0752
Leapense <iso9661@gmail.com>
2025-02-12 19:11:08 +0900

    문제 31845: C++ 솔루션 구현

--- 31845번： 카드 교환/카드 교환.cpp
+++ 31845번： 카드 교환/카드 교환.cpp
@@ -1,11 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 31845                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/31845                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 18:49:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    int k_max = min(M, (N + 1) / 2);
+
+    sort(A.begin(), A.end(), greater<int>());
+
+    long long totalScore = 0;
+    for (int i = 0; i < k_max; i++)
+    {
+        if (A[i] > 0)
+        {
+            totalScore += A[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    cout << totalScore << "\n";
+
+    return 0;
+}
