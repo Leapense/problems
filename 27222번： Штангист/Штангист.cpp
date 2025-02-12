f5f93c21420691ef916714b5e2f341bbdfaf3259
Leapense <iso9661@gmail.com>
2025-02-12 13:01:36 +0900

    문제 27222: C++ 솔루션 구현

--- 27222번： Штангист/Штангист.cpp
+++ 27222번： Штангист/Штангист.cpp
@@ -1,11 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 27222                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/27222                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 12:45:58 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> training(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> training[i];
+    }
+
+    long long total_gain = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int morning, evening;
+        cin >> morning >> evening;
+        // 운동한 날이고 체중이 증가했을 경우에만 근육량 증가분을 더한다.
+        if (training[i] == 1 && evening > morning)
+        {
+            total_gain += (evening - morning);
+        }
+    }
+
+    cout << total_gain << "\n";
+
+    return 0;
+}
