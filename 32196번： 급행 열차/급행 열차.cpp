1ba63d0e0c483ff5a21ead851d559bd6df8eaa9e
Leapense <iso9661@gmail.com>
2025-02-13 19:34:07 +0900

    Implement C++ solution for problem 32196: 급행 열차

--- 32196번： 급행 열차/급행 열차.cpp
+++ 32196번： 급행 열차/급행 열차.cpp
@@ -1,11 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32196                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32196                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/13 19:27:16 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    long long K, X, Y;
+    cin >> N >> M >> K >> X >> Y;
+
+    vector<long long> cost(N);
+    for (int i = 0; i < N; i++)
+    {
+        long long A, B;
+        cin >> A >> B;
+        cost[i] = X * A - Y * B;
+    }
+
+    sort(cost.begin(), cost.end());
+
+    long long total = K * (X + Y);
+    for (int i = 0; i < M; i++)
+    {
+        total += cost[i];
+    }
+
+    cout << total << "\n";
+    return 0;
+}
