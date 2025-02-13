ab673da4e8e632008bdd2c154a1786192cada838
Leapense <iso9661@gmail.com>
2025-02-13 18:56:49 +0900

    Add comments for clarity in C++ solution for problem 32174: 조커 찾기 2

--- 32174번： 조커 찾기 2/조커 찾기 2.cpp
+++ 32174번： 조커 찾기 2/조커 찾기 2.cpp
@@ -10,6 +10,7 @@ int main()
     int N, M;
     cin >> N >> M;
 
+    // 0-indexed: 초기 조커 위치는 0 (즉, 맨 위)
     int *pos = new int[M + 1];
     pos[0] = 0;
 
@@ -21,6 +22,7 @@ int main()
         {
             ll x;
             cin >> x;
+            // x번 반복하므로 x mod N만큼 이동
             int delta = (int)(x % N);
             pos[i] = (pos[i - 1] + delta) % N;
         }
@@ -29,18 +31,21 @@ int main()
             ll y;
             cin >> y;
             int delta = (int)(y % N);
+            // 음수가 되지 않도록 N을 더한 후 모듈러 연산
             pos[i] = ((pos[i - 1] - delta) % N + N) % N;
         }
         else if (op == 3)
         {
             int z;
             cin >> z;
+            // z번 셔플 후의 상태로 복원 (z는 0 이상, i 미만)
             pos[i] = pos[z];
         }
-
-        cout << pos[M] + 1 << "\n";
-
-        delete[] pos;
-        return 0;
     }
+
+    // 최종 상태: pos[M] (0-indexed), 1-indexed로 출력
+    cout << pos[M] + 1 << "\n";
+
+    delete[] pos;
+    return 0;
 }
