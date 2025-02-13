3df86aa4248cc6aca503e98827c93868cb2fca59
Leapense <iso9661@gmail.com>
2025-02-13 22:45:44 +0900

    Implement C++ solution for problem 32317: Fixing the Tournament

--- 32317번： Fixing the Tournament/Fixing the Tournament.cpp
+++ 32317번： Fixing the Tournament/Fixing the Tournament.cpp
@@ -1,11 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32317                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32317                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/13 22:25:24 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n, r;
+    cin >> n >> r;
+
+    int total = 1 << n;
+    int x = total - r + 1;
+
+    int wins = 0;
+    while ((1 << (wins + 1)) <= x)
+    {
+        wins++;
+    }
+
+    cout << wins + 1 << "\n";
+
+    return 0;
+}
