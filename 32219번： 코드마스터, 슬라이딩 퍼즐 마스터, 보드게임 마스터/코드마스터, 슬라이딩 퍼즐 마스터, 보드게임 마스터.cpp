a948c5711ef732eae53db0ecbc7aead30319bb3b
Leapense <iso9661@gmail.com>
2025-02-13 20:16:33 +0900

    Implement C++ solution for problem 32219: 코드마스터, 슬라이딩 퍼즐 마스터, 보드게임 마스터

--- 32219번： 코드마스터, 슬라이딩 퍼즐 마스터, 보드게임 마스터/코드마스터, 슬라이딩 퍼즐 마스터, 보드게임 마스터.cpp
+++ 32219번： 코드마스터, 슬라이딩 퍼즐 마스터, 보드게임 마스터/코드마스터, 슬라이딩 퍼즐 마스터, 보드게임 마스터.cpp
@@ -1,11 +1,44 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32219                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32219                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/13 20:03:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int r, c;
+        cin >> r >> c;
+
+        int x = r - 1, y = c - 1;
+        int nim = x ^ y;
+
+        if (nim == 0)
+        {
+            cout << "dohoon\n";
+        }
+        else
+        {
+            cout << "jinseo\n";
+
+            int new_r = r, new_c = c;
+
+            if ((x ^ nim) < x)
+            {
+                new_r = (x ^ nim) + 1;
+            }
+            else
+            {
+                new_c = (y ^ nim) + 1;
+            }
+
+            cout << new_r << " " << new_c << "\n";
+        }
+    }
+
+    return 0;
+}
