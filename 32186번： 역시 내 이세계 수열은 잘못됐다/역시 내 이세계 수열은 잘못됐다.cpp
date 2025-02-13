a8d3b09b7f90752f4dc9b66e65ca3bdc2dc6804f
Leapense <iso9661@gmail.com>
2025-02-13 19:24:09 +0900

    Implement C++ solution for problem 32186: 역시 내 이세계 수열은 잘못됐다

--- 32186번： 역시 내 이세계 수열은 잘못됐다/역시 내 이세계 수열은 잘못됐다.cpp
+++ 32186번： 역시 내 이세계 수열은 잘못됐다/역시 내 이세계 수열은 잘못됐다.cpp
@@ -1,11 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32186                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32186                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/13 18:58:17 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    ll K;
+    cin >> N >> K;
+
+    vector<ll> A(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    ll ans = 0;
+
+    for (int i = 0; i < N / 2; i++)
+    {
+        ll x = min(A[i], A[N - i - 1]);
+        ll y = max(A[i], A[N - i - 1]);
+        ll d = y - x;
+
+        if (d == 0)
+            continue;
+
+        ll q = d / K;
+        ll r = d % K;
+
+        if (r == 0)
+        {
+            ans += q;
+        }
+        else
+        {
+            ll cost1 = q + r;
+            ll cost2 = q + (K - r + 1);
+
+            ans += min(cost1, cost2);
+        }
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
