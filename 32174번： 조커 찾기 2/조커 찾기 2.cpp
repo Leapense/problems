81965543cd080d30df7bbbb27f4bb591a6b18ae5
Leapense <iso9661@gmail.com>
2025-02-13 18:55:20 +0900

    Implement C++ solution for problem 32174: 조커 찾기 2

--- 32174번： 조커 찾기 2/조커 찾기 2.cpp
+++ 32174번： 조커 찾기 2/조커 찾기 2.cpp
@@ -1,11 +1,46 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32174                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32174                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/12 23:00:26 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    int *pos = new int[M + 1];
+    pos[0] = 0;
+
+    for (int i = 1; i <= M; i++)
+    {
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            ll x;
+            cin >> x;
+            int delta = (int)(x % N);
+            pos[i] = (pos[i - 1] + delta) % N;
+        }
+        else if (op == 2)
+        {
+            ll y;
+            cin >> y;
+            int delta = (int)(y % N);
+            pos[i] = ((pos[i - 1] - delta) % N + N) % N;
+        }
+        else if (op == 3)
+        {
+            int z;
+            cin >> z;
+            pos[i] = pos[z];
+        }
+
+        cout << pos[M] + 1 << "\n";
+
+        delete[] pos;
+        return 0;
+    }
+}
