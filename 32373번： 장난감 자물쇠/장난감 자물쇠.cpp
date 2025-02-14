ebdbbc88b3752478a720e2a10a43639cbcfb2cb5
Leapense <iso9661@gmail.com>
2025-02-14 19:01:33 +0900

    Implement C++ solution for problem 32373: 장난감 자물쇠

--- 32373번： 장난감 자물쇠/장난감 자물쇠.cpp
+++ 32373번： 장난감 자물쇠/장난감 자물쇠.cpp
@@ -1,11 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32373                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32373                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/14 18:52:53 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, k;
+    cin >> N >> k;
+
+    int *A = new int[N + 1];
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> A[i];
+    }
+
+    bool possible = true;
+
+    for (int i = 1; i <= N; i++)
+    {
+        if ((i % k) != ((A[i] + 1) % k))
+        {
+            possible = false;
+            break;
+        }
+    }
+
+    cout << (possible ? "Yes" : "No") << "\n";
+
+    delete[] A;
+    return 0;
+}
