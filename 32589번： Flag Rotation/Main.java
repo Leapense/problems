9fb4f7bd69b50d69a22dd831dbef88eebeae31b7
Leapense <iso9661@gmail.com>
2025-02-14 22:51:44 +0900

    Implement Java solution for problem 32589: Flag Rotation

--- 32589번： Flag Rotation/Main.java
+++ 32589번： Flag Rotation/Main.java
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 32589 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/32589 #+# #+# #+# */
-/* Solved: 2025/02/14 22:44:26 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] tokens = br.readLine().split(" ");
+
+        Map<Integer, Long> freqMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int color = Integer.parseInt(tokens[i]);
+            freqMap.put(color, freqMap.getOrDefault(color, 0L) + 1);
+        }
+
+        long sameCells = 0;
+
+        for (long freq : freqMap.values()) {
+            sameCells += freq * freq;
+        }
+
+        long totalCells = (long) n * n;
+        long result = totalCells - sameCells;
+
+        System.out.println(result);
+    }
+}
