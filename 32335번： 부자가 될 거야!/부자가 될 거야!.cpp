60920081898541025bf6aac52703f9b98d0edbbc
Leapense <iso9661@gmail.com>
2025-02-14 17:26:24 +0900

    Implement C++ solution for problem 32335: 부자가 될 거야!

--- 32335번： 부자가 될 거야!/부자가 될 거야!.cpp
+++ 32335번： 부자가 될 거야!/부자가 될 거야!.cpp
@@ -1,11 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32335                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32335                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/14 17:10:19 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+    string S;
+    cin >> S;
+
+    for (int i = 0; i < N; i++)
+    {
+        int digit = S[i] - '0';
+        if (digit != 0)
+        {
+            int cost = 10 - digit;
+            if (M >= cost)
+            {
+                S[i] = '0';
+                M -= cost;
+            }
+        }
+    }
+
+    int r = M % 10;
+    int lastDigit = S[N - 1] - '0';
+    int newDigit = (lastDigit + r) % 10;
+    S[N - 1] = char(newDigit + '0');
+
+    cout << S;
+    return 0;
+}
