088476346eee46aa291435b935f540488df02373
Leapense <iso9661@gmail.com>
2025-02-14 21:43:17 +0900

    Refactor C++ solution for problem 32549: Avoiding the Abyss to improve safety exit and entry point calculations

--- 32549번： Avoiding the Abyss/Avoiding the Abyss.cpp
+++ 32549번： Avoiding the Abyss/Avoiding the Abyss.cpp
@@ -11,42 +11,54 @@ int main()
     cin >> x_t >> y_t;
     cin >> x_p >> y_p;
 
-    const int EXT = 1000000000;
+    const int M = 1000000000; // 안전한 극한 좌표
 
-    if ((x_s - x_p) * (x_t - x_p) > 0)
+    // 시작점의 안전 출구 A 결정
+    int A_x, A_y;
+    if (x_s != x_p)
     {
-        int Y;
-        if (y_s > y_p)
-            Y = EXT;
+        if (x_s < x_p)
+            A_x = -M;
         else
-            Y = -EXT;
-        cout << 2 << "\n";
-        cout << x_s << " " << Y << "\n";
-        cout << x_t << " " << Y << "\n";
-    }
-    else if ((y_s - y_p) * (y_t - y_p) > 0)
-    {
-        int X;
-        if (x_s > x_p)
-            X = EXT;
-        else
-            X = -EXT;
-        cout << 2 << "\n";
-        cout << X << " " << y_s << "\n";
-        cout << X << " " << y_t << "\n";
+            A_x = M;
+        A_y = y_s;
     }
     else
     {
-        int X;
-        if (x_s < x_p)
-            X = -EXT;
+        A_x = x_s;
+        if (y_s < y_p)
+            A_y = -M;
         else
-            X = EXT;
-        cout << 3 << "\n";
-        cout << X << " " << y_s << "\n";
-        cout << X << " " << y_t << "\n";
-        cout << x_t << " " << y_t << "\n";
+            A_y = M;
     }
+
+    // 도착점의 안전 입구 B 결정
+    int B_x, B_y;
+    if (x_t != x_p)
+    {
+        if (x_t < x_p)
+            B_x = -M;
+        else
+            B_x = M;
+        B_y = y_t;
+    }
+    else
+    {
+        B_x = x_t;
+        if (y_t < y_p)
+            B_y = -M;
+        else
+            B_y = M;
+    }
+
+    // 교차점 C: A의 x좌표와 B의 y좌표
+    int C_x = A_x, C_y = B_y;
+
+    // 중간 점은 A, C, B 총 3개
+    cout << 3 << "\n";
+    cout << A_x << " " << A_y << "\n";
+    cout << C_x << " " << C_y << "\n";
+    cout << B_x << " " << B_y << "\n";
 
     return 0;
 }
