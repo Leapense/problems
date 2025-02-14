bbd6b9710f5333a7f449350302d9a545ee383c74
Leapense <iso9661@gmail.com>
2025-02-14 21:57:13 +0900

    Implement Java solution for problem 32549: Avoiding the Abyss

--- 32549번： Avoiding the Abyss/Main.java
+++ 32549번： Avoiding the Abyss/Main.java
@@ -1,11 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32549                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32549                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/14 21:55:03 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x_s = sc.nextInt();
+        int y_s = sc.nextInt();
+        int x_t = sc.nextInt();
+        int y_t = sc.nextInt();
+        int x_p = sc.nextInt();
+        int y_p = sc.nextInt();
+
+        int safeY = 20001;
+
+        System.out.println(2);
+        System.out.println(x_s + " " + safeY);
+        System.out.println(x_t + " " + safeY);
+
+        sc.close();
+    }
+}
