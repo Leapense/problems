e45446caafb46db2f44d3e3a333ef6e42374196a
Leapense <iso9661@gmail.com>
2025-02-15 18:12:04 +0900

    Implement C++ solution for problem 32788: Big Integers

--- 32788번： Big Integers/Big Integers.cpp
+++ 32788번： Big Integers/Big Integers.cpp
@@ -1,11 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32788                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32788                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/15 18:05:28 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int charValue(char c)
+{
+    if (c >= '0' && c <= '9')
+        return c - '0';
+    if (c >= 'a' && c <= 'z')
+        return c - 'a' + 10;
+    return c - 'A' + 36;
+}
+
+bool isSmallerNumeric(const string &s1, const string &s2)
+{
+    if (s1.size() != s2.size())
+        return s1.size() < s2.size();
+
+    for (size_t i = 0; i < s1.size(); i++)
+    {
+        int v1 = charValue(s1[i]);
+        int v2 = charValue(s2[i]);
+        if (v1 != v2)
+            return v1 < v2;
+    }
+
+    return false;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        string candidate = (s1 < s2) ? s1 : s2;
+        string numericMin = isSmallerNumeric(s1, s2) ? s1 : s2;
+
+        cout << ((candidate == numericMin) ? "YES" : "NO") << "\n";
+    }
+
+    return 0;
+}
