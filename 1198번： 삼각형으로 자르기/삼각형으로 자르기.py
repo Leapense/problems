4a7ecb50775f979fc4647f1417143e76330b12b9
Leapense <iso9661@gmail.com>
2025-02-15 23:02:54 +0900

    Implement solution for problem 1198: 삼각형으로 자르기 with triangle area calculation

--- 1198번： 삼각형으로 자르기/삼각형으로 자르기.py
+++ 1198번： 삼각형으로 자르기/삼각형으로 자르기.py
@@ -1,11 +1,27 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 1198                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/1198                           #+#        #+#      #+#     #
-#    Solved: 2025/02/15 22:54:52 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+#!/usr/bin/env python3
+import sys
+
+def triangle_area(p, q, r):
+    return abs(p[0]*(q[1]-r[1]) + q[0]*(r[1]-p[1]) + r[0]*(p[1]-q[1])) / 2.0
+
+def main():
+    input = sys.stdin.readline
+    N = int(input().strip())
+    points = []
+    for _ in range(N):
+        x, y = map(int, input().split())
+        points.append((x, y))
+
+    max_area = 0.0
+
+    for i in range(N):
+        for j in range(i+1, N):
+            for k in range(j+1, N):
+                area = triangle_area(points[i], points[j], points[k])
+                if area > max_area:
+                    max_area = area
+
+    print(f"{max_area:.9f}")
+
+if __name__ == "__main__":
+    main()
