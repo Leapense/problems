bbad34d805aa32750a29d0f643bd6f8d2e7cf0dd
Leapense <iso9661@gmail.com>
2025-02-15 21:39:25 +0900

    Implement solution for problem 33108: Double Chunks with main function and logic for maximum pairs calculation

--- 33108번： Double Chunks/Double Chunks.py
+++ 33108번： Double Chunks/Double Chunks.py
@@ -1,11 +1,33 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 33108                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/33108                          #+#        #+#      #+#     #
-#    Solved: 2025/02/15 21:32:19 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    N = int(input().strip())
+    A = list(map(int, input().split()))
+
+    pairs_by_sum = {}
+
+    for i in range(N - 1):
+        pair_sum = A[i] + A[i + 1]
+        if pair_sum not in pairs_by_sum:
+            pairs_by_sum[pair_sum] = []
+        pairs_by_sum[pair_sum].append(i)
+
+    def max_pairs(indices):
+        count = 0
+        last = -2
+
+        for idx in indices:
+            if idx >= last + 2:
+                count += 1
+                last = idx
+        return count
+    
+    answer = 0
+    for indices in pairs_by_sum.values():
+        answer = max(answer, max_pairs(indices))
+
+    print(answer)
+
+if __name__ == "__main__":
+    main()
