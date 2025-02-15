8c9d5b16431ecfcb96d3557ea3741918ce378fff
Leapense <iso9661@gmail.com>
2025-02-16 00:57:53 +0900

    Implement main function for problem 1411: 비슷한 단어 with canonical pattern matching

--- 1411번： 비슷한 단어/비슷한 단어.py
+++ 1411번： 비슷한 단어/비슷한 단어.py
@@ -1,11 +1,34 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 1411                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/1411                           #+#        #+#      #+#     #
-#    Solved: 2025/02/16 00:45:44 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+#!/usr/bin/env python3
+import sys
+from collections import defaultdict
+
+def get_canonical(word):
+    mapping = {}
+    pattern = []
+    next_num = 0
+    for ch in word:
+        if ch not in mapping:
+            mapping[ch] = next_num
+            next_num += 1
+        pattern.append(mapping[ch])
+    return tuple(pattern)
+
+def main():
+    input = sys.stdin.readline
+    N = int(input().strip())
+    pattern_count = defaultdict(int)
+
+    for _ in range(N):
+        word = input().strip()
+        canonical_pattern = get_canonical(word)
+        pattern_count[canonical_pattern] += 1
+
+    similar_pairs = 0
+
+    for count in pattern_count.values():
+        similar_pairs += count * (count - 1) // 2
+
+    print(similar_pairs)
+
+if __name__ == '__main__':
+    main()
