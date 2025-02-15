a936f74e2dfcdafa28ac776e0d7ccec362a94b3a
Leapense <iso9661@gmail.com>
2025-02-15 20:10:39 +0900

    Implement solution for problem 33011: 홀수와 짝수 게임 with main solve function

--- 33011번： 홀수와 짝수 게임/홀수와 짝수 게임.py
+++ 33011번： 홀수와 짝수 게임/홀수와 짝수 게임.py
@@ -1,11 +1,18 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 33011                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/33011                          #+#        #+#      #+#     #
-#    Solved: 2025/02/15 20:00:25 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def solve():
+    import sys
+    input = sys.stdin.readline
+
+    T = int(input().strip())
+    for _ in range(T):
+        N = int(input().strip())
+        cards = list(map(int, input().split()))
+        odd = sum(1 for c in cards if c % 2 == 1)
+        even = N - odd
+
+        if (odd > even and odd % 2 == 1) or (even > odd and even % 2 == 1):
+            print("amsminn")
+        else:
+            print("heeda0528")
+
+if __name__ == "__main__":
+    solve()
