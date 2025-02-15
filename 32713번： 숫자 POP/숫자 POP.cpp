fb4d8214d9da1084ac2490600438a243162b9fc0
Leapense <iso9661@gmail.com>
2025-02-15 17:56:49 +0900

    Implement C++ solution for problem 32713: 숫자 POP

--- 32713번： 숫자 POP/숫자 POP.cpp
+++ 32713번： 숫자 POP/숫자 POP.cpp
@@ -1,11 +1,47 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32713                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32713                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/15 17:46:31 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    unordered_map<int, vector<int>> pos;
+    for (int i = 0; i < N; i++)
+    {
+        pos[A[i]].push_back(i);
+    }
+
+    int ans = 0;
+
+    for (auto &entry : pos)
+    {
+        vector<int> &indices = entry.second;
+        int sz = indices.size();
+        int i = 0;
+
+        for (int j = 0; j < sz; j++)
+        {
+            while (indices[j] - indices[i] - (j - i) > K)
+            {
+                i++;
+            }
+
+            ans = max(ans, j - i + 1);
+        }
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
