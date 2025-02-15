10908ec09c1ee95d00601bbfbfce7354c476e8eb
Leapense <iso9661@gmail.com>
2025-02-15 19:55:41 +0900

    Rename process_trip to compute_effective_times and update logic for trip time calculations

--- 32970번： Autobus/Autobus.py
+++ 32970번： Autobus/Autobus.py
@@ -2,7 +2,7 @@ def convert_to_minutes(time_str):
     h, m = map(int, time_str.split(':'))
     return h * 60 + m
 
-def process_trip(depart_str, arrive_str):
+def compute_effective_times(depart_str, arrive_str):
     dep = convert_to_minutes(depart_str)
     arr = convert_to_minutes(arrive_str)
     if arr < dep:
@@ -29,16 +29,17 @@ def main():
         depart_time, arrive_time = times.split('--')
         city_from, city_to = route.split('-')
 
-        dep, arr = process_trip(depart_time, arrive_time)
-
         if city_from == "Zagreb" and city_to == "Graz":
-            trips_from_zagreb.append((dep, arr))
+            dep, eff_arr = compute_effective_times(depart_time, arrive_time)
+            trips_from_zagreb.append((dep, eff_arr))
         elif city_from == "Graz" and city_to == "Wroclaw":
-            trips_from_graz.append((convert_to_minutes(depart_time), convert_to_minutes(arrive_time) if convert_to_minutes(arrive_time) >= convert_to_minutes(depart_time) else convert_to_minutes(arrive_time) + 1440))
+            base_dep, eff_arr = compute_effective_times(depart_time, arrive_time)
+            trips_from_graz.append((base_dep, eff_arr))
+
     best_time = None
 
     for dep1, eff_arr1 in trips_from_zagreb:
-        for base_dep2, base_arr2 in trips_from_graz:
+        for base_dep2, base_eff_arr2 in trips_from_graz:
             if base_dep2 > eff_arr1:
                 k = 0
             else:
@@ -46,15 +47,14 @@ def main():
 
             abs_dep2 = base_dep2 + k * 1440
 
-            if base_arr2 < base_dep2:
-                abs_arr2 = base_arr2 + (k + 1) * 1440 + 1
-            else:
-                abs_arr2 = base_arr2 + k * 1440 + 1
-            
-            if abs_dep2 > eff_arr1:
-                total_time = abs_arr2 - dep1
-                if best_time is None or total_time < best_time:
-                    best_time = total_time
+            if abs_dep2 <= eff_arr1:
+                continue
+
+            abs_eff_arr2 = base_eff_arr2 + k * 1440
+            total_time = abs_eff_arr2 - dep1
+            if best_time is None or total_time < best_time:
+                best_time = total_time
+
     if best_time is None:
         print("NEMOGUCE")
     else:
