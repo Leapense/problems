bda706dec953e032bdb41371aca3bc98ed73e281
Leapense <iso9661@gmail.com>
2025-02-15 13:46:08 +0900

    Implement Java solution for problem 32649: GLCCDM

--- 32649번： GLCCDM/Main.java
+++ 32649번： GLCCDM/Main.java
@@ -1,11 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 32649 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/32649 #+# #+# #+# */
-/* Solved: 2025/02/14 23:08:21 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        if (B % A != 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        int x = B / A;
+
+        ArrayList<Integer> divisors = new ArrayList<>();
+        for (int i = 1; i * i <= x; i++) {
+            if (x % i == 0) {
+                divisors.add(i);
+                if (i != x / i) {
+                    divisors.add(x / i);
+                }
+            }
+        }
+
+        Collections.sort(divisors);
+
+        if (divisors.size() < K) {
+            System.out.println(-1);
+            return;
+        }
+
+        ArrayList<Integer> chosen = new ArrayList<>();
+        chosen.add(1);
+        chosen.add(x);
+        for (int d : divisors) {
+            if (chosen.size() >= K)
+                break;
+            if (d == 1 || d == x)
+                continue;
+            chosen.add(d);
+        }
+
+        for (int num : chosen) {
+            System.out.print(A * num + " ");
+        }
+    }
+}
