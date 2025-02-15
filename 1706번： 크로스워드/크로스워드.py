574452b50e1315440d4112d24c1082150810fd53
Leapense <iso9661@gmail.com>
2025-02-16 01:52:26 +0900

    Implement main function for problem 1706: 크로스워드 to extract and sort words from a grid

--- 1706번： 크로스워드/크로스워드.py
+++ 1706번： 크로스워드/크로스워드.py
@@ -1,11 +1,50 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 1706                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/1706                           #+#        #+#      #+#     #
-#    Solved: 2025/02/16 01:46:06 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    input = sys.stdin.readline
+
+    R, C = map(int, input().split())
+    grid = [input().strip() for _ in range(R)]
+
+    words = []
+
+    for i in range(R):
+        j = 0
+        while j < C:
+            if grid[i][j] != '#':
+                if j == 0 or grid[i][j-1] == '#':
+                    word = ""
+                    k = j
+                    while k < C and grid[i][k] != '#':
+                        word += grid[i][k]
+                        k += 1
+                    if len(word) >= 2:
+                        words.append(word)
+                    j = k
+                else:
+                    j += 1
+            else:
+                j += 1
+
+    for j in range(C):
+        i = 0
+        while i < R:
+            if grid[i][j] != '#':
+                if i == 0 or grid[i-1][j] == '#':
+                    word = ""
+                    k = i
+                    while k < R and grid[k][j] != '#':
+                        word += grid[k][j]
+                        k += 1
+                    if len(word) >= 2:
+                        words.append(word)
+                    i = k
+                else:
+                    i += 1
+            else:
+                i += 1
+
+    words.sort()
+    print(words[0])
+
+if __name__ == "__main__":
+    main()
