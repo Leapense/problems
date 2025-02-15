9f56293d21a70d235ccebfddda3d82b5d308d645
Leapense <iso9661@gmail.com>
2025-02-15 21:31:26 +0900

    Implement solution for problem 33094: Diet Plan with main function and logic for maximum diet days calculation

--- 33094번： Diet Plan/Diet Plan.py
+++ 33094번： Diet Plan/Diet Plan.py
@@ -1,11 +1,23 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 33094                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/33094                          #+#        #+#      #+#     #
-#    Solved: 2025/02/15 21:23:18 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def max_diet_days(N, M, K, P):
+    max_days = 0
+    total = 0
+    for d in range(1, N + 1):
+        total += P[d - 1]
+        biscuits_to_use = min(K, d)
+        top_biscuit_days = sorted(P[:d], reverse=True)[:biscuits_to_use]
+        milk_required = total - sum(top_biscuit_days)
+
+        if milk_required <= M:
+            max_days = d
+        else:
+            break
+    return max_days
+
+if __name__ == '__main__':
+    import sys
+    input_data = sys.stdin.read().strip().split()
+    if input_data:
+        N, M, K = map(int, input_data[:3])
+        P = list(map(int, input_data[3:3+N]))
+        result = max_diet_days(N, M, K, P)
+        print(result)
