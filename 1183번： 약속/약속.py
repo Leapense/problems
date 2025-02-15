6ab2c9a115c755c32e83b389609dec6c1b267099
Leapense <iso9661@gmail.com>
2025-02-15 22:53:20 +0900

    Implement main function for problem 1183: 약속 with input handling and logic for output

--- 1183번： 약속/약속.py
+++ 1183번： 약속/약속.py
@@ -1,11 +1,23 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 1183                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/1183                           #+#        #+#      #+#     #
-#    Solved: 2025/02/15 22:46:18 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+#!/usr/bin/env python3
+import sys
+
+def main():
+    input = sys.stdin.readline
+    N = int(input().strip())
+
+    arr = []
+    for _ in range(N):
+        A, B = map(int, input().split())
+        arr.append(-(A - B))
+
+    arr.sort()
+
+    if N % 2 == 1:
+        print(1)
+    else:
+        lower = arr[N // 2 - 1]
+        upper = arr[N // 2]
+        print(upper - lower + 1)
+
+if __name__ == "__main__":
+    main()
