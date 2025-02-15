386fa048aca7412c26d5dcbd47a40adfac6050af
Leapense <iso9661@gmail.com>
2025-02-15 17:44:47 +0900

    Implement C++ solution for problem 32711: 홀수로 나눠라! 짝수로 나눠라!

--- 32711번： 홀수로 나눠라! 짝수로 나눠라!/홀수로 나눠라! 짝수로 나눠라!.cpp
+++ 32711번： 홀수로 나눠라! 짝수로 나눠라!/홀수로 나눠라! 짝수로 나눠라!.cpp
@@ -1,11 +1,43 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32711                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32711                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/15 17:32:09 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<long long> A(N);
+    long long total = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+        total += A[i];
+    }
+
+    if (total % 2 == 1)
+    {
+        cout << 1;
+        return 0;
+    }
+
+    long long prefix = 0;
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        prefix += A[i];
+
+        if (prefix % 2 == 0)
+        {
+            cout << 1;
+            return 0;
+        }
+    }
+
+    cout << 0;
+    return 0;
+}
