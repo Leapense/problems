194e29b5d6350d300f80739b53fbd3017e6924fe
Leapense <iso9661@gmail.com>
2025-02-15 18:33:32 +0900

    Implement C++ solution for problem 32848: Herb Mixing

--- 32848번： Herb Mixing/Herb Mixing.cpp
+++ 32848번： Herb Mixing/Herb Mixing.cpp
@@ -1,11 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 32848                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/32848                          #+#        #+#      #+#    */
-/*   Solved: 2025/02/15 18:27:15 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int green, red;
+    cin >> green >> red;
+
+    int totalBoost = 0;
+
+    int pairs = min(green, red);
+    totalBoost += pairs * 10;
+    green -= pairs;
+
+    int groups3 = green / 3;
+    totalBoost += groups3 * 10;
+    green %= 3;
+
+    int groups2 = green / 2;
+    totalBoost += groups2 * 3;
+    green %= 2;
+
+    totalBoost += green * 1;
+
+    cout << totalBoost << endl;
+
+    return 0;
+}
