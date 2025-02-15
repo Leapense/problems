67818df125917e573fe8f6cbee80e5bc685f53c4
Leapense <iso9661@gmail.com>
2025-02-15 14:13:04 +0900

    Implement Java solution for problem 32668: Expected Flight Delay

--- 32668번： Expected Flight Delay/Main.java
+++ 32668번： Expected Flight Delay/Main.java
@@ -1,11 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 32668 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/32668 #+# #+# #+# */
-/* Solved: 2025/02/15 13:47:11 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    static final String[] DAYS = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int meetDayInput = sc.nextInt();
+        int pWdayPercent = sc.nextInt();
+        int pWendPercent = sc.nextInt();
+        int pImpPercent = sc.nextInt();
+        sc.close();
+
+        double pWday = pWdayPercent / 100.0;
+        double pWend = pWendPercent / 100.0;
+        double pImp = pImpPercent / 100.0;
+
+        int i = 1;
+        double cumulativeSuccess = 0.0;
+        double cumulativeFailure = 1.0;
+        int depDayIndex = -1;
+
+        int meetDayIndex = (meetDayInput - 1) % 7;
+
+        while (cumulativeSuccess + 1e-9 < pImp) {
+            int currentDayIndex = (meetDayIndex - i + 7) % 7;
+            double currentP;
+            if (DAYS[currentDayIndex].equals("Sunday") || DAYS[currentDayIndex].equals("Saturday")) {
+                currentP = pWend;
+            } else {
+                currentP = pWday;
+            }
+
+            cumulativeSuccess += cumulativeFailure * currentP;
+            cumulativeFailure *= (1 - currentP);
+
+            depDayIndex = currentDayIndex;
+
+            if (cumulativeSuccess + 1e-9 >= pImp)
+                break;
+            i++;
+        }
+
+        String depDay = DAYS[depDayIndex];
+        String meetDay = DAYS[meetDayIndex];
+        String dayOrDays = (i == 1) ? "day" : "days";
+
+        System.out.printf("Try to leave on %s, %d %s before the %s meeting.", depDay, i, dayOrDays, meetDay);
+
+    }
+}
