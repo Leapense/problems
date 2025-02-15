ca43b1863df97335a667e3fbb21ca1f995b882f0
Leapense <iso9661@gmail.com>
2025-02-15 14:35:09 +0900

    Implement Java solution for problem 32675: Pascal Multiple

--- 32675번： Pascal Multiple/Main.java
+++ 32675번： Pascal Multiple/Main.java
@@ -1,11 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 32675 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/32675 #+# #+# #+# */
-/* Solved: 2025/02/15 14:24:29 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[][] pascal = new int[N + 1][N + 1];
+
+        pascal[0][0] = 1 % K;
+
+        for (int i = 1; i <= N; i++) {
+            pascal[i][0] = 1 % K;
+            pascal[i][i] = 1 % K;
+
+            for (int j = 1; j < i; j++) {
+                pascal[i][j] = (pascal[i - 1][j - 1] + pascal[i - 1][j]) % K;
+            }
+        }
+
+        long count = 0;
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (pascal[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
