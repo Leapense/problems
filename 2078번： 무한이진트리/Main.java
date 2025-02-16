6677916817eed2a2a6afa306fa55d7fc84943ef4
Leapense <iso9661@gmail.com>
2025-02-16 12:16:10 +0900

    Implement solution for problem 2078: 무한이진트리

--- 2078번： 무한이진트리/Main.java
+++ 2078번： 무한이진트리/Main.java
@@ -1,11 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 2078 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/2078 #+# #+# #+# */
-/* Solved: 2025/02/16 11:58:19 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        sc.close();
+
+        long leftCount = 0, rightCount = 0;
+
+        while (!(A == 1 && B == 1)) {
+            if (A > B) {
+                long quotient = (A - 1) / B;
+                leftCount += quotient;
+                A -= quotient * B;
+            } else {
+                long quotient = (B - 1) / A;
+                rightCount += quotient;
+                B -= quotient * A;
+            }
+        }
+
+        System.out.println(leftCount + " " + rightCount);
+    }
+}
