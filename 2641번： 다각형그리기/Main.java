908cafab0c76c65450e53c791da2be72413a0df6
Leapense <iso9661@gmail.com>
2025-02-16 17:48:38 +0900

    Update font size in VSCode settings to improve readability

--- 2641번： 다각형그리기/Main.java
+++ 2641번： 다각형그리기/Main.java
@@ -1,11 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 2641 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/2641 #+# #+# #+# */
-/* Solved: 2025/02/16 17:36:29 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private static boolean isSamePolygon(int[] sample, int[] candidate) {
+        int len = sample.length;
+        for (int start = 0; start < len; start++) {
+            boolean match = true;
+            for (int j = 0; j < len; j++) {
+                if (candidate[j] != sample[(start + j) % len]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
