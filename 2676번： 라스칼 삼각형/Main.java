e204862c7e4aeab841c8d638ae09741fbf4c5ab3
Leapense <iso9661@gmail.com>
2025-02-16 18:25:57 +0900

    Implement solution for problem 2676: 라스칼 삼각형 with main logic and input handling

--- 2676번： 라스칼 삼각형/Main.java
+++ 2676번： 라스칼 삼각형/Main.java
@@ -1,11 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/* ::: ::: ::: */
-/* Problem Number: 2676 :+: :+: :+: */
-/* +:+ +:+ +:+ */
-/* By: cjhool <boj.kr/u/cjhool> +#+ +#+ +#+ */
-/* +#+ +#+ +#+ */
-/* https://boj.kr/2676 #+# #+# #+# */
-/* Solved: 2025/02/16 18:11:50 by cjhool ### ### ##.kr */
-/*                                                                            */
-/* ************************************************************************** */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            long n = sc.nextLong();
+            long m = sc.nextLong();
+
+            long k = Math.min(m, n - m);
+            long result = k * (n - k) + 1;
+
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb);
+        sc.close();
+    }
+}
