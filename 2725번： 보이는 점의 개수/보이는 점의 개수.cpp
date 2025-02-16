eab226eb321bc7f0a80139839a211de9a21c517d
Leapense <iso9661@gmail.com>
2025-02-16 19:44:12 +0900

    Added BOJ2725 Solution Code

--- 2725번： 보이는 점의 개수/보이는 점의 개수.cpp
+++ 2725번： 보이는 점의 개수/보이는 점의 개수.cpp
@@ -1,11 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 2725                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/2725                           #+#        #+#      #+#    */
-/*   Solved: 2025/02/16 19:30:08 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX = 1000;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> phi(MAX + 1);
+    for (int i = 0; i <= MAX; i++)
+    {
+        phi[i] = i;
+    }
+
+    for (int i = 2; i <= MAX; i++)
+    {
+        if (phi[i] == i)
+        {
+            for (int j = i; j <= MAX; j += i)
+            {
+                phi[j] -= phi[j] / i;
+            }
+        }
+    }
+
+    vector<int> prefix(MAX + 1, 0);
+    for (int i = 1; i <= MAX; i++)
+    {
+        prefix[i] = prefix[i - 1] + phi[i];
+    }
+
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << 2 * prefix[N] + 1 << "\n";
+    }
+
+    return 0;
+}
