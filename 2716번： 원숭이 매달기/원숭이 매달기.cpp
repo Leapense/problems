799f0068550675b5c7013c4cebbaf5952b79c6c5
Leapense <iso9661@gmail.com>
2025-02-16 19:28:18 +0900

    Update font size in VSCode settings and implement solution for problem 2716: 원숭이 매달기

--- 2716번： 원숭이 매달기/원숭이 매달기.cpp
+++ 2716번： 원숭이 매달기/원숭이 매달기.cpp
@@ -1,11 +1,42 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 2716                              :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/2716                           #+#        #+#      #+#    */
-/*   Solved: 2025/02/16 19:08:28 by cjhool        ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include <numeric>
+
+using namespace std;
+typedef long long ll;
+string s;
+int idx;
+
+ll parse()
+{
+    if (idx >= s.size() || s[idx] == ']')
+        return 1;
+    idx++;
+    ll left = parse();
+    ll right = parse();
+    idx++;
+    ll g = gcd(left, right);
+    ll l = (left / g) * right;
+    return 2 * l;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    cin.ignore();
+
+    while (T--)
+    {
+        getline(cin, s);
+        if (s == "")
+        {
+            cout << 1 << "\n";
+            continue;
+        }
+        idx = 0;
+        cout << parse() << "\n";
+    }
+
+    return 0;
+}
