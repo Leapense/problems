0ec66fa5c0ab66a4dc6e88b4a3547c21c8bc2b98
Leapense <iso9661@gmail.com>
2025-02-16 11:27:09 +0900

    Implement main function for problem 1793: Tiling with dynamic programming solution

--- 1793번： 타일링/타일링.py
+++ 1793번： 타일링/타일링.py
@@ -1,11 +1,25 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 1793                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: cjhool <boj.kr/u/cjhool>                    +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/1793                           #+#        #+#      #+#     #
-#    Solved: 2025/02/16 11:20:54 by cjhool        ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+def main():
+    import sys
+    input_data = sys.stdin.read().strip().split()
+    if not input_data:
+        return
+    
+    test_cases = [int(n) for n in input_data]
+    max_n = max(test_cases)
+    dp = [0] * (max_n + 1)
+
+    dp[0] = 1
+    if max_n >= 1:
+        dp[1] = 1
+
+    for i in range(2, max_n + 1):
+        dp[i] = dp[i-1] + 2 * dp[i-2]
+
+    results = []
+    for n in test_cases:
+        results.append(str(dp[n]))
+
+    sys.stdout.write("\n".join(results))
+
+if __name__ == "__main__":
+    main()
