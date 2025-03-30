6bb01d8a312ab5ff0d539d1cd51ffdd06d80607b
Leapense <iso9661@gmail.com>
2025-03-30 17:57:31 +0900

    Add 22144번: Епрестановка directory

--- 22144번： Епрестановка/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/monika/Documents/problems/22144번： Епрестановка
-# Build directory: /home/monika/Documents/problems/22144번： Епрестановка
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
