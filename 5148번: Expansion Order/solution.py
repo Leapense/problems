19e32fcc064ae3db9a92a92a2cdbefc5b7ce31f0
Leapense <iso9661@gmail.com>
2025-05-17 23:47:08 +0900

    Unsolved Python 3 solution code of BOJ 5148.

--- 5148번: Expansion Order/solution.py
+++ 5148번: Expansion Order/solution.py
@@ -1,43 +1,38 @@
-def process_data_set(n, m, routes):
-    connected = set([1])
-    remaining = [(i, set(route)) for i, route in enumerate(routes)]
-    order = []
-    for _ in range(m):
-        found = False
-        for idx, (route_idx, stations) in enumerate(remaining):
-            if stations & connected:
-                found = True
+# solution.py
+from collections import deque
+
+
+def solve():
+    K = int(input())
+    for data_set in range(1, K + 1):
+        n, m = map(int, input().split())
+        routes = [set(map(int, input().split())) for _ in range(m)]
+
+        connected = set([1])
+        order = []
+        remaining_routes = set(range(m))
+
+        while remaining_routes:
+            progress = False
+
+            for i in sorted(remaining_routes):
+                if routes[i] & connected:
+                    connected |= routes[i]
+                    order.append(i + 1)  # routes are 1-indexed
+                    remaining_routes.remove(i)
+                    progress = True
+                    break
+
+            if not progress:
                 break
-        if not found:
-            return None
-        order.append(route_idx + 1)
-        connected |= stations
-        del remaining[idx]
-    return order
 
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().splitlines()
-    K = int(data[0])
-    res = []
-    line = 1
-
-    for data_set_number in range(1, K + 1):
-        n, m = map(int, data[line].split())
-        routes = []
-        for i in range(m):
-            route = list(map(int, data[line + 1 + i].split()))
-            routes.append(route)
-        order = process_data_set(n, m, routes)
-        res.append(f"Data Set {data_set_number}:")
-        if order is None:
-            res.append("Impossible")
+        print(f"Data Set {data_set}:")
+        if remaining_routes:
+            print("Impossible")
         else:
-            for idx in order:
-                res.append(str(idx))
-        line += 1 + m
-    print('\n'.join(res))
+            for route_id in order:
+                print(route_id)
+
 
 if __name__ == "__main__":
-    main()
+    solve()
